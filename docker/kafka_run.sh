acbdfbe56dd40b3c28d23279cdb1540612808340
ppatierno <ppatierno@live.com>
2016-05-06 15:09:57 +0200

    Added gettext installation for envsubst in the Dockerfile Fixed wrong path in the kafka_run.sh Added Kubernetes YAML files for zookeeper replication controller and service

--- docker/kafka_run.sh
+++ docker/kafka_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 envsubst < /opt/kafka_$SCALA_VERSION-$KAFKA_VERSION/config/server.properties.template > /tmp/server.properties
-exec /opt/kafka_$SCALA_VERSION-$KAFKA_VERSION/bin/kafka-server-start.sh tmp/server.properties
+exec /opt/kafka_$SCALA_VERSION-$KAFKA_VERSION/bin/kafka-server-start.sh /tmp/server.properties
