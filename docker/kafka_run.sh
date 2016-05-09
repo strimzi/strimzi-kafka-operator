158f263e009fb09f0c7752fc439da3139acea2e6
ppatierno <ppatierno@live.com>
2016-05-09 16:42:05 +0200

    Modified Kafka run script in order to support Kafka auto broker id generation and advertised host Modified server.properties template file in order to support Kafka auto broker id generation and advertised host

--- docker/kafka_run.sh
+++ docker/kafka_run.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
+if [[ -z "$KAFKA_BROKER_ID" ]]; then
+    # auto broker id generation
+    export KAFKA_BROKER_ID=-1
+fi
+
+if [[ -z "$KAFKA_ADVERTISED_HOST" ]]; then
+    export KAFKA_ADVERTISED_HOST=$(hostname -I)
+fi
 envsubst < /opt/kafka_$SCALA_VERSION-$KAFKA_VERSION/config/server.properties.template > /tmp/server.properties
 exec /opt/kafka_$SCALA_VERSION-$KAFKA_VERSION/bin/kafka-server-start.sh /tmp/server.properties
