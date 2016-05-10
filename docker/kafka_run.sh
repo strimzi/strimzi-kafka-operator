209c3daa2119e9b1a31b270ea4f0a4f8fe73cd64
ppatierno <ppatierno@live.com>
2016-05-10 12:55:05 +0200

    Added Kafka binding port and Kafka advertising port to the server properties template file and Kafka run script

--- docker/kafka_run.sh
+++ docker/kafka_run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ -z "$KAFKA_PORT" ]]; then
+    export KAFKA_PORT=9092
+fi
+
 if [[ -z "$KAFKA_BROKER_ID" ]]; then
     # auto broker id generation
     export KAFKA_BROKER_ID=-1
@@ -7,6 +11,10 @@ fi
 
 if [[ -z "$KAFKA_ADVERTISED_HOST" ]]; then
     export KAFKA_ADVERTISED_HOST=$(hostname -I)
+fi
+
+if [[ -z "$KAFKA_ADVERTISED_PORT" ]]; then
+    export KAFKA_ADVERTISED_PORT=9092
 fi
 
 # environment variables substitution in the server configuration template file
