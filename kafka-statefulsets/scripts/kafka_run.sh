e539639bf6cab81a40008ec6435352b0be9876dc
ppatierno <ppatierno@live.com>
2017-03-21 18:35:19 +0100

    Added first POC of "Kafka Stateful sets" deployment

---
+++ kafka-statefulsets/scripts/kafka_run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# volume for saving Kafka server logs
+export KAFKA_VOLUME="/tmp/kafka/"
+# base name for Kafka server data dir and application logs
+export KAFKA_LOG_BASE_NAME="kafka-log"
+export KAFKA_APP_LOGS_BASE_NAME="logs"
+
+export KAFKA_BROKER_ID=$(hostname | awk -F'-' '{print $2}')
+echo "KAFKA_BROKER_ID=$KAFKA_BROKER_ID"
+
+# create data dir
+export KAFKA_LOG_DIRS=$KAFKA_VOLUME$KAFKA_LOG_BASE_NAME$KAFKA_BROKER_ID
+echo "KAFKA_LOG_DIRS=$KAFKA_LOG_DIRS"
+
+# dir for saving application logs
+export LOG_DIR=$KAFKA_VOLUME$KAFKA_APP_LOGS_BASE_NAME$KAFKA_BROKER_ID
+echo "LOG_DIR=$LOG_DIR"
+
+# check why ZOOKEEPER_SERVICE_HOST and ZOOKEEPER_SERVICE_PORT don't work
+
+# starting Kafka server with final configuration
+exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties \
+--override broker.id=$KAFKA_BROKER_ID \
+--override zookeeper.connect=zookeeper:2181 \
+--override log.dirs=$KAFKA_LOG_DIRS
---
+++ kafka-statefulsets/scripts/zookeeper_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# volume for saving Kafka server logs
+export ZOOKEEPER_VOLUME="/tmp/zookeeper/"
+# base name for Kafka server data dir and application logs
+export ZOOKEEPER_DATA_BASE_NAME="data"
+export ZOOKEEPER_LOG_BASE_NAME="logs"
+
+export ZOOKEEPER_ID=$(hostname | awk -F'-' '{print $2}')
+echo "ZOOKEEPER_ID=$ZOOKEEPER_ID"
+
+# create data dir
+export ZOOKEEPER_DATA_DIR=$ZOOKEEPER_VOLUME$ZOOKEEPER_DATA_BASE_NAME$ZOOKEEPER_ID
+echo "ZOOKEEPER_DATA_DIR=$ZOOKEEPER_DATA_DIR"
+
+# dir for saving application logs
+export LOG_DIR=$ZOOKEEPER_VOLUME$ZOOKEEPER_LOG_BASE_NAME$ZOOKEEPER_ID
+echo "LOG_DIR=$LOG_DIR"
+
+# environment variables substitution in the server configuration template file
+envsubst < $KAFKA_HOME/config/zookeeper.properties.template > /tmp/zookeeper.properties
+
+# starting Zookeeper with final configuration
+exec $KAFKA_HOME/bin/zookeeper-server-start.sh /tmp/zookeeper.properties
