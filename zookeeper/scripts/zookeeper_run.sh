aeff502ceca467d976da82b04504db1893404f6d
Jakub Scholz <www@scholzj.com>
2017-10-13 17:15:50 +0200

    Make Zookeeper able to run in HA Re #22

--- zookeeper/scripts/zookeeper_run.sh
+++ zookeeper/scripts/zookeeper_run.sh
@@ -7,21 +7,52 @@ export ZOOKEEPER_DATA_BASE_NAME="data"
 export ZOOKEEPER_LOG_BASE_NAME="logs"
 # Disable JMX until we need it
 export JMXDISABLE=true
+export BASE_HOSTNAME=$(hostname | rev | cut -d "-" -f2- | rev)
 
-# Until we run Zookeeper clusters we can hardcode the ID
-export ZOOKEEPER_ID=1
-echo "ZOOKEEPER_ID=$ZOOKEEPER_ID"
-
-# create data dir
-export ZOOKEEPER_DATA_DIR=$ZOOKEEPER_VOLUME$ZOOKEEPER_DATA_BASE_NAME$ZOOKEEPER_ID
-echo "ZOOKEEPER_DATA_DIR=$ZOOKEEPER_DATA_DIR"
+# Detect the server ID based on the hostname.
+# StatefulSets are numbered from 0 so we have to always increment by 1
+export ZOOKEEPER_ID=$(hostname | awk -F'-' '{print $NF+1}')
+echo "Detected Zookeeper ID $ZOOKEEPER_ID"
 
 # dir for saving application logs
-export LOG_DIR=$ZOOKEEPER_VOLUME$ZOOKEEPER_LOG_BASE_NAME$ZOOKEEPER_ID
-echo "LOG_DIR=$LOG_DIR"
+export LOG_DIR=$ZOOKEEPER_VOLUME$ZOOKEEPER_LOG_BASE_NAME
 
-# environment variables substitution in the server configuration template file
-envsubst < $ZOOKEEPER_HOME/config/zookeeper.properties.template > /tmp/zookeeper.properties
+# create data dir
+export ZOOKEEPER_DATA_DIR=$ZOOKEEPER_VOLUME$ZOOKEEPER_DATA_BASE_NAME
+mkdir -p $ZOOKEEPER_DATA_DIR
+
+# Create myid file
+echo $ZOOKEEPER_ID > $ZOOKEEPER_DATA_DIR/myid
+
+# Write the config file
+cat > /tmp/zookeeper.properties <<EOF
+timeTick=2000
+initLimit=5
+syncLimit=2
+
+# the directory where the snapshot is stored.
+dataDir=${ZOOKEEPER_DATA_DIR}
+clientPort=2181
+quorumListenOnAllIPs=true
+maxClientCnxns=0
+
+# Snapshot autopurging
+autopurge.snapRetainCount=3
+autopurge.purgeInterval=1
+
+# Ensemble configuration
+EOF
+
+NODE=1
+while [ $NODE -le $ZOOKEEPER_NODE_COUNT ]; do
+    echo "server.${NODE}=${BASE_HOSTNAME}-$((NODE-1)).zookeeper-headless:2888:3888" >> /tmp/zookeeper.properties
+    let NODE=NODE+1 
+done
+
+echo "Starting Zookeeper with configuration:"
+cat /tmp/zookeeper.properties
+echo ""
 
 # starting Zookeeper with final configuration
 exec $ZOOKEEPER_HOME/bin/zkServer.sh start-foreground /tmp/zookeeper.properties
+exec java -cp zookeeper-3.4.10.jar:lib/slf4j-api-1.6.1.jar:lib/slf4j-log4j12-1.6.1.jar:lib/log4j-1.2.16.jar:conf -D org.apache.zookeeper.server.quorum.QuorumPeerMain
