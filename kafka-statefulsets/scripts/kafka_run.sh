f3f971b2c3968099b39977fb19562f761d804463
Jakub Scholz <www@scholzj.com>
2017-10-17 15:01:28 +0200

    Rollback to using IP address instead of DNS name from #50

--- kafka-statefulsets/scripts/kafka_run.sh
+++ kafka-statefulsets/scripts/kafka_run.sh
@@ -20,6 +20,6 @@ echo "LOG_DIR=$LOG_DIR"
 # starting Kafka server with final configuration
 exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties \
 --override broker.id=$KAFKA_BROKER_ID \
---override advertised.host.name=$(hostname -f) \
+--override advertised.host.name=$(hostname -I) \
 --override zookeeper.connect=$ZOOKEEPER_SERVICE_HOST:$ZOOKEEPER_SERVICE_PORT \
 --override log.dirs=$KAFKA_LOG_DIRS
