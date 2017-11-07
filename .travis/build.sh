e151819289ad9e783de9f763bd128852f5512dcf
Jakub Scholz <www@scholzj.com>
2017-11-07 17:47:12 +0100

    Let the build push to Docker Hub for TAGs

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,15 +3,18 @@ set -e
 
 export PULL_REQUEST=${PULL_REQUEST:-true}
 export BRANCH=${BRANCH:-master}
+export TAG=${TAG:-latest}
 export DOCKER_ORG=${DOCKER_ORG:-$USER}
 export DOCKER_REGISTRY=${DOCKER_REGISTRY:-docker.io}
-export DOCKER_TAG=${TAG:-latest}
+export DOCKER_TAG=$TAG
 export DOCKER_VERSION_ARG=${COMMIT:-latest}
 
 make docker_build
 
-if [ "$PULL_REQUEST" != "false" ] || [ "$BRANCH" != "master" ] ; then
-    echo "Building Pull Request or side branch - nothing to push"
+if [ "$PULL_REQUEST" != "false" ]; then
+    echo "Building Pull Request - nothing to push"
+elif [ "$TAG" == "latest" ] && [ "$BRANCH" != "master" ]; then
+    echo "Not in master branch and not in release tag - nothing to push"
 else
     echo "Login into Docker Hub ..."
     docker login -u $DOCKER_USER -p $DOCKER_PASS
