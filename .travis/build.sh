d649d60036cf368ca7d6df6361213097f9578703
Jakub Scholz <www@scholzj.com>
2017-12-06 11:21:50 +0100

    Make the script sh compatible

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,24 +2,18 @@
 set -e
 
 export PULL_REQUEST=${PULL_REQUEST:-true}
-echo $PULL_REQUEST
 export BRANCH=${BRANCH:-master}
-echo $BRANCH
 export TAG=${TAG:-latest}
-echo $TAG
 export DOCKER_ORG=${DOCKER_ORG:-$USER}
-echo $DOCKER_ORG
 export DOCKER_REGISTRY=${DOCKER_REGISTRY:-docker.io}
-echo $DOCKER_REGISTRY
 export DOCKER_TAG=$TAG
-echo $DOCKER_TAG
 export DOCKER_VERSION_ARG=${COMMIT:-latest}
-echo $DOCKER_VERSION_ARG
 
-make docker_build
+#make docker_build
 
-if [ "$PULL_REQUEST" != "false" ]; then
+if [ "$PULL_REQUEST" != "false" ] ; then
     echo "Building Pull Request - nothing to push"
+
 elif [ "$TAG" = "latest" ] && [ "$BRANCH" != "master" ]; then
     echo "Not in master branch and not in release tag - nothing to push"
 else
