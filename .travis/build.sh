0a202bbed8c5757b7af0a7d3b77437f35b094caf
Jakub Scholz <www@scholzj.com>
2017-12-06 11:09:30 +0100

    Debug travis build

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,12 +2,19 @@
 set -e
 
 export PULL_REQUEST=${PULL_REQUEST:-true}
+echo $PULL_REQUEST
 export BRANCH=${BRANCH:-master}
+echo $BRANCH
 export TAG=${TAG:-latest}
+echo $TAG
 export DOCKER_ORG=${DOCKER_ORG:-$USER}
+echo $DOCKER_ORG
 export DOCKER_REGISTRY=${DOCKER_REGISTRY:-docker.io}
+echo $DOCKER_REGISTRY
 export DOCKER_TAG=$TAG
+echo $DOCKER_TAG
 export DOCKER_VERSION_ARG=${COMMIT:-latest}
+echo $DOCKER_VERSION_ARG
 
 make docker_build
 
