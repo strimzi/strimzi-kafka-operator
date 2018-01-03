0fcb95d8ec3b3b1b036ac8a6f249ebf03928dc8d
Tom Bentley <tbentley@redhat.com>
2018-01-03 15:13:10 +0000

    wip

---
+++ topic-controller/src/test/java/io/strimzi/controller/topic/ControllerIntegrationTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.strimzi.controller.topic;
+
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class ControllerIntegrationTest {
+
+    @Test
+    public void testTopicAdded(TestContext context) {
+        context.fail("Implement this");
+    }
+
+    @Test
+    public void testTopicDeleted(TestContext context) {
+        context.fail("Implement this");
+    }
+
+    @Test
+    public void testTopicConfigChanged(TestContext context) {
+        context.fail("Implement this");
+    }
+
+    @Test
+    public void testTopicNumPartitionsChanged(TestContext context) {
+        context.fail("Implement this");
+    }
+
+    @Test
+    public void testTopicNumReplicasChanged(TestContext context) {
+        context.fail("Implement this");
+    }
+
+    @Test
+    public void testConfigMapAdded(TestContext context) {
+        context.fail("Implement this");
+    }
+
+    @Test
+    public void testConfigMapDeleted(TestContext context) {
+        context.fail("Implement this");
+    }
+
+    @Test
+    public void testConfigMapModified(TestContext context) {
+        context.fail("Implement this");
+    }
+}
