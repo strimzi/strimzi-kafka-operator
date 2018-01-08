0c028ee65cb709504852028b98cf2ec93a917633
Paolo Patierno <ppatierno@live.com>
2018-01-08 12:23:44 +0100

    Added the "volume-mount-hack" when the cluster is a Kubernetes one

--- cluster-controller/src/main/java/io/enmasse/barnabas/controller/cluster/operations/CreateZookeeperClusterOperation.java
+++ cluster-controller/src/main/java/io/enmasse/barnabas/controller/cluster/operations/CreateZookeeperClusterOperation.java
@@ -43,7 +43,7 @@ public class CreateZookeeperClusterOperation extends ZookeeperClusterOperation {
                 OperationExecutor.getInstance().execute(new CreateServiceOperation(zk.generateHeadlessService()), futureHeadlessService.completer());
 
                 Future<Void> futureStatefulSet = Future.future();
-                OperationExecutor.getInstance().execute(new CreateStatefulSetOperation(zk.generateStatefulSet()), futureStatefulSet.completer());
+                OperationExecutor.getInstance().execute(new CreateStatefulSetOperation(zk.generateStatefulSet(k8s.isOpenShift())), futureStatefulSet.completer());
 
                 CompositeFuture.join(futureConfigMap, futureService, futureHeadlessService, futureStatefulSet).setHandler(ar -> {
                     if (ar.succeeded()) {
