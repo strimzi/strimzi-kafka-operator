47ef5efe72a579fa96b15f9e765299b446f476f1
Tom Bentley <tbentley@redhat.com>
2018-01-17 14:51:54 +0000

    ControllerIntegrationTest: Tidy up and test some exceptional cases

--- topic-controller/src/test/java/io/strimzi/controller/topic/Oc.java
+++ topic-controller/src/test/java/io/strimzi/controller/topic/Oc.java
@@ -17,22 +17,61 @@
 
 package io.strimzi.controller.topic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
 public class Oc {
 
+    private static final Logger logger = LoggerFactory.getLogger(Oc.class);
+
+    private void exec(String[] cmd) throws IOException, InterruptedException, OcException {
+        logger.info("{}", Arrays.toString(cmd));
+        ProcessBuilder pb = new ProcessBuilder(cmd);
+        pb.inheritIO();
+        Process p = pb.start();
+        int sc = p.waitFor();
+        if (sc != 0) {
+            throw new OcException(sc, Arrays.toString(cmd) + " got status code " + sc);
+        }
+    }
+
+
+    public boolean isClusterUp() throws Exception {
+        try {
+            exec(new String[]{"oc", "cluster", "status"});
+            return true;
+        } catch (OcException e) {
+            if (e.statusCode == 1) {
+                return false;
+            }
+            throw e;
+        }
+    }
+
     public void clusterUp() throws Exception {
         exec(new String[]{"oc", "cluster", "up"});
     }
 
-    private void exec(String[] cmd) throws IOException, InterruptedException, OcException {
-        ProcessBuilder pb = new ProcessBuilder(cmd);
-        Process p = pb.start();
-        int sc = p.waitFor();
-        if (sc != 0) {
-            throw new OcException(Arrays.toString(cmd) + " got status code " + sc);
+    public void loginSystemAdmin() throws Exception {
+        exec(new String[]{"oc", "login", "-u", "system:admin"});
+        exec("oc create role strimzi-role --resource=cm --resource=events --verb=get --verb=list --verb=watch --verb=create --verb=patch --verb=update --verb=delete".split(" "));
+        exec("oc create rolebinding strimzi-rolebinding --role=strimzi-role --user=developer".split(" "));
+    }
+
+    public void loginDeveloper() throws Exception {
+        exec(new String[]{"oc", "login", "-u", "developer"});
+
+    }
+
+    public void apply(File file) throws InterruptedException, OcException, IOException {
+        if (!file.exists()) {
+            throw new RuntimeException("File " + file.getAbsolutePath() + " does not exist");
         }
+        exec(new String[]{"oc", "apply", "-f", file.getAbsolutePath()});
     }
 
     public void clusterDown() throws Exception {
