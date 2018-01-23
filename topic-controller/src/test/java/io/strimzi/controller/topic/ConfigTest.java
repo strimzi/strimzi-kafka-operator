a528e6692bd28ef69be425f16f37067566d2542f
Paolo Patierno <ppatierno@live.com>
2018-01-23 15:26:01 +0100

    Made STRIMZI_ZOOKEEPER_CONNECT and STRIMZI_KAFKA_BOOTSTRAP_SERVERS mandatory Updated unit tests accordingly

--- topic-controller/src/test/java/io/strimzi/controller/topic/ConfigTest.java
+++ topic-controller/src/test/java/io/strimzi/controller/topic/ConfigTest.java
@@ -20,39 +20,60 @@ package io.strimzi.controller.topic;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class ConfigTest {
 
+    private static final Map<String, String> mandatory = new HashMap<>();
+
+    static {
+        mandatory.put(Config.ZOOKEEPER_CONNECT.key, "localhost:2181");
+        mandatory.put(Config.KAFKA_BOOTSTRAP_SERVERS.key, "localhost:9092");
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void unknownKey() {
         new Config(Collections.singletonMap("foo", "bar"));
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void empty() {
         Config c = new Config(Collections.emptyMap());
-        assertEquals(Config.ZOOKEEPER_CONNECT.defaultValue, c.get(Config.ZOOKEEPER_CONNECT));
-        assertEquals(Config.KAFKA_BOOTSTRAP_SERVERS.defaultValue, c.get(Config.KAFKA_BOOTSTRAP_SERVERS));
+    }
+
+    @Test
+    public void defaults() {
+        Map<String, String> map = new HashMap<>(mandatory);
+        Config c = new Config(map);
         assertEquals(20_000, c.get(Config.ZOOKEEPER_SESSION_TIMEOUT_MS).intValue());
     }
 
     @Test
     public void override() {
-        Config c = new Config(Collections.singletonMap(Config.ZOOKEEPER_SESSION_TIMEOUT_MS.key, "13 seconds"));
-        assertEquals(Config.ZOOKEEPER_CONNECT.defaultValue, c.get(Config.ZOOKEEPER_CONNECT));
-        assertEquals(Config.KAFKA_BOOTSTRAP_SERVERS.defaultValue, c.get(Config.KAFKA_BOOTSTRAP_SERVERS));
+        Map<String, String> map = new HashMap<>(mandatory);
+        map.put(Config.ZOOKEEPER_SESSION_TIMEOUT_MS.key, "13 seconds");
+
+        Config c = new Config(map);
         assertEquals(13_000, c.get(Config.ZOOKEEPER_SESSION_TIMEOUT_MS).intValue());
     }
 
     @Test
     public void intervals() {
-        new Config(Collections.singletonMap(Config.ZOOKEEPER_SESSION_TIMEOUT_MS.key, "13 seconds"));
-        new Config(Collections.singletonMap(Config.ZOOKEEPER_SESSION_TIMEOUT_MS.key, "13seconds"));
+        Map<String, String> map = new HashMap<>(mandatory);
+
+        map.put(Config.ZOOKEEPER_SESSION_TIMEOUT_MS.key, "13 seconds");
+        new Config(map);
+
+        map.put(Config.ZOOKEEPER_SESSION_TIMEOUT_MS.key, "13seconds");
+        new Config(map);
+
         try {
-            new Config(Collections.singletonMap(Config.ZOOKEEPER_SESSION_TIMEOUT_MS.key, "13foos"));
+            map.put(Config.ZOOKEEPER_SESSION_TIMEOUT_MS.key, "13foos");
+            new Config(map);
             fail();
         } catch (IllegalArgumentException e) {
 
