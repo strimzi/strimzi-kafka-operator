d3b721aa8e1acf3a9fd5b95387a8b03c587db673
Tom Bentley <tbentley@redhat.com>
2018-01-25 12:07:30 +0000

    Findbugs: Field can be static

--- cluster-controller/src/main/java/io/strimzi/controller/cluster/resources/ZookeeperCluster.java
+++ cluster-controller/src/main/java/io/strimzi/controller/cluster/resources/ZookeeperCluster.java
@@ -18,7 +18,7 @@ public class ZookeeperCluster extends AbstractCluster {
 
     public static final String TYPE = "zookeeper";
 
-    private final int clientPort = 2181;
+    private static final int CLIENT_PORT = 2181;
     private final String clientPortName = "clients";
     private final int clusteringPort = 2888;
     private final String clusteringPortName = "clustering";
@@ -248,7 +248,7 @@ public class ZookeeperCluster extends AbstractCluster {
     public Service generateService() {
 
         return createService("ClusterIP",
-                Collections.singletonList(createServicePort(clientPortName, clientPort, clientPort, "TCP")));
+                Collections.singletonList(createServicePort(clientPortName, CLIENT_PORT, CLIENT_PORT, "TCP")));
     }
 
     public Service generateHeadlessService() {
@@ -311,7 +311,7 @@ public class ZookeeperCluster extends AbstractCluster {
 
     private List<ServicePort> getServicePortList() {
         List<ServicePort> portList = new ArrayList<>();
-        portList.add(createServicePort(clientPortName, clientPort, clientPort, "TCP"));
+        portList.add(createServicePort(clientPortName, CLIENT_PORT, CLIENT_PORT, "TCP"));
         portList.add(createServicePort(clusteringPortName, clusteringPort, clusteringPort, "TCP"));
         portList.add(createServicePort(leaderElectionPortName, leaderElectionPort, leaderElectionPort, "TCP"));
 
@@ -320,7 +320,7 @@ public class ZookeeperCluster extends AbstractCluster {
 
     private List<ContainerPort> getContainerPortList() {
         List<ContainerPort> portList = new ArrayList<>();
-        portList.add(createContainerPort(clientPortName, clientPort, "TCP"));
+        portList.add(createContainerPort(clientPortName, CLIENT_PORT, "TCP"));
         portList.add(createContainerPort(clusteringPortName, clusteringPort, "TCP"));
         portList.add(createContainerPort(leaderElectionPortName, leaderElectionPort,"TCP"));
         if (isMetricsEnabled) {
