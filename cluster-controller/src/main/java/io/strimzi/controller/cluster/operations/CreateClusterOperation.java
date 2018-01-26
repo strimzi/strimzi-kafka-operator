1f115d5331ced033326c252ab98a6f0c4e63ffcf
Tom Bentley <tbentley@redhat.com>
2018-01-26 12:22:38 +0000

    Create abstraction for Create*ClusterOperation, using template method pattern and encapsulating locking

---
+++ cluster-controller/src/main/java/io/strimzi/controller/cluster/operations/CreateClusterOperation.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.strimzi.controller.cluster.operations;
+
+import io.strimzi.controller.cluster.K8SUtils;
+import io.strimzi.controller.cluster.resources.AbstractCluster;
+import io.strimzi.controller.cluster.resources.KafkaCluster;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.shareddata.Lock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Abstract cluster creation, for a generic cluster type {@code C}.
+ * This class applies the template method pattern, first obtaining the desired cluster configuration
+ * ({@link #getCluster(K8SUtils, Handler, Lock)}),
+ * then creating resources to match ({@link #creationFutures(K8SUtils, AbstractCluster)}).
+ *
+ * This class manages a per-cluster-type and per-cluster locking strategy so only one operation per cluster
+ * can proceed at once. 
+ * @param <C>
+ */
+public abstract class CreateClusterOperation<C extends AbstractCluster> extends ClusterOperation {
+    private static final Logger log = LoggerFactory.getLogger(CreateKafkaClusterOperation.class.getName());
+    private final String clusterType;
+
+    protected CreateClusterOperation(String clusterType, String namespace, String name) {
+        super(namespace, name);
+        this.clusterType = clusterType;
+    }
+
+    @Override
+    protected String getLockName() {
+        return "lock::"+ clusterType +"::" + namespace + "::" + name;
+    }
+
+    @Override
+    public void execute(Vertx vertx, K8SUtils k8s, Handler<AsyncResult<Void>> handler) {
+        vertx.sharedData().getLockWithTimeout(getLockName(), LOCK_TIMEOUT, res -> {
+            if (res.succeeded()) {
+                Lock lock = res.result();
+
+                C cluster;
+                try {
+                    cluster = getCluster(k8s, handler, lock);
+                    log.info("Creating {} cluster {} in namespace {}", clusterType, cluster.getName(), namespace);
+                } catch (Exception ex) {
+                    log.error("Error while getting required {} cluster state", clusterType, ex);
+                    handler.handle(Future.failedFuture("getCluster error"));
+                    lock.release();
+                    return;
+                }
+                List<Future> list = creationFutures(k8s, cluster);
+
+                CompositeFuture.join(list).setHandler(ar -> {
+                    if (ar.succeeded()) {
+                        log.info("{} cluster {} successfully created in namespace {}", clusterType, cluster.getName(), namespace);
+                        handler.handle(Future.succeededFuture());
+                        lock.release();
+                    } else {
+                        log.error("{} cluster {} failed to create in namespace {}", clusterType, cluster.getName(), namespace);
+                        handler.handle(Future.failedFuture("Failed to create Kafka cluster"));
+                        lock.release();
+                    }
+                });
+            } else {
+                log.error("Failed to acquire lock to create {} cluster {}", clusterType, getLockName());
+                handler.handle(Future.failedFuture("Failed to acquire lock to create "+ clusterType + " cluster"));
+            }
+        });
+    }
+
+    /** Create the resources in Kubernetes according to the given {@code cluster} */
+    protected abstract List<Future> creationFutures(K8SUtils k8s, C cluster);
+
+    /** Get the desired Cluster instance */
+    protected abstract C getCluster(K8SUtils k8s, Handler<AsyncResult<Void>> handler, Lock lock);
+
+}
--- cluster-controller/src/main/java/io/strimzi/controller/cluster/operations/CreateZookeeperClusterOperation.java
+++ cluster-controller/src/main/java/io/strimzi/controller/cluster/operations/CreateZookeeperClusterOperation.java
@@ -7,65 +7,46 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.vertx.core.shareddata.Lock;
 
-public class CreateZookeeperClusterOperation extends ZookeeperClusterOperation {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateZookeeperClusterOperation extends CreateClusterOperation<ZookeeperCluster> {
     private static final Logger log = LoggerFactory.getLogger(CreateZookeeperClusterOperation.class.getName());
 
     public CreateZookeeperClusterOperation(String namespace, String name) {
-        super(namespace, name);
+        super("zookeeper", namespace, name);
     }
 
     @Override
-    public void execute(Vertx vertx, K8SUtils k8s, Handler<AsyncResult<Void>> handler) {
-        vertx.sharedData().getLockWithTimeout(getLockName(), LOCK_TIMEOUT, res -> {
-            if (res.succeeded()) {
-                Lock lock = res.result();
+    protected ZookeeperCluster getCluster(K8SUtils k8s, Handler<AsyncResult<Void>> handler, Lock lock) {
+        return ZookeeperCluster.fromConfigMap(k8s.getConfigmap(namespace, name));
+    }
 
-                ZookeeperCluster zk;
-                try {
+    @Override
+    protected List<Future> creationFutures(K8SUtils k8s, ZookeeperCluster zk) {
+        List<Future> result = new ArrayList<>(4);
+        // start creating configMap operation only if metrics are enabled,
+        // otherwise the future is already complete (for the "join")
+        Future<Void> futureConfigMap = Future.future();
+        if (zk.isMetricsEnabled()) {
+            OperationExecutor.getInstance().executeK8s(CreateOperation.createConfigMap(zk.generateMetricsConfigMap()), futureConfigMap.completer());
+        } else {
+            futureConfigMap.complete();
+        }
+        result.add(futureConfigMap);
 
-                    zk = ZookeeperCluster.fromConfigMap(k8s.getConfigmap(namespace, name));
-                    log.info("Creating Zookeeper cluster {} in namespace {}", zk.getName(), namespace);
+        Future<Void> futureService = Future.future();
+        OperationExecutor.getInstance().executeFabric8(CreateOperation.createService(zk.generateService()), futureService.completer());
+        result.add(futureService);
 
-                } catch (Exception ex) {
-                    log.error("Error while parsing cluster ConfigMap", ex);
-                    handler.handle(Future.failedFuture("ConfigMap parsing error"));
-                    lock.release();
-                    return;
-                }
+        Future<Void> futureHeadlessService = Future.future();
+        OperationExecutor.getInstance().executeFabric8(CreateOperation.createService(zk.generateHeadlessService()), futureHeadlessService.completer());
+        result.add(futureHeadlessService);
 
-                // start creating configMap operation only if metrics are enabled,
-                // otherwise the future is already complete (for the "join")
-                Future<Void> futureConfigMap = Future.future();
-                if (zk.isMetricsEnabled()) {
-                    OperationExecutor.getInstance().executeK8s(CreateOperation.createConfigMap(zk.generateMetricsConfigMap()), futureConfigMap.completer());
-                } else {
-                    futureConfigMap.complete();
-                }
+        Future<Void> futureStatefulSet = Future.future();
+        OperationExecutor.getInstance().executeK8s(CreateOperation.createStatefulSet(zk.generateStatefulSet(k8s.isOpenShift())), futureStatefulSet.completer());
+        result.add(futureStatefulSet);
 
-                Future<Void> futureService = Future.future();
-                OperationExecutor.getInstance().executeFabric8(CreateOperation.createService(zk.generateService()), futureService.completer());
-
-                Future<Void> futureHeadlessService = Future.future();
-                OperationExecutor.getInstance().executeFabric8(CreateOperation.createService(zk.generateHeadlessService()), futureHeadlessService.completer());
-
-                Future<Void> futureStatefulSet = Future.future();
-                OperationExecutor.getInstance().executeK8s(CreateOperation.createStatefulSet(zk.generateStatefulSet(k8s.isOpenShift())), futureStatefulSet.completer());
-
-                CompositeFuture.join(futureConfigMap, futureService, futureHeadlessService, futureStatefulSet).setHandler(ar -> {
-                    if (ar.succeeded()) {
-                        log.info("Zookeeper cluster {} successfully created in namespace {}", zk.getName(), namespace);
-                        handler.handle(Future.succeededFuture());
-                        lock.release();
-                    } else {
-                        log.error("Zookeeper cluster {} failed to create in namespace {}", zk.getName(), namespace);
-                        handler.handle(Future.failedFuture("Failed to create Zookeeper cluster"));
-                        lock.release();
-                    }
-                });
-            } else {
-                log.error("Failed to acquire lock to create Zookeeper cluster {}", getLockName());
-                handler.handle(Future.failedFuture("Failed to acquire lock to create Zookeeper cluster"));
-            }
-        });
+        return result;
     }
 }
