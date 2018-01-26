dea08891ec20000895072d795ad378618ba9b1cf
Tom Bentley <tbentley@redhat.com>
2018-01-26 12:22:38 +0000

    Create abstraction for Create*ClusterOperation, using template method pattern and encapsulating locking

--- cluster-controller/src/main/java/io/strimzi/controller/cluster/operations/CreateKafkaConnectClusterOperation.java
+++ cluster-controller/src/main/java/io/strimzi/controller/cluster/operations/CreateKafkaConnectClusterOperation.java
@@ -8,51 +8,42 @@ import io.vertx.core.shareddata.Lock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CreateKafkaConnectClusterOperation extends KafkaConnectClusterOperation {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateKafkaConnectClusterOperation extends CreateClusterOperation<KafkaConnectCluster> {
     private static final Logger log = LoggerFactory.getLogger(CreateKafkaConnectClusterOperation.class.getName());
 
     public CreateKafkaConnectClusterOperation(String namespace, String name) {
-        super(namespace, name);
+        super("kafka-connect", namespace, name);
     }
 
     @Override
-    public void execute(Vertx vertx, K8SUtils k8s, Handler<AsyncResult<Void>> handler) {
-        vertx.sharedData().getLockWithTimeout(getLockName(), LOCK_TIMEOUT, res -> {
-            if (res.succeeded()) {
-                Lock lock = res.result();
-
-                KafkaConnectCluster connect = KafkaConnectCluster.fromConfigMap(k8s, k8s.getConfigmap(namespace, name));
-                log.info("Creating Kafka Connect cluster {} in namespace {}", connect.getName(), namespace);
-
-                Future<Void> futureService = Future.future();
-                OperationExecutor.getInstance().executeFabric8(CreateOperation.createService(connect.generateService()), futureService.completer());
-
-                Future<Void> futureDeployment = Future.future();
-                OperationExecutor.getInstance().executeK8s(CreateOperation.createDeployment(connect.generateDeployment()), futureDeployment.completer());
-
-                Future<Void> futureS2I;
-                if (connect.getS2I() != null) {
-                    futureS2I = Future.future();
-                    OperationExecutor.getInstance().executeOpenShift(new CreateS2IOperation(connect.getS2I()), futureS2I.completer());
-                } else {
-                    futureS2I = Future.succeededFuture();
-                }
-
-                CompositeFuture.join(futureService, futureDeployment, futureS2I).setHandler(ar -> {
-                    if (ar.succeeded()) {
-                        log.info("Kafka Connect cluster {} successfully created in namespace {}", connect.getName(), namespace);
-                        handler.handle(Future.succeededFuture());
-                        lock.release();
-                    } else {
-                        log.error("Kafka Connect cluster {} failed to create in namespace {}", connect.getName(), namespace);
-                        handler.handle(Future.failedFuture("Failed to create Kafka Connect cluster"));
-                        lock.release();
-                    }
-                });
-            } else {
-                log.error("Failed to acquire lock to create Kafka Connect cluster {}", getLockName());
-                handler.handle(Future.failedFuture("Failed to acquire lock to create Kafka Connect cluster"));
-            }
-        });
+    protected KafkaConnectCluster getCluster(K8SUtils k8s, Handler<AsyncResult<Void>> handler, Lock lock) {
+        return KafkaConnectCluster.fromConfigMap(k8s, k8s.getConfigmap(namespace, name));
     }
+
+    @Override
+    protected List<Future> creationFutures(K8SUtils k8s, KafkaConnectCluster connect) {
+        List<Future> result = new ArrayList<>(4);
+        Future<Void> futureService = Future.future();
+        OperationExecutor.getInstance().executeFabric8(CreateOperation.createService(connect.generateService()), futureService.completer());
+        result.add(futureService);
+
+        Future<Void> futureDeployment = Future.future();
+        OperationExecutor.getInstance().executeK8s(CreateOperation.createDeployment(connect.generateDeployment()), futureDeployment.completer());
+        result.add(futureDeployment);
+
+        Future<Void> futureS2I;
+        if (connect.getS2I() != null) {
+            futureS2I = Future.future();
+            OperationExecutor.getInstance().executeOpenShift(new CreateS2IOperation(connect.getS2I()), futureS2I.completer());
+        } else {
+            futureS2I = Future.succeededFuture();
+        }
+        result.add(futureS2I);
+
+        return result;
+    }
+
 }
