dff42c09485c6c33e50f7e5b58a2aa53d55db921
Paolo Patierno <ppatierno@live.com>
2018-02-12 16:18:27 +0100

    Added a new reconcilation interval parameter to the Cluster Controller configuration from related env var

--- cluster-controller/src/main/java/io/strimzi/controller/cluster/ClusterControllerConfig.java
+++ cluster-controller/src/main/java/io/strimzi/controller/cluster/ClusterControllerConfig.java
@@ -7,18 +7,34 @@ public class ClusterControllerConfig {
 
     public static final String STRIMZI_NAMESPACE = "STRIMZI_NAMESPACE";
     public static final String STRIMZI_CONFIGMAP_LABELS = "STRIMZI_CONFIGMAP_LABELS";
+    public static final String STRIMZI_FULL_RECONCILIATION_INTERVAL = "STRIMZI_FULL_RECONCILIATION_INTERVAL";
+
+    private static final long DEFAULT_FULL_RECONCILIATION_INTERVAL = 120000; // in ms (2 minutes)
 
     private Map<String, String> labels;
     private String namespace;
+    private long reconciliationInterval;
 
-    public ClusterControllerConfig(String namespace, Map<String, String> labels) {
+    public ClusterControllerConfig(String namespace, Map<String, String> labels, long reconciliationInterval) {
         this.namespace = namespace;
         this.labels = labels;
+        this.reconciliationInterval = reconciliationInterval;
+    }
+
+    public ClusterControllerConfig(String namespace, Map<String, String> labels) {
+        this(namespace, labels, DEFAULT_FULL_RECONCILIATION_INTERVAL);
     }
 
     public static ClusterControllerConfig fromEnv() {
+
         String namespace = System.getenv(ClusterControllerConfig.STRIMZI_NAMESPACE);
         String stringLabels = System.getenv(ClusterControllerConfig.STRIMZI_CONFIGMAP_LABELS);
+        long reconciliationInterval = DEFAULT_FULL_RECONCILIATION_INTERVAL;
+
+        String reconciliationIntervalEnvVar = System.getenv(ClusterControllerConfig.STRIMZI_FULL_RECONCILIATION_INTERVAL);
+        if (reconciliationIntervalEnvVar != null) {
+            reconciliationInterval = Long.valueOf(reconciliationIntervalEnvVar);
+        }
 
         Map<String, String> labelsMap = new HashMap<>();
 
@@ -28,7 +44,7 @@ public class ClusterControllerConfig {
             labelsMap.put(fields[0].trim(), fields[1].trim());
         }
 
-        return new ClusterControllerConfig(namespace, labelsMap);
+        return new ClusterControllerConfig(namespace, labelsMap, reconciliationInterval);
     }
 
     public Map<String, String> getLabels() {
@@ -45,5 +61,13 @@ public class ClusterControllerConfig {
 
     public void setNamespace(String namespace) {
         this.namespace = namespace;
+    }
+
+    public long getReconciliationInterval() {
+        return reconciliationInterval;
+    }
+
+    public void setReconciliationInterval(long reconciliationInterval) {
+        this.reconciliationInterval = reconciliationInterval;
     }
 }
