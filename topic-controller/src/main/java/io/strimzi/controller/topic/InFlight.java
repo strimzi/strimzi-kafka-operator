ed44ed8ed2539153886de0a3b65529d9e2d70812
Tom Bentley <tbentley@redhat.com>
2018-02-12 15:41:05 +0000

    Assign copyright to Strimzi authors

--- topic-controller/src/main/java/io/strimzi/controller/topic/InFlight.java
+++ topic-controller/src/main/java/io/strimzi/controller/topic/InFlight.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Red Hat Inc.
+ * Copyright 2018, Strimzi Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
