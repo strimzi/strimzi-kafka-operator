1ccd0995d794f3e346ec651615205faed84b8b3d
Paolo Patierno <ppatierno@live.com>
2018-02-13 11:00:22 +0100

    Added check on namespace and labels in the Cluster Controller configuration Added few other unit tests

--- cluster-controller/src/test/java/io/strimzi/controller/cluster/ClusterControllerConfigTest.java
+++ cluster-controller/src/test/java/io/strimzi/controller/cluster/ClusterControllerConfigTest.java
@@ -2,6 +2,7 @@ package io.strimzi.controller.cluster;
 
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,18 +12,24 @@ import static org.junit.Assert.assertEquals;
 public class ClusterControllerConfigTest {
 
     private static Map<String, String> labels = new HashMap<>(1);
+    private static Map<String, String> envVars = new HashMap<>(3);
 
     static {
         labels.put("strimzi.io/kind", "cluster");
+
+        envVars.put(ClusterControllerConfig.STRIMZI_NAMESPACE, "namespace");
+        envVars.put(ClusterControllerConfig.STRIMZI_CONFIGMAP_LABELS, "strimzi.io/kind=cluster");
+        envVars.put(ClusterControllerConfig.STRIMZI_FULL_RECONCILIATION_INTERVAL, "30000");
     }
 
     @Test
     public void testDefaultConfig() {
+
         ClusterControllerConfig config = new ClusterControllerConfig("namespace", labels);
 
         assertEquals("namespace", config.getNamespace());
         assertEquals(labels, config.getLabels());
-        assertEquals(120_000, config.getReconciliationInterval());
+        assertEquals(ClusterControllerConfig.DEFAULT_FULL_RECONCILIATION_INTERVAL, config.getReconciliationInterval());
     }
 
     @Test
@@ -38,15 +45,48 @@ public class ClusterControllerConfigTest {
     @Test
     public void testEnvVars() {
 
-        Map<String, String> envVars = new HashMap<>(2);
-        envVars.put(ClusterControllerConfig.STRIMZI_NAMESPACE, "namespace");
-        envVars.put(ClusterControllerConfig.STRIMZI_CONFIGMAP_LABELS, "strimzi.io/kind=cluster");
-        envVars.put(ClusterControllerConfig.STRIMZI_FULL_RECONCILIATION_INTERVAL, "30000");
-
         ClusterControllerConfig config = ClusterControllerConfig.fromMap(envVars);
 
         assertEquals("namespace", config.getNamespace());
         assertEquals(labels, config.getLabels());
         assertEquals(30_000, config.getReconciliationInterval());
+    }
+
+    @Test
+    public void testEnvVarsDefault() {
+
+        Map<String, String> envVars = new HashMap<>(2);
+        envVars.put(ClusterControllerConfig.STRIMZI_NAMESPACE, "namespace");
+        envVars.put(ClusterControllerConfig.STRIMZI_CONFIGMAP_LABELS, "strimzi.io/kind=cluster");
+
+        ClusterControllerConfig config = ClusterControllerConfig.fromMap(envVars);
+
+        assertEquals("namespace", config.getNamespace());
+        assertEquals(labels, config.getLabels());
+        assertEquals(ClusterControllerConfig.DEFAULT_FULL_RECONCILIATION_INTERVAL, config.getReconciliationInterval());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNoNamespace() {
+
+        Map<String, String> envVars = new HashMap<>(1);
+        envVars.put(ClusterControllerConfig.STRIMZI_CONFIGMAP_LABELS, "strimzi.io/kind=cluster");
+
+        ClusterControllerConfig.fromMap(envVars);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNoLabels() {
+
+        Map<String, String> envVars = new HashMap<>(1);
+        envVars.put(ClusterControllerConfig.STRIMZI_NAMESPACE, "namespace");
+
+        ClusterControllerConfig.fromMap(envVars);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyEnvVars() {
+
+        ClusterControllerConfig.fromMap(Collections.emptyMap());
     }
 }
