12196719fce9081cb1e87848c82b1108c979f60d
Paolo Patierno <ppatierno@live.com>
2018-02-22 16:57:46 +0100

    Fixed checkstyle on Assert imports

--- cluster-controller/src/test/java/io/strimzi/controller/cluster/resources/TopicControllerTest.java
+++ cluster-controller/src/test/java/io/strimzi/controller/cluster/resources/TopicControllerTest.java
@@ -15,7 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 
 public class TopicControllerTest {
 
