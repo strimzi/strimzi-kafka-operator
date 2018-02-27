5de67ea5e8d908d40ceefcfba7d2da9f8cfa7213
Tom Bentley <tbentley@redhat.com>
2018-02-27 17:40:27 +0000

    Fixup

--- common-test/src/main/java/io/strimzi/test/k8s/BaseKubeClient.java
+++ common-test/src/main/java/io/strimzi/test/k8s/BaseKubeClient.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 import static java.util.Arrays.asList;
