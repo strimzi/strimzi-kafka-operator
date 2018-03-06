b3016e70328fd7270efd49b1e0ab9d95725c7146
Jakub Scholz <www@scholzj.com>
2018-03-06 14:15:20 +0100

    Implement workaround hack to avoid RABC errors when kubernetes client asks if it is on OpenShift - Closes #286

--- cluster-controller/src/main/java/io/strimzi/controller/cluster/Main.java
+++ cluster-controller/src/main/java/io/strimzi/controller/cluster/Main.java
@@ -23,9 +23,12 @@ import io.strimzi.controller.cluster.operations.resource.StatefulSetOperations;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -34,15 +37,25 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        run(Vertx.vertx(), new DefaultKubernetesClient(), System.getenv()).setHandler(ar -> {
-            if (ar.failed()) {
-                log.error("Unable to start controller for 1 or more namespace", ar.cause());
+        Vertx vertx = Vertx.vertx();
+        KubernetesClient client = new DefaultKubernetesClient();
+
+        isOnOpenShift(vertx, client).setHandler(os -> {
+            if (os.succeeded()) {
+                run(vertx, client, os.result().booleanValue(), System.getenv()).setHandler(ar -> {
+                    if (ar.failed()) {
+                        log.error("Unable to start controller for 1 or more namespace", ar.cause());
+                        System.exit(1);
+                    }
+                });
+            } else {
+                log.error("Failed to distinguish between Kubernetes and OpenShift", os.cause());
                 System.exit(1);
             }
         });
     }
 
-    static CompositeFuture run(Vertx vertx, KubernetesClient client, Map<String, String> env) {
+    static CompositeFuture run(Vertx vertx, KubernetesClient client, boolean isOpenShift, Map<String, String> env) {
         ClusterControllerConfig config = ClusterControllerConfig.fromMap(env);
 
         ServiceOperations serviceOperations = new ServiceOperations(vertx, client);
@@ -53,7 +66,6 @@ public class Main {
         PodOperations podOperations = new PodOperations(vertx, client);
         EndpointOperations endpointOperations = new EndpointOperations(vertx, client);
 
-        boolean isOpenShift = Boolean.TRUE.equals(client.isAdaptable(OpenShiftClient.class));
         KafkaClusterOperations kafkaClusterOperations = new KafkaClusterOperations(vertx, isOpenShift, config.getOperationTimeoutMs(), configMapOperations, serviceOperations, statefulSetOperations, pvcOperations, podOperations, endpointOperations, deploymentOperations);
         KafkaConnectClusterOperations kafkaConnectClusterOperations = new KafkaConnectClusterOperations(vertx, isOpenShift, configMapOperations, deploymentOperations, serviceOperations);
 
@@ -93,5 +105,39 @@ public class Main {
                 });
         }
         return CompositeFuture.join(futures);
+    }
+
+    static Future<Boolean> isOnOpenShift(Vertx vertx, KubernetesClient client)  {
+        URL kubernetesApi = client.getMasterUrl();
+        Future<Boolean> fut = Future.future();
+
+        HttpClientOptions httpClientOptions = new HttpClientOptions();
+        httpClientOptions.setDefaultHost(kubernetesApi.getHost());
+
+        if (kubernetesApi.getPort() == -1) {
+            httpClientOptions.setDefaultPort(kubernetesApi.getDefaultPort());
+        } else {
+            httpClientOptions.setDefaultPort(kubernetesApi.getPort());
+        }
+
+        if (kubernetesApi.getProtocol().equals("https")) {
+            httpClientOptions.setSsl(true);
+            httpClientOptions.setTrustAll(true);
+        }
+
+        HttpClient httpClient = vertx.createHttpClient(httpClientOptions);
+
+        httpClient.getNow("/oapi", res -> {
+            if (res.statusCode() == 200) {
+                log.info("{} returned 200. We should be in OpenShift. It is safe to check 'isAdaptable'", res.request().absoluteURI());
+                Boolean isOpenShift = Boolean.TRUE.equals(client.isAdaptable(OpenShiftClient.class));
+                fut.complete(isOpenShift);
+            } else {
+                log.info("{} returned {}. We are not on OpenShift.", res.request().absoluteURI(), res.statusCode());
+                fut.complete(Boolean.FALSE);
+            }
+        });
+
+        return fut;
     }
 }
