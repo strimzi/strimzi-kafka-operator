05e1f13ed492590d38c22c0f0fa4dbd303c75524
Tom Bentley <tbentley@redhat.com>
2018-03-30 00:23:53 +0100

    Refactor delete

--- common-test/src/main/java/io/strimzi/test/StrimziRunner.java
+++ common-test/src/main/java/io/strimzi/test/StrimziRunner.java
@@ -30,7 +30,6 @@ import java.lang.annotation.Repeatable;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
