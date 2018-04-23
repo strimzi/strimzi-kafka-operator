cfa1a6a1096f8e3763473235d5d8114248771e47
Jakub Scholz <www@scholzj.com>
2018-04-23 22:35:51 +0200

    Use mvn verify instead of mvn test to run the integration tests

--- systemtest/scripts/run_tests.sh
+++ systemtest/scripts/run_tests.sh
@@ -5,4 +5,4 @@ if [ -n "$TESTCASE" ]; then
     EXTRA_ARGS="-Dtest=$TESTCASE"
 fi
 
-mvn test -pl systemtest -Djava.net.preferIPv4Stack=true -DtrimStackTrace=false $EXTRA_ARGS
+mvn verify -pl systemtest -Djava.net.preferIPv4Stack=true -DtrimStackTrace=false $EXTRA_ARGS
