8c9341a22b27efdb61e7f0b3a86cd832397abb25
Jakub Scholz <www@scholzj.com>
2018-04-24 16:43:41 +0200

    Remove unused imports :-/

--- systemtest/src/test/java/io/strimzi/systemtest/AbstractClusterIT.java
+++ systemtest/src/test/java/io/strimzi/systemtest/AbstractClusterIT.java
@@ -21,7 +21,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
