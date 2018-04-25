af14516567b4ba802efaf40f4c1e535f354bb516
Tom Bentley <tbentley@redhat.com>
2018-04-25 23:16:11 +0100

    And ZK and connect

--- docker-images/zookeeper/scripts/zookeeper_run.sh
+++ docker-images/zookeeper/scripts/zookeeper_run.sh
@@ -65,5 +65,16 @@ if [ "$ZOOKEEPER_METRICS_ENABLED" = "true" ]; then
   export KAFKA_OPTS="-javaagent:/opt/prometheus/jmx_prometheus_javaagent.jar=9404:/opt/prometheus/config/config.yml"
 fi
 
+if [ -z "$KAFKA_HEAP_OPTS" -a -n "${DYNAMIC_HEAP_FRACTION}" ]; then
+    . ./dynamic_resources.sh
+    # Calculate a max heap size based some DYNAMIC_HEAP_FRACTION of the heap
+    # available to a jvm using 100% of the GCroup-aware memory
+    # up to some optional DYNAMIC_HEAP_MAX
+    CALC_MAX_HEAP=$(get_heap_size ${DYNAMIC_HEAP_FRACTION} ${DYNAMIC_HEAP_MAX})
+    if [ -n "$CALC_MAX_HEAP" ]; then
+      export KAFKA_HEAP_OPTS="-Xms${CALC_MAX_HEAP} -Xmx${CALC_MAX_HEAP}"
+    fi
+fi
+
 # starting Zookeeper with final configuration
 exec $KAFKA_HOME/bin/zookeeper-server-start.sh /tmp/zookeeper.properties
