ce90a51d8f5ee4ac514af5d9ab9c831a89216ab8
Jakub Scholz <www@scholzj.com>
2018-05-03 10:53:03 +0200

    Fix unused import

--- cluster-controller/src/test/java/io/strimzi/controller/cluster/model/KafkaConnectConfigurationTest.java
+++ cluster-controller/src/test/java/io/strimzi/controller/cluster/model/KafkaConnectConfigurationTest.java
@@ -9,7 +9,6 @@ import io.vertx.core.json.JsonObject;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class KafkaConnectConfigurationTest {
     @Test
