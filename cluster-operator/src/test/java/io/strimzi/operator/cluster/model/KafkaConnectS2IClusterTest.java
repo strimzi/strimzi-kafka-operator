c5ac10455e22bf7cf3a10dcf5c6b97aa32df2282
Jakub Scholz <www@scholzj.com>
2018-05-09 16:19:56 +0200

    Add missing import

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/model/KafkaConnectS2IClusterTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/model/KafkaConnectS2IClusterTest.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class KafkaConnectS2IClusterTest {
