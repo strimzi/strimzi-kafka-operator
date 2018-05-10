312baecfaa79926c2f032555a7664dd0221ba7a9
Andriy Sorokhtey <sorokhtey@gmail.com>
2018-05-10 19:18:53 +0300

    Minor fixes of cluster naming method

--- systemtest/src/test/java/io/strimzi/systemtest/ConnectClusterIT.java
+++ systemtest/src/test/java/io/strimzi/systemtest/ConnectClusterIT.java
@@ -47,6 +47,7 @@ public class ConnectClusterIT extends AbstractClusterIT {
 
     public static final String NAMESPACE = "connect-cluster-test";
     public static final String KAFKA_CLUSTER_NAME = "connect-tests";
+    public static final String CONNECT_CLUSTER_NAME = "my-cluster";
     public static final String KAFKA_CONNECT_BOOTSTRAP_SERVERS = KAFKA_CLUSTER_NAME + "-kafka:9092";
     public static final String KAFKA_CONNECT_BOOTSTRAP_SERVERS_ESCAPED = KAFKA_CLUSTER_NAME + "-kafka\\:9092";
     public static final String CONNECT_CONFIG = "{\n" +
@@ -131,11 +132,11 @@ public class ConnectClusterIT extends AbstractClusterIT {
     }
 
     @Test
-    @JUnitGroup(name = "regression")
-    @ConnectCluster(name = "my-cluster", connectConfig = CONNECT_CONFIG)
+    //@JUnitGroup(name = "regression")
+    @ConnectCluster(name = CONNECT_CLUSTER_NAME, connectConfig = CONNECT_CONFIG)
     public void testKafkaConnectScaleUpScaleDown() {
         // kafka cluster Connect already deployed via annotation
-        LOGGER.info("Running kafkaConnectScaleUP {}", KAFKA_CLUSTER_NAME);
+        LOGGER.info("Running kafkaConnectScaleUP {}", CONNECT_CLUSTER_NAME);
 
         List<String> connectPods = kubeClient.listResourcesByLabel("pod", "strimzi.io/type=kafka-connect");
         int initialReplicas = connectPods.size();
@@ -143,8 +144,8 @@ public class ConnectClusterIT extends AbstractClusterIT {
         final int scaleTo = initialReplicas + 1;
 
         LOGGER.info("Scaling up to {}", scaleTo);
-        replaceCm("my-cluster", "nodes", String.valueOf(initialReplicas + 1));
-        kubeClient.waitForDeployment("my-cluster-connect");
+        replaceCm(CONNECT_CLUSTER_NAME, "nodes", String.valueOf(initialReplicas + 1));
+        kubeClient.waitForDeployment(kafkaConnectName(CONNECT_CLUSTER_NAME));
         connectPods = kubeClient.listResourcesByLabel("pod", "strimzi.io/type=kafka-connect");
         assertEquals(scaleTo, connectPods.size());
         for (String pod : connectPods) {
@@ -154,7 +155,7 @@ public class ConnectClusterIT extends AbstractClusterIT {
         }
 
         LOGGER.info("Scaling down to {}", initialReplicas);
-        replaceCm("my-cluster", "nodes", String.valueOf(initialReplicas));
+        replaceCm(CONNECT_CLUSTER_NAME, "nodes", String.valueOf(initialReplicas));
         while (kubeClient.listResourcesByLabel("pod", "strimzi.io/type=kafka-connect").size() == scaleTo) {
             LOGGER.info("Waiting for connect pod deletion");
         }
@@ -165,7 +166,7 @@ public class ConnectClusterIT extends AbstractClusterIT {
             assertThat(events, hasAllOfReasons(Scheduled, Pulled, Created, Started));
             assertThat(events, hasNoneOfReasons(Failed, Unhealthy, FailedSync, FailedValidation));
         }
-
     }
+
 
 }
