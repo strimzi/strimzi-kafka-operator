d7ef8dedf7f24c07fcf037631514aa45cb5c7cc7
Paolo Patierno <ppatierno@live.com>
2018-06-20 13:43:28 +0200

    Added a new CLIENT_ENC port 9093 for encrypted traffic (from clients) Updated the REPLICATION listener to be encrypted by default Removed enable/disable encryption

--- docker-images/kafka/scripts/kafka_config_generator.sh
+++ docker-images/kafka/scripts/kafka_config_generator.sh
@@ -5,9 +5,9 @@ cat <<EOF
 broker.id=${KAFKA_BROKER_ID}
 broker.rack=${KAFKA_RACK}
 # Listeners
-listeners=CLIENT://0.0.0.0:9092,REPLICATION://0.0.0.0:9091
-advertised.listeners=CLIENT://$(hostname -f):9092,REPLICATION://$(hostname -f):9091
-listener.security.protocol.map=CLIENT:${KAFKA_LISTENER_SECURITY_PROTOCOL:-PLAINTEXT},REPLICATION:${KAFKA_LISTENER_SECURITY_PROTOCOL:-PLAINTEXT}
+listeners=CLIENT://0.0.0.0:9092,REPLICATION://0.0.0.0:9091,CLIENTENC://0.0.0.0:9093
+advertised.listeners=CLIENT://$(hostname -f):9092,REPLICATION://$(hostname -f):9091,CLIENTENC://$(hostname -f):9093
+listener.security.protocol.map=CLIENT:PLAINTEXT,REPLICATION:SSL,CLIENTENC:SSL
 inter.broker.listener.name=REPLICATION
 # Zookeeper
 zookeeper.connect=${KAFKA_ZOOKEEPER_CONNECT:-zookeeper:2181}
