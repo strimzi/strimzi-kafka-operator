0720016421b609f2601e590c553a2db14583afd6
Jakub Scholz <www@scholzj.com>
2018-07-04 15:57:09 +0200

    Travis should be able to build PRs from forks (#533)
    * Do not push CI images, taging should be enough
    
    * docker login needs to be moved, not just copied :-/
    
    * Use minikube docker-env
    
    * Tag without org
    
    * Another attempt
    
    * Revert changes after System tests
    
    * Finishing touches
    
    * Fix typo in comment

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,19 +10,26 @@ export DOCKER_TAG=$COMMIT
 
 make docker_build
 
-echo "Login into Docker Hub ..."
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-
+# Use local registry for system tests
+OLD_DOCKER_REGISTRY=$DOCKER_REGISTRY
+export DOCKER_REGISTRY="localhost:5000"
 export DOCKER_TAG=$BRANCH
-echo "Pushing to docker org $DOCKER_ORG under tag $DOCKER_TAG"
+echo "Docker push with registry $DOCKER_REGISTRY and org $DOCKER_ORG under tag $DOCKER_TAG"
 make docker_push
 
 export DOCKER_TAG=$COMMIT
-echo "Pushing to docker org $DOCKER_ORG under tag $DOCKER_TAG"
+echo "Docker push with registry $DOCKER_REGISTRY and org $DOCKER_ORG under tag $DOCKER_TAG"
 make docker_push
+
+OLD_DOCKER_ORG=$DOCKER_ORG
+export DOCKER_ORG="localhost:5000/strimzici"
 
 echo "Running systemtests"
 ./systemtest/scripts/run_tests.sh ${SYSTEMTEST_ARGS}
+
+# Revert modified DOCKER_REGISTRY and DOCKER_ORG after system tests
+export DOCKER_REGISTRY=$OLD_DOCKER_REGISTRY
+export DOCKER_ORG=$OLD_DOCKER_ORG
 
 # If that worked we can push to the real docker org
 if [ "$PULL_REQUEST" != "false" ] ; then
@@ -30,6 +37,9 @@ if [ "$PULL_REQUEST" != "false" ] ; then
 elif [ "$TAG" = "latest" ] && [ "$BRANCH" != "master" ]; then
     echo "Not in master branch and not in release tag - nothing to push"
 else
+    echo "Login into Docker Hub ..."
+    docker login -u $DOCKER_USER -p $DOCKER_PASS
+
     export DOCKER_ORG=strimzi
     export DOCKER_TAG=$TAG
     echo "Pushing to docker org $DOCKER_ORG"
