28e9a6e19faa1d3ed535284895a87ac387680038
Jakub Scholz <www@scholzj.com>
2018-07-08 10:16:51 +0200

    Log exceptions from `createOrUpdate` properly (#546)
    * Log the exception properly
    
    * Fix typo

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/AbstractAssemblyOperator.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/AbstractAssemblyOperator.java
@@ -199,7 +199,7 @@ public abstract class AbstractAssemblyOperator {
                                         if (createResult.cause() instanceof InvalidConfigMapException) {
                                             log.error(createResult.cause().getMessage());
                                         } else {
-                                            log.error(createResult.cause().toString());
+                                            log.error("{}: createOrUpdate failed", reconciliation, createResult.cause());
                                         }
                                     } else {
                                         handler.handle(createResult);
