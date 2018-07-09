d755f094ec04a94e97db979be0af65597bbc3c97
kyguy <kliberti@redhat.com>
2018-07-09 10:55:40 -0400

    Stunnel sidecar container (#543)
    * WIP: Stunnel sidecar container
    
    * Updated stunnel version
    
    * Put Stunnel process in the foreground
    
    * Used the internal CA cert for stunnel client configuration
    
    * Removed the need for declaring listening internal ports for Zookeeper container
    Fixed ports for the stunnel sidecar accordingly
    
    * Added default stunnel Zookeeper image parameter in CO deployment
    
    * Fixed ST getting pods images, now that we have multi-containers pods
    
    * Revert "Fixed ST getting pods images, now that we have multi-containers pods"
    
    This reverts commit c6b48f29c2a97619f66a3bc1e1a9fa5b453794c6.
    
    * Fixed ST getting pods images, now that we have multi-containers pods

--- docker-images/zookeeper/scripts/zookeeper_config_generator.sh
+++ docker-images/zookeeper/scripts/zookeeper_config_generator.sh
@@ -12,7 +12,10 @@ ${ZOOKEEPER_CONFIGURATION}
 EOF
 
 NODE=1
+FOLLOWER_PORT=$(expr 10 \* 2888)
+ELECTION_PORT=$(expr 10 \* 3888)
 while [ $NODE -le $ZOOKEEPER_NODE_COUNT ]; do
-    echo "server.${NODE}=${BASE_HOSTNAME}-$((NODE-1)).${BASE_FQDN}:2888:3888"
-    let NODE=NODE+1
+  echo "127.0.0.1:$(expr $FOLLOWER_PORT + $NODE ):$(expr $ELECTION_PORT + $NODE )"
+  let NODE=NODE+1
 done
+echo "127.0.0.1:28880:38880"
