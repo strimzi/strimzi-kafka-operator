c976935965c454c2a44cfc8700a7fd91c1b53c8e
Paolo Patierno <ppatierno@live.com>
2018-07-10 12:36:49 +0200

    Fixed Stunnel configuration about certs, binding localhost interface and others Fixed Zookeeper configuration missing "server.X" fields

--- docker-images/stunnel-zookeeper/scripts/stunnel_config_generator.sh
+++ docker-images/stunnel-zookeeper/scripts/stunnel_config_generator.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
-	      
-CERTS=/etc/stunnel/certs/
+
+# path were the Secret with certificates is mounted
+CERTS=/etc/stunnel/certs
 
 echo "foreground = yes"
-#echo "pid=/tmp/stunnel.pid"
 echo "debug = info"
  
 declare -a PORTS=("2888" "3888")
+
 for port in "${PORTS[@]}"
 do
-NODE=1
- while [ $NODE -le $ZOOKEEPER_NODE_COUNT ]; do
-	        
-		PEER=${BASE_HOSTNAME}-$((NODE-1))
-		KEY_AND_CERT=${CERTS}${PEER}	
-		PEM=/tmp/${PEER}.pem
+    NODE=1
+    while [ $NODE -le $ZOOKEEPER_NODE_COUNT ]
+    do
+        # current node configuration is not needed
+        if [ $NODE -ne $ZOOKEEPER_ID ]; then
+        PEER=${BASE_HOSTNAME}-$((NODE-1))
 
-		# Not necessarily the way to create pem file, might have to use openssl
-		cat ${KEY_AND_CERT}.crt ${KEY_AND_CERT}.key > ${PEM}
-		chmod 640 ${PEM}
-
-		# Stunnel client configuration
+		# configuration for outgoing connection to a peer
 		cat <<-EOF
-		[${PEER})]
+		[${PEER}]
 		client=yes
-		#cert=${PEM}
-		CAfile=${CERTS}internal-ca.crt
+		CAfile=${CERTS}/internal-ca.crt
 		accept=127.0.0.1:$(expr $port \* 10 + $NODE - 1)
 		connect=${PEER}.${BASE_FQDN}:$port
 
 		EOF
+		fi
 		let NODE=NODE+1
-  done
-	# Zookeeper port where stunnel forwards received traffic
+    done
+
+    # Zookeeper port where stunnel forwards received traffic
 	CONNECTOR_PORT=$(expr $port \* 10 + $ZOOKEEPER_ID - 1)
 
-	# Stunnel listener configuration
+    # private key and related cert for current Zookeeper instance into one PEM
+	CURRENT=${BASE_HOSTNAME}-$((ZOOKEEPER_ID-1))
+	KEY_AND_CERT=/tmp/${CURRENT}.pem
+
+    cat ${CERTS}/${CURRENT}.crt ${CERTS}/${CURRENT}.key > ${KEY_AND_CERT}
+    chmod 640 ${KEY_AND_CERT}
+
+	# listener configuration for incoming connection from peers
 	cat <<-EOF
 	[listener-$port]
 	client=no
-	cert=${PEM}
-	accept=127.0.0.1:$port
+	cert=${KEY_AND_CERT}
+	accept=$port
 	connect=127.0.0.1:$CONNECTOR_PORT
 
 	EOF
