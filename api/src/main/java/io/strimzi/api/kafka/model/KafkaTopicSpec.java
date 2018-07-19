5e555853e3eb9e7be183e39b0f6f84ca6c9c4d5d
Tom Bentley <tbentley@redhat.com>
2018-07-19 10:50:17 +0100

    wip

---
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaTopicSpec.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2018, Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.api.kafka.model;
+
+public class KafkaTopicSpec {
+}
