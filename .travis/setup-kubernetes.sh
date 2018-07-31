39a2810543ced08e90a5a129e81b02adb2283786
Tom Bentley <tbentley@redhat.com>
2018-07-31 19:12:30 +0100

    helm init

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -27,6 +27,7 @@ function install_helm {
     curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
     chmod 700 get_helm.sh
     sudo ./get_helm.sh --version ${TEST_HELM_VERSION}
+    helm init
 }
 
 function wait_for_minikube {
