ec961f1b9df8c79701d722ffc099d47d4f2d8f2f
Tom Bentley <tbentley@redhat.com>
2018-07-31 18:00:21 +0100

    I could weep

--- .travis/install_yq.sh
+++ .travis/install_yq.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/mikefarah/yq/releases/download/2.1.1/yq_linux_amd64 > /usr/bin/yq
+curl -L https://github.com/mikefarah/yq/releases/download/2.1.1/yq_linux_amd64 > yq && chmod +x yq
+sudo cp yq /usr/bin/
