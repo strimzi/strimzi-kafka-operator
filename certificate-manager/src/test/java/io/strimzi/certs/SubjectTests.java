0dbe1006015ddfb6be674e4768c4d6cdf4bdeaa0
Jakub Scholz <www@scholzj.com>
2018-08-02 15:55:53 +0200

    Organization should not be in the subject when it is not set

---
+++ certificate-manager/src/test/java/io/strimzi/certs/SubjectTests.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2017-2018, Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.certs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SubjectTests {
+    @Test
+    public void testSubjectToString()   {
+        Subject sbj = new Subject();
+        sbj.setCommonName("joe");
+        assertEquals("/CN=joe", sbj.toString());
+
+        sbj = new Subject();
+        sbj.setOrganizationName("MyOrg");
+        assertEquals("/O=MyOrg", sbj.toString());
+
+        sbj = new Subject();
+        sbj.setCommonName("joe");
+        sbj.setOrganizationName("MyOrg");
+        assertEquals("/O=MyOrg/CN=joe", sbj.toString());
+    }
+}
