8a14e36e83dd544d2cdd27a71b1e9f67b15ee82d
Jakub Scholz <www@scholzj.com>
2018-08-20 09:52:21 +0200

    When Spec is required, OpenShift 3.10 doesn't like it :-/ (#774)

--- api/src/main/java/io/strimzi/api/kafka/model/KafkaUser.java
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaUser.java
@@ -14,7 +14,6 @@ import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonDeserializer;
@@ -91,7 +90,6 @@ public class KafkaUser extends CustomResource {
     }
 
     @Description("The specification of the user.")
-    @JsonProperty(required = true)
     public KafkaUserSpec getSpec() {
         return spec;
     }
