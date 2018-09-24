904a01fd24f1750966d4a5a73b6c1aad17330cf7
Paolo Patierno <ppatierno@live.com>
2018-09-24 10:22:44 +0200

    Added logging Added heap options

--- docker-images/kafka-mirror-maker/scripts/kafka_mirror_maker_run.sh
+++ docker-images/kafka-mirror-maker/scripts/kafka_mirror_maker_run.sh
@@ -20,9 +20,29 @@ echo "Kafka Mirror Maker producer configuration:"
 ./kafka_mirror_maker_producer_config_generator.sh | tee /tmp/strimzi-producer.properties
 echo ""
 
+# Disable Kafka's GC logging (which logs to a file)...
+export GC_LOG_ENABLED="false"
+# ... but enable equivalent GC logging to stdout
+export KAFKA_GC_LOG_OPTS="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps"
+
+if [ -z "$KAFKA_LOG4J_OPTS" ]; then
+export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:$KAFKA_HOME/custom-config/log4j.properties"
+fi
+
 # enabling Prometheus JMX exporter as Java agent
 if [ "$KAFKA_MIRRORMAKER_METRICS_ENABLED" = "true" ]; then
   export KAFKA_OPTS="-javaagent:/opt/prometheus/jmx_prometheus_javaagent.jar=9404:$KAFKA_HOME/custom-config/metrics-config.yml"
+fi
+
+if [ -z "$KAFKA_HEAP_OPTS" -a -n "${DYNAMIC_HEAP_FRACTION}" ]; then
+    . ./dynamic_resources.sh
+    # Calculate a max heap size based some DYNAMIC_HEAP_FRACTION of the heap
+    # available to a jvm using 100% of the GCroup-aware memory
+    # up to some optional DYNAMIC_HEAP_MAX
+    CALC_MAX_HEAP=$(get_heap_size ${DYNAMIC_HEAP_FRACTION} ${DYNAMIC_HEAP_MAX})
+    if [ -n "$CALC_MAX_HEAP" ]; then
+      export KAFKA_HEAP_OPTS="-Xms${CALC_MAX_HEAP} -Xmx${CALC_MAX_HEAP}"
+    fi
 fi
 
 if [ -n "$KAFKA_MIRRORMAKER_WHITELIST" ]; then
