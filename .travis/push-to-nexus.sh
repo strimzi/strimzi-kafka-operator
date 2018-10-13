8d11bdff572eceec2d0e46bbfc21fe2cbde232a0
Jakub Scholz <www@scholzj.com>
2018-10-13 15:43:32 +0200

    Snapshot push to Nexus - attempt XIV

--- .travis/push-to-nexus.sh
+++ .travis/push-to-nexus.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_57e4d0a26fa7_key -iv $encrypted_57e4d0a26fa7_iv -in .travis/signing.gpg.enc -out signing.gpg -d
+openssl aes-256-cbc -K $encrypted_cdaf52794220_key -iv $encrypted_cdaf52794220_iv -in .travis/signing.gpg.enc -out signing.gpg -d
 gpg --import signing.gpg
 
 pushd ./api
