ffaad55d157fc23c6850af69606c65b3d8f037c0
Jakub Scholz <www@scholzj.com>
2018-10-14 15:58:51 +0200

    Try to deploy with parent pom

--- .travis/push-to-nexus.sh
+++ .travis/push-to-nexus.sh
@@ -3,9 +3,7 @@
 openssl aes-256-cbc -K $encrypted_cdaf52794220_key -iv $encrypted_cdaf52794220_iv -in .travis/signing.gpg.enc -out signing.gpg -d
 gpg --import signing.gpg
 
-pushd ./api
-GPG_EXECUTABLE=gpg mvn -DskipTests -s ../.travis/settings.xml clean package gpg:sign deploy -P ossrh
-popd
+GPG_EXECUTABLE=gpg mvn -pl .,api -DskipTests -s .travis/settings.xml clean package gpg:sign deploy -P ossrh
 
 rm -rf signing.gpg
 gpg --delete-keys
