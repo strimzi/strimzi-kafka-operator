6bd7b4bad689f2a3ae74bf603443cfa62e3d8496
Jakub Scholz <www@scholzj.com>
2018-10-14 21:06:42 +0200

    Revert changes

--- .travis/push-to-nexus.sh
+++ .travis/push-to-nexus.sh
@@ -3,7 +3,9 @@
 openssl aes-256-cbc -K $encrypted_cdaf52794220_key -iv $encrypted_cdaf52794220_iv -in .travis/signing.gpg.enc -out signing.gpg -d
 gpg --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn -pl .,api -DskipTests -s .travis/settings.xml clean package gpg:sign deploy -P ossrh
+pushd ./api
+GPG_EXECUTABLE=gpg mvn -DskipTests -s ../.travis/settings.xml clean package gpg:sign deploy -P ossrh
+popd
 
 rm -rf signing.gpg
 gpg --delete-keys
