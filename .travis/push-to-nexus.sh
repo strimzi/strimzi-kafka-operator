cf88c38539a02ed5da45d2fb6019a91e00374bdb
Jakub Scholz <www@scholzj.com>
2018-10-21 12:08:43 +0200

    Deploy only the right projects including the parent pom

--- .travis/push-to-nexus.sh
+++ .travis/push-to-nexus.sh
@@ -3,9 +3,7 @@
 openssl aes-256-cbc -K $encrypted_cdaf52794220_key -iv $encrypted_cdaf52794220_iv -in .travis/signing.gpg.enc -out signing.gpg -d
 gpg --import signing.gpg
 
-pushd ./api
-GPG_EXECUTABLE=gpg mvn -DskipTests -s ../.travis/settings.xml clean package gpg:sign deploy -P ossrh
-popd
+GPG_EXECUTABLE=gpg mvn -DskipTests -s ./.travis/settings.xml  -pl ./,crd-generator,api -P ossrh clean package gpg:sign deploy
 
 rm -rf signing.gpg
 gpg --delete-keys
