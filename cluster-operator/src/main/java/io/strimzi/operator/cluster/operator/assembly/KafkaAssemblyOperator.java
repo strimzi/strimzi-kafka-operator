ca7cbd725cf29e2932e6a339d649e7c4143c4b1c
Stanislav Knot <sknot@redhat.com>
2018-10-25 08:02:18 +0200

    maybeImprove

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaAssemblyOperator.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaAssemblyOperator.java
@@ -511,17 +511,20 @@ public class KafkaAssemblyOperator extends AbstractAssemblyOperator<KubernetesCl
         }
 
         Future<ReconciliationState> zkScaleUp() {
-            int currentReplicas = 0;
-            StatefulSet ss = zkSetOperations.get(namespace, ZookeeperCluster.zookeeperClusterName(name));
-            if (ss != null) {
-                currentReplicas = ss.getSpec().getReplicas();
+            Future<StatefulSet> futss = zkSetOperations.getAsync(namespace, ZookeeperCluster.zookeeperClusterName(name));
+            if (futss != null) {
+                return withVoid(futss.map(ss -> ss == null ? 0 : ss.getSpec().getReplicas())
+                    .compose(currentReplicas -> {
+                        Future<Integer> result = Future.succeededFuture(currentReplicas);
+                        for (int i = currentReplicas + 1; i <= zkCluster.getReplicas(); i++) {
+                            // When scaling up a Zk cluster in one step the quorum can be lost. It is better to use steps.
+                            result.compose(scaleTo -> zkSetOperations.scaleUp(namespace, zkCluster.getName(), scaleTo + 1));
+                        }
+                        return result;
+                    }));
+            } else {
+                return withVoid(Future.succeededFuture(0));
             }
-            Future<Integer> result = Future.succeededFuture(currentReplicas);
-            for (int i = currentReplicas + 1; i <= zkCluster.getReplicas(); i++) {
-                // When scaling up a Zk cluster in one step the quorum can be lost. It is better to use steps.
-                result.compose(scaleTo -> zkSetOperations.scaleUp(namespace, zkCluster.getName(), scaleTo + 1));
-            }
-            return withVoid(result);
         }
 
         Future<ReconciliationState> zkServiceEndpointReadiness() {
