6dc661e501ef510d0eb39d893a8218531ac41171
Stanislav Knot <sknot@redhat.com>
2018-10-30 13:32:19 +0100

    printing Strimzi version to the CO log (on startup) (#1041)

--- kafka-init/src/main/java/io/strimzi/kafka/init/Main.java
+++ kafka-init/src/main/java/io/strimzi/kafka/init/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        log.info("Init-kafka {} is starting", Main.class.getPackage().getImplementationVersion());
         InitWriterConfig config = InitWriterConfig.fromMap(System.getenv());
         KubernetesClient client = new DefaultKubernetesClient();
 
