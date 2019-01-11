80c6675e3f1100cfe8463bfc13aa93788f7ce0d4
Jakub Scholz <www@scholzj.com>
2019-01-11 13:27:39 +0100

    Docs: Add documentation for Pod Disruption Budgets (#1202)
    * Add documentation for Pod Disruption Budgets
    
    * Review comments
    
    * Updated API description => Needs regenerated docs
    
    * Review comments II

--- api/src/main/java/io/strimzi/api/kafka/model/template/PodDisruptionBudgetTemplate.java
+++ api/src/main/java/io/strimzi/api/kafka/model/template/PodDisruptionBudgetTemplate.java
@@ -35,7 +35,7 @@ public class PodDisruptionBudgetTemplate implements Serializable {
     private int maxUnavailable = 1;
     private Map<String, Object> additionalProperties = new HashMap<>(0);
 
-    @Description("Metadata which should be applied to the resource.")
+    @Description("Metadata which should be applied to the `PodDistruptionBugetTemplate` resource.")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     public MetadataTemplate getMetadata() {
         return metadata;
