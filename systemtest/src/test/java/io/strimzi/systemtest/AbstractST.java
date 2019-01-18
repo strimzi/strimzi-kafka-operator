1e035b61c371f166c4d1973f89eaa7498517b884
Jakub Stejskal <stejskinek@gmail.com>
2019-01-18 10:24:36 +0100

    WIP: StrimziExtension refactor

--- systemtest/src/test/java/io/strimzi/systemtest/AbstractST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/AbstractST.java
@@ -47,6 +47,8 @@ import io.strimzi.test.k8s.KubeClusterResource;
 import io.strimzi.test.k8s.ProcessResult;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -88,6 +90,8 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public abstract class AbstractST {
 
+    private static final String CO_INSTALL_DIR = "../install/cluster-operator";
+
     static {
         Crds.registerCustomKinds();
     }
@@ -119,6 +123,11 @@ public abstract class AbstractST {
     public static KubeClusterResource cluster = new KubeClusterResource();
     protected static DefaultKubernetesClient client = new DefaultKubernetesClient();
     static KubeClient<?> kubeClient = cluster.client();
+
+    private static List<String> deploymentResources;
+    private static List<String> deploymentNamespaces;
+    private static String clientNamespace;
+    private static Map<File, String> clusterOperatorMap;
 
     Resources resources;
     static Resources testClassResources;
@@ -1163,6 +1172,74 @@ public abstract class AbstractST {
 
     static void applyRoleBindings(String namespace) {
         applyRoleBindings(namespace, namespace);
+    }
+
+    static void applyClusterOperatorInstallFiles() {
+        clusterOperatorMap = Arrays.stream(new File(CO_INSTALL_DIR).listFiles()).sorted().filter(file ->
+                !file.getName().matches(".*(Binding|Deployment)-.*")
+        ).collect(Collectors.toMap(file -> file, f -> TestUtils.getContent(f, TestUtils::toYamlString), (x, y) -> x, LinkedHashMap::new));
+        for (Map.Entry<File, String> entry : clusterOperatorMap.entrySet()) {
+            LOGGER.info("Creating possibly modified version of {}", entry.getKey());
+            kubeClient.clientWithAdmin().applyContent(entry.getValue());
+        }
+    }
+
+    static void deleteClusterOperatorInstallFiles() {
+        for (Map.Entry<File, String> entry : clusterOperatorMap.entrySet()) {
+            LOGGER.info("Deleting {}", entry.getKey());
+            kubeClient.clientWithAdmin().deleteContent(entry.getValue());
+        }
+    }
+
+    static void createCustomResrouces(List<String> resources) {
+        deploymentResources = resources;
+        for (String resource : resources) {
+            LOGGER.info("Creating resources {}", resource);
+            kubeClient.create(resource);
+        }
+    }
+
+    static void deleteCustomResrouces() {
+        for (String resource : deploymentResources) {
+            LOGGER.info("Deleting resources {}", resource);
+            kubeClient.delete(resource);
+        }
+    }
+
+    static void createNamespaces(String useNamespace, List<String> namespaces) {
+        clientNamespace = useNamespace;
+        deploymentNamespaces = namespaces;
+        for (String namespace: namespaces) {
+            LOGGER.info("Creating namespace: {}", namespace);
+            kubeClient.createNamespace(namespace);
+            kubeClient.waitForResourceCreation("Namespace", namespace);
+            LOGGER.info("Namespace {} created", namespace);
+        }
+        LOGGER.info("Using namespace {}", useNamespace);
+        kubeClient.namespace(useNamespace);
+    }
+
+    static void deleteNamespaces() {
+        for (String namespace: deploymentNamespaces) {
+            LOGGER.info("Deleting namespace: {}", namespace);
+            kubeClient.createNamespace(namespace);
+            kubeClient.waitForResourceDeletion("Namespace", namespace);
+            LOGGER.info("Namespace {} deleted", namespace);
+        }
+        LOGGER.info("Using namespace {}", clientNamespace);
+        kubeClient.namespace(clientNamespace);
+    }
+
+    static void prepareEnvForOperator(String clientNamespace, List<String> namespaces, List<String> resources) {
+        createNamespaces(clientNamespace, namespaces);
+        createCustomResrouces(resources);
+        applyClusterOperatorInstallFiles();
+    }
+
+    static void tearodownEnvForOperator(String clientNamespace, String... namespaces) {
+        deleteCustomResrouces();
+        deleteClusterOperatorInstallFiles();
+        deleteNamespaces();
     }
 
     @BeforeEach
