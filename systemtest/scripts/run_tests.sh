f1d95d4140cc68649e1f9bfcd9efeb73a5d60559
Jakub Stejskal <stejskinek@gmail.com>
2019-01-18 10:26:12 +0100

    NEW: Add correct profile for execute acceptance tests on travis

--- systemtest/scripts/run_tests.sh
+++ systemtest/scripts/run_tests.sh
@@ -5,4 +5,4 @@ if [ -n "$TESTCASE" ]; then
     EXTRA_ARGS="-Dit.test=$TESTCASE"
 fi
 
-mvn -e -q verify -pl systemtest -Djava.net.preferIPv4Stack=true -DtrimStackTrace=false -DjunitTags=acceptance $EXTRA_ARGS
+mvn -e -q verify -pl systemtest -P systemtests -Djava.net.preferIPv4Stack=true -DtrimStackTrace=false -DjunitTags=acceptance $EXTRA_ARGS
