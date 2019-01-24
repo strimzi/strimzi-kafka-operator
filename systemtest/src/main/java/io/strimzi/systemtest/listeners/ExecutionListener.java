86ffb86382a320cef79e1339dededde07e780a14
Jakub Stejskal <stejskinek@gmail.com>
2019-01-24 13:22:37 +0100

    FIX: remove logger from execution listener

--- systemtest/src/main/java/io/strimzi/systemtest/listeners/ExecutionListener.java
+++ systemtest/src/main/java/io/strimzi/systemtest/listeners/ExecutionListener.java
@@ -5,15 +5,12 @@
 package io.strimzi.systemtest.listeners;
 
 import io.strimzi.test.timemeasuring.TimeMeasuringSystem;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestPlan;
 
 import static io.strimzi.systemtest.AbstractST.TEST_LOG_DIR;
 
 public class ExecutionListener implements TestExecutionListener {
-    private static final Logger LOGGER = LogManager.getLogger(ExecutionListener.class);
 
     @Override
     public void testPlanExecutionFinished(TestPlan testPlan) {
