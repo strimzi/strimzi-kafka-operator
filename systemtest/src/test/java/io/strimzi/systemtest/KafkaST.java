64d1e5f8e1bb7fbe90d7bd82e0054efeceaeca67
Jakub Stejskal <stejskinek@gmail.com>
2019-01-29 12:23:52 +0100

    fixup! NEW: Reuse already implemented logic for rolling check

--- systemtest/src/test/java/io/strimzi/systemtest/KafkaST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/KafkaST.java
@@ -34,7 +34,6 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
