6f622cae9d7895920e906c2f1b0f992b96ee2ed1
Tom Bentley <tombentley@users.noreply.github.com>
2019-03-01 16:18:13 +0100

    Fix problem with upgrade (#1397)
    * Fixes a bug observed when upgrading a Strimz 0.8.x cluster to 0.11.0. When the CO the CO is upgraded it looks for the version annotation on the Kafka SS. This was not present in 0.8.x or 0.9.0 so it uses the default version. The default version is now 2.1.x, so the CO sees this and when the spec.kafka.version in the CR is 2.0.0 the CO erroneously starts a downgrade. The pods then end up crashlooping.

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaAssemblyOperator.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaAssemblyOperator.java
@@ -493,7 +493,8 @@ public class KafkaAssemblyOperator extends AbstractAssemblyOperator<KubernetesCl
                     log.debug("Does SS {} need to be upgraded?", ss.getMetadata().getName());
                     Future<?> result;
                     // Get the current version of the cluster
-                    KafkaVersion currentVersion = versions.version(Annotations.annotations(ss).get(ANNO_STRIMZI_IO_KAFKA_VERSION));
+                    // Strimzi 0.8.x and 0.9.0 didn't set the annotation, so if it's absent we know it must be 2.0.0
+                    KafkaVersion currentVersion = versions.version(Annotations.annotations(ss).getOrDefault(ANNO_STRIMZI_IO_KAFKA_VERSION, "2.0.0"));
                     log.debug("SS {} has current version {}", ss.getMetadata().getName(), currentVersion);
                     String fromVersionAnno = Annotations.annotations(ss).get(ANNO_STRIMZI_IO_FROM_VERSION);
                     KafkaVersion fromVersion;
