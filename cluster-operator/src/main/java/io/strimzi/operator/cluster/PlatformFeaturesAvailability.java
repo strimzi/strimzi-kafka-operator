f83fe892df87108e60670b423791e51e2fa30f3b
Jakub Scholz <www@scholzj.com>
2019-04-10 16:51:05 +0200

    Add more detailed OpenShift detection

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/PlatformFeaturesAvailability.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/PlatformFeaturesAvailability.java
@@ -4,23 +4,138 @@
  */
 package io.strimzi.operator.cluster;
 
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.VersionInfo;
 import io.strimzi.operator.cluster.operator.KubernetesVersion;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Gives a info about certain features availability regarding to kubernetes version
  */
 public class PlatformFeaturesAvailability {
+    private static final Logger log = LogManager.getLogger(PlatformFeaturesAvailability.class.getName());
 
-    private boolean isOpenshift;
-    private final KubernetesVersion kubernetesVersion;
+    private boolean routes = false;
+    private boolean builds = false;
+    private boolean images = false;
+    private boolean apps = false;
+    private KubernetesVersion kubernetesVersion;
 
-    public PlatformFeaturesAvailability(boolean isOpenshift, KubernetesVersion kubernetesVersion) {
-        this.isOpenshift = isOpenshift;
+    public static Future<PlatformFeaturesAvailability> create(Vertx vertx, KubernetesClient client) {
+        Future<PlatformFeaturesAvailability> pfaFuture = Future.future();
+        Future<Void> composeFuture = Future.future();
+        OkHttpClient httpClient = getOkHttpClient(client);
+
+        PlatformFeaturesAvailability pfa = new PlatformFeaturesAvailability();
+
+        Future<VersionInfo> futureVersion = getVersionInfo(vertx, client);
+
+        futureVersion.compose(versionInfo -> {
+            String major = versionInfo.getMajor().equals("") ? Integer.toString(KubernetesVersion.MINIMAL_SUPPORTED_MAJOR) : versionInfo.getMajor();
+            String minor = versionInfo.getMinor().equals("") ? Integer.toString(KubernetesVersion.MINIMAL_SUPPORTED_MINOR) : versionInfo.getMinor();
+            pfa.setKubernetesVersion(new KubernetesVersion(Integer.parseInt(major.split("\\D")[0]), Integer.parseInt(minor.split("\\D")[0])));
+
+            return checkApiAvailability(vertx, httpClient, client.getMasterUrl().toString(), "route.openshift.io", "v1");
+        }).compose(supported -> {
+            pfa.setRoutes(supported);
+            return checkApiAvailability(vertx, httpClient, client.getMasterUrl().toString(), "build.openshift.io", "v1");
+        }).compose(supported -> {
+            pfa.setBuilds(supported);
+            return checkApiAvailability(vertx, httpClient, client.getMasterUrl().toString(), "apps.openshift.io", "v1");
+        }).compose(supported -> {
+            pfa.setApps(supported);
+            return checkApiAvailability(vertx, httpClient, client.getMasterUrl().toString(), "image.openshift.io", "v1");
+        }).compose(supported -> {
+            pfa.setImages(supported);
+            composeFuture.complete();
+        }, composeFuture);
+
+        composeFuture.setHandler(res -> {
+            if (res.succeeded())  {
+                pfaFuture.complete(pfa);
+            } else {
+                pfaFuture.fail(res.cause());
+            }
+        });
+
+        return pfaFuture;
+    }
+
+    private static OkHttpClient getOkHttpClient(KubernetesClient client)   {
+        if (client.isAdaptable(OkHttpClient.class)) {
+            return client.adapt(OkHttpClient.class);
+        } else {
+            log.error("Cannot adapt KubernetesClient to OkHttpClient");
+            throw new RuntimeException("Cannot adapt KubernetesClient to OkHttpClient");
+        }
+    }
+
+    private static Future<VersionInfo> getVersionInfo(Vertx vertx, KubernetesClient client)   {
+        Future<VersionInfo> fut = Future.future();
+
+        vertx.executeBlocking(request -> {
+            try {
+                request.complete(client.getVersion());
+            } catch (Exception e) {
+                log.error("Detection of Kuberetes version failed.", e);
+                request.fail(e);
+            }
+        }, fut.completer());
+
+        return fut;
+    }
+
+    private static Future<Boolean> checkApiAvailability(Vertx vertx, OkHttpClient httpClient, String masterUrl, String api, String version)   {
+        Future<Boolean> fut = Future.future();
+
+        vertx.executeBlocking(request -> {
+            try {
+                Boolean isSupported;
+
+                Response resp = httpClient.newCall(new Request.Builder().get().url(masterUrl + "apis/" + api + "/" + version).build()).execute();
+                if (resp.code() >= 200 && resp.code() < 300) {
+                    log.debug("{} returned {}. This API is supported.", resp.request().url(), resp.code());
+                    isSupported = true;
+                } else {
+                    log.debug("{} returned {}. This API is not supported.", resp.request().url(), resp.code());
+                    isSupported = false;
+                }
+
+                resp.close();
+                request.complete(isSupported);
+            } catch (Exception e) {
+                log.error("Detection of {}/{} API failed.", api, version, e);
+                request.fail(e);
+            }
+        }, fut.completer());
+
+        return fut;
+    }
+
+    private PlatformFeaturesAvailability() {}
+
+    /**
+     * This constructor is used in tests. It sets all OpenShift APIs to true or false depending on the isOpenShift paremeter
+     *
+     * @param isOpenShift           Set all OpenShift APIs to true
+     * @param kubernetesVersion     Set the Kubernetes version
+     */
+    /* test */public PlatformFeaturesAvailability(boolean isOpenShift, KubernetesVersion kubernetesVersion) {
         this.kubernetesVersion = kubernetesVersion;
+        this.routes = isOpenShift;
+        this.images = isOpenShift;
+        this.builds = isOpenShift;
+        this.apps = isOpenShift;
     }
 
     public boolean isOpenshift() {
-        return this.isOpenshift;
+        return this.isRoutes();
     }
 
     public boolean isNamespaceAndPodSelectorNetworkPolicySupported() {
@@ -29,5 +144,52 @@ public class PlatformFeaturesAvailability {
 
     public KubernetesVersion getKubernetesVersion() {
         return this.kubernetesVersion;
+    }
+
+    private void setKubernetesVersion(KubernetesVersion kubernetesVersion) {
+        this.kubernetesVersion = kubernetesVersion;
+    }
+
+    public boolean isRoutes() {
+        return routes;
+    }
+
+    private void setRoutes(boolean routes) {
+        this.routes = routes;
+    }
+
+    public boolean isBuilds() {
+        return builds;
+    }
+
+    private void setBuilds(boolean builds) {
+        this.builds = builds;
+    }
+
+    public boolean isImages() {
+        return images;
+    }
+
+    private void setImages(boolean images) {
+        this.images = images;
+    }
+
+    public boolean isApps() {
+        return apps;
+    }
+
+    private void setApps(boolean apps) {
+        this.apps = apps;
+    }
+
+    @Override
+    public String toString() {
+        return "ClusterOperatorConfig(" +
+                "KubernetesVersion=" + kubernetesVersion +
+                ",OpenShiftRoutes=" + routes +
+                ",OpenShiftBuilds=" + builds +
+                ",OpenShiftImageStreams=" + images +
+                ",OpenShiftDeploymentConfigs=" + apps +
+                ")";
     }
 }
--- cluster-operator/src/test/java/io/strimzi/operator/cluster/PlatformFeaturesAvailabilityTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/PlatformFeaturesAvailabilityTest.java
@@ -5,41 +5,40 @@
 package io.strimzi.operator.cluster;
 
 
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.strimzi.operator.cluster.operator.KubernetesVersion;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(VertxUnitRunner.class)
 public class PlatformFeaturesAvailabilityTest {
+    protected Vertx vertx;
 
-    @Test
-    public void versionTest() {
+    @Before
+    public void before() {
+        vertx = Vertx.vertx();
+    }
 
-        KubernetesVersion kv1p9 = new KubernetesVersion(1, 5);
-        assertTrue(kv1p9.compareTo(KubernetesVersion.V1_8) < 0);
-        assertTrue(kv1p9.compareTo(KubernetesVersion.V1_9) < 0);
-        assertTrue(kv1p9.compareTo(KubernetesVersion.V1_10) < 0);
-        assertTrue(kv1p9.compareTo(KubernetesVersion.V1_11) < 0);
-
-        assertTrue(KubernetesVersion.V1_8.compareTo(KubernetesVersion.V1_8) == 0);
-        assertTrue(KubernetesVersion.V1_8.compareTo(KubernetesVersion.V1_9) < 0);
-        assertTrue(KubernetesVersion.V1_8.compareTo(KubernetesVersion.V1_10) < 0);
-        assertTrue(KubernetesVersion.V1_8.compareTo(KubernetesVersion.V1_11) < 0);
-
-
-        assertTrue(KubernetesVersion.V1_12.compareTo(KubernetesVersion.V1_8) > 0);
-        assertTrue(KubernetesVersion.V1_12.compareTo(KubernetesVersion.V1_9) > 0);
-        assertTrue(KubernetesVersion.V1_12.compareTo(KubernetesVersion.V1_10) > 0);
-        assertTrue(KubernetesVersion.V1_12.compareTo(KubernetesVersion.V1_11) > 0);
-        assertTrue(KubernetesVersion.V1_12.compareTo(KubernetesVersion.V1_12) == 0);
-
-        KubernetesVersion kv2p9 = new KubernetesVersion(2, 9);
-        assertTrue(kv2p9.compareTo(KubernetesVersion.V1_8) > 0);
-        assertTrue(kv2p9.compareTo(KubernetesVersion.V1_9) > 0);
-        assertTrue(kv2p9.compareTo(KubernetesVersion.V1_10) > 0);
-        assertTrue(kv2p9.compareTo(KubernetesVersion.V1_11) > 0);
+    @After
+    public void after() {
+        vertx.close();
     }
 
     @Test
@@ -51,8 +50,191 @@ public class PlatformFeaturesAvailabilityTest {
     }
 
     @Test
-    public void versionsEqualTest() {
-        KubernetesVersion kv1p9 = new KubernetesVersion(1, 9);
-        assertEquals(kv1p9, KubernetesVersion.V1_9);
+    public void testVersionDetectionOpenShift39(TestContext context)  {
+        String version = "{\n" +
+                "  \"major\": \"1\",\n" +
+                "  \"minor\": \"9\",\n" +
+                "  \"gitVersion\": \"v1.9.1+a0ce1bc657\",\n" +
+                "  \"gitCommit\": \"a0ce1bc\",\n" +
+                "  \"gitTreeState\": \"clean\",\n" +
+                "  \"buildDate\": \"2018-06-24T01:54:00Z\",\n" +
+                "  \"goVersion\": \"go1.9\",\n" +
+                "  \"compiler\": \"gc\",\n" +
+                "  \"platform\": \"linux/amd64\"\n" +
+                "}";
+
+        HttpServer mockHttp = mockApi(version, Collections.EMPTY_LIST);
+
+        KubernetesClient client = new DefaultKubernetesClient("127.0.0.1:8080");
+        Future<PlatformFeaturesAvailability> futurePfa = PlatformFeaturesAvailability.create(vertx, client);
+
+        Async async = context.async();
+
+        futurePfa.setHandler(res -> {
+            if (res.succeeded())    {
+                context.assertEquals(KubernetesVersion.V1_9, res.result().getKubernetesVersion(), "Versions are not equal");
+                async.complete();
+            } else {
+                context.fail("Failed to create PlatformFeaturesAvailability object");
+                async.complete();
+            }
+        });
+
+        async.awaitSuccess();
+        mockHttp.close();
+    }
+
+    @Test
+    public void testVersionDetectionMinikube114(TestContext context)  {
+        String version = "{\n" +
+                "  \"major\": \"1\",\n" +
+                "  \"minor\": \"14\",\n" +
+                "  \"gitVersion\": \"v1.14.0\",\n" +
+                "  \"gitCommit\": \"641856db18352033a0d96dbc99153fa3b27298e5\",\n" +
+                "  \"gitTreeState\": \"clean\",\n" +
+                "  \"buildDate\": \"2019-03-25T15:45:25Z\",\n" +
+                "  \"goVersion\": \"go1.12.1\",\n" +
+                "  \"compiler\": \"gc\",\n" +
+                "  \"platform\": \"linux/amd64\"\n" +
+                "}";
+
+        HttpServer mockHttp = mockApi(version, Collections.EMPTY_LIST);
+
+        KubernetesClient client = new DefaultKubernetesClient("127.0.0.1:8080");
+        Future<PlatformFeaturesAvailability> futurePfa = PlatformFeaturesAvailability.create(vertx, client);
+
+        Async async = context.async();
+
+        futurePfa.setHandler(res -> {
+            if (res.succeeded())    {
+                context.assertEquals(KubernetesVersion.V1_14, res.result().getKubernetesVersion(), "Versions are not equal");
+                async.complete();
+            } else {
+                context.fail("Failed to create PlatformFeaturesAvailability object");
+                async.complete();
+            }
+        });
+
+        async.awaitSuccess();
+        mockHttp.close();
+    }
+
+    @Test
+    public void testApiDetectionOce(TestContext context)  {
+        List<String> apis = new ArrayList<>();
+        apis.add("/apis/route.openshift.io/v1");
+        apis.add("/apis/build.openshift.io/v1");
+
+        HttpServer mockHttp = mockApi(apis);
+
+        KubernetesClient client = new DefaultKubernetesClient("127.0.0.1:8080");
+        Future<PlatformFeaturesAvailability> futurePfa = PlatformFeaturesAvailability.create(vertx, client);
+
+        Async async = context.async();
+
+        futurePfa.setHandler(res -> {
+            if (res.succeeded())    {
+                PlatformFeaturesAvailability pfa = res.result();
+                context.assertTrue(pfa.isRoutes());
+                context.assertTrue(pfa.isBuilds());
+                context.assertFalse(pfa.isImages());
+                context.assertFalse(pfa.isApps());
+                async.complete();
+            } else {
+                context.fail("Failed to create PlatformFeaturesAvailability object");
+                async.complete();
+            }
+        });
+
+        async.awaitSuccess();
+        mockHttp.close();
+    }
+
+    @Test
+    public void testApiDetectionOpenshift(TestContext context)  {
+        List<String> apis = new ArrayList<>();
+        apis.add("/apis/route.openshift.io/v1");
+        apis.add("/apis/build.openshift.io/v1");
+        apis.add("/apis/apps.openshift.io/v1");
+        apis.add("/apis/image.openshift.io/v1");
+
+        HttpServer mockHttp = mockApi(apis);
+
+        KubernetesClient client = new DefaultKubernetesClient("127.0.0.1:8080");
+        Future<PlatformFeaturesAvailability> futurePfa = PlatformFeaturesAvailability.create(vertx, client);
+
+        Async async = context.async();
+
+        futurePfa.setHandler(res -> {
+            if (res.succeeded())    {
+                PlatformFeaturesAvailability pfa = res.result();
+                context.assertTrue(pfa.isRoutes());
+                context.assertTrue(pfa.isBuilds());
+                context.assertTrue(pfa.isImages());
+                context.assertTrue(pfa.isApps());
+                async.complete();
+            } else {
+                context.fail("Failed to create PlatformFeaturesAvailability object");
+                async.complete();
+            }
+        });
+
+        async.awaitSuccess();
+        mockHttp.close();
+    }
+
+    @Test
+    public void testApiDetectionKubernetes(TestContext context)  {
+        HttpServer mockHttp = mockApi(Collections.EMPTY_LIST);
+
+        KubernetesClient client = new DefaultKubernetesClient("127.0.0.1:8080");
+        Future<PlatformFeaturesAvailability> futurePfa = PlatformFeaturesAvailability.create(vertx, client);
+
+        Async async = context.async();
+
+        futurePfa.setHandler(res -> {
+            if (res.succeeded())    {
+                PlatformFeaturesAvailability pfa = res.result();
+                context.assertFalse(pfa.isRoutes());
+                context.assertFalse(pfa.isBuilds());
+                context.assertFalse(pfa.isImages());
+                context.assertFalse(pfa.isApps());
+                async.complete();
+            } else {
+                context.fail("Failed to create PlatformFeaturesAvailability object");
+                async.complete();
+            }
+        });
+
+        async.awaitSuccess();
+        mockHttp.close();
+    }
+
+    public HttpServer mockApi(String version, List<String> apis)   {
+        return vertx.createHttpServer().requestHandler(request -> {
+            if (HttpMethod.GET.equals(request.method()) && apis.contains(request.uri()))   {
+                request.response().setStatusCode(200).end();
+            } else if (HttpMethod.GET.equals(request.method()) && "/version".equals(request.uri())) {
+                request.response().setStatusCode(200).end(version);
+            } else {
+                request.response().setStatusCode(404).end();
+            }
+        }).listen(8080);
+    }
+
+    public HttpServer mockApi(List<String> apis)    {
+        String version = "{\n" +
+                "  \"major\": \"1\",\n" +
+                "  \"minor\": \"9\",\n" +
+                "  \"gitVersion\": \"v1.9.1+a0ce1bc657\",\n" +
+                "  \"gitCommit\": \"a0ce1bc\",\n" +
+                "  \"gitTreeState\": \"clean\",\n" +
+                "  \"buildDate\": \"2018-06-24T01:54:00Z\",\n" +
+                "  \"goVersion\": \"go1.9\",\n" +
+                "  \"compiler\": \"gc\",\n" +
+                "  \"platform\": \"linux/amd64\"\n" +
+                "}";
+
+        return mockApi(version, apis);
     }
 }
