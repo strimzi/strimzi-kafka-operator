e5c4c3a0b81b106510dfebb41409caa44cc5e5b4
Stanislav Knot <sknot@redhat.com>
2019-05-16 15:46:18 +0200

    mods

--- report.sh
+++ report.sh
@@ -10,6 +10,7 @@ get_yamls() {
 	resources=`oc get "$1"s  -l strimzi.io/cluster=$cluster | tail -n +2` # remove legend
 	while read -r line; do
 		resource=`echo $line | cut -f 1 -d " "`
+		echo $resource
 		oc get "$1" $resource -o yaml >> ./reports/"$1"s/"$resource".txt
 	done <<< $resources;
 }
@@ -23,6 +24,7 @@ mkdir -p ./reports/podLogs
 pods=`oc get pods -l strimzi.io/cluster=$cluster | tail -n +2` # remove legend
 while read -r line; do
 	pod=`echo $line | cut -f 1 -d " "`
+	echo $pod
 	if [[ $pod == *"-entity-operator-"* ]]; then
 	  oc logs $pod -c tls-sidecar >> ./reports/podLogs/"$pod"-tls-sidecar.txt
 	  oc logs $pod -c topic-operator >> ./reports/podLogs/"$pod"-topic-operator.txt
@@ -48,16 +50,18 @@ mkdir -p ./reports/crs
 crs=`oc get crd | tail -n +2` # remove legend
 while read -r line; do
 	cr=`echo $line | cut -f 1 -d " "`
+	echo $cr
 	resources=`oc get $cr -l strimzi.io/cluster=$cluster | tail -n +2` # remove legend
 	if ! [[ -z "$resources" &&  $cr == *"kafka.strimzi.io" ]]; then
 		while read -r line; do
 			resource=`echo $line | cut -f 1 -d " "`
 			oc get $cr $resource -o yaml >> ./reports/crs/"$cr"-"$resource".txt
+			echo "   "$resource
 		done <<< $resources;
 	fi
 done <<< $crs;
 
-zip -r report.zip reports
+zip -qr report.zip reports
 rm -rf reports
 
 echo "Done"
