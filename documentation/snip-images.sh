7b6712c6da909224ac89c80d4fdf1d866b058835
Tom Bentley <tombentley@users.noreply.github.com>
2019-06-04 17:59:26 +0100

    Warning doc (#1680)
    * Warning on generated doc

--- documentation/snip-images.sh
+++ documentation/snip-images.sh
@@ -7,6 +7,8 @@
 
 FILE=$1
 cat <<EOF
+// Auto generated content - DO NOT EDIT BY HAND
+// Edit documentation/snip-images.sh instead
 [table,stripes=none]
 |===
 |Container image |Namespace/Repository |Description
