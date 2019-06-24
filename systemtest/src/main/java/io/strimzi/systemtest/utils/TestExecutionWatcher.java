c8ea995e3c29443cc2ac3ecf90baefce2fb915b4
Jakub Stejskal <stejskinek@gmail.com>
2019-06-24 23:27:30 +0200

    ST: Improve log collection during BeforeAll and BeforeEach (#1713)
    * WIP
    
    * NEW: collect logs even during before/after all

--- systemtest/src/main/java/io/strimzi/systemtest/utils/TestExecutionWatcher.java
+++ systemtest/src/main/java/io/strimzi/systemtest/utils/TestExecutionWatcher.java
@@ -8,26 +8,38 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.LifecycleMethodExecutionExceptionHandler;
 
 import java.io.File;
-import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static io.strimzi.systemtest.AbstractST.TEST_LOG_DIR;
 import static io.strimzi.test.BaseITST.kubeClient;
 
-public class TestExecutionWatcher implements AfterTestExecutionCallback {
+public class TestExecutionWatcher implements AfterTestExecutionCallback, LifecycleMethodExecutionExceptionHandler {
     private static final Logger LOGGER = LogManager.getLogger(TestExecutionWatcher.class);
 
     @Override
     public void afterTestExecution(ExtensionContext extensionContext) {
-        Method testMethod = extensionContext.getRequiredTestMethod();
-        Class testClass = extensionContext.getRequiredTestClass();
-        collectLogs(testMethod.getName(), testClass.getName());
+        collectLogs(extensionContext);
     }
 
-    void collectLogs(String testMethod, String testClass) {
+    @Override
+    public void handleBeforeAllMethodExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        collectLogs(context);
+        throw throwable;
+    }
+
+    @Override
+    public void handleBeforeEachMethodExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        collectLogs(context);
+        throw throwable;
+    }
+
+    void collectLogs(ExtensionContext extensionContext) {
+        String testMethod = extensionContext.getRequiredTestMethod().getName();
+        String testClass = extensionContext.getRequiredTestClass().getName();
         // Get current date to create a unique folder
         String currentDate = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
         String logDir = !testMethod.isEmpty() ?
