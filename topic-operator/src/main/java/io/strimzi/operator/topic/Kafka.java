cafb090e4ff64b7ee35d6ec070285a5c3d8c34df
Tom Bentley <tbentley@redhat.com>
2019-07-31 10:56:12 +0100

    javadoc

--- topic-operator/src/main/java/io/strimzi/operator/topic/Kafka.java
+++ topic-operator/src/main/java/io/strimzi/operator/topic/Kafka.java
@@ -4,7 +4,6 @@
  */
 package io.strimzi.operator.topic;
 
-import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 
 import java.util.Set;
@@ -16,9 +15,9 @@ import java.util.Set;
 public interface Kafka {
 
     /**
-     * Asynchronously create the given topic in Kafka. Invoke the given
-     * handler with the result. If the operation fails the given handler
-     * will be called with a failed AsyncResult whose {@code cause()} is the
+     * Asynchronously create the given topic in Kafka,
+     * completing the returned Future when the topic has been created.
+     * If the operation fails the returned Future will be failed with the
      * KafkaException (not an ExecutionException).
      * @param newTopic The topic to create.
      * @return A future which is completed once the topic has been created.
@@ -26,9 +25,9 @@ public interface Kafka {
     Future<Void> createTopic(Topic newTopic);
 
     /**
-     * Asynchronously delete the given topic in Kafka. Invoke the given
-     * handler with the result. If the operation fails the given handler
-     * will be called with a failed AsyncResult whose {@code cause()} is the
+     * Asynchronously delete the given topic in Kafka,
+     * completing the returned Future when the topic has been deleted.
+     * If the operation fails the returned Future will be failed with the
      * KafkaException (not an ExecutionException).
      * @param topicName The name of the topic to delete.
      * @return A future which is completed once the topic has been deleted.
@@ -36,9 +35,9 @@ public interface Kafka {
     Future<Void> deleteTopic(TopicName topicName);
 
     /**
-     * Asynchronously update the topic config in Kafka. Invoke the given
-     * handler with the result. If the operation fails the given handler
-     * will be called with a failed AsyncResult whose {@code cause()} is the
+     * Asynchronously update the topic config in Kafka,
+     * completing the returned Future when the topic has been updated.
+     * If the operation fails the returned Future will be failed with the
      * KafkaException (not an ExecutionException).
      * @param topic The topic config to update.
      * @return A future which is completed once the topic has been updated.
@@ -46,9 +45,9 @@ public interface Kafka {
     Future<Void> updateTopicConfig(Topic topic);
 
     /**
-     * Asynchronously increase the topic's partitions in Kafka. Invoke the given
-     * handler with the result. If the operation fails the given handler
-     * will be called with a failed AsyncResult whose {@code cause()} is the
+     * Asynchronously increase the topic's partitions in Kafka,
+     * completing the returned Future when the topic has been updated.
+     * If the operation fails the returned Future will be failed with the
      * KafkaException (not an ExecutionException).
      * @param topic The topic.
      * @return A future which is completed once the topic has been updated.
@@ -56,9 +55,9 @@ public interface Kafka {
     Future<Void> increasePartitions(Topic topic);
 
     /**
-     * Asynchronously change the topic's replication factor in Kafka. Invoke the given
-     * handler with the result. If the operation fails the given handler
-     * will be called with a failed AsyncResult whose {@code cause()} is the
+     * Asynchronously change the topic's replication factor in Kafka,
+     * completing the returned Future when the topic has been updated.
+     * If the operation fails the returned Future will be failed with the
      * KafkaException (not an ExecutionException).
      * @param topic The topic.
      * @return A future which is completed once the topic has been updated.
@@ -66,20 +65,20 @@ public interface Kafka {
     Future<Void> changeReplicationFactor(Topic topic);
 
     /**
-     * Asynchronously fetch the topic metadata in Kafka. Invoke the given
-     * handler with the result. If the operation fails the given handler
-     * will be called with a failed AsyncResult whose {@code cause()} is the
+     * Asynchronously fetch the topic metadata in Kafka,
+     * completing the returned Future with the requested metadata.
+     * If the topic does not exist the returned Future will be completed with null result.
+     * If the operation fails the returned Future will be failed with the
      * KafkaException (not an ExecutionException).
-     * If the topic does not exist the {@link AsyncResult#result()} will be null.
      * @param topicName The name of the topic to get the metadata of.
      * @return A future which is completed with the requested metadata.
      */
     Future<TopicMetadata> topicMetadata(TopicName topicName);
 
     /**
-     * Asynchronously list the topics available in Kafka. Invoke the given
-     * handler with the result. If the operation fails the given handler
-     * will be called with a failed AsyncResult whose {@code cause()} is the
+     * Asynchronously list the names of the topics available in Kafka,
+     * completing the returned Future with the topic names.
+     * If the operation fails the returned Future will be failed with the
      * KafkaException (not an ExecutionException).
      * @return A future which is completed with the list of topics.
      */
