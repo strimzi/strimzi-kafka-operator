bd95a121334d55caa9eec4298b41d804936bbcaa
Seequick1 <morsak@redhat.com>
2019-08-01 13:40:27 +0200

    [MO] - [system test] -> edit labels doesnt break cluster

--- systemtest/src/test/java/io/strimzi/systemtest/RollingUpdateST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/RollingUpdateST.java
@@ -67,7 +67,7 @@ class RollingUpdateST extends AbstractST {
         String reconciliation = TimeMeasuringSystem.startOperation(Operation.NEXT_RECONCILIATION);
 
         LOGGER.info("Wait till another rolling update starts");
-        TestUtils.waitFor("Wmy-cluster-kafka-0ait till another rolling update starts", Constants.CO_OPERATION_TIMEOUT_POLL, Constants.CO_OPERATION_TIMEOUT,
+        TestUtils.waitFor("Wait till another rolling update starts", Constants.CO_OPERATION_TIMEOUT_POLL, Constants.CO_OPERATION_TIMEOUT,
             () -> !cmdKubeClient().searchInLog("deploy", "strimzi-cluster-operator", TimeMeasuringSystem.getCurrentDuration(testClass, testName, reconciliation), RECONCILIATION_PATTERN).isEmpty());
 
         TimeMeasuringSystem.stopOperation(reconciliation);
