0e3751f3ad58fd0860600acfd3c0f986b67a479c
Tom Bentley <tbentley@redhat.com>
2019-08-13 08:50:07 +0100

    Delete file commited by mistake

--- topic-operator/src/test/java/io/strimzi/operator/topic/Expt.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2019, Strimzi authors.
- * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
- */
-package io.strimzi.operator.topic;
-
-public class Expt {
-}
