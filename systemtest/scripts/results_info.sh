6ce8ffa80407137fe488e1682a701ba54ed6f495
Jakub Stejskal <stejskinek@gmail.com>
2019-08-16 11:26:26 +0200

    fixup! fixup! fixup! fixup! fixup! WIP: better info collection

--- systemtest/scripts/results_info.sh
+++ systemtest/scripts/results_info.sh
@@ -26,13 +26,13 @@ TEST_SKIPPED_COUNT=$(get_test_count "skipped")
 
 SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ERRORS_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ERRORS_COUNT}\n**SKIPPED:** ${TEST_SKIPPED_COUNT}"
 
-FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 sed -n "s#\(<testcase.*[^\/]>\)#\1#p" | awk -F '"' '{print "- " $2 " in "  $4}')
+FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 sed -n "s#\(<testcase.*[^\/]>\)#\1#p" | awk -F '"' '{print "\n- " $2 " in "  $4}')
 
 echo "Creating body ..."
 
 if [ -n "${FAILED_TESTS}" ]
 then
-  FAILED_TEST_BODY="### :heavy_exclamation_mark: Test Failures :heavy_exclamation_mark:\n${FAILED_TESTS}"
+  FAILED_TEST_BODY="### :heavy_exclamation_mark: Test Failures :heavy_exclamation_mark:${FAILED_TESTS}"
 fi
 
 if [ "${TEST_COUNT}" == 0 ]
