b8d6e0ca6153e02d30b8c7c944ce2b691a0c783b
Jakub Stejskal <stejskinek@gmail.com>
2019-08-16 13:00:54 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! WIP: better info collection

--- systemtest/scripts/results_info.sh
+++ systemtest/scripts/results_info.sh
@@ -8,7 +8,7 @@ JSON_FILE_RESULTS=results.json
 
 function get_test_count () {
     _TEST_TYPE=${1}
-    _VALUES=$(find "${RESULTS_PATH}" -name "TEST*.xml" -type f -print0 | xargs -0 sed -n "s#.*${_TEST_TYPE}=\"\([0-9]*\)\" .*#\1#p")
+    _VALUES=$(find "${RESULTS_PATH}" -name "TEST*.xml" -type f -print0 | xargs -0 sed -n "s#.*${_TEST_TYPE}=\"\([0-9]*\)\".*#\1#p")
     _TEST_COUNTS_ARR=$(echo "${_VALUES}" | tr " " "\n")
     _TEST_COUNT=0
 
@@ -27,7 +27,7 @@ TEST_FAILURES_COUNT=$(get_test_count "failures")
 
 TEST_ALL_FAILED_COUNT=$((TEST_ERRORS_COUNT + TEST_FAILURES_COUNT))
 
-SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ALL_FAILED_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ALL_FAILED_COUNT}\n**SKIP:** ${TEST_SKIPPED_COUNT}"
+SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ALL_FAILED_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ALL_FAILED_COUNT}\n**SKIP:** ${TEST_SKIPPED_COUNT}\n"
 
 FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 sed -n "s#\(<testcase.*[^\/]>\)#\1#p" | awk -F '"' '{print "\\n- " $2 " in "  $4}')
 echo ${FAILED_TESTS}
@@ -47,7 +47,7 @@ else
   then
     BODY="{\"body\":\"### :heavy_check_mark: Test Summary :heavy_check_mark:\n${SUMMARY}${FAILED_TEST_BODY}\"}"
   else
-    BODY="{\"body\":\"### :x: Test Summary :x:\n${SUMMARY}\n${FAILED_TEST_BODY}\"}"
+    BODY="{\"body\":\"### :x: Test Summary :x:\n${SUMMARY}${FAILED_TEST_BODY}\"}"
   fi
 fi
 
