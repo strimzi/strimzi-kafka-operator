264354ff61a386134e3256565c9d0b8dc19ff372
Jakub Stejskal <stejskinek@gmail.com>
2019-08-16 12:12:29 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! WIP: better info collection

--- systemtest/scripts/results_info.sh
+++ systemtest/scripts/results_info.sh
@@ -23,12 +23,16 @@ function get_test_count () {
 TEST_COUNT=$(get_test_count "tests")
 TEST_ERRORS_COUNT=$(get_test_count "errors")
 TEST_SKIPPED_COUNT=$(get_test_count "skipped")
+TEST_FAILURES_COUNT=$(get_test_count "failures")
 
-SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ERRORS_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ERRORS_COUNT}\n**SKIPPED:** ${TEST_SKIPPED_COUNT}"
+TEST_ALL_FAILED_COUNT=$((TEST_ERRORS_COUNT + TEST_FAILURES_COUNT))
 
-FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 sed -n "s#\(<testcase.*[^\/]>\)#\1#p" | awk -F '"' '{print "\n- " $2 " in "  $4}')
+SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ALL_FAILED_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ALL_FAILED_COUNT}\n**SKIP:** ${TEST_SKIPPED_COUNT}"
 
+FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 sed -n "s#\(<testcase.*[^\/]>\)#\1#p" | awk -F '"' '{print "\\n- " $2 " in "  $4}')
+echo ${FAILED_TESTS}
 echo "Creating body ..."
+
 
 if [ -n "${FAILED_TESTS}" ]
 then
@@ -39,9 +43,9 @@ if [ "${TEST_COUNT}" == 0 ]
 then
   BODY="{\"body\":\":heavy_exclamation_mark: **Build Failed** :heavy_exclamation_mark:\"}"
 else
-  if [ "${TEST_ERRORS_COUNT}" == 0 ]
+  if [ "${TEST_ALL_FAILED_COUNT}" == 0 ]
   then
-    BODY="{\"body\":\"### :heavy_check_mark: Test Summary :heavy_check_mark:\n${SUMMARY}\n${FAILED_TEST_BODY}\"}"
+    BODY="{\"body\":\"### :heavy_check_mark: Test Summary :heavy_check_mark:\n${SUMMARY}${FAILED_TEST_BODY}\"}"
   else
     BODY="{\"body\":\"### :x: Test Summary :x:\n${SUMMARY}\n${FAILED_TEST_BODY}\"}"
   fi
