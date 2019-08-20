5a34fddd32052f8135c1e6da2982667dda459879
Seequick1 <morsak@redhat.com>
2019-08-20 16:09:12 +0200

    [MO] - [refactor] -> fixed

--- systemtest/src/test/java/io/strimzi/systemtest/SecurityST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/SecurityST.java
@@ -6,10 +6,7 @@ package io.strimzi.systemtest;
 
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.api.model.SecretBuilder;
-import io.fabric8.kubernetes.api.model.networking.NetworkPolicyPeer;
-import io.fabric8.kubernetes.api.model.networking.NetworkPolicyPeerBuilder;
 import io.strimzi.api.kafka.model.KafkaResources;
-import io.strimzi.api.kafka.model.KafkaUser;
 import io.strimzi.systemtest.annotations.OpenShiftOnly;
 import io.strimzi.systemtest.utils.StUtils;
 import io.strimzi.test.TestUtils;
