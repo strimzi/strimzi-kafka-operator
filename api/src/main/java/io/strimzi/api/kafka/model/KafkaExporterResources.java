a8999c99b2e9b4ce2f6b40f2667ba0058bbf232d
Jakub Scholz <www@scholzj.com>
2019-09-11 18:09:10 +0200

    Review comments
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- api/src/main/java/io/strimzi/api/kafka/model/KafkaExporterResources.java
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaExporterResources.java
@@ -13,7 +13,7 @@ public class KafkaExporterResources {
 
     /**
      * Returns the name of the Kafka Exporter {@code Deployment}.
-     * @param kafkaClusterName  The {@code metadata.name} of the {@code KafkaExporter} resource.
+     * @param kafkaClusterName  The {@code metadata.name} of the {@code Kafka} resource.
      * @return The name of the corresponding Kafka Exporter {@code Deployment}.
      */
     public static String deploymentName(String kafkaClusterName) {
@@ -22,7 +22,7 @@ public class KafkaExporterResources {
 
     /**
      * Returns the name of the Kafka Exporter {@code ServiceAccount}.
-     * @param kafkaClusterName  The {@code metadata.name} of the {@code KafkaExporter} resource.
+     * @param kafkaClusterName  The {@code metadata.name} of the {@code Kafka} resource.
      * @return The name of the corresponding Kafka Exporter {@code ServiceAccount}.
      */
     public static String serviceAccountName(String kafkaClusterName) {
@@ -31,7 +31,7 @@ public class KafkaExporterResources {
 
     /**
      * Returns the name of the Prometheus {@code Service}.
-     * @param kafkaClusterName  The {@code metadata.name} of the {@code KafkaExporter} resource.
+     * @param kafkaClusterName  The {@code metadata.name} of the {@code Kafka} resource.
      * @return The name of the corresponding {@code Service}.
      */
     public static String serviceName(String kafkaClusterName) {
@@ -39,9 +39,10 @@ public class KafkaExporterResources {
     }
 
     /**
-     * Returns the name of the KAfka Exporter {@code Secret} for a {@code Kafka} cluster of the given name.
+     * Returns the name of the Kafka Exporter {@code Secret} for a {@code Kafka} cluster of the given name.
      * This {@code Secret} will only exist if {@code Kafka.spec.kafkaExporter} is configured in the
      * {@code Kafka} resource with the given name.
+     *
      * @param clusterName  The {@code metadata.name} of the {@code Kafka} resource.
      * @return The name of the corresponding Kafka Exporter {@code Secret}.
      */
