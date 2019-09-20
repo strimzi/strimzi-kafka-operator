a5848cbafd62c0fe3db0dcffd0c514edecce3e22
Pame Plúas <pamepluas@gmail.com>
2019-09-20 15:40:06 -0500

    FIX: Extract Cluster class to a top level one (#1498)

---
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaConnectorSpecCluster.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2019, Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.api.kafka.model;
+
+public class KafkaConnectorSpecCluster {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+}
