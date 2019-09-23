c292e6abea608fe1855d3c5b09e705eab9ed815e
Tom Bentley <tbentley@redhat.com>
2019-09-23 18:24:40 +0100

    Hook KafkaConnector API into the build
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- api/src/main/java/io/strimzi/api/kafka/model/KafkaConnectorSpecCluster.java
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaConnectorSpecCluster.java
@@ -4,18 +4,49 @@
  */
 package io.strimzi.api.kafka.model;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.strimzi.crdgenerator.annotations.Description;
+import io.sundr.builder.annotations.Buildable;
+import lombok.EqualsAndHashCode;
 
-public class KafkaConnectorSpecCluster implements Serializable {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+
+@Buildable(
+        editableEnabled = false,
+        generateBuilderPackage = false,
+        builderPackage = "io.fabric8.kubernetes.api.builder"
+)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode
+public class KafkaConnectorSpecCluster implements Serializable, UnknownPropertyPreserving {
     private static final long serialVersionUID = 1L;
 
     private String url;
+    private Map<String, Object> additionalProperties;
 
+    @Description("The URL for accessing  the Kafka Connect REST API.")
     public String getUrl() {
         return url;
     }
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties != null ? this.additionalProperties : emptyMap();
+    }
+
+    @Override
+    public void setAdditionalProperty(String name, Object value) {
+        if (this.additionalProperties == null) {
+            this.additionalProperties = new HashMap<>();
+        }
+        this.additionalProperties.put(name, value);
     }
 }
