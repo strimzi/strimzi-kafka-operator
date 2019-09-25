abd0846d75f70d8cb46fa6d90fee443ca64f5e25
Seequick1 <morsak@redhat.com>
2019-09-26 00:38:33 +0200

    [MO] - [system test] - ST for support SASL-scramsha in ConnectS2I
    Signed-off-by: Seequick1 <morsak@redhat.com>

--- systemtest/src/test/java/io/strimzi/systemtest/ConnectS2IST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/ConnectS2IST.java
@@ -19,40 +19,112 @@ import java.util.List;
 import java.util.Map;
 
 import static io.strimzi.systemtest.Constants.ACCEPTANCE;
+import static io.strimzi.systemtest.Constants.NODEPORT_SUPPORTED;
 import static io.strimzi.systemtest.Constants.REGRESSION;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 
 @Tag(REGRESSION)
 class ConnectS2IST extends AbstractST {
 
     public static final String NAMESPACE = "connect-s2i-cluster-test";
     public static final String CONNECT_CLUSTER_NAME = "my-connect-cluster";
-    public static final String CONNECT_DEPLOYMENT_NAME = CONNECT_CLUSTER_NAME + "-connect";
     private static final Logger LOGGER = LogManager.getLogger(ConnectS2IST.class);
+
+    private static final String CONNECT_S2I_TOPIC_NAME = "connect-s2i-topic-example";
 
     @Test
     @OpenShiftOnly
     @Tag(ACCEPTANCE)
-    void testDeployS2IWithMongoDBPlugin() throws IOException {
+    void testDeployS2IWithMongoDBPlugin() {
         testClassResources().kafkaConnectS2I(CONNECT_CLUSTER_NAME, 1, CLUSTER_NAME)
             .editMetadata()
                 .addToLabels("type", "kafka-connect-s2i")
             .endMetadata()
             .done();
 
-        Map<String, String> connectSnapshot = StUtils.depConfigSnapshot(CONNECT_DEPLOYMENT_NAME);
+        Map<String, String> connectSnapshot = StUtils.depConfigSnapshot(CLUSTER_NAME + "-connect");
 
         File dir = StUtils.downloadAndUnzip("https://repo1.maven.org/maven2/io/debezium/debezium-connector-mongodb/0.7.5/debezium-connector-mongodb-0.7.5-plugin.zip");
 
         // Start a new image build using the plugins directory
-        cmdKubeClient().exec("oc", "start-build", CONNECT_DEPLOYMENT_NAME, "--from-dir", dir.getAbsolutePath(), "-n", NAMESPACE);
+        cmdKubeClient().exec("oc", "start-build", CLUSTER_NAME + "-connect", "--from-dir", dir.getAbsolutePath(), "-n", NAMESPACE);
         // Wait for rolling update connect pods
-        StUtils.waitTillDepConfigHasRolled(CONNECT_DEPLOYMENT_NAME, 1, connectSnapshot);
+        StUtils.waitTillDepConfigHasRolled(CLUSTER_NAME + "-connect", 1, connectSnapshot);
         String connectS2IPodName = kubeClient().listPods("type", "kafka-connect-s2i").get(0).getMetadata().getName();
         String plugins = cmdKubeClient().execInPod(connectS2IPodName, "curl", "-X", "GET", "http://localhost:8083/connector-plugins").out();
 
         assertThat(plugins, containsString("io.debezium.connector.mongodb.MongoDbConnector"));
+    }
+
+    @Test
+    @Tag(NODEPORT_SUPPORTED)
+    void testSecretsWithKafkaConnectS2IWithTlsAndScramShaAuthentication() throws Exception {
+        testMethodResources().kafkaEphemeral(CLUSTER_NAME, 3)
+            .editSpec()
+                .editKafka()
+                    .editListeners()
+                        .withNewTls()
+                            .withNewKafkaListenerAuthenticationScramSha512Auth()
+                            .endKafkaListenerAuthenticationScramSha512Auth()
+                        .endTls()
+                        .withNewKafkaListenerExternalNodePort()
+                            .withNewKafkaListenerAuthenticationScramSha512Auth()
+                            .endKafkaListenerAuthenticationScramSha512Auth()
+                        .endKafkaListenerExternalNodePort()
+                    .endListeners()
+                .endKafka()
+            .endSpec()
+            .done();
+
+        final String userName = "user-example-one";
+
+        testMethodResources().scramShaUser(CLUSTER_NAME, userName).done();
+
+        StUtils.waitForSecretReady(userName);
+
+        testMethodResources().kafkaConnectS2I(CONNECT_CLUSTER_NAME, 1, CLUSTER_NAME)
+                .editMetadata()
+                    .addToLabels("type", "kafka-connect-s2i")
+                .endMetadata()
+                .editSpec()
+                    .addToConfig("key.converter.schemas.enable", false)
+                    .addToConfig("value.converter.schemas.enable", false)
+                    .withNewTls()
+                        .addNewTrustedCertificate()
+                            .withSecretName(CLUSTER_NAME + "-cluster-ca-cert")
+                            .withCertificate("ca.crt")
+                        .endTrustedCertificate()
+                    .endTls()
+                    .withBootstrapServers(CLUSTER_NAME + "-kafka-bootstrap:9093")
+                    .withNewKafkaClientAuthenticationScramSha512()
+                        .withUsername(userName)
+                        .withNewPasswordSecret()
+                            .withSecretName(userName)
+                            .withPassword("password")
+                        .endPasswordSecret()
+                    .endKafkaClientAuthenticationScramSha512()
+                .endSpec()
+                .done();
+
+        testMethodResources().topic(CLUSTER_NAME, CONNECT_S2I_TOPIC_NAME).done();
+
+        String kafkaConnectS2IPodName = kubeClient().listPods("type", "kafka-connect-s2i").get(0).getMetadata().getName();
+        String kafkaConnectS2ILogs = kubeClient().logs(kafkaConnectS2IPodName);
+
+        LOGGER.info("Verifying that in kafka connect logs are everything fine");
+        assertThat(kafkaConnectS2ILogs, not(containsString("ERROR")));
+
+        LOGGER.info("Creating FileStreamSink connector in pod {} with topic {}", kafkaConnectS2IPodName, CONNECT_S2I_TOPIC_NAME);
+        StUtils.createFileSinkConnector(kafkaConnectS2IPodName, CONNECT_S2I_TOPIC_NAME);
+
+        waitForClusterAvailabilityScramSha(userName, NAMESPACE, CLUSTER_NAME, CONNECT_S2I_TOPIC_NAME, 2);
+
+        StUtils.waitForMessagesInKafkaConnectFileSink(kafkaConnectS2IPodName);
+
+        assertThat(cmdKubeClient().execInPod(kafkaConnectS2IPodName, "/bin/bash", "-c", "cat /tmp/test-file-sink.txt").out(),
+                containsString("0\n1\n"));
     }
 
     @BeforeEach
