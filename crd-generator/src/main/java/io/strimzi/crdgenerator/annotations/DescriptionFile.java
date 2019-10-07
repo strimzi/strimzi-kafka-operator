8d452c004ebad7ab4d1003417622fcf52d70220d
Jakub Scholz <www@scholzj.com>
2019-10-07 11:14:50 +0200

    Add JavaDoc to the DescriptionFile annotation
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- crd-generator/src/main/java/io/strimzi/crdgenerator/annotations/DescriptionFile.java
+++ crd-generator/src/main/java/io/strimzi/crdgenerator/annotations/DescriptionFile.java
@@ -9,6 +9,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The DescribeFile annotation indicates that given class has an additional file with Asciidoc description.
+ * The file has to be placed in the documentation/book/api folder and the filename has to be a fully classified class
+ * name (e.g. io.strimzi.api.kafka.MyApiClass). The asciidoc file will be included into the API reference for given
+ * class.
+ *
+ * If the annotation is set but the file does not exist, the API reference build will fail.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface DescriptionFile {
