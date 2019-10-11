b7e91c2dbb2007780f171f0634a8869027a74dbb
Tom Bentley <tbentley@redhat.com>
2019-10-11 11:35:57 +0100

    Fixup
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaConnectorAssemblyOperator.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaConnectorAssemblyOperator.java
@@ -28,10 +28,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- * <p>Assembly operator for a "Kafka Connector" assembly, which manages:</p>
- * <ul>
- *     <li>A Kafka Connector Deployment and related Services</li>
- * </ul>
+ * Operator for a {@code KafkaConnector} CRs.
+ * This is managed via the {@code KafkaConnectAssemblyOperator}, rather than directly by the {@code ClusterOperator}.
  */
 public class KafkaConnectorAssemblyOperator extends
         AbstractOperator<KafkaConnector, CrdOperator<KubernetesClient, KafkaConnector, KafkaConnectorList, DoneableKafkaConnector>> {
@@ -80,63 +78,6 @@ public class KafkaConnectorAssemblyOperator extends
             return Future.failedFuture(t);
         }
     }
-
-
-    /**
-     * Updates the Status field of the Kafka Bridge CR. It diffs the desired status against the current status and calls
-     * the update only when there is any difference in non-timestamp fields.
-     * <p>
-     * //     * @param kafkaBridgeAssembly The CR of Kafka Bridge
-     * //     * @param reconciliation Reconciliation information
-     * //     * @param desiredStatus The KafkaBridgeStatus which should be set
-     * <p>
-     * //     * @return
-     */
-//    Future<Void> updateStatus(KafkaBridge kafkaBridgeAssembly, Reconciliation reconciliation, KafkaBridgeStatus desiredStatus) {
-//        Future<Void> updateStatusFuture = Future.future();
-//
-//        resourceOperator.getAsync(kafkaBridgeAssembly.getMetadata().getNamespace(), kafkaBridgeAssembly.getMetadata().getName()).setHandler(getRes -> {
-//            if (getRes.succeeded()) {
-//                KafkaBridge kafkaBridge = getRes.result();
-//
-//                if (kafkaBridge != null) {
-//                    KafkaBridgeStatus currentStatus = kafkaBridge.getStatus();
-//
-//                    StatusDiff ksDiff = new StatusDiff(currentStatus, desiredStatus);
-//
-//                    if (!ksDiff.isEmpty()) {
-//                        KafkaBridge resourceWithNewStatus = new KafkaBridgeBuilder(kafkaBridge).withStatus(desiredStatus).build();
-//                        ((CrdOperator<KubernetesClient, KafkaBridge, KafkaBridgeList, DoneableKafkaBridge>) resourceOperator).updateStatusAsync(resourceWithNewStatus).setHandler(updateRes -> {
-//                            if (updateRes.succeeded()) {
-//                                log.debug("{}: Completed status update", reconciliation);
-//                                updateStatusFuture.complete();
-//                            } else {
-//                                log.error("{}: Failed to update status", reconciliation, updateRes.cause());
-//                                updateStatusFuture.fail(updateRes.cause());
-//                            }
-//                        });
-//                    } else {
-//                        log.debug("{}: Status did not change", reconciliation);
-//                        updateStatusFuture.complete();
-//                    }
-//                } else {
-//                    log.error("{}: Current Kafka Bridge resource not found", reconciliation);
-//                    updateStatusFuture.fail("Current Kafka Bridge resource not found");
-//                }
-//            } else {
-//                log.error("{}: Failed to get the current Kafka Bridge resource and its status", reconciliation, getRes.cause());
-//                updateStatusFuture.fail(getRes.cause());
-//            }
-//        });
-//
-//        return updateStatusFuture;
-//    }
-
-//    Future<ReconcileResult<ServiceAccount>> kafkaBridgeServiceAccount(String namespace, KafkaBridgeCluster bridge) {
-//        return serviceAccountOperations.reconcile(namespace,
-//                KafkaBridgeResources.serviceAccountName(bridge.getCluster()),
-//                bridge.generateServiceAccount());
-//    }
 
     @Override
     protected Future<Boolean> delete(Reconciliation reconciliation) {
