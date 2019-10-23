f599e922cdc5d2cec2a8e15021bddd251d673a7a
Stanislav Knot <sknot@redhat.com>
2019-10-23 22:42:55 +0200

    Added resources requirements for the Connect S2I build (#2049)
    * Added resources requirements for the Connect S2I build
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * update docs
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- api/src/main/java/io/strimzi/api/kafka/model/KafkaConnectS2ISpec.java
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaConnectS2ISpec.java
@@ -6,6 +6,7 @@ package io.strimzi.api.kafka.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.fabric8.kubernetes.api.model.ResourceRequirements;
 import io.strimzi.crdgenerator.annotations.Description;
 import io.sundr.builder.annotations.Buildable;
 import lombok.EqualsAndHashCode;
@@ -16,7 +17,7 @@ import lombok.EqualsAndHashCode;
         builderPackage = "io.fabric8.kubernetes.api.builder"
 )
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "replicas", "image",
+@JsonPropertyOrder({ "replicas", "image", "buildResources",
         "livenessProbe", "readinessProbe", "jvmOptions",
         "affinity", "logging", "metrics", "template"})
 @EqualsAndHashCode(callSuper = true)
@@ -24,7 +25,19 @@ public class KafkaConnectS2ISpec extends KafkaConnectSpec {
 
     private static final long serialVersionUID = 1L;
 
+    private ResourceRequirements buildResources;
+
     private boolean insecureSourceRepository = false;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Description("CPU and memory resources to reserve.")
+    public ResourceRequirements getBuildResources() {
+        return buildResources;
+    }
+
+    public void setBuildResources(ResourceRequirements buildResources) {
+        this.buildResources = buildResources;
+    }
 
     @Description("When true this configures the source repository with the 'Local' reference policy " +
             "and an import policy that accepts insecure source tags.")
