139823dc54edac9295e8b754da1035f9b1736ca8
Thomas Cooper <tcooper@redhat.com>
2019-11-08 09:44:46 +0000

    Allow the setting of custom Kafka binaries in versions file: (#2128)
    * Allow the setting of custom Kafka binaries in versions file:
    
        * Moved kafka source url to versions file
        * Added option to allow a binary archive file address to be specified
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Changed from tmp file to fixed file location and use better filename parsing
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Minor changes to build path and added more output
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Added additional checksums, mulitplatform commands and formatting
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Fixed make clean behaviour
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Switched to using curl file:// prefix for local kafka binaries
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Switch tar unpacking to outside docker file
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Fix docker build tagging and address other PR comments
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Fixed unwanted binary unpack on non-docker_build targets
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>
    
    * Fixed upacking test bug and 3rd party lib map
    
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -6,6 +6,7 @@ source $(dirname $(realpath $0))/../tools/kafka-versions-tools.sh
 # Image directories
 base_images="base"
 java_images="operator"
+kafka_image="kafka"
 kafka_images="kafka test-client"
 
 function dependency_check { 
@@ -13,18 +14,18 @@ function dependency_check {
     # Check for bash >= 4
     if [ -z ${BASH_VERSINFO+x} ]
     then
-        echo -e "No bash version information available. Aborting."
+        >&2 echo "No bash version information available. Aborting."
         exit 1
     fi
 
     if [ "$BASH_VERSINFO" -lt 4 ]
     then 
-        echo -e "You need bash version >= 4 to build Strimzi.\nRefer to HACKING.md for more information"
+        >&2 echo "You need bash version >= 4 to build Strimzi. Refer to HACKING.md for more information"
         exit 1
     fi
 
     # Check that yq is installed 
-    command -v yq >/dev/null 2>&1 || { printf "You need yq installed to build Strimzi.\nRefer to HACKING.md for more information"; exit 1; }
+    command -v yq >/dev/null 2>&1 || { >&2 echo "You need yq installed to build Strimzi. Refer to HACKING.md for more information"; exit 1; }
 
 }
 
@@ -37,12 +38,98 @@ function alternate_base {
     fi
 }
 
+
+function fetch_and_unpack_kafka_binaries {
+
+    local tag=$1
+    local java_version=$2
+
+    # Create map from version to checksumed and unpacked dist directory
+    declare -Ag version_dist_dirs
+
+    # Set the cache folder to store the binary files
+    binary_file_dir="$kafka_image/tmp"
+    test -d "$binary_file_dir" || mkdir -p "$binary_file_dir"
+
+    for kafka_version in "${!version_checksums[@]}"
+    do
+
+        echo "Fetching and unpacking binary for Kafka $kafka_version"
+
+        expected_sha=${version_checksums[$kafka_version]}
+
+        if [ ${version_binary_urls[$kafka_version]} ]
+        then
+            binary_file_url=${version_binary_urls[$kafka_version]}
+            binary_file_name=$(basename "$binary_file_url")
+            binary_file_path="$binary_file_dir/$binary_file_name"
+            expected_kafka_checksum="$expected_sha  $binary_file_path"
+        else
+            >&2 echo "No URL for Kafka version $kafka_version"
+        fi
+
+        # Check if there is an existing binary file and checksum it against the checksum listed in the kafka-versions file.
+        local get_file=0
+        local do_checksum=1
+
+        if [ -f "$binary_file_path" ]
+        then
+            echo "A file named $binary_file_name is already present in the build directory"
+
+            # We need case insensitive matching as sha512sum output is lowercase and version file checksums are uppercase.
+            shopt -s nocasematch
+            if [[ "$expected_kafka_checksum" == "$(sha512sum "$binary_file_path")" ]]
+            then
+                do_checksum=0
+            else
+                echo "The checksum of the existing $binary_file_name did not match the expected checksum. This file will be replaced."
+                get_file=1
+            fi
+            shopt -u nocasematch
+
+        else
+            get_file=1
+        fi
+
+        # If there is not an existing file, or there is one and it failed the checksum, then download/copy the binary
+        if [ $get_file -gt 0 ]
+        then
+            echo "Fetching Kafka $kafka_version binaries from: $binary_file_url"
+            curl --output "$binary_file_path" "$binary_file_url"
+        fi
+
+        # If we haven't already checksum'd the file do it now before the build.
+        if [ $do_checksum -gt 0 ]
+        then
+            # Do the checksum on the Kafka binaries
+            kafka_checksum_filepath="$binary_file_path.sha512"
+            echo "$expected_kafka_checksum" > "$kafka_checksum_filepath"
+            echo "Checking binary archive file: $binary_file_path"
+            sha512sum --check "$kafka_checksum_filepath"
+        fi
+
+        # We now have a verified tar archive for this version of Kafka. Unpack it into the temp dir
+        dist_dir="$binary_file_dir/$kafka_version"
+        # If an unpacked directory with the same name exists then delete it
+        test -d "$dist_dir" && rm -r "$dist_dir"
+        mkdir -p "$dist_dir"
+        echo "Unpacking binary archive"
+        tar xvfz "$binary_file_path" -C "$dist_dir" --strip-components=1
+
+        # Store the folder address for use in the image build 
+        version_dist_dirs["$kafka_version"]="./tmp/$kafka_version"
+
+    done
+
+}
+
 function build {
    
-    # This function comes from the tools/kafka-versions-tools.sh script and provides two associative arrays
-    # versions_checksums and versions_libs which map from version string to sha512 checksum and third party
-    # library version respectively
-    get_checksum_lib_maps
+    # This function comes from the tools/kafka-versions-tools.sh script and provides several associative arrays
+    # version_binary_urls, version_checksums and version_libs which map from version string 
+    # to source tar url (or file if specified), sha512 checksum for those tar files and third party library 
+    # version respectively.
+    get_version_maps
     
     local targets=$*
     local tag="${DOCKER_TAG:-latest}"
@@ -60,14 +147,23 @@ function build {
         DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg JAVA_VERSION=${java_version} $(alternate_base $image)" make -C "$image" "$targets"
     done
 
-    # Images depending on Kafka version (possibly indirectly thru FROM)
+    if [[ $targets == *"docker_build"* ]]
+    then
+        fetch_and_unpack_kafka_binaries "$tag" "$java_version"
+    fi
+
     for kafka_version in "${!version_checksums[@]}"
     do
-        sha=${version_checksums[$kafka_version]}
         lib_directory=${version_libs[$kafka_version]}
+
+        if [[ $targets == *"docker_build"* ]]
+        then
+            relative_dist_dir=${version_dist_dirs[$kafka_version]}
+        fi
+
         for image in $kafka_images
         do
-            DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg JAVA_VERSION=${java_version} --build-arg KAFKA_VERSION=${kafka_version} --build-arg KAFKA_SHA512=${sha} --build-arg THIRD_PARTY_LIBS=${lib_directory} $(alternate_base "$image")" \
+            DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg JAVA_VERSION=${java_version} --build-arg KAFKA_VERSION=${kafka_version} --build-arg KAFKA_DIST_DIR=${relative_dist_dir} --build-arg THIRD_PARTY_LIBS=${lib_directory} $(alternate_base "$image")" \
             DOCKER_TAG="${tag}-kafka-${kafka_version}" \
             BUILD_TAG="build-kafka-${kafka_version}" \
             KAFKA_VERSION="${kafka_version}" \
@@ -75,8 +171,8 @@ function build {
             make -C "$image" "$targets"
         done
     done
+
 }
 
 dependency_check
 build "$@"
-
--- tools/kafka-versions-tools.sh
+++ tools/kafka-versions-tools.sh
@@ -33,49 +33,48 @@ function get_default_kafka_version {
 }
 
 function get_kafka_versions {
-
     eval versions="$(yq read $VERSIONS_FILE '*.version' -j | tr '[],' '() ')"
+}
 
+function get_kafka_urls {
+    eval binary_urls="$(yq read $VERSIONS_FILE '*.url' -j | tr '[],' '() ')"
 }
 
 function get_kafka_checksums {
-
     eval checksums="$(yq read $VERSIONS_FILE '*.checksum' -j | tr '[],' '() ')"
-
 }
 
 function get_kafka_third_party_libs {
-
     eval libs="$(yq read "$VERSIONS_FILE" '*.third-party-libs' -j | tr '[],' '() ')"
-
 }
 
 function get_kafka_protocols {
-
     eval protocols="$(yq read $VERSIONS_FILE '*.protocol' -j | tr '[],' '() ')"
-
 }
 
 function get_kafka_formats {
-
     eval formats="$(yq read $VERSIONS_FILE '*.format' -j | tr '[],' '() ')"
-
 }
 
-# Parses the Kafka versions file and creates two associative arrays:
-# "version_checksums": Maps from version string to sha512 checksum
-# "version_libs": Maps from version string to third party library version string
-function get_checksum_lib_maps {
+# Parses the Kafka versions file and creates three associative arrays:
+# "version_binary_urls": Maps from version string to url from which the kafka source 
+# tar will be downloaded.
+# "version_checksums": Maps from version string to sha512 checksum.
+# "version_libs": Maps from version string to third party library version string.
+function get_version_maps {
 
     get_kafka_versions
+    get_kafka_urls
     get_kafka_checksums
     get_kafka_third_party_libs
 
+    declare -Ag version_binary_urls
     declare -Ag version_checksums
     declare -Ag version_libs
 
     for i in "${!versions[@]}"
     do 
+        version_binary_urls[${versions[$i]}]=${binary_urls[$i]}
         version_checksums[${versions[$i]}]=${checksums[$i]}
         version_libs[${versions[$i]}]=${libs[$i]}
     done
