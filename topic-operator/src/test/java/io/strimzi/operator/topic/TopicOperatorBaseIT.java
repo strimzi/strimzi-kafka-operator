85eaa7fea4fee32ea0582dd918a4e8f1fe3d87da
Paolo Patierno <ppatierno@live.com>
2019-11-18 13:04:32 +0100

    Added logging exception and finally to clean (#2199)
    * Added logging exception and finally to clean
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Try different delete policy
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Trying cascading true
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Trying with cascading true in one more place
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Added try/catch for better logging
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Removed try/catch for logging
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- topic-operator/src/test/java/io/strimzi/operator/topic/TopicOperatorBaseIT.java
+++ topic-operator/src/test/java/io/strimzi/operator/topic/TopicOperatorBaseIT.java
@@ -188,44 +188,47 @@ public abstract class TopicOperatorBaseIT extends BaseITST {
 
     @AfterEach
     public void teardown() throws InterruptedException, TimeoutException, ExecutionException {
-        LOGGER.info("Tearing down test");
+        try {
+            LOGGER.info("Tearing down test");
 
-        boolean deletionEnabled = "true".equals(kafkaClusterConfig().getOrDefault(
-                KafkaConfig$.MODULE$.DeleteTopicEnableProp(), "true"));
+            boolean deletionEnabled = "true".equals(kafkaClusterConfig().getOrDefault(
+                    KafkaConfig$.MODULE$.DeleteTopicEnableProp(), "true"));
 
-        if (deletionEnabled && kubeClient != null) {
-            List<KafkaTopic> items = operation().inNamespace(NAMESPACE).list().getItems();
+            if (deletionEnabled && kubeClient != null) {
+                List<KafkaTopic> items = operation().inNamespace(NAMESPACE).list().getItems();
 
-            // Wait for the operator to delete all the existing topics in Kafka
-            for (KafkaTopic item : items) {
-                LOGGER.info("Deleting {} from Kube", item.getMetadata().getName());
-                operation().inNamespace(NAMESPACE).withName(item.getMetadata().getName()).delete();
-                LOGGER.info("Awaiting deletion of {} in Kafka", item.getMetadata().getName());
-                waitForTopicInKafka(new TopicName(item).toString(), false);
-                waitForTopicInKube(item.getMetadata().getName(), false);
+                // Wait for the operator to delete all the existing topics in Kafka
+                for (KafkaTopic item : items) {
+                    LOGGER.info("Deleting {} from Kube", item.getMetadata().getName());
+                    operation().inNamespace(NAMESPACE).withName(item.getMetadata().getName()).cascading(true).delete();
+                    LOGGER.info("Awaiting deletion of {} in Kafka", item.getMetadata().getName());
+                    waitForTopicInKafka(new TopicName(item).toString(), false);
+                    waitForTopicInKube(item.getMetadata().getName(), false);
+                }
+                Thread.sleep(5_000);
             }
-            Thread.sleep(5_000);
-        }
 
-        stopTopicOperator();
+            stopTopicOperator();
 
-        if (!deletionEnabled && kubeClient != null) {
-            List<KafkaTopic> items = operation().inNamespace(NAMESPACE).list().getItems();
+            if (!deletionEnabled && kubeClient != null) {
+                List<KafkaTopic> items = operation().inNamespace(NAMESPACE).list().getItems();
 
-            // Wait for the operator to delete all the existing topics in Kafka
-            for (KafkaTopic item : items) {
-                operation().inNamespace(NAMESPACE).withName(item.getMetadata().getName()).delete();
-                waitForTopicInKube(item.getMetadata().getName(), false);
+                // Wait for the operator to delete all the existing topics in Kafka
+                for (KafkaTopic item : items) {
+                    operation().inNamespace(NAMESPACE).withName(item.getMetadata().getName()).delete();
+                    waitForTopicInKube(item.getMetadata().getName(), false);
+                }
             }
-        }
+        } finally {
 
-        adminClient.close();
-        if (kafkaCluster != null) {
-            kafkaCluster.shutdown();
+            adminClient.close();
+            if (kafkaCluster != null) {
+                kafkaCluster.shutdown();
+            }
+            Runtime.getRuntime().removeShutdownHook(kafkaHook);
+            LOGGER.info("Finished tearing down test");
+            vertx.close();
         }
-        Runtime.getRuntime().removeShutdownHook(kafkaHook);
-        LOGGER.info("Finished tearing down test");
-        vertx.close();
     }
 
     protected void startTopicOperator() throws InterruptedException, ExecutionException, TimeoutException {
@@ -594,7 +597,7 @@ public abstract class TopicOperatorBaseIT extends BaseITST {
 
     protected void deleteInKube(String resourceName) throws InterruptedException, ExecutionException, TimeoutException {
         // can now delete the topicResource
-        operation().inNamespace(NAMESPACE).withName(resourceName).delete();
+        operation().inNamespace(NAMESPACE).withName(resourceName).cascading(true).delete();
         waitFor(() -> {
             return operation().inNamespace(NAMESPACE).withName(resourceName).get() == null;
         }, "verified deletion of KafkaTopic " + resourceName);
