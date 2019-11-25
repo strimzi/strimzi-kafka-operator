45340a48c1e3b061dfa11eae27a88d5c8f04f486
Stanislav Knot <sknot@redhat.com>
2019-11-25 14:00:45 +0100

    Fabric8 deletion behaviour fix
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- topic-operator/src/test/java/io/strimzi/operator/topic/TopicOperatorBaseIT.java
+++ topic-operator/src/test/java/io/strimzi/operator/topic/TopicOperatorBaseIT.java
@@ -203,7 +203,7 @@ public abstract class TopicOperatorBaseIT {
                 // Wait for the operator to delete all the existing topics in Kafka
                 for (KafkaTopic item : items) {
                     LOGGER.info("Deleting {} from Kube", item.getMetadata().getName());
-                    operation().inNamespace(NAMESPACE).withName(item.getMetadata().getName()).cascading(true).delete();
+                    operation().inNamespace(NAMESPACE).withName(item.getMetadata().getName()).delete();
                     LOGGER.info("Awaiting deletion of {} in Kafka", item.getMetadata().getName());
                     waitForTopicInKafka(new TopicName(item).toString(), false);
                     waitForTopicInKube(item.getMetadata().getName(), false);
