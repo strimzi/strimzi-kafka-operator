cb8b50801a52f9e7eda134f2570eed35403e0793
Jakub Scholz <www@scholzj.com>
2020-01-03 11:40:37 +0100

    Update cluster role in CO
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -45,6 +45,7 @@ if [ -n "$CHANGED_DERIVED" ] || [ -n "$GENERATED_FILES" ] ; then
   echo "Run the following to add up-to-date resources:"
   echo "  mvn clean verify -DskipTests -DskipITs \\"
   echo "    && make crd_install \\"
+  echo "    && make helm_install \\"
   echo "    && git add install/ helm-charts/ documentation/modules/appendix_crds.adoc cluster-operator/src/main/resources/cluster-roles"
   echo "    && git commit -s -m 'Update derived resources'"
   exit 1
