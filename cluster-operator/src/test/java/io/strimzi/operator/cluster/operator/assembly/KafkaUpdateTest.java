8a6cf06b45c9828cfa0ee07b6d6c9ad5fc2762fe
Jakub Scholz <www@scholzj.com>
2020-01-03 02:20:48 +0100

    Fix unused import
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaUpdateTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaUpdateTest.java
@@ -57,7 +57,6 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(VertxExtension.class)
