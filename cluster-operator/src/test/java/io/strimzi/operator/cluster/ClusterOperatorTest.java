b112770b146d385e76e02344411a4aff40d58f40
Jakub Scholz <www@scholzj.com>
2020-01-07 10:56:46 +0100

    Fix imports
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/ClusterOperatorTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/ClusterOperatorTest.java
@@ -27,9 +27,7 @@ import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
