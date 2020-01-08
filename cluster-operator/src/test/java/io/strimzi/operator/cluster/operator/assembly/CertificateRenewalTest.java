ac2b2df5a6c4e824bd80cffaf86caf2d539610f5
Jakub Scholz <www@scholzj.com>
2020-01-08 01:43:19 +0100

    Fix some more rebase gliches
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/CertificateRenewalTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/CertificateRenewalTest.java
@@ -79,6 +79,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(VertxExtension.class)
