2c165dae01a17bb47ceaa1a04bdea81319e8e57a
Jakub Scholz <www@scholzj.com>
2020-01-28 11:22:46 +0100

    Unused import :-(
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaAssemblyOperator.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaAssemblyOperator.java
@@ -97,7 +97,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.quartz.CronExpression;
 
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
