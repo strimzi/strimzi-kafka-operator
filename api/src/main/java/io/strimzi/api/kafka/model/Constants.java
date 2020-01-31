d265aba30b98161b802edb467e572170cfe493c4
Samuel Hawker <samuel.hawker@ibm.com>
2020-01-31 14:06:51 +0000

    feat: Refactor Resource group constants
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>

---
+++ api/src/main/java/io/strimzi/api/kafka/model/Constants.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.strimzi.api.kafka.model;
+
+public class Constants {
+    public static final String RESOURCE_GROUP_NAME = "kafka.eventstreams.ibm";
+
+    public static final String V1BETA1 = "v1beta1";
+    public static final String V1ALPHA1 = "v1alpha1";
+
+    public static final String V1BETA1_API_VERSION = "apiextensions.k8s.io/" + V1BETA1;
+}
