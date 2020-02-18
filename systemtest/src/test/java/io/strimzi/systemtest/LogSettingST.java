2e46a8df5b5e835c2bd6f7896a7a77db55d03420
Stanislav Knot <sknot@redhat.com>
2020-02-18 11:20:59 +0100

    Rolling update in ST does not work with 1 zk replica (#2553)
    * Rolling update in ST does not work with 1 zk replica
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * comment
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- systemtest/src/test/java/io/strimzi/systemtest/LogSettingST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/LogSettingST.java
@@ -28,7 +28,6 @@ import io.strimzi.test.timemeasuring.Operation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
@@ -221,7 +220,6 @@ class LogSettingST extends BaseST {
 
     @Test
     @Order(11)
-    @Disabled("From some unknown reasons, rolling update of Kafka my-cluster never happen during an expected timeframe. Issue was raised and this test should be enabled after fix.")
     void testGcLoggingSetDisabled() {
         String connectName = KafkaConnectResources.deploymentName(CONNECT_NAME);
         String mmName = KafkaMirrorMakerResources.deploymentName(MM_NAME);
@@ -356,7 +354,7 @@ class LogSettingST extends BaseST {
 
         timeMeasuringSystem.setOperationID(startDeploymentMeasuring());
 
-        KafkaResource.kafkaEphemeral(CLUSTER_NAME, 3, 1)
+        KafkaResource.kafkaPersistent(CLUSTER_NAME, 3, 1)
             .editSpec()
                 .editKafka()
                     .withNewInlineLogging()
@@ -395,7 +393,7 @@ class LogSettingST extends BaseST {
             .endSpec()
             .done();
 
-        KafkaResource.kafkaEphemeral(GC_LOGGING_SET_NAME, 1, 1)
+        KafkaResource.kafkaPersistent(GC_LOGGING_SET_NAME, 1, 1)
             .editSpec()
                 .editKafka()
                     .withNewJvmOptions()
