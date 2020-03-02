6c30c71c5087375146ade93ed9cfe4218c339021
Stanislav Knot <sknot@redhat.com>
2020-03-02 10:06:10 +0100

    Fix jvmOpts (#2619)
    * Fix jvmOpts
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * revert eo
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- user-operator/scripts/user_operator_run.sh
+++ user-operator/scripts/user_operator_run.sh
@@ -3,6 +3,7 @@ if [ -f /opt/user-operator/custom-config/log4j2.properties ];
 then
     export JAVA_OPTS="${JAVA_OPTS} -Dlog4j2.configurationFile=file:/opt/user-operator/custom-config/log4j2.properties"
 fi
+
 export JAVA_CLASSPATH=lib/io.strimzi.@project.build.finalName@.@project.packaging@:@project.dist.classpath@
 export JAVA_MAIN=io.strimzi.operator.user.Main
 exec ${STRIMZI_HOME}/bin/launch_java.sh
