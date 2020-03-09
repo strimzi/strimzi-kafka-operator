d2fcdd6961ab41ac37fa3199476fe6b03697d839
Jakub Stejskal <xstejs24@gmail.com>
2020-03-09 13:09:34 +0100

    fixup! WIP: improve error logging
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- systemtest/src/main/java/io/strimzi/systemtest/logs/TestExecutionWatcher.java
+++ systemtest/src/main/java/io/strimzi/systemtest/logs/TestExecutionWatcher.java
@@ -7,7 +7,6 @@ package io.strimzi.systemtest.logs;
 import io.strimzi.systemtest.Environment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.LifecycleMethodExecutionExceptionHandler;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
