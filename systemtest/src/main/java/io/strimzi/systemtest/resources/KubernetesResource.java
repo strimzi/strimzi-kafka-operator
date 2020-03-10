0d1aa314673ab0a830809e3b94231ebc808f5800
Jakub Stejskal <xstejs24@gmail.com>
2020-03-10 15:33:47 +0100

    Add another fix for cover access form clients pod to connet with NP
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- systemtest/src/main/java/io/strimzi/systemtest/resources/KubernetesResource.java
+++ systemtest/src/main/java/io/strimzi/systemtest/resources/KubernetesResource.java
@@ -7,6 +7,7 @@ package io.strimzi.systemtest.resources;
 import io.fabric8.kubernetes.api.model.DoneableService;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.LabelSelector;
 import io.fabric8.kubernetes.api.model.LabelSelectorBuilder;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.api.model.ServiceBuilder;
@@ -332,7 +333,7 @@ public class KubernetesResource {
      */
     public static void allowNetworkPolicySettingsForResource(HasMetadata resource, String deploymentName, String clusterName) {
         String clientsDeploymentName = clusterName + "-" + Constants.KAFKA_CLIENTS;
-        Map<String, String> labels = kubeClient().listPodsByPrefixInName(clientsDeploymentName).get(0).getMetadata().getLabels();
+        LabelSelector labelSelector = kubeClient().getDeployment(clientsDeploymentName).getSpec().getSelector();
 
         LOGGER.info("Apply NetworkPolicy access to {} from {}", deploymentName, clientsDeploymentName);
 
@@ -345,7 +346,7 @@ public class KubernetesResource {
                 .withNewSpec()
                     .addNewIngress()
                         .addNewFrom()
-                            .withPodSelector(new LabelSelectorBuilder().addToMatchLabels(labels).build())
+                            .withPodSelector(labelSelector)
                         .endFrom()
                         .addNewPort()
                             .withNewPort(8083)
