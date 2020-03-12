4296a5e1cfc1e77105680474e68609aa7f78f6b8
Samuel Hawker <samuel.hawker@ibm.com>
2020-03-12 23:59:06 +0000

    fix: typo commend to command
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>

--- prerequisites-check.sh
+++ prerequisites-check.sh
@@ -3,14 +3,14 @@
 RED="\033[0;31m"
 NO_COLOUR="\033[0m"
 
-function check_commend_present() {
+function check_command_present() {
     command -v "${1}" >/dev/null 2>&1 || { echo -e >&2 "${RED}I require ${1} but it's not installed.  Aborting.${NO_COLOUR}"; exit 1; }
 }
 
-check_commend_present yq
-check_commend_present mvn
-check_commend_present git
-check_commend_present docker
+check_command_present yq
+check_command_present mvn
+check_command_present git
+check_command_present docker
 
 YQ_VERSION="$(yq --version | ${SED} 's/^.* //g')"
 if [[ ${YQ_VERSION} != "3."* ]]; then
