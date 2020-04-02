ba97ca66c7a7d8a174c957a947d8f6a79d7c46f4
Jakub Scholz <www@scholzj.com>
2020-04-03 00:59:08 +0200

    Fix quotas for TLS users and deletion of SCRAM users with quotas
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- user-operator/src/main/java/io/strimzi/operator/user/operator/KafkaUserOperator.java
+++ user-operator/src/main/java/io/strimzi/operator/user/operator/KafkaUserOperator.java
@@ -293,9 +293,9 @@ public class KafkaUserOperator extends AbstractOperator<KafkaUser,
         return CompositeFuture.join(secretOperations.reconcile(namespace, KafkaUserModel.getSecretName(user), null),
                 aclOperations.reconcile(KafkaUserModel.getTlsUserName(user), null),
                 aclOperations.reconcile(KafkaUserModel.getScramUserName(user), null),
-                kafkaUserQuotasOperator.reconcile(KafkaUserModel.getTlsUserName(user), null),
-                kafkaUserQuotasOperator.reconcile(KafkaUserModel.getScramUserName(user), null),
-                scramShaCredentialOperator.reconcile(KafkaUserModel.getScramUserName(user), null))
+                scramShaCredentialOperator.reconcile(KafkaUserModel.getScramUserName(user), null)
+                        .compose(ignore -> kafkaUserQuotasOperator.reconcile(KafkaUserModel.getTlsUserName(user), null))
+                        .compose(ignore -> kafkaUserQuotasOperator.reconcile(KafkaUserModel.getScramUserName(user), null)))
             .map(Boolean.TRUE);
     }
 
