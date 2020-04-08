88c5cf3905da35770c11347a663d339583ca903c
morsak <xorsak02@stud.fit.vutbr.cz>
2020-04-08 11:55:02 +0200

    [MO] -[exception] -> change
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- test/src/main/java/io/strimzi/test/TestUtils.java
+++ test/src/main/java/io/strimzi/test/TestUtils.java
@@ -97,7 +97,7 @@ public final class TestUtils {
 
     /**
      * Poll the given {@code ready} function every {@code pollIntervalMs} milliseconds until it returns true,
-     * or throw a TimeoutException if it doesn't returns true within {@code timeoutMs} milliseconds.
+     * or throw a RuntimeException if it doesn't returns true within {@code timeoutMs} milliseconds.
      * @return The remaining time left until timeout occurs
      * (helpful if you have several calls which need to share a common timeout),
      * */
@@ -121,11 +121,7 @@ public final class TestUtils {
             }
             if (timeLeft <= 0) {
                 onTimeout.run();
-                try {
-                    throw new TimeoutException("Timeout after " + timeoutMs + " ms waiting for " + description);
-                } catch (TimeoutException e) {
-                    e.printStackTrace();
-                }
+                throw new RuntimeException("Timeout after " + timeoutMs + " ms waiting for " + description);
             }
             long sleepTime = Math.min(pollIntervalMs, timeLeft);
             if (LOGGER.isTraceEnabled()) {
