6235d85c88e4216f22737f97cbcb59d45e3654c4
morsak <xorsak02@stud.fit.vutbr.cz>
2020-04-16 12:39:33 +0200

    [MO] - [tag] -> regression
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/test/java/io/strimzi/systemtest/security/SslConfigurationST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/security/SslConfigurationST.java
@@ -13,14 +13,17 @@ import io.strimzi.systemtest.utils.kubeUtils.objects.PodUtils;
 import org.apache.kafka.common.config.SslConfigs;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static io.strimzi.systemtest.Constants.REGRESSION;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
+@Tag(REGRESSION)
 public class SslConfigurationST extends SecurityST {
 
     private static final Logger LOGGER = LogManager.getLogger(SslConfigurationST.class);
