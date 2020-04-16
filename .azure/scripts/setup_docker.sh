bdb28d5c4a4adc98a9e777d9d914935a5452201a
Jakub Stejskal <xstejs24@gmail.com>
2020-04-16 18:38:20 +0200

    Install docker in different way
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- .azure/scripts/setup_docker.sh
+++ .azure/scripts/setup_docker.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Remove moby-engine which use docker 3.0.8 on azure
 sudo apt-get remove --purge iotedge
 sudo apt-get remove --purge moby-cli
 sudo apt-get remove --purge moby-engine
 
-wget -O docker.deb https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_18.03.1~ce~3-0~ubuntu_amd64.deb
-sudo dpkg -i ./docker.deb
+# Do update and install docker
+sudo apt update
+sudo apt install docker.io
+sudo systemctl unmask docker
+
+sudo mkdir /mnt/docker
+
+sudo sh -c "sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
+
+sudo systemctl daemon-reload
+sudo rsync -aqxP /var/lib/docker/ /mnt/docker
+
+sudo systemctl start docker
