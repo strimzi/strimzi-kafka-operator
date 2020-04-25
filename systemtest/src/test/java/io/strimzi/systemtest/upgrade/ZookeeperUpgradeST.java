15ccbff7777ffc6e4dc21055faae0c487684d450
Jakub Stejskal <xstejs24@gmail.com>
2020-04-25 16:36:17 +0200

    fixup! Remove reversing the list which is not working properly on jenkins
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/upgrade/ZookeeperUpgradeST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/upgrade/ZookeeperUpgradeST.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
