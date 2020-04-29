798cf8c857e8bbc3e2f54346d3b4a2cb34dab899
Jakub Scholz <www@scholzj.com>
2020-04-30 00:40:17 +0200

    Fix docs, CRDs and APi description
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- api/src/main/java/io/strimzi/api/kafka/model/ZookeeperClusterSpec.java
+++ api/src/main/java/io/strimzi/api/kafka/model/ZookeeperClusterSpec.java
@@ -69,7 +69,7 @@ public class ZookeeperClusterSpec implements UnknownPropertyPreserving, Serializ
     private ZookeeperClusterTemplate template;
     private Map<String, Object> additionalProperties = new HashMap<>(0);
 
-    @Description("The ZooKeeper broker config. Properties with the following prefixes cannot be set: " + FORBIDDEN_PREFIXES)
+    @Description("The ZooKeeper broker config. Properties with the following prefixes cannot be set: " + FORBIDDEN_PREFIXES + " (with the exception of: " + FORBIDDEN_PREFIX_EXCEPTIONS + ").")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     public Map<String, Object> getConfig() {
         return config;
