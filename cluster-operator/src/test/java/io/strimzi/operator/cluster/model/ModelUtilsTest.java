05079a950b73d88b2c4fbf21e26fe6ef2414820b
Samuel Hawker <samuel.hawker@ibm.com>
2020-05-12 22:29:33 +0100

    Refactor pod dns (#2998)
    * refactor: PodDns helper functions
    
    Refactored this code so that the way hostnames
    are constructed is guaranteed to be consistent across
    models
    
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>
    
    * feat: add service DNS name generator
    
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>
    
    * feat: Documentation cleanup and tests
    
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>
    
    * fix: Correct helper to not directly reference sts
    
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/model/ModelUtilsTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/model/ModelUtilsTest.java
@@ -303,4 +303,28 @@ public class ModelUtilsTest {
         assertThat(ModelUtils.doExistingCertificatesDiffer(defaultSecret, changedScaleUpSecret), is(true));
         assertThat(ModelUtils.doExistingCertificatesDiffer(defaultSecret, changedScaleDownSecret), is(true));
     }
+
+    @Test
+    public void testPodDnsName()  {
+        assertThat(ModelUtils.podDnsName("my-ns", "my-service", "my-pod-1"),
+                is("my-pod-1.my-service.my-ns.svc.cluster.local"));
+    }
+
+    @Test
+    public void testPodDnsNameWithoutClusterDomain()  {
+        assertThat(ModelUtils.podDnsNameWithoutClusterDomain("my-ns", "my-service", "my-pod-1"),
+                is("my-pod-1.my-service.my-ns.svc"));
+    }
+
+    @Test
+    public void testServiceDnsName()  {
+        assertThat(ModelUtils.serviceDnsName("my-ns", "my-service"),
+                is("my-service.my-ns.svc.cluster.local"));
+    }
+
+    @Test
+    public void testServiceDnsNameWithoutClusterDomain()  {
+        assertThat(ModelUtils.serviceDnsNameWithoutClusterDomain("my-ns", "my-service"),
+                is("my-service.my-ns.svc"));
+    }
 }
