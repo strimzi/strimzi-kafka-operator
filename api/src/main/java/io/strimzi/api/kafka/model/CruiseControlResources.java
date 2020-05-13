7bb684c6436be73344a6eb0ce27f7a3e40747558
kyguy <kliberti@redhat.com>
2020-05-13 19:00:40 -0400

    Enable CO to access CC service across namespaces + bump to CC 2.0.103 (#3009)
    * Enable CO to access CC service across namespaces
    
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>
    
    * Bump CC to 2.0.103
    
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

--- api/src/main/java/io/strimzi/api/kafka/model/CruiseControlResources.java
+++ api/src/main/java/io/strimzi/api/kafka/model/CruiseControlResources.java
@@ -38,6 +38,17 @@ public class CruiseControlResources {
     }
 
     /**
+     * Returns qualified name of the service which works across different namespaces.
+     *
+     * @param clusterName   The {@code metadata.name} of the {@code Kafka} resource.
+     * @param namespace     Namespace of the Cruise Control deployment
+     * @return              qualified namespace in the format "&lt;service-name&gt;.&lt;namespace&gt;.svc"
+     */
+    public static String qualifiedServiceName(String clusterName, String namespace) {
+        return serviceName(clusterName) + "." + namespace + ".svc";
+    }
+
+    /**
      * Returns the name of the Cruise Control {@code Secret} for a {@code Kafka} cluster of the given name.
      * This {@code Secret} will only exist if {@code Kafka.spec.cruiseControl} is configured in the
      * {@code Kafka} resource with the given name.
