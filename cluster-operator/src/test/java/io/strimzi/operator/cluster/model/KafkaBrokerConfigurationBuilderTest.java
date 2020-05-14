d7f4ddf0f717b92ce5c4ec58f280ecdc9029eb07
Marko Strukelj <marko.strukelj@gmail.com>
2020-05-14 17:35:58 +0200

    New configuration options for Strimzi OAuth 0.5.0 features (#2974)
    * Add additional config options available in Strimzi Kafka OAuth 0.5.0
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Add Helm charts
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Relax the oauth config constraints for some options
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Add staging repo for strimzi-kafka-oauth 0.5.0
    
    TODO: Remove this before merging the PR
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Update staging repo for strimzi-kafka-oauth 0.5.0 (RC2)
    
    TODO: Remove this before merging the PR
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Systemtests failures
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Remove scope from listener config, and improve CRD docs.
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Fix error found by spotbugs
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Some documentation
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Remove strimzi-kafka-oauth-0.5.0 staging repo
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Fix documentation
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Add @SuppressWarnings rather than use suppressions.xml
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: PaulRMellor <47596553+PaulRMellor@users.noreply.github.com>
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Documentation fixes
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Documentation fixes
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bentley <tombentley@users.noreply.github.com>
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Documentation fixes
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * More proper fix for NPathComplexity warning
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    * Fix test failures for KafkaBrokerConfigurationBuilder
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>
    
    Co-authored-by: PaulRMellor <47596553+PaulRMellor@users.noreply.github.com>
    Co-authored-by: Tom Bentley <tombentley@users.noreply.github.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/model/KafkaBrokerConfigurationBuilderTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/model/KafkaBrokerConfigurationBuilderTest.java
@@ -33,6 +33,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -899,34 +900,54 @@ public class KafkaBrokerConfigurationBuilderTest {
     public void testOAuthOptions()  {
         KafkaListenerAuthenticationOAuth auth = new KafkaListenerAuthenticationOAuthBuilder()
                 .withValidIssuerUri("http://valid-issuer")
+                .withCheckIssuer(false)
                 .withJwksEndpointUri("http://jwks-endpoint")
                 .withIntrospectionEndpointUri("http://introspection-endpoint")
+                .withUserInfoEndpointUri("http://userinfo-endpoint")
                 .withJwksExpirySeconds(160)
                 .withJwksRefreshSeconds(50)
                 .withEnableECDSA(true)
                 .withUserNameClaim("preferred_username")
+                .withFallbackUserNameClaim("client_id")
+                .withFallbackUserNamePrefix("client-account-")
                 .withCheckAccessTokenType(false)
                 .withClientId("my-kafka-id")
                 .withAccessTokenIsJwt(false)
+                .withValidTokenType("access_token")
                 .withDisableTlsHostnameVerification(true)
                 .build();
 
         List<String> expectedOptions = new ArrayList<>(5);
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_CLIENT_ID, "my-kafka-id"));
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_VALID_ISSUER_URI, "http://valid-issuer"));
+        expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_CHECK_ISSUER, false));
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_JWKS_ENDPOINT_URI, "http://jwks-endpoint"));
         expectedOptions.add(String.format("%s=\"%d\"", ServerConfig.OAUTH_JWKS_REFRESH_SECONDS, 50));
         expectedOptions.add(String.format("%s=\"%d\"", ServerConfig.OAUTH_JWKS_EXPIRY_SECONDS, 160));
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_CRYPTO_PROVIDER_BOUNCYCASTLE, true));
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_INTROSPECTION_ENDPOINT_URI, "http://introspection-endpoint"));
+        expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_USERINFO_ENDPOINT_URI, "http://userinfo-endpoint"));
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_USERNAME_CLAIM, "preferred_username"));
+        expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_FALLBACK_USERNAME_CLAIM, "client_id"));
+        expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_FALLBACK_USERNAME_PREFIX, "client-account-"));
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_ACCESS_TOKEN_IS_JWT, false));
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_CHECK_ACCESS_TOKEN_TYPE, false));
+        expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_VALID_TOKEN_TYPE, "access_token"));
         expectedOptions.add(String.format("%s=\"%s\"", ServerConfig.OAUTH_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM, ""));
 
         List<String> actualOptions = KafkaBrokerConfigurationBuilder.getOAuthOptions(auth);
 
         assertThat(actualOptions, is(equalTo(expectedOptions)));
+    }
+
+    @Test
+    public void testOAuthDefaultOptions()  {
+        KafkaListenerAuthenticationOAuth auth = new KafkaListenerAuthenticationOAuthBuilder()
+                .build();
+
+        List<String> actualOptions = KafkaBrokerConfigurationBuilder.getOAuthOptions(auth);
+
+        assertThat(actualOptions, is(equalTo(Collections.emptyList())));
     }
 
     static class IsEquivalent extends TypeSafeMatcher<String> {
