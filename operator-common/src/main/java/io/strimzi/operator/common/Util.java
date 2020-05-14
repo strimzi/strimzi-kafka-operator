aa9efa33c3c1b4d680c641cf10d51b28254e8da4
Jakub Scholz <www@scholzj.com>
2020-05-14 15:14:29 +0200

    Disable the HTTP2 protocol as a workaround for Java 1.8.0-252 issues
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- operator-common/src/main/java/io/strimzi/operator/common/Util.java
+++ operator-common/src/main/java/io/strimzi/operator/common/Util.java
@@ -251,6 +251,18 @@ public class Util {
         } else {
             return value;
         }
+    }
 
+    /**
+     * OkHttp wrongfully detects JDK8u251 and higher as JDK9 which enables Http2 unsupported for JDK8.
+     * This method is used as workaround as described in https://github.com/fabric8io/kubernetes-client/issues/2212
+     *
+     * Thsi shoud be removed after migrating to Fabric8 4.10.2 or higher or after migration to Java 11.
+     *
+     * @return true if JDK8 is detected and the environment variable HTTP2_DISABLE is not set
+     */
+    public static boolean shouldDisableHttp2() {
+        return System.getProperty("java.version", "").startsWith("1.8")
+                    && System.getenv("HTTP2_DISABLE") == null;
     }
 }
