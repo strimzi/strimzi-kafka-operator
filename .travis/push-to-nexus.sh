d2c2c2804a56ffb2c479c5cf672d46c5b0678eab
Orsák Maroš <30839163+see-quick@users.noreply.github.com>
2020-05-20 09:54:16 +0200

    [MO] - [client tests] -> simple smoke client tests (#2969)

--- .travis/push-to-nexus.sh
+++ .travis/push-to-nexus.sh
@@ -3,7 +3,7 @@
 openssl aes-256-cbc -K $encrypted_cdaf52794220_key -iv $encrypted_cdaf52794220_iv -in .travis/signing.gpg.enc -out signing.gpg -d
 gpg --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn -B $MVN_ARGS -DskipTests -s ./.travis/settings.xml  -pl ./,crd-generator,api -P ossrh package gpg:sign deploy
+GPG_EXECUTABLE=gpg mvn -B $MVN_ARGS -DskipTests -s ./.travis/settings.xml  -pl ./,crd-generator,api,test-container -P ossrh package gpg:sign deploy
 
 rm -rf signing.gpg
 gpg --delete-keys
