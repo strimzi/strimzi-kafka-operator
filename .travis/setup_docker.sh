e7e03702d85533432d04238b48a091ca4254d226
morsak <xorsak02@stud.fit.vutbr.cz>
2020-05-21 16:00:41 +0200

    dsds
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- .travis/setup_docker.sh
+++ .travis/setup_docker.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
+sudo apt update
+sudo apt install docker.io -y
+sudo systemctl unmask docker
 
-# Do update and install docker
+sudo mkdir /mnt/docker
 
-apt update
-apt install docker.io -y
-systemctl unmask docker
+sudo sh -c "sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
 
-mkdir /mnt/docker
+sudo systemctl daemon-reload
+sudo rsync -aqxP /var/lib/docker/ /mnt/docker
 
-sh -c "sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
-
-systemctl daemon-reload
-rsync -aqxP /var/lib/docker/ /mnt/docker
-
-systemctl start docker
+sudo systemctl start docker
