be14a13542b43cd7ed4a9603219a820a11c4159d
morsak <xorsak02@stud.fit.vutbr.cz>
2020-05-25 14:44:41 +0200

    dsd
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- .travis/install_yq.sh
+++ .travis/install_yq.sh
@@ -2,16 +2,3 @@
 
 curl -L https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 > yq && chmod +x yq
 sudo cp yq /usr/bin/
-
-sudo apt update
-sudo apt install docker.io -y
-sudo systemctl unmask docker
-
-sudo mkdir /mnt/docker
-
-sudo sh -c "sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd -g /mnt/docker -H fd://#' /lib/systemd/system/docker.service"
-
-sudo systemctl daemon-reload
-sudo rsync -aqxP /var/lib/docker/ /mnt/docker
-
-sudo systemctl start docker
