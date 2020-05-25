bc73b9aae49571836da752895a798b6d9557abe4
morsak <xorsak02@stud.fit.vutbr.cz>
2020-05-25 17:46:36 +0200

    checkstyle
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- test-container/src/main/java/io/strimzi/StrimziKafkaContainer.java
+++ test-container/src/main/java/io/strimzi/StrimziKafkaContainer.java
@@ -7,18 +7,15 @@ package io.strimzi;
 import com.github.dockerjava.api.command.ExecCreateCmdResponse;
 import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.api.model.ContainerNetwork;
-import com.github.dockerjava.core.command.ExecStartResultCallback;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
