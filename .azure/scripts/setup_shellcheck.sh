dcc2f3a3450c42ad1b4390c0465ff00dcd1b18de
Jakub Scholz <www@scholzj.com>
2020-05-27 23:24:24 +0200

    Install newer version of shellcheck
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/setup_shellcheck.sh
+++ .azure/scripts/setup_shellcheck.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-sudo apt install shellcheck
+wget https://github.com/koalaman/shellcheck/releases/download/v0.7.1/shellcheck-v0.7.1.linux.x86_64.tar.xz -O shellcheck.tar.xz
+tar xzf shellcheck.tar.xz -C /tmp --strip-components 1
+chmod +x /tmp/shellcheck
+sudo mv /tmp/shellcheck /usr/bin
