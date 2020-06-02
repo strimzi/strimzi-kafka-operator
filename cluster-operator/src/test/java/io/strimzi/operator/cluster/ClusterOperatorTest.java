9d9752720bdc6bfaee2b608f75b345a13016421f
Stanislav Knot <sknot@redhat.com>
2020-06-02 15:54:09 +0200

    Build Strimzi with Java11 (#3031)
    * Build Strimzi with Java11
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * revert jenkins
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * revert me later: debug j11 build
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * patch
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * comments
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * test closing
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * another try
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * comment
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/ClusterOperatorTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/ClusterOperatorTest.java
@@ -21,12 +21,13 @@ import io.strimzi.operator.KubernetesVersion;
 import io.strimzi.operator.PlatformFeaturesAvailability;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.micrometer.MicrometerMetricsOptions;
 import io.vertx.micrometer.VertxPrometheusOptions;
 import okhttp3.OkHttpClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -37,6 +38,8 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.Arrays.asList;
@@ -52,6 +55,7 @@ import static org.mockito.Mockito.when;
 @ExtendWith(VertxExtension.class)
 public class ClusterOperatorTest {
     private static Vertx vertx;
+    private static final Logger log = LogManager.getLogger(ClusterOperatorTest.class);
 
     private static Map<String, String> buildEnv(String namespaces) {
         Map<String, String> env = new HashMap<>();
@@ -80,32 +84,32 @@ public class ClusterOperatorTest {
     }
 
     @Test
-    public void testStartStopSingleNamespaceOnOpenShift(VertxTestContext context) {
+    public void testStartStopSingleNamespaceOnOpenShift(VertxTestContext context) throws InterruptedException {
         startStop(context, "namespace", true);
     }
 
     @Test
-    public void testStartStopMultiNamespaceOnOpenShift(VertxTestContext context) {
+    public void testStartStopMultiNamespaceOnOpenShift(VertxTestContext context) throws InterruptedException {
         startStop(context, "namespace1,namespace2", true);
     }
 
     @Test
-    public void testStartStopSingleNamespaceOnK8s(VertxTestContext context) {
+    public void testStartStopSingleNamespaceOnK8s(VertxTestContext context) throws InterruptedException {
         startStop(context, "namespace", false);
     }
 
     @Test
-    public void testStartStopMultiNamespaceOnK8s(VertxTestContext context) {
+    public void testStartStopMultiNamespaceOnK8s(VertxTestContext context) throws InterruptedException {
         startStop(context, "namespace1,namespace2", false);
     }
 
     @Test
-    public void testStartStopAllNamespacesOnOpenShift(VertxTestContext context) {
+    public void testStartStopAllNamespacesOnOpenShift(VertxTestContext context) throws InterruptedException {
         startStopAllNamespaces(context, "*", true);
     }
 
     @Test
-    public void testStartStopAllNamespacesOnK8s(VertxTestContext context) {
+    public void testStartStopAllNamespacesOnK8s(VertxTestContext context) throws InterruptedException {
         startStopAllNamespaces(context, "*", false);
     }
 
@@ -114,7 +118,7 @@ public class ClusterOperatorTest {
      * @param context test context passed in for assertions
      * @param namespaces namespaces the operator should be watching and operating on
      */
-    private void startStop(VertxTestContext context, String namespaces, boolean openShift) {
+    private void startStop(VertxTestContext context, String namespaces, boolean openShift) throws InterruptedException {
         AtomicInteger numWatchers = new AtomicInteger(0);
 
         KubernetesClient client;
@@ -165,21 +169,29 @@ public class ClusterOperatorTest {
 
         Map<String, String> env = buildEnv(namespaces);
 
-        Checkpoint async = context.checkpoint();
+        CountDownLatch latch = new CountDownLatch(namespaceList.size() + 1);
+
         Main.run(vertx, client, new PlatformFeaturesAvailability(openShift, KubernetesVersion.V1_9),
                     ClusterOperatorConfig.fromMap(env, KafkaVersionTestUtils.getKafkaVersionLookup()))
             .onComplete(context.succeeding(v -> context.verify(() -> {
                 assertThat("A verticle per namespace", vertx.deploymentIDs(), hasSize(namespaceList.size()));
-
                 for (String deploymentId: vertx.deploymentIDs()) {
-                    vertx.undeploy(deploymentId, context.succeeding());
+                    vertx.undeploy(deploymentId, asyncResult -> {
+                        if (asyncResult.failed()) {
+                            log.error("Failed to undeploy {}", deploymentId);
+                            context.failNow(asyncResult.cause());
+                        }
+                        latch.countDown();
+                    });
                 }
 
                 int maximumExpectedNumberOfWatchers = (openShift ? 9 : 7) * namespaceList.size(); // we do not have connectS2I on k8s
                 assertThat("Looks like there were more watchers than namespaces",
                         numWatchers.get(), lessThanOrEqualTo(maximumExpectedNumberOfWatchers));
-                async.flag();
+                latch.countDown();
             })));
+        latch.await(10, TimeUnit.SECONDS);
+        context.completeNow();
     }
 
     /**
@@ -187,7 +199,7 @@ public class ClusterOperatorTest {
      * @param context test context passed in for assertions
      * @param namespaces namespaces the operator should be watching and operating on
      */
-    private void startStopAllNamespaces(VertxTestContext context, String namespaces, boolean openShift) {
+    private void startStopAllNamespaces(VertxTestContext context, String namespaces, boolean openShift) throws InterruptedException {
         AtomicInteger numWatchers = new AtomicInteger(0);
         KubernetesClient client;
         if (openShift) {
@@ -234,18 +246,26 @@ public class ClusterOperatorTest {
 
         Map<String, String> env = buildEnv(namespaces);
 
-        Checkpoint async = context.checkpoint();
+        CountDownLatch latch = new CountDownLatch(2);
         Main.run(vertx, client, new PlatformFeaturesAvailability(openShift, KubernetesVersion.V1_9),
                 ClusterOperatorConfig.fromMap(env, KafkaVersionTestUtils.getKafkaVersionLookup()))
             .onComplete(context.succeeding(v -> context.verify(() -> {
                 assertThat("A verticle per namespace", vertx.deploymentIDs(), hasSize(1));
                 for (String deploymentId: vertx.deploymentIDs()) {
-                    vertx.undeploy(deploymentId, context.succeeding());
+                    vertx.undeploy(deploymentId, asyncResult -> {
+                        if (asyncResult.failed()) {
+                            log.error("Failed to undeploy {}", deploymentId);
+                            context.failNow(asyncResult.cause());
+                        }
+                        latch.countDown();
+                    });
                 }
 
                 int maximumExpectedNumberOfWatchers = openShift ? 9 : 7; // we do not have connectS2I on k8s
                 assertThat("Looks like there were more watchers than namespaces", numWatchers.get(), lessThanOrEqualTo(maximumExpectedNumberOfWatchers));
-                async.flag();
+                latch.countDown();
             })));
+        latch.await(10, TimeUnit.SECONDS);
+        context.completeNow();
     }
 }
