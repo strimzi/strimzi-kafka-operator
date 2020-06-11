1344d206c9de33b1065ede69d935f68700222201
Jakub Scholz <www@scholzj.com>
2020-06-11 15:20:38 +0200

    Make it possible to run tests against any Kubernetes cluster (#3184)
    * Make it possible to run tests against any Kubernetes cluster
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Fix javadoc
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>

---
+++ test/src/main/java/io/strimzi/test/k8s/cluster/Kubernetes.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.test.k8s.cluster;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.strimzi.test.executor.Exec;
+import io.strimzi.test.k8s.KubeClient;
+import io.strimzi.test.k8s.cmdClient.KubeCmdClient;
+import io.strimzi.test.k8s.cmdClient.Kubectl;
+import io.strimzi.test.k8s.exceptions.KubeClusterException;
+
+/**
+ * A {@link KubeCluster} implementation for any {@code Kubernetes} cluster.
+ */
+public class Kubernetes implements KubeCluster {
+
+    public static final String CMD = "kubectl";
+    private static final String OLM_NAMESPACE = "operators";
+
+    @Override
+    public boolean isAvailable() {
+        return Exec.isExecutableOnPath(CMD);
+    }
+
+    @Override
+    public boolean isClusterUp() {
+        try {
+            return Exec.exec(CMD, "cluster-info").exitStatus();
+        } catch (KubeClusterException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public KubeCmdClient defaultCmdClient() {
+        return new Kubectl();
+    }
+
+    @Override
+    public KubeClient defaultClient() {
+        return new KubeClient(new DefaultKubernetesClient(CONFIG), "default");
+    }
+
+    public String toString() {
+        return CMD;
+    }
+
+    @Override
+    public String defaultOlmNamespace() {
+        return OLM_NAMESPACE;
+    }
+}
