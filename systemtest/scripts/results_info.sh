dcf48fc84e347cac63390ca36fb11641969bf0a3
Jakub Stejskal <xstejs24@gmail.com>
2020-06-29 12:32:04 +0200

    Update surefire version to 3.0.0-M5 (#3234)
    * WIP: migration to surefire 3
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * fixup! WIP: migration to surefire 3
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Make it working and update azure and travis and pr
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * fixup! Make it working and update azure and travis and pr
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Workaround SUREFIRE-1809 and set rerun properly
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * fixup! Workaround SUREFIRE-1809 and set rerun properly
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- systemtest/scripts/results_info.sh
+++ systemtest/scripts/results_info.sh
@@ -45,7 +45,7 @@ fi
 SUMMARY="**TEST_PROFILE**: ${TEST_PROFILE}\n${ADDITIONAL_INFO}**TEST_CASE:** ${TEST_CASE}\n**TOTAL:** ${TEST_COUNT}\n**PASS:** $((TEST_COUNT - TEST_ALL_FAILED_COUNT - TEST_SKIPPED_COUNT))\n**FAIL:** ${TEST_ALL_FAILED_COUNT}\n**SKIP:** ${TEST_SKIPPED_COUNT}\n**BUILD_NUMBER:** ${BUILD_ID}\n**BUILD_ENV:** ${BUILD_ENV}\n"
 
 
-FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 sed -n "s#\(<testcase.*time=\"[0-9]*,\{0,1\}[0-9]\{1,3\}\..*[^\/]>\)#\1#p" | awk -F '"' '{print "\\n- " $2 " in "  $4}')
+FAILED_TESTS=$(find "${RESULTS_PATH}" -name 'TEST*.xml' -type f -print0 | xargs -0 awk '/<testcase.*>/{ getline x; if (x ~ "<error" || x ~ "<failure") {  gsub(/classname=|name=|\"/, "", $0); print "\\n- " $2 " in " $3 }}')
 echo ${FAILED_TESTS}
 echo "Creating body ..."
 
