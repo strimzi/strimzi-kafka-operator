85fc2983e1437120bd5ee60dffd5da701e1bcaec
Lukas Kral <lukywill16@gmail.com>
2020-06-29 21:21:19 +0200

    change the timeout
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/main/java/io/strimzi/systemtest/resources/ResourceOperation.java
+++ systemtest/src/main/java/io/strimzi/systemtest/resources/ResourceOperation.java
@@ -67,6 +67,6 @@ public class ResourceOperation {
      * rollingUpdateTimeout returns a reasonable timeout in milliseconds for a number of pods in a to roll on update
      */
     public static long rollingUpdateTimeout(int numberOfPods) {
-        return Duration.ofMinutes(6).toMillis() * numberOfPods;
+        return Duration.ofMinutes(5).toMillis() * numberOfPods;
     }
 }
