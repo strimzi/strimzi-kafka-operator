10be4ade5dd4176c1e2d272f4aa721fa46e5a2a5
Jakub Stejskal <xstejs24@gmail.com>
2020-07-08 10:45:15 +0200

    fixup! Rebase fixups + change PR pipeline to allow run more tests against helm
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- test/src/main/java/io/strimzi/test/k8s/HelmClient.java
+++ test/src/main/java/io/strimzi/test/k8s/HelmClient.java
@@ -22,10 +22,13 @@ public class HelmClient {
     private static final Logger LOGGER = LogManager.getLogger(HelmClient.class);
 
     private static final String HELM_CMD = "helm";
+    private static final String HELM_3_CMD = "helm3";
     private static final String INSTALL_TIMEOUT_SECONDS = "120s";
 
     private boolean initialized;
     private String namespace;
+
+    private static String helmCommand = HELM_CMD;
 
     public HelmClient(String namespace) {
         this.namespace = namespace;
@@ -33,15 +36,6 @@ public class HelmClient {
 
     public HelmClient namespace(String namespace) {
         return new HelmClient(namespace);
-    }
-
-    /** Initialize the Helm Tiller server on the cluster */
-    public HelmClient init() {
-        if (!initialized) {
-            Exec.exec(wait(command("init", "--service-account", "tiller")));
-            initialized = true;
-        }
-        return this;
     }
 
     /** Install a chart given its local path, release name, and values to override */
@@ -65,12 +59,18 @@ public class HelmClient {
     }
 
     public boolean clientAvailable() {
-        return Exec.isExecutableOnPath(HELM_CMD);
+        if (Exec.isExecutableOnPath(HELM_CMD)) {
+            return true;
+        } else if (Exec.isExecutableOnPath(HELM_3_CMD)) {
+            helmCommand = HELM_3_CMD;
+            return true;
+        }
+        return false;
     }
 
     private List<String> command(String... rest) {
         List<String> result = new ArrayList<>();
-        result.add(HELM_CMD);
+        result.add(helmCommand);
         result.addAll(asList(rest));
         return result;
     }
