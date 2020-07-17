29b5ef6f7370f8adef14855b9382a57a1389a781
Stanislav Knot <sknot@redhat.com>
2020-07-17 08:45:02 +0200

    default conf
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- cluster-operator/scripts/cluster_operator_run.sh
+++ cluster-operator/scripts/cluster_operator_run.sh
@@ -2,5 +2,8 @@
 export JAVA_CLASSPATH=lib/io.strimzi.@project.build.finalName@.@project.packaging@:@project.dist.classpath@
 export JAVA_MAIN=io.strimzi.operator.cluster.Main
 
-export JAVA_OPTS="${JAVA_OPTS} -Dlog4j2.configurationFile=file:/opt/strimzi/custom-config/log4j2.properties"
+if [ -f /opt/strimzi/custom-config/log4j2.properties ]; then
+    # if ConfigMap was not mounted and thus this file was not created, use properties file from the classpath
+    export JAVA_OPTS="${JAVA_OPTS} -Dlog4j2.configurationFile=file:/opt/strimzi/custom-config/log4j2.properties"
+fi
 exec "${STRIMZI_HOME}/bin/launch_java.sh"
