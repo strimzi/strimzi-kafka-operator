69f6a4f219451c82b7bc26a7925ed3225d03edd1
Stanislav Knot <sknot@redhat.com>
2020-08-05 08:42:50 +0200

    Tom's comments
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- cluster-operator/scripts/cluster_operator_run.sh
+++ cluster-operator/scripts/cluster_operator_run.sh
@@ -5,5 +5,7 @@ export JAVA_MAIN=io.strimzi.operator.cluster.Main
 if [ -f /opt/strimzi/custom-config/log4j2.properties ]; then
     # if ConfigMap was not mounted and thus this file was not created, use properties file from the classpath
     export JAVA_OPTS="${JAVA_OPTS} -Dlog4j2.configurationFile=file:/opt/strimzi/custom-config/log4j2.properties"
+else
+    echo "Configuration file log4j2.properties not found. Using default static logging setting. Dynamic updates of logging configuration will not work."
 fi
 exec "${STRIMZI_HOME}/bin/launch_java.sh"
