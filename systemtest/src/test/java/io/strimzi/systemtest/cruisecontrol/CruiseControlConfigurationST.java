167cd41108f048e75f6db496f331c3d89d2512c4
Thomas Cooper <tcooper@redhat.com>
2020-08-17 18:59:38 +0100

    Upgraded Cruise Control for Kafka 2.6 support (#3516)
    Signed-off-by: Thomas Cooper <tcooper@redhat.com>

--- systemtest/src/test/java/io/strimzi/systemtest/cruisecontrol/CruiseControlConfigurationST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/cruisecontrol/CruiseControlConfigurationST.java
@@ -23,7 +23,6 @@ import io.vertx.core.json.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
@@ -52,9 +51,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @Tag(REGRESSION)
 @Tag(CRUISE_CONTROL)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//until fix of CC for Kafka version 2.6.0 and new version of CC will be released, these tests will be disabled
-//TODO: enable these tests when all issues with CC and Kafka version 2.6.0 will be resolved
-@Disabled
 public class CruiseControlConfigurationST extends AbstractST {
 
     private static final Logger LOGGER = LogManager.getLogger(CruiseControlConfigurationST.class);
