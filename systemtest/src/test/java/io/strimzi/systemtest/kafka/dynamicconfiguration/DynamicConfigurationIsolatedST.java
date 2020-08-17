d151f9c44ef7b1c761b44ff4be9cd6ededbdbbc3
morsak <xorsak02@stud.fit.vutbr.cz>
2020-08-17 19:06:54 +0200

    move
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/test/java/io/strimzi/systemtest/kafka/dynamicconfiguration/DynamicConfigurationIsolatedST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/kafka/dynamicconfiguration/DynamicConfigurationIsolatedST.java
@@ -2,7 +2,7 @@
  * Copyright Strimzi authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-package io.strimzi.systemtest.dynamicconfiguration;
+package io.strimzi.systemtest.kafka.dynamicconfiguration;
 
 import io.strimzi.api.kafka.model.KafkaClusterSpec;
 import io.strimzi.api.kafka.model.KafkaResources;
