d3e2150dc19dc53050e3568d908848fda1236fc0
Jakub Scholz <www@scholzj.com>
2020-08-19 18:52:04 +0200

    Add support for different types under one field in a CRD (#3512)
    * Add support for different types under one field in a CRD
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Add some comments
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Add possibility to deprecate Type
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Fix spotbugs
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Fix default HashSet size
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Use static ObjectMapper
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Replace @OneOfType with @Alternation and @Alternative (#4)
    
    Signed-off-by: Tom Bentley <tbentley@redhat.com>
    
    * Improve the MapOrList deserializer
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    Co-authored-by: Tom Bentley <tombentley@users.noreply.github.com>

---
+++ crd-annotations/src/main/java/io/strimzi/api/annotations/DeprecatedType.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.api.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DeprecatedType {
+    /**
+     * @return The type which should be used as replacement
+     */
+    Class<?> replacedWithType();
+}
