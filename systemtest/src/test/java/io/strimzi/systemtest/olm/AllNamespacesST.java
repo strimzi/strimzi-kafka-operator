2afd4576a61be20912c3c298eb6bf4b18bdb1fa9
Lukas Kral <lukywill16@gmail.com>
2020-08-26 08:13:17 +0200

    remove tags from olm STs
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/olm/AllNamespacesST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/olm/AllNamespacesST.java
@@ -13,11 +13,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import static io.strimzi.systemtest.Constants.BRIDGE;
-import static io.strimzi.systemtest.Constants.CONNECT;
-import static io.strimzi.systemtest.Constants.CONNECT_S2I;
-import static io.strimzi.systemtest.Constants.MIRROR_MAKER;
-import static io.strimzi.systemtest.Constants.MIRROR_MAKER2;
 import static io.strimzi.systemtest.Constants.OLM;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -45,35 +40,30 @@ public class AllNamespacesST extends OlmAbstractST {
     }
 
     @Test
-    @Tag(CONNECT)
     @Order(4)
     void testDeployExampleKafkaConnect() {
         doTestDeployExampleKafkaConnect();
     }
 
     @Test
-    @Tag(CONNECT_S2I)
     @Order(5)
     void testDeployExampleKafkaConnectS2I() {
         doTestDeployExampleKafkaConnectS2I();
     }
 
     @Test
-    @Tag(BRIDGE)
     @Order(6)
     void testDeployExampleKafkaBridge() {
         doTestDeployExampleKafkaBridge();
     }
 
     @Test
-    @Tag(MIRROR_MAKER)
     @Order(7)
     void testDeployExampleKafkaMirrorMaker() {
         doTestDeployExampleKafkaMirrorMaker();
     }
 
     @Test
-    @Tag(MIRROR_MAKER2)
     @Order(8)
     void testDeployExampleKafkaMirrorMaker2() {
         doTestDeployExampleKafkaMirrorMaker2();
