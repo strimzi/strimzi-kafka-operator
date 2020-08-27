24af306a0b3cde7160523f70b34cb2dc169661fb
Lukas Kral <lukywill16@gmail.com>
2020-08-27 13:48:52 +0200

    azure change
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- .azure/scripts/setup_upgrade.sh
+++ .azure/scripts/setup_upgrade.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-sed -i "s#:latest#:${DOCKER_TAG}#g" systemtest/src/main/resources/StrimziUpgradeST.json install/cluster-operator/050-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#:latest#:${DOCKER_TAG}#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 sed -i "s#strimzi/test-client:${DOCKER_TAG}#${DOCKER_REGISTRY}/strimzi/test-client:${DOCKER_TAG}#g" systemtest/src/main/resources/StrimziUpgradeST.json
-sed -i "s#strimzi/#${DOCKER_REGISTRY}/strimzi/#g" install/cluster-operator/050-Deployment-strimzi-cluster-operator.yaml
-sed -i "s#/opt/${DOCKER_REGISTRY}#/opt#g" install/cluster-operator/050-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#strimzi/#${DOCKER_REGISTRY}/strimzi/#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#/opt/${DOCKER_REGISTRY}#/opt#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#strimzi/#${DOCKER_ORG}/#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#strimzi/kafka:latest#${DOCKER_ORG}/kafka:latest#g" systemtest/src/main/resources/StrimziUpgradeST.json
+sed -i "s#strimzi/operator:latest#${DOCKER_ORG}/operator:latest#g" systemtest/src/main/resources/StrimziUpgradeST.json
 
-cat install/cluster-operator/050-Deployment-strimzi-cluster-operator.yaml
+cat install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 cat systemtest/src/main/resources/StrimziUpgradeST.json
