6ce99d5788293dad78808b7ce5392fc27cd66313
Lukas Kral <lukywill16@gmail.com>
2020-08-28 11:04:02 +0200

    fixup! fixup! azure change
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- .azure/scripts/setup_upgrade.sh
+++ .azure/scripts/setup_upgrade.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-sed -i "s#:latest#:${DOCKER_TAG}#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 sed -i "s#strimzi/test-client:${DOCKER_TAG}#${DOCKER_REGISTRY}/${DOCKER_ORG}/test-client:${DOCKER_TAG}#g" systemtest/src/main/resources/StrimziUpgradeST.json
+
+sed -i "s#:latest#:${DOCKER_TAG}#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 sed -i "s#strimzi/#${DOCKER_REGISTRY}/strimzi/#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 sed -i "s#/opt/${DOCKER_REGISTRY}#/opt#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
-
 sed -i "s#strimzi/kafka#${DOCKER_ORG}/kafka#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 sed -i "s#strimzi/operator#${DOCKER_ORG}/operator#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 sed -i "s#strimzi/jmxtrans#${DOCKER_ORG}/jmxtrans#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 
-sed -i "s#strimzi/kafka:latest#${DOCKER_ORG}/kafka:latest#g" systemtest/src/main/resources/StrimziUpgradeST.json
-sed -i "s#strimzi/operator:latest#${DOCKER_ORG}/operator:latest#g" systemtest/src/main/resources/StrimziUpgradeST.json
+sed -i "s#strimzi/kafka:latest#${DOCKER_REGISTRY}/${DOCKER_ORG}/kafka:${DOCKER_TAG}#g" systemtest/src/main/resources/StrimziUpgradeST.json
+sed -i "s#strimzi/operator:latest#${DOCKER_REGISTRY}/${DOCKER_ORG}/operator:${DOCKER_TAG}#g" systemtest/src/main/resources/StrimziUpgradeST.json
 
 cat install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 cat systemtest/src/main/resources/StrimziUpgradeST.json
