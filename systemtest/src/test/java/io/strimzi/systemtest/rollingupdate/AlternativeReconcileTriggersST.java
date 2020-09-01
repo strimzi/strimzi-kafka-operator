67b1646d6ada46b8e67a3a12acce5c91bb5a957e
Jakub Stejskal <xstejs24@gmail.com>
2020-09-01 16:02:36 +0200

    WIP: transactional clients
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/rollingupdate/AlternativeReconcileTriggersST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/rollingupdate/AlternativeReconcileTriggersST.java
@@ -70,6 +70,8 @@ class AlternativeReconcileTriggersST extends AbstractST {
         // Setup topic, which has 3 replicas and 2 min.isr to see if producer will be able to work during rolling update
         KafkaTopicResource.topic(CLUSTER_NAME, continuousTopicName, 3, 3, 2).done();
         String producerAdditionConfiguration = "delivery.timeout.ms=20000\nrequest.timeout.ms=20000";
+        producerAdditionConfiguration = producerAdditionConfiguration.concat("\ntransactional.id=" + continuousTopicName + ".1");
+        producerAdditionConfiguration = producerAdditionConfiguration.concat("\nenable.idempotence=true");
 
         KafkaBasicClientResource kafkaBasicClientJob = new KafkaBasicClientResource(producerName, consumerName,
             KafkaResources.plainBootstrapAddress(CLUSTER_NAME), continuousTopicName, continuousClientsMessageCount, producerAdditionConfiguration, ClientUtils.generateRandomConsumerGroup(), 1000);
@@ -228,3 +230,4 @@ class AlternativeReconcileTriggersST extends AbstractST {
         installClusterOperator(NAMESPACE, Constants.CO_OPERATION_TIMEOUT_DEFAULT);
     }
 }
+
