d878f285d13e47796063018172fea53a60c7b1c7
morsak <xorsak02@stud.fit.vutbr.cz>
2020-10-08 15:12:59 +0200

    tracing fix
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/test/java/io/strimzi/systemtest/tracing/TracingST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/tracing/TracingST.java
@@ -842,6 +842,7 @@ public class TracingST extends AbstractST {
             .withNamespaceName(NAMESPACE)
             .withClusterName(CLUSTER_NAME)
             .withMessageCount(MESSAGE_COUNT)
+            .withListenerName(Constants.PLAIN_LISTENER_DEFAULT_NAME)
             .build();
 
         internalKafkaClient.checkProducedAndConsumedMessages(
