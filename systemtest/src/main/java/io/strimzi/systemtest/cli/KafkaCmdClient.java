8049e8de459197cb5c86e5db1872834186f956de
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2020-10-16 13:35:32 +0200

    change deprecated --zookeeper to --bootstrap-server in cmd client and user test (#3821)
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/main/java/io/strimzi/systemtest/cli/KafkaCmdClient.java
+++ systemtest/src/main/java/io/strimzi/systemtest/cli/KafkaCmdClient.java
@@ -18,20 +18,20 @@ import static io.strimzi.test.k8s.KubeClusterResource.kubeClient;
 
 public class KafkaCmdClient {
 
-    private static final int PORT = 12181;
+    private static final int PORT = 9092;
 
     public KafkaCmdClient() { }
 
-    public static List<String> listTopicsUsingPodCli(String clusterName, int zkPodId) {
-        String podName = KafkaResources.zookeeperPodName(clusterName, zkPodId);
+    public static List<String> listTopicsUsingPodCli(String clusterName, int kafkaPodId) {
+        String podName = KafkaResources.kafkaPodName(clusterName, kafkaPodId);
         return Arrays.asList(cmdKubeClient().execInPod(podName, "/bin/bash", "-c",
-            "bin/kafka-topics.sh --list --zookeeper localhost:" + PORT).out().split("\\s+"));
+            "bin/kafka-topics.sh --list --bootstrap-server localhost:" + PORT).out().split("\\s+"));
     }
 
-    public static String createTopicUsingPodCli(String clusterName, int zkPodId, String topic, int replicationFactor, int partitions) {
-        String podName = KafkaResources.zookeeperPodName(clusterName, zkPodId);
+    public static String createTopicUsingPodCli(String clusterName, int kafkaPodId, String topic, int replicationFactor, int partitions) {
+        String podName = KafkaResources.kafkaPodName(clusterName, kafkaPodId);
         String response = cmdKubeClient().execInPod(podName, "/bin/bash", "-c",
-            "bin/kafka-topics.sh --zookeeper localhost:" + PORT + " --create " + " --topic " + topic +
+            "bin/kafka-topics.sh --bootstrap-server localhost:" + PORT + " --create " + " --topic " + topic +
                 " --replication-factor " + replicationFactor + " --partitions " + partitions).out();
 
         KafkaTopicUtils.waitForKafkaTopicCreation(topic);
@@ -42,21 +42,21 @@ public class KafkaCmdClient {
         return response;
     }
 
-    public static String deleteTopicUsingPodCli(String clusterName, int zkPodId, String topic) {
-        String podName = KafkaResources.zookeeperPodName(clusterName, zkPodId);
+    public static String deleteTopicUsingPodCli(String clusterName, int kafkaPodId, String topic) {
+        String podName = KafkaResources.kafkaPodName(clusterName, kafkaPodId);
         return cmdKubeClient().execInPod(podName, "/bin/bash", "-c",
-            "bin/kafka-topics.sh --zookeeper localhost:" + PORT + " --delete --topic " + topic).out();
+            "bin/kafka-topics.sh --bootstrap-server localhost:" + PORT + " --delete --topic " + topic).out();
     }
 
-    public static List<String> describeTopicUsingPodCli(String clusterName, int zkPodId, String topic) {
-        String podName = KafkaResources.zookeeperPodName(clusterName, zkPodId);
+    public static List<String> describeTopicUsingPodCli(String clusterName, int kafkaPodId, String topic) {
+        String podName = KafkaResources.kafkaPodName(clusterName, kafkaPodId);
         return Arrays.asList(cmdKubeClient().execInPod(podName, "/bin/bash", "-c",
-            "bin/kafka-topics.sh --zookeeper localhost:" + PORT + " --describe --topic " + topic).out().replace(": ", ":").split("\\s+"));
+            "bin/kafka-topics.sh --bootstrap-server localhost:" + PORT + " --describe --topic " + topic).out().replace(": ", ":").split("\\s+"));
     }
 
-    public static String updateTopicPartitionsCountUsingPodCli(String clusterName, int zkPodId, String topic, int partitions) {
-        String podName = KafkaResources.zookeeperPodName(clusterName, zkPodId);
+    public static String updateTopicPartitionsCountUsingPodCli(String clusterName, int kafkaPodId, String topic, int partitions) {
+        String podName = KafkaResources.kafkaPodName(clusterName, kafkaPodId);
         return cmdKubeClient().execInPod(podName, "/bin/bash", "-c",
-            "bin/kafka-topics.sh --zookeeper localhost:" + PORT + " --alter --topic " + topic + " --partitions " + partitions).out();
+            "bin/kafka-topics.sh --bootstrap-server localhost:" + PORT + " --alter --topic " + topic + " --partitions " + partitions).out();
     }
 }
