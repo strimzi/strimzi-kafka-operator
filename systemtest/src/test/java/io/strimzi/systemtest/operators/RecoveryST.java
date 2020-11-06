8f2ed2f6cff7e1396dedd922bee9b1665ae78261
Lukas Kral <lukywill16@gmail.com>
2020-11-06 10:42:22 +0100

    change javadoc again
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/operators/RecoveryST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/operators/RecoveryST.java
@@ -12,6 +12,7 @@ import io.strimzi.api.kafka.model.KafkaResources;
 import io.strimzi.systemtest.AbstractST;
 import io.strimzi.systemtest.Constants;
 import io.strimzi.systemtest.resources.crd.KafkaClientsResource;
+import io.strimzi.systemtest.rollingupdate.KafkaRollerST;
 import io.strimzi.systemtest.utils.kafkaUtils.KafkaUtils;
 import io.strimzi.systemtest.utils.kubeUtils.controllers.ConfigMapUtils;
 import io.strimzi.systemtest.utils.kubeUtils.controllers.DeploymentUtils;
@@ -238,13 +239,15 @@ class RecoveryST extends AbstractST {
     }
 
     /**
-     * The main difference between this test and {@link io.strimzi.systemtest.rollingupdate.KafkaRollerST#testKafkaPodPending() testKafkaPodPending}
+     * The main difference between this test and KafkaRollerST#testKafkaPodPending()
      * is that in this test, we are deploying Kafka cluster with an impossible memory request,
-     * but in the {@link io.strimzi.systemtest.rollingupdate.KafkaRollerST#testKafkaPodPending() testKafkaPodPending}
+     * but in the KafkaRollerST#testKafkaPodPending()
      * we first deploy Kafka cluster with a correct configuration, then change the configuration to an unschedulable one, waiting
      * for one Kafka pod to be in the `Pending` phase. In this test, all 3 Kafka pods are `Pending`. After we
      * check that Kafka pods are stable in `Pending` phase (for one minute), we change the memory request so that the pods are again schedulable
      * and wait until the Kafka cluster recovers and becomes `Ready`.
+     *
+     * @see KafkaRollerST#testKafkaPodPending()
      */
     @Test
     void testRecoveryFromImpossibleMemoryRequest() {
