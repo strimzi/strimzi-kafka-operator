5d368c8a039a6b3cfee002afb697107495e5a446
David Kornel <kornys@outlook.com>
2020-12-02 11:30:32 +0100

    Log selected test classes for test run (#4032)
    Signed-off-by: kornys <kornys@outlook.com>

--- systemtest/src/main/java/io/strimzi/systemtest/listeners/ExecutionListener.java
+++ systemtest/src/main/java/io/strimzi/systemtest/listeners/ExecutionListener.java
@@ -6,13 +6,41 @@ package io.strimzi.systemtest.listeners;
 
 import io.strimzi.systemtest.Environment;
 import io.strimzi.test.timemeasuring.TimeMeasuringSystem;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
-public class ExecutionListener implements TestExecutionListener {
+import java.util.Arrays;
 
-    @Override
+public class ExecutionListener implements TestExecutionListener {
+    private static final Logger LOGGER = LogManager.getLogger(TestExecutionListener.class);
+
+    public void testPlanExecutionStarted(TestPlan testPlan) {
+        LOGGER.info("=======================================================================");
+        LOGGER.info("=======================================================================");
+        LOGGER.info("                        Test run started");
+        LOGGER.info("=======================================================================");
+        LOGGER.info("=======================================================================");
+        printSelectedTestClasses(testPlan);
+    }
+
     public void testPlanExecutionFinished(TestPlan testPlan) {
         TimeMeasuringSystem.getInstance().printAndSaveResults(Environment.TEST_LOG_DIR);
+        LOGGER.info("=======================================================================");
+        LOGGER.info("=======================================================================");
+        LOGGER.info("                        Test run finished");
+        LOGGER.info("=======================================================================");
+        LOGGER.info("=======================================================================");
+    }
+
+    private void printSelectedTestClasses(TestPlan plan) {
+        LOGGER.info("Following testclasses are selected for run:");
+        Arrays.asList(plan.getChildren(plan.getRoots()
+                .toArray(new TestIdentifier[0])[0])
+                .toArray(new TestIdentifier[0])).forEach(testIdentifier -> LOGGER.info("-> {}", testIdentifier.getLegacyReportingName()));
+        LOGGER.info("=======================================================================");
+        LOGGER.info("=======================================================================");
     }
 }
