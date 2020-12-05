ed64445f4d2c53a49c032be6a56639948b91f46b
Jakub Scholz <www@scholzj.com>
2020-12-05 02:05:00 +0100

    Remove Java 8 support from our shell scripts
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- docker-images/kafka/scripts/set_kafka_gc_options.sh
+++ docker-images/kafka/scripts/set_kafka_gc_options.sh
@@ -4,12 +4,7 @@ set -e
 # expand gc options based upon java version
 function get_gc_opts {
   # The first segment of the version number is '1' for releases < 9; then '9', '10', '11', ...
-  JAVA_MAJOR_VERSION=$(java -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
-  if [ "$JAVA_MAJOR_VERSION" -ge "9" ] ; then
-    echo "-Xlog:gc*:stdout:time"
-  else
-    echo "-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps"
-  fi
+  echo "-Xlog:gc*:stdout:time"
 }
 
 if [ "${STRIMZI_KAFKA_GC_LOG_ENABLED}" == "true" ]; then
--- docker-images/test-client/scripts/launch_java.sh
+++ docker-images/test-client/scripts/launch_java.sh
@@ -8,13 +8,7 @@ shift
 # expand gc options based upon java version
 function get_gc_opts {
   if [ "${STRIMZI_GC_LOG_ENABLED}" == "true" ]; then
-    # The first segment of the version number is '1' for releases < 9; then '9', '10', '11', ...
-    JAVA_MAJOR_VERSION=$(java -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
-    if [ "$JAVA_MAJOR_VERSION" -ge "9" ] ; then
-      echo "-Xlog:gc*:stdout:time -XX:NativeMemoryTracking=summary"
-    else
-      echo "-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:NativeMemoryTracking=summary"
-    fi
+    echo "-Xlog:gc*:stdout:time -XX:NativeMemoryTracking=summary"
   else
     # no gc options
     echo ""
@@ -35,9 +29,6 @@ JAVA_OPTS="${JAVA_OPTS} -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/
 JAVA_OPTS="${JAVA_OPTS} $(get_gc_opts)"
 
 # Deny illegal access option is supported only on Java 9 and higher
-JAVA_MAJOR_VERSION=$(java -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
-if [ "$JAVA_MAJOR_VERSION" -ge "9" ] ; then
-  JAVA_OPTS="${JAVA_OPTS} --illegal-access=deny"
-fi
+JAVA_OPTS="${JAVA_OPTS} --illegal-access=deny"
 
 exec java $JAVA_OPTS -jar $JAR $@
