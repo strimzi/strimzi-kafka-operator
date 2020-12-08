6595839fad7e992fc187843ffdb3dc4047d10e12
Lukas Kral <lukywill16@gmail.com>
2020-12-08 22:06:38 +0100

    fixes for failing tests after my changes
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/specific/SpecificST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/specific/SpecificST.java
@@ -307,7 +307,7 @@ public class SpecificST extends AbstractST {
     @Tag(REGRESSION)
     void testDeployUnsupportedKafka() {
         String nonExistingVersion = "6.6.6";
-        String nonExistingVersionMessage = "Version " + nonExistingVersion + " is not supported. Supported versions are.*";
+        String nonExistingVersionMessage = "Version " + nonExistingVersion + " is not supported. Supported versions are";
 
         KafkaResource.kafkaWithoutWait(KafkaResource.defaultKafka(CLUSTER_NAME, 1, 1)
             .editSpec()
