79fae812509a07d2dc889a1c4c3d35095c6ee6b5
Paolo Patierno <ppatierno@live.com>
2021-01-11 17:12:23 +0100

    Removed ZooKeeper dependency and used Admin Client for user quotas
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    Excluded REC_CATCH_EXCEPTION which doesn't make much sense
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    Fixed check user quotas deleted in ZooKeeper
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- user-operator/src/main/java/io/strimzi/operator/user/operator/KafkaUserQuotasOperator.java
+++ user-operator/src/main/java/io/strimzi/operator/user/operator/KafkaUserQuotasOperator.java
@@ -4,36 +4,34 @@
  */
 package io.strimzi.operator.user.operator;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.fabric8.zjsonpatch.JsonDiff;
 import io.strimzi.api.kafka.model.KafkaUserQuotas;
 import io.strimzi.operator.common.operator.resource.ReconcileResult;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
-import org.I0Itec.zkclient.ZkClient;
-import org.I0Itec.zkclient.serialize.BytesPushThroughSerializer;
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.common.quota.ClientQuotaAlteration;
+import org.apache.kafka.common.quota.ClientQuotaFilter;
+import org.apache.kafka.common.quota.ClientQuotaFilterComponent;
+import org.apache.kafka.common.quota.ClientQuotaEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class KafkaUserQuotasOperator {
     private static final Logger log = LogManager.getLogger(KafkaUserQuotasOperator.class.getName());
 
-    private final static int CONNECTION_TIMEOUT = 30_000;
+    private final Vertx vertx;
+    private final Admin adminClient;
 
-    private ZkClient zkClient;
-    private Vertx vertx;
-
-    public KafkaUserQuotasOperator(Vertx vertx, String zookeeperUrl, int zookeeperSessionTimeout) {
-        this.zkClient = new ZkClient(zookeeperUrl, zookeeperSessionTimeout, CONNECTION_TIMEOUT, new BytesPushThroughSerializer());
+    public KafkaUserQuotasOperator(Vertx vertx, Admin adminClient) {
         this.vertx = vertx;
+        this.adminClient = adminClient;
     }
 
     Future<ReconcileResult<KafkaUserQuotas>> reconcile(String username, KafkaUserQuotas quotas) {
@@ -69,130 +67,22 @@ public class KafkaUserQuotasOperator {
      *
      * @param username The name of the user which should be created or updated
      * @param quotas The desired user quotas
+     * @throws Exception when altering quotas fails
      */
-    public void createOrUpdate(String username, KafkaUserQuotas quotas) {
-        String encodedUsername = encodeUsername(username);
-
-        byte[] data = zkClient.readData("/config/users/" + encodedUsername, true);
-
-        if (data != null)   {
+    public void createOrUpdate(String username, KafkaUserQuotas quotas) throws Exception {
+        KafkaUserQuotas current = describeUserQuotas(username);
+        if (current != null) {
             log.debug("Checking quota updates for user {}", username);
-            JsonNode diff = null;
-
-            try {
-                ObjectMapper objectMapper = new ObjectMapper();
-                diff = JsonDiff.asJson(objectMapper.readTree(data), objectMapper.readTree(createOrUpdateUserJson(data, quotas)));
-            } catch (IOException e) {
-                log.error("Failed to diff user configuration for user {}", username, e);
-            }
-
-            if (diff != null && diff.size() > 0) {
+            if (!quotasEquals(current, quotas)) {
                 log.debug("Updating quotas for user {}", username);
-                zkClient.writeData("/config/users/" + encodedUsername, createOrUpdateUserJson(data, quotas));
-                notifyChanges(username);
+                alterUserQuotas(username, toClientQuotaAlterationOps(quotas));
             } else {
                 log.debug("Nothing to update in quotas for user {}", username);
             }
         } else {
             log.debug("Creating quotas for user {}", username);
-            ensurePath("/config/users");
-            zkClient.createPersistent("/config/users/" + encodedUsername, createUserJson(quotas));
-            notifyChanges(username);
+            alterUserQuotas(username, toClientQuotaAlterationOps(quotas));
         }
-    }
-
-    /**
-     * Generates the JSON with the credentials
-     *
-     * @param quotas  quotas
-     * @return  Returns the generated JSON as byte array
-     */
-    protected byte[] createUserJson(KafkaUserQuotas quotas)   {
-        return createOrUpdateUserJson(null, quotas);
-    }
-
-    /**
-     * Updates the quotas in existing JSON. If the existing JSON is null, new JSON will be created.
-     *
-     * @param user user configuration
-     * @param quotas  quotas
-     *
-     * @return  Returns the updated JSON as byte array
-     */
-    protected byte[] createOrUpdateUserJson(byte[] user, KafkaUserQuotas quotas)   {
-        JsonObject json;
-
-        if (user != null) {
-            json = new JsonObject(new String(user, StandardCharsets.UTF_8));
-            validateJsonVersion(json);
-        } else {
-            json = new JsonObject()
-                    .put("version", 1)
-                    .put("config", new JsonObject());
-        }
-
-        JsonObject config = json.getJsonObject("config", new JsonObject());
-
-        if (quotas != null && quotas.getProducerByteRate() != null) {
-            config.put("producer_byte_rate", quotas.getProducerByteRate().toString());
-        } else {
-            if (config.getString("producer_byte_rate") != null) {
-                config.remove("producer_byte_rate");
-            }
-        }
-
-        if (quotas != null && quotas.getConsumerByteRate() != null) {
-            config.put("consumer_byte_rate", quotas.getConsumerByteRate().toString());
-        } else {
-            if (config.getString("consumer_byte_rate") != null) {
-                config.remove("consumer_byte_rate");
-            }
-        }
-
-        if (quotas != null && quotas.getRequestPercentage() != null) {
-            config.put("request_percentage", quotas.getRequestPercentage().toString());
-        } else {
-            if (config.getString("request_percentage") != null) {
-                config.remove("request_percentage");
-            }
-        }
-
-        json.put("config", config);
-
-        return json.encode().getBytes(StandardCharsets.UTF_8);
-    }
-
-    /**
-     * This notifies Kafka about the changes we have made
-     *
-     * @param username  Name of the user whose configuration changed
-     */
-    private void notifyChanges(String username) {
-        String encodedUsername = encodeUsername(username);
-
-        log.debug("Notifying changes for user {}", username);
-
-        ensurePath("/config/changes");
-
-        JsonObject json = new JsonObject().put("version", 2).put("entity_path", "users/" + encodedUsername);
-        zkClient.createPersistentSequential("/config/changes/config_change_", json.encode().getBytes(StandardCharsets.UTF_8));
-    }
-
-    /**
-     * Ensures that the path in Zookeeper exists.
-     * It checks whether it already exists and in case it doesn't, it will create the path.
-     *
-     * @param path The Zookeeper path which should exist
-     */
-    private void ensurePath(String path)    {
-        if (!zkClient.exists(path))   {
-            zkClient.createPersistent(path, true);
-        }
-    }
-
-    /* test */
-    boolean isPathExist(String path)    {
-        return zkClient.exists(path);
     }
 
     /**
@@ -202,34 +92,8 @@ public class KafkaUserQuotasOperator {
      *
      * @return True if the user exists
      */
-    boolean exists(String username) {
-        String encodedUsername = encodeUsername(username);
-
-        byte[] data = zkClient.readData("/config/users/" + encodedUsername, true);
-
-        if (data != null)   {
-            String jsonString = new String(data, StandardCharsets.UTF_8);
-            JsonObject json = new JsonObject(jsonString);
-            validateJsonVersion(json);
-            JsonObject config = json.getJsonObject("config");
-
-            if (config != null) {
-                String prod = config.getString("producer_byte_rate");
-                String cons = config.getString("consumer_byte_rate");
-                String perc = config.getString("request_percentage");
-
-                if (prod != null || cons != null || perc != null) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean configJsonIsEmpty(JsonObject json) {
-        validateJsonVersion(json);
-        JsonObject config = json.getJsonObject("config");
-        return config.isEmpty();
+    boolean exists(String username) throws Exception {
+        return describeUserQuotas(username) != null;
     }
 
     /**
@@ -237,84 +101,96 @@ public class KafkaUserQuotasOperator {
      * It is not an error if the user doesn't exist, or doesn't currently have any quotas.
      *
      * @param username Name of the user
+     * @throws Exception when altering quotas fails
      */
-    public void delete(String username) {
-        String encodedUsername = encodeUsername(username);
-
-        byte[] data = zkClient.readData("/config/users/" + encodedUsername, true);
-
-        if (data != null)   {
+    public void delete(String username) throws Exception {
+        KafkaUserQuotas current = describeUserQuotas(username);
+        if (current != null) {
             log.debug("Deleting quotas for user {}", username);
-            JsonObject deleteJson = removeQuotasFromJsonUser(data);
-            if (configJsonIsEmpty(deleteJson)) {
-                zkClient.deleteRecursive("/config/users/" + encodedUsername);
-                log.debug("User {} deleted from ZK store", username);
-            } else {
-                zkClient.writeData("/config/users/" + encodedUsername, deleteJson.toBuffer().getBytes());
-            }
-            notifyChanges(username);
+            current.setProducerByteRate(null);
+            current.setConsumerByteRate(null);
+            current.setRequestPercentage(null);
+            alterUserQuotas(username, toClientQuotaAlterationOps(current));
         } else {
             log.warn("Quotas for user {} already don't exist", username);
         }
     }
 
-    /**
-     * Deletes the quotas from existing JSON
-     *
-     * @param userConfig JSON string with existing userConfig configuration as byte[]
-     *
-     * @return  Returns the updated JSON without the quotas
-     */
-    protected JsonObject removeQuotasFromJsonUser(byte[] userConfig)   {
-        JsonObject json = new JsonObject(new String(userConfig, StandardCharsets.UTF_8));
-
-        validateJsonVersion(json);
-        JsonObject config = json.getJsonObject("config");
-        if (config == null) {
-            json.put("config", new JsonObject());
-        } else {
-            if (config.getString("producer_byte_rate") != null) {
-                config.remove("producer_byte_rate");
-            }
-            if (config.getString("consumer_byte_rate") != null) {
-                config.remove("consumer_byte_rate");
-            }
-            if (config.getString("request_percentage") != null) {
-                config.remove("request_percentage");
-            }
-        }
-        return json;
-    }
-
-    protected void validateJsonVersion(JsonObject json) {
-        if (json.getInteger("version") != 1) {
-            throw new RuntimeException("Failed to validate the user JSON. The version is missing or has an invalid value.");
-        }
-    }
-
-    protected JsonObject getQuotas(String username) {
-        String encodedUsername = encodeUsername(username);
-
-        byte[] data = zkClient.readData("/config/users/" + encodedUsername, true);
-
-        if (data != null) {
-            String jsonString = new String(data, StandardCharsets.UTF_8);
-            JsonObject json = new JsonObject(jsonString);
-            return json;
-        } else return null;
-    }
-
-    /**
-     * Encodes the username with URL Encoder
-     *
-     * @param username  Username which should be encoded
-     * @return          Encoded username
-     */
-    protected static String encodeUsername(String username) {
+    protected void alterUserQuotas(String username, Set<ClientQuotaAlteration.Op> ops) throws Exception {
+        ClientQuotaEntity cqe = new ClientQuotaEntity(Collections.singletonMap(ClientQuotaEntity.USER, username));
+        ClientQuotaAlteration cqa = new ClientQuotaAlteration(cqe, ops);
         try {
-            return URLEncoder.encode(username, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("Failed to encode username", e);
+            adminClient.alterClientQuotas(Collections.singleton(cqa)).all().get();
+        } catch (Exception e) {
+            log.error("Creating/Altering quotas for user {} failed", username, e);
+            throw e;
         }
+    }
+
+    protected KafkaUserQuotas describeUserQuotas(String username) throws Exception {
+        ClientQuotaFilterComponent c = ClientQuotaFilterComponent.ofEntity(ClientQuotaEntity.USER, username);
+        ClientQuotaFilter f =  ClientQuotaFilter.contains(Collections.singleton(c));
+        KafkaUserQuotas current = null;
+        try {
+            ClientQuotaEntity cqe = new ClientQuotaEntity(Collections.singletonMap(ClientQuotaEntity.USER, username));
+            Map<ClientQuotaEntity, Map<String, Double>> map = adminClient.describeClientQuotas(f).entities().get();
+            if (map.containsKey(cqe)) {
+                current = fromClientQuota(map.get(cqe));
+            }
+        } catch (Exception e) {
+            log.error("Getting quotas for user {} failed", username, e);
+            throw e;
+        }
+        return current;
+    }
+
+    /**
+     * Returns a KafkaUserQuotas instance from a map of quotas key-value pairs
+     *
+     * @param map map of quotas key-value pairs
+     * @return KafkaUserQuotas instance
+     */
+    protected KafkaUserQuotas fromClientQuota(Map<String, Double> map) {
+        KafkaUserQuotas kuq = new KafkaUserQuotas();
+        if (map.containsKey("producer_byte_rate")) {
+            kuq.setProducerByteRate(map.get("producer_byte_rate").intValue());
+        }
+        if (map.containsKey("consumer_byte_rate")) {
+            kuq.setConsumerByteRate(map.get("consumer_byte_rate").intValue());
+        }
+        if (map.containsKey("request_percentage")) {
+            kuq.setRequestPercentage(map.get("request_percentage").intValue());
+        }
+        return kuq;
+    }
+
+    /**
+     * Map a KafkaUserQuotas instance to a corresponding set of ClientQuotaAlteration operations for the Admin Client
+     *
+     * @param quotas KafkaUserQuotas instance to map
+     * @return ClientQuotaAlteration operations for the Admin Client
+     */
+    protected Set<ClientQuotaAlteration.Op> toClientQuotaAlterationOps(KafkaUserQuotas quotas) {
+        Set<ClientQuotaAlteration.Op> ops = new HashSet<>(3);
+        ops.add(new ClientQuotaAlteration.Op("producer_byte_rate",
+                quotas.getProducerByteRate() != null ? Double.valueOf(quotas.getProducerByteRate()) : null));
+        ops.add(new ClientQuotaAlteration.Op("consumer_byte_rate",
+                quotas.getConsumerByteRate() != null ? Double.valueOf(quotas.getConsumerByteRate()) : null));
+        ops.add(new ClientQuotaAlteration.Op("request_percentage",
+                quotas.getRequestPercentage() != null ? Double.valueOf(quotas.getRequestPercentage()) : null));
+        return ops;
+    }
+
+    /**
+     * Check if two KafkaUserQuotas instances are equal
+     *
+     * @param kuq1 first instance to compare
+     * @param kuq2 second instance to compare
+     * @return true if they are equals, false otherwise
+     */
+    private boolean quotasEquals(KafkaUserQuotas kuq1, KafkaUserQuotas kuq2) {
+        return Objects.equals(kuq1.getProducerByteRate(), kuq2.getProducerByteRate()) &&
+                Objects.equals(kuq1.getConsumerByteRate(), kuq2.getConsumerByteRate()) &&
+                Objects.equals(kuq1.getRequestPercentage(), kuq2.getRequestPercentage());
     }
 }
