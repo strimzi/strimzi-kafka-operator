831cce161403883170ee1b84e95f4464b60322a2
Lukas Kral <lukywill16@gmail.com>
2021-01-21 11:12:30 +0100

    J&J comments
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/connect/ConnectBuilderST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/connect/ConnectBuilderST.java
@@ -21,6 +21,7 @@ import io.strimzi.systemtest.AbstractST;
 import io.strimzi.systemtest.Constants;
 import io.strimzi.systemtest.annotations.OpenShiftOnly;
 import io.strimzi.systemtest.kafkaclients.internalClients.InternalKafkaClient;
+import io.strimzi.systemtest.resources.KubernetesResource;
 import io.strimzi.systemtest.resources.ResourceManager;
 import io.strimzi.systemtest.resources.crd.KafkaClientsResource;
 import io.strimzi.systemtest.resources.crd.KafkaConnectResource;
@@ -61,7 +62,18 @@ class ConnectBuilderST extends AbstractST {
     private static final Logger LOGGER = LogManager.getLogger(ConnectBuilderST.class);
 
     private static final String ECHO_SINK_CLASS_NAME = "cz.scholz.kafka.connect.echosink.EchoSinkConnector";
-    private static final String CAMEL_TELEGRAM_CLASS_NAME = "org.apache.camel.kafkaconnector.telegram.CamelTelegramSinkConnector";
+    private static final String CAMEL_CONNECTOR_CLASS_NAME = "org.apache.camel.kafkaconnector.http.CamelHttpSinkConnector";
+
+    private static final String ECHO_SINK_JAR_URL = "https://github.com/scholzj/echo-sink/releases/download/1.1.0/echo-sink-1.1.0.jar";
+    private static final String ECHO_SINK_TGZ_URL = "https://github.com/scholzj/echo-sink/archive/1.1.0.tar.gz";
+
+    private static final String CAMEL_CONNECTOR_TGZ_URL = "https://repo.maven.apache.org/maven2/org/apache/camel/kafkaconnector/camel-http-kafka-connector/0.7.0/camel-http-kafka-connector-0.7.0-package.tar.gz";
+
+    private static final String ECHO_SINK_JAR_CHECKSUM = "b7da48d5ecd1e4199886d169ced1bf702ffbdfd704d69e0da97e78ff63c1bcece2f59c2c6c751f9c20be73472b8cb6a31b6fd4f75558c1cb9d96daa9e9e603d2";
+    private static final String ECHO_SINK_TGZ_CHECKSUM = "5318b1f031d4e5eeab6f8b774c76de297237574fc51d1e81b03a10e0b5d5435a46a108b85fdb604c644529f38830ae83239c17b6ec91c90a60ac790119bb2950";
+    private static final String ECHO_SINK_JAR_WRONG_CHECKSUM = "f1f167902325062efc8c755647bc1b782b2b067a87a6e507ff7a3f6205803220";
+
+    private static final String CAMEL_CONNECTOR_TGZ_CHECKSUM = "d0bb8c6a9e50b68eee3e4d70b6b7e5ae361373883ed3156bc11771330095b66195ac1c12480a0669712da4e5f38e64f004ffecabca4bf70d312f3f7ae0ad51b5";
 
     private String imageName = "";
 
@@ -69,12 +81,12 @@ class ConnectBuilderST extends AbstractST {
         .withName("connector-with-tar-and-jar")
         .withArtifacts(
             new JarArtifactBuilder()
-                .withNewUrl("https://github.com/scholzj/echo-sink/releases/download/1.1.0/echo-sink-1.1.0.jar")
-                .withNewSha512sum("b7da48d5ecd1e4199886d169ced1bf702ffbdfd704d69e0da97e78ff63c1bcece2f59c2c6c751f9c20be73472b8cb6a31b6fd4f75558c1cb9d96daa9e9e603d2")
+                .withNewUrl(ECHO_SINK_JAR_URL)
+                .withNewSha512sum(ECHO_SINK_JAR_CHECKSUM)
                 .build(),
             new TgzArtifactBuilder()
-                .withNewUrl("https://github.com/scholzj/echo-sink/archive/1.1.0.tar.gz")
-                .withNewSha512sum("5318b1f031d4e5eeab6f8b774c76de297237574fc51d1e81b03a10e0b5d5435a46a108b85fdb604c644529f38830ae83239c17b6ec91c90a60ac790119bb2950")
+                .withNewUrl(ECHO_SINK_TGZ_URL)
+                .withNewSha512sum(ECHO_SINK_TGZ_CHECKSUM)
                 .build())
         .build();
 
@@ -83,12 +95,18 @@ class ConnectBuilderST extends AbstractST {
         Plugin pluginWithWrongChecksum = new PluginBuilder()
             .withName("connector-with-empty-checksum")
             .withArtifacts(new JarArtifactBuilder()
-                .withNewUrl("https://github.com/scholzj/echo-sink/releases/download/1.1.0/echo-sink-1.1.0.jar")
-                .withNewSha512sum("ads")
+                .withNewUrl(ECHO_SINK_JAR_URL)
+                .withNewSha512sum(ECHO_SINK_JAR_WRONG_CHECKSUM)
                 .build())
             .build();
 
+        KafkaClientsResource.create(KafkaClientsResource.deployKafkaClients(false, kafkaClientsName).build());
+        String kafkaClientsPodName = kubeClient().listPodsByPrefixInName(kafkaClientsName).get(0).getMetadata().getName();
+
         KafkaConnectResource.kafkaConnectWithoutWait(KafkaConnectResource.kafkaConnect(clusterName, 1)
+            .editMetadata()
+                .addToAnnotations(Annotations.STRIMZI_IO_USE_CONNECTOR_RESOURCES, "true")
+            .endMetadata()
             .editOrNewSpec()
                 .withNewBuild()
                     .withPlugins(pluginWithWrongChecksum)
@@ -103,27 +121,40 @@ class ConnectBuilderST extends AbstractST {
         KafkaConnectUtils.waitUntilKafkaConnectStatusConditionContainsMessage(clusterName, NAMESPACE, "The Kafka Connect build .* failed");
 
         LOGGER.info("Checking if KafkaConnect status condition contains message about build failure");
-        KafkaConnect connect = KafkaConnectResource.kafkaConnectClient().inNamespace(NAMESPACE).withName(clusterName).get();
-        Condition connectCondition = connect.getStatus().getConditions().stream().findFirst().get();
+        KafkaConnect kafkaConnect = KafkaConnectResource.kafkaConnectClient().inNamespace(NAMESPACE).withName(clusterName).get();
+
+        LOGGER.info("Deploying network policies for KafkaConnect");
+        KubernetesResource.deployNetworkPolicyForResource(kafkaConnect, KafkaConnectResources.deploymentName(clusterName));
+
+        Condition connectCondition = kafkaConnect.getStatus().getConditions().stream().findFirst().get();
 
         assertTrue(connectCondition.getMessage().matches("The Kafka Connect build .* failed"));
         assertThat(connectCondition.getType(), is(NotReady.toString()));
 
         LOGGER.info("Replacing plugin's checksum with right one");
-        KafkaConnectResource.replaceKafkaConnectResource(clusterName, kafkaConnect -> {
+        KafkaConnectResource.replaceKafkaConnectResource(clusterName, kC -> {
             Plugin pluginWithRightChecksum = new PluginBuilder()
                 .withName("connector-with-empty-checksum")
                 .withArtifacts(new JarArtifactBuilder()
-                    .withNewUrl("https://github.com/scholzj/echo-sink/releases/download/1.1.0/echo-sink-1.1.0.jar")
-                    .withNewSha512sum("b7da48d5ecd1e4199886d169ced1bf702ffbdfd704d69e0da97e78ff63c1bcece2f59c2c6c751f9c20be73472b8cb6a31b6fd4f75558c1cb9d96daa9e9e603d2")
+                    .withNewUrl(ECHO_SINK_JAR_URL)
+                    .withNewSha512sum(ECHO_SINK_JAR_CHECKSUM)
                     .build())
                 .build();
 
-            kafkaConnect.getSpec().getBuild().getPlugins().remove(0);
-            kafkaConnect.getSpec().getBuild().getPlugins().add(pluginWithRightChecksum);
+            kC.getSpec().getBuild().getPlugins().remove(0);
+            kC.getSpec().getBuild().getPlugins().add(pluginWithRightChecksum);
         });
 
         KafkaConnectUtils.waitForConnectReady(clusterName);
+
+        LOGGER.info("Checking if KafkaConnect API contains EchoSink connector");
+        String plugins = cmdKubeClient().execInPod(kafkaClientsPodName, "curl", "-X", "GET", "http://" + KafkaConnectResources.serviceName(clusterName) + ":8083/connector-plugins").out();
+
+        assertTrue(plugins.contains(ECHO_SINK_CLASS_NAME));
+
+        LOGGER.info("Checking if KafkaConnect resource contains EchoSink connector in status");
+        kafkaConnect = KafkaConnectResource.kafkaConnectClient().inNamespace(NAMESPACE).withName(clusterName).get();
+        assertTrue(kafkaConnect.getStatus().getConnectorPlugins().stream().anyMatch(connectorPlugin -> connectorPlugin.getConnectorClass().contains(ECHO_SINK_CLASS_NAME)));
     }
 
     @Test
@@ -201,6 +232,9 @@ class ConnectBuilderST extends AbstractST {
         kubeClient().getClient().adapt(OpenShiftClient.class).imageStreams().create(imageStream);
 
         KafkaConnectResource.create(KafkaConnectResource.kafkaConnect(clusterName, 1)
+            .editMetadata()
+                .addToAnnotations(Annotations.STRIMZI_IO_USE_CONNECTOR_RESOURCES, "true")
+            .endMetadata()
             .editOrNewSpec()
                 .withNewBuild()
                     .withPlugins(PLUGIN_WITH_TAR_AND_JAR)
@@ -218,6 +252,9 @@ class ConnectBuilderST extends AbstractST {
         assertThat(kafkaConnect.getSpec().getBuild().getOutput().getType(), is("imagestream"));
         assertThat(kafkaConnect.getSpec().getBuild().getOutput().getImage(), is(imageStreamName + ":latest"));
         assertThat(kafkaConnect.getStatus().getConditions().get(0).getType(), is(Ready.toString()));
+
+        assertTrue(kafkaConnect.getStatus().getConnectorPlugins().size() > 0);
+        assertTrue(kafkaConnect.getStatus().getConnectorPlugins().stream().anyMatch(connectorPlugin -> connectorPlugin.getConnectorClass().contains(ECHO_SINK_CLASS_NAME)));
     }
 
     @Test
@@ -229,8 +266,8 @@ class ConnectBuilderST extends AbstractST {
             .withName("camel-telegram")
             .withArtifacts(
                 new TgzArtifactBuilder()
-                    .withNewUrl("https://repo.maven.apache.org/maven2/org/apache/camel/kafkaconnector/camel-telegram-kafka-connector/0.7.0/camel-telegram-kafka-connector-0.7.0-package.tar.gz")
-                    .withNewSha512sum("a9b1ac63e3284bea7836d7d24d84208c49cdf5600070e6bd1535de654f6920b74ad950d51733e8020bf4187870699819f54ef5859c7846ee4081507f48873479")
+                    .withNewUrl(CAMEL_CONNECTOR_TGZ_URL)
+                    .withNewSha512sum(CAMEL_CONNECTOR_TGZ_CHECKSUM)
                     .build())
             .build();
 
@@ -280,7 +317,7 @@ class ConnectBuilderST extends AbstractST {
         LOGGER.info("Checking that KafkaConnect API contains EchoSink connector and not Camel-Telegram Connector class name");
         String plugins = cmdKubeClient().execInPod(kafkaClientsPodName, "curl", "-X", "GET", "http://" + KafkaConnectResources.serviceName(clusterName) + ":8083/connector-plugins").out();
 
-        assertFalse(plugins.contains(CAMEL_TELEGRAM_CLASS_NAME));
+        assertFalse(plugins.contains(CAMEL_CONNECTOR_CLASS_NAME));
         assertTrue(plugins.contains(ECHO_SINK_CLASS_NAME));
 
         LOGGER.info("Adding one more connector to the KafkaConnect");
@@ -293,7 +330,7 @@ class ConnectBuilderST extends AbstractST {
         LOGGER.info("Creating Camel-Telegram connector");
         KafkaConnectorResource.create(KafkaConnectorResource.kafkaConnector(camelConnector, clusterName)
             .editOrNewSpec()
-                .withClassName(CAMEL_TELEGRAM_CLASS_NAME)
+                .withClassName(CAMEL_CONNECTOR_CLASS_NAME)
                 .withConfig(connectorConfig)
             .endSpec()
             .build());
@@ -305,7 +342,7 @@ class ConnectBuilderST extends AbstractST {
         assertThat(kafkaConnect.getSpec().getBuild().getPlugins().size(), is(2));
 
         assertTrue(kafkaConnect.getStatus().getConnectorPlugins().stream().anyMatch(connectorPlugin -> connectorPlugin.getConnectorClass().contains(ECHO_SINK_CLASS_NAME)));
-        assertTrue(kafkaConnect.getStatus().getConnectorPlugins().stream().anyMatch(connectorPlugin -> connectorPlugin.getConnectorClass().contains(CAMEL_TELEGRAM_CLASS_NAME)));
+        assertTrue(kafkaConnect.getStatus().getConnectorPlugins().stream().anyMatch(connectorPlugin -> connectorPlugin.getConnectorClass().contains(CAMEL_CONNECTOR_CLASS_NAME)));
     }
 
     @BeforeAll
