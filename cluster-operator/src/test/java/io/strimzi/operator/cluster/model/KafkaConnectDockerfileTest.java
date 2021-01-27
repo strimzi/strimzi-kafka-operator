ecb14dace0fbe60b530723198dc910ee93228e4b
Stanislav Knot <sknot@redhat.com>
2021-01-27 17:12:34 +0100

    Support zip artifacts for KC Build (#4315)
    * Support zip artifacts for KC Build
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>
    
    * review cmnts
    
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/model/KafkaConnectDockerfileTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/model/KafkaConnectDockerfileTest.java
@@ -10,6 +10,7 @@ import io.strimzi.api.kafka.model.connect.build.BuildBuilder;
 import io.strimzi.api.kafka.model.connect.build.JarArtifactBuilder;
 import io.strimzi.api.kafka.model.connect.build.PluginBuilder;
 import io.strimzi.api.kafka.model.connect.build.TgzArtifactBuilder;
+import io.strimzi.api.kafka.model.connect.build.ZipArtifactBuilder;
 import org.junit.jupiter.api.Test;
 
 import static io.strimzi.operator.cluster.model.KafkaBrokerConfigurationBuilderTest.IsEquivalent.isEquivalent;
@@ -31,8 +32,17 @@ public class KafkaConnectDockerfileTest {
             .withUrl("https://mydomain.tld/my.tgz")
             .build();
 
+    private final Artifact zipArtifactNoChecksum = new ZipArtifactBuilder()
+            .withUrl("https://mydomain.tld/my.zip")
+            .build();
+
     private final Artifact tgzArtifactWithChecksum = new TgzArtifactBuilder()
             .withUrl("https://mydomain.tld/my2.tgz")
+            .withSha512sum("sha-512-checksum")
+            .build();
+
+    private final Artifact zipArtifactWithChecksum = new ZipArtifactBuilder()
+            .withUrl("https://mydomain.tld/my2.zip")
             .withSha512sum("sha-512-checksum")
             .build();
 
@@ -148,6 +158,51 @@ public class KafkaConnectDockerfileTest {
     }
 
     @Test
+    public void testNoChecksumZipArtifact()   {
+        Build connectBuild = new BuildBuilder()
+                .withPlugins(new PluginBuilder()
+                        .withName("my-connector-plugin")
+                        .withArtifacts(zipArtifactNoChecksum)
+                        .build())
+                .build();
+
+        KafkaConnectDockerfile df = new KafkaConnectDockerfile("myImage:latest", connectBuild);
+
+        assertThat(df.getDockerfile(), isEquivalent("FROM myImage:latest",
+                "USER root:root",
+                "RUN mkdir -p /opt/kafka/plugins/my-connector-plugin/d8d533bc \\",
+                "      && curl -L --output /opt/kafka/plugins/my-connector-plugin/my.zip https://mydomain.tld/my.zip \\",
+                "      && unzip /opt/kafka/plugins/my-connector-plugin/my.zip -d /opt/kafka/plugins/my-connector-plugin/d8d533bc \\",
+                "      && find /opt/kafka/plugins/my-connector-plugin/d8d533bc -type l | xargs rm -f \\",
+                "      && rm -vf /opt/kafka/plugins/my-connector-plugin/my.zip",
+                "USER 1001"));
+    }
+
+    @Test
+    public void testChecksumZipArtifact()   {
+        Build connectBuild = new BuildBuilder()
+                .withPlugins(new PluginBuilder()
+                        .withName("my-connector-plugin")
+                        .withArtifacts(zipArtifactWithChecksum)
+                        .build())
+                .build();
+
+        KafkaConnectDockerfile df = new KafkaConnectDockerfile("myImage:latest", connectBuild);
+
+        assertThat(df.getDockerfile(), isEquivalent("FROM myImage:latest",
+                "USER root:root",
+                "RUN mkdir -p /opt/kafka/plugins/my-connector-plugin/90e04094 \\",
+                "      && curl -L --output /opt/kafka/plugins/my-connector-plugin/my2.zip https://mydomain.tld/my2.zip \\",
+                "      && echo \"sha-512-checksum /opt/kafka/plugins/my-connector-plugin/my2.zip\" > /opt/kafka/plugins/my-connector-plugin/my2.zip.sha512 \\",
+                "      && sha512sum --check /opt/kafka/plugins/my-connector-plugin/my2.zip.sha512 \\",
+                "      && rm -f /opt/kafka/plugins/my-connector-plugin/my2.zip.sha512 \\",
+                "      && unzip /opt/kafka/plugins/my-connector-plugin/my2.zip -d /opt/kafka/plugins/my-connector-plugin/90e04094 \\",
+                "      && find /opt/kafka/plugins/my-connector-plugin/90e04094 -type l | xargs rm -f \\",
+                "      && rm -vf /opt/kafka/plugins/my-connector-plugin/my2.zip",
+                "USER 1001"));
+    }
+
+    @Test
     public void testChecksumTgzArtifact()   {
         Build connectBuild = new BuildBuilder()
                 .withPlugins(new PluginBuilder()
@@ -194,6 +249,35 @@ public class KafkaConnectDockerfileTest {
                 "      && rm -f /opt/kafka/plugins/my-connector-plugin/my2.tgz.sha512 \\",
                 "      && tar xvfz /opt/kafka/plugins/my-connector-plugin/my2.tgz -C /opt/kafka/plugins/my-connector-plugin/638bd501 \\",
                 "      && rm -vf /opt/kafka/plugins/my-connector-plugin/my2.tgz",
+                "USER 1001"));
+    }
+
+    @Test
+    public void testMultipleZipArtifact()   {
+        Build connectBuild = new BuildBuilder()
+                .withPlugins(new PluginBuilder()
+                        .withName("my-connector-plugin")
+                        .withArtifacts(zipArtifactNoChecksum, zipArtifactWithChecksum)
+                        .build())
+                .build();
+
+        KafkaConnectDockerfile df = new KafkaConnectDockerfile("myImage:latest", connectBuild);
+
+        assertThat(df.getDockerfile(), isEquivalent("FROM myImage:latest",
+                "USER root:root",
+                "RUN mkdir -p /opt/kafka/plugins/my-connector-plugin/d8d533bc \\",
+                "      && curl -L --output /opt/kafka/plugins/my-connector-plugin/my.zip https://mydomain.tld/my.zip \\",
+                "      && unzip /opt/kafka/plugins/my-connector-plugin/my.zip -d /opt/kafka/plugins/my-connector-plugin/d8d533bc \\",
+                "      && find /opt/kafka/plugins/my-connector-plugin/d8d533bc -type l | xargs rm -f \\",
+                "      && rm -vf /opt/kafka/plugins/my-connector-plugin/my.zip",
+                "RUN mkdir -p /opt/kafka/plugins/my-connector-plugin/90e04094 \\",
+                "      && curl -L --output /opt/kafka/plugins/my-connector-plugin/my2.zip https://mydomain.tld/my2.zip \\",
+                "      && echo \"sha-512-checksum /opt/kafka/plugins/my-connector-plugin/my2.zip\" > /opt/kafka/plugins/my-connector-plugin/my2.zip.sha512 \\",
+                "      && sha512sum --check /opt/kafka/plugins/my-connector-plugin/my2.zip.sha512 \\",
+                "      && rm -f /opt/kafka/plugins/my-connector-plugin/my2.zip.sha512 \\",
+                "      && unzip /opt/kafka/plugins/my-connector-plugin/my2.zip -d /opt/kafka/plugins/my-connector-plugin/90e04094 \\",
+                "      && find /opt/kafka/plugins/my-connector-plugin/90e04094 -type l | xargs rm -f \\",
+                "      && rm -vf /opt/kafka/plugins/my-connector-plugin/my2.zip",
                 "USER 1001"));
     }
 
