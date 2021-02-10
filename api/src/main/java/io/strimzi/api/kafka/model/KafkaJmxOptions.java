192ec6a495212d3daaad31af167b5c771786648b
PaulRMellor <47596553+PaulRMellor@users.noreply.github.com>
2021-02-10 15:38:24 +0000

    [DOC] Update to config examples for new JMX support (#4372)
    * [DOC] Update to config examples for new JMX support
    
    Signed-off-by: prmellor <pmellor@redhat.com>
    
    * review edit PM
    
    Signed-off-by: prmellor <pmellor@redhat.com>
    
    * remove JMX config from Kafka, KC and MM2 examples
    
    Signed-off-by: prmellor <pmellor@redhat.com>
    
    * review edits DL
    
    Signed-off-by: prmellor <pmellor@redhat.com>
    
    * review edits DL
    
    Signed-off-by: prmellor <pmellor@redhat.com>

--- api/src/main/java/io/strimzi/api/kafka/model/KafkaJmxOptions.java
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaJmxOptions.java
@@ -7,6 +7,7 @@ package io.strimzi.api.kafka.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.strimzi.crdgenerator.annotations.Description;
+import io.strimzi.crdgenerator.annotations.DescriptionFile;
 import io.sundr.builder.annotations.Buildable;
 import lombok.EqualsAndHashCode;
 
@@ -14,6 +15,7 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+@DescriptionFile 
 @Buildable(
         editableEnabled = false,
         builderPackage = Constants.FABRIC8_KUBERNETES_API
