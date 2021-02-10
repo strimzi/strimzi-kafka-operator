208420da44d2771fe97ffed0be96cfbfa6f39ad8
Jakub Stejskal <xstejs24@gmail.com>
2021-02-10 14:53:45 +0100

    fixup! fixup! try to install kvm and use kvm driver for minikube
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- .azure/scripts/setup-kvm.sh
+++ .azure/scripts/setup-kvm.sh
@@ -3,8 +3,12 @@ set -x
 
 # Install kvm
 sudo apt-get install -y qemu-kvm libvirt-bin virtinst bridge-utils cpu-checker
+# Add user to libvirt groups
+sudo usermod -aG kvm $USER
+sudo usermod -aG libvirt $USER
 # Start libvirtd
 sudo systemctl start libvirtd
+kvm-ok
 # Setup nested virtualization
 sudo modprobe -r kvm_intel
 sudo modprobe kvm_intel nested=1
