31ca63d30d67250bbaece8e178537c61d837615e
morsak <xorsak02@stud.fit.vutbr.cz>
2021-02-26 13:35:44 +0100

    checkstyle
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/test/java/io/strimzi/systemtest/connect/ConnectST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/connect/ConnectST.java
@@ -37,7 +37,6 @@ import io.strimzi.systemtest.AbstractST;
 import io.strimzi.systemtest.Constants;
 import io.strimzi.systemtest.Environment;
 import io.strimzi.systemtest.annotations.OpenShiftOnly;
-import io.strimzi.systemtest.annotations.ParallelSuite;
 import io.strimzi.systemtest.annotations.ParallelTest;
 import io.strimzi.systemtest.kafkaclients.externalClients.BasicExternalKafkaClient;
 import io.strimzi.systemtest.kafkaclients.internalClients.InternalKafkaClient;
--- systemtest/src/test/java/io/strimzi/systemtest/metrics/MetricsST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/metrics/MetricsST.java
@@ -58,7 +58,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.io.IOException;
@@ -234,7 +233,7 @@ public class MetricsST extends AbstractST {
                 }
 
                 for (Map.Entry<String, String> item : kafkaExporterMetricsData.entrySet()) {
-                    if (!item.getValue().isEmpty() == false) {
+                    if (item.getValue().isEmpty()) {
                         return false;
                     }
                 }
