adb5e862271643f1367e02a2c119b97c59932ef8
Lukas Kral <lukywill16@gmail.com>
2021-03-10 15:24:18 +0100

    checkstyle
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/kafka/KafkaST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/kafka/KafkaST.java
@@ -42,7 +42,6 @@ import io.strimzi.systemtest.Constants;
 import io.strimzi.systemtest.Environment;
 import io.strimzi.systemtest.annotations.IsolatedTest;
 import io.strimzi.systemtest.annotations.OpenShiftOnly;
-import io.strimzi.systemtest.annotations.ParallelTest;
 import io.strimzi.systemtest.cli.KafkaCmdClient;
 import io.strimzi.systemtest.kafkaclients.internalClients.InternalKafkaClient;
 import io.strimzi.systemtest.resources.ResourceManager;
