354b6233ee3c1d12226f740792603c177beaaeca
Jakub Stejskal <xstejs24@gmail.com>
2021-03-12 16:58:24 +0100

    Use packaging/install... in upgrade preparations on azure (#4563)
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- .azure/scripts/setup_upgrade.sh
+++ .azure/scripts/setup_upgrade.sh
@@ -2,16 +2,16 @@
 
 sed -i "s#quay.io/strimzi/test-client:latest#${DOCKER_REGISTRY}/${DOCKER_ORG}/test-client:${DOCKER_TAG}#g" systemtest/src/test/resources/upgrade/StrimziUpgradeST.json
 
-sed -i "s#:latest#:${DOCKER_TAG}#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
-sed -i "s#/opt/${DOCKER_REGISTRY}#/opt#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
-sed -i "s#quay.io/strimzi/kafka#${DOCKER_REGISTRY}/${DOCKER_ORG}/kafka#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
-sed -i "s#quay.io/strimzi/operator#${DOCKER_REGISTRY}/${DOCKER_ORG}/operator#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
-sed -i "s#quay.io/strimzi/jmxtrans#${DOCKER_REGISTRY}/${DOCKER_ORG}/jmxtrans#g" install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#:latest#:${DOCKER_TAG}#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#/opt/${DOCKER_REGISTRY}#/opt#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#quay.io/strimzi/kafka#${DOCKER_REGISTRY}/${DOCKER_ORG}/kafka#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#quay.io/strimzi/operator#${DOCKER_REGISTRY}/${DOCKER_ORG}/operator#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#quay.io/strimzi/jmxtrans#${DOCKER_REGISTRY}/${DOCKER_ORG}/jmxtrans#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 
 sed -i "s#strimzi/kafka:latest#${DOCKER_ORG}/kafka:${DOCKER_TAG}#g" systemtest/src/test/resources/upgrade/StrimziUpgradeST.json
 sed -i "s#strimzi/operator:latest#${DOCKER_ORG}/operator:${DOCKER_TAG}#g" systemtest/src/test/resources/upgrade/StrimziUpgradeST.json
 
-cat install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+cat packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 cat systemtest/src/test/resources/upgrade/StrimziUpgradeST.json
 cat systemtest/src/test/resources/upgrade/StrimziDowngradeST.json
 
