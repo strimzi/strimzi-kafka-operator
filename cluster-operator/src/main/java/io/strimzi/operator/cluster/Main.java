048790f9cdbdb676c80001499bce3c365b851845
cutiechi <superchijinpeng@gmail.com>
2021-04-02 22:38:54 +0800

    Change instance reference to static reference (#4696)
    Signed-off-by: cutiechi <superchijinpeng@gmail.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/Main.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/Main.java
@@ -185,7 +185,7 @@ public class Main {
                         new InputStreamReader(Main.class.getResourceAsStream("/cluster-roles/" + clusterRole.getValue()),
                                 StandardCharsets.UTF_8))) {
                     String yaml = br.lines().collect(Collectors.joining(System.lineSeparator()));
-                    ClusterRole role = cro.convertYamlToClusterRole(yaml);
+                    ClusterRole role = ClusterRoleOperator.convertYamlToClusterRole(yaml);
                     Future fut = cro.reconcile(role.getMetadata().getName(), role);
                     futures.add(fut);
                 } catch (IOException e) {
