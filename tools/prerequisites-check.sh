223dcc48e166644dff17d169aede2f4d481b969d
Liam Clarke-Hutchinson <lclarkeh@redhat.com>
2021-04-06 19:56:57 +1200

    Add shellcheck to prerequisites check
    Signed-off-by: Liam Clarke-Hutchinson <lclarkeh@redhat.com>

--- tools/prerequisites-check.sh
+++ tools/prerequisites-check.sh
@@ -14,6 +14,7 @@ check_command_present yq
 check_command_present mvn
 check_command_present git
 check_command_present "${DOCKER_CMD:-docker}"
+check_command_present shellcheck
 
 # After version 3.3.1, yq --version sends the string to STDERR instead of STDOUT
 YQ_VERSION="$(yq --version 2>&1 | ${SED} 's/^.* //g')"
