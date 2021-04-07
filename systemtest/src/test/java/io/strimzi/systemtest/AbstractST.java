2243a22b8b86b478b3b96dc15cd220f5c990f9d6
morsak <xorsak02@stud.fit.vutbr.cz>
2021-04-07 17:07:25 +0200

    commends from Jakub and Lukas
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/test/java/io/strimzi/systemtest/AbstractST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/AbstractST.java
@@ -53,6 +53,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
