83df561f19dfd50ec05d3d3f2ef258d7cb187005
Stanislav Knot <sknot@redhat.com>
2021-04-09 11:48:26 +0200

    apply rc
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- api/src/main/java/io/strimzi/api/kafka/model/status/KafkaTopicStatus.java
+++ api/src/main/java/io/strimzi/api/kafka/model/status/KafkaTopicStatus.java
@@ -20,7 +20,7 @@ import lombok.ToString;
         builderPackage = Constants.FABRIC8_KUBERNETES_API
 )
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "topicName", "conditions", "observedGeneration" })
+@JsonPropertyOrder({ "conditions", "observedGeneration", "topicName" })
 @EqualsAndHashCode
 @ToString(callSuper = true)
 public class KafkaTopicStatus extends Status {
