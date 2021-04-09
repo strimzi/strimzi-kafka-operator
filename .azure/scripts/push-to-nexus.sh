98a02dfb0ef8cec7423422080ae513b09e3605e2
Paolo Patierno <ppatierno@live.com>
2021-04-09 18:09:00 +0200

    Fixed comments Marking test artifact as test scoped in api module
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- .azure/scripts/push-to-nexus.sh
+++ .azure/scripts/push-to-nexus.sh
@@ -5,7 +5,7 @@ export GPG_TTY=$(tty)
 echo $GPG_SIGNING_KEY | base64 -d > signing.gpg
 gpg --batch --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn -B $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -pl ./,crd-generator,crd-annotations,api,api-conversion,test-container -P ossrh deploy
+GPG_EXECUTABLE=gpg mvn -B $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -pl ./,crd-annotations,crd-generator,api,api-conversion,test-container -P ossrh deploy
 
 rm -rf signing.gpg
 gpg --delete-keys
