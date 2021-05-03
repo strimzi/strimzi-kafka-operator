605850b76ad3d4d1c7387380de4c2dbd3e6f0b32
Jakub Stejskal <xstejs24@gmail.com>
2021-05-03 17:35:43 +0200

    Enhance mm2 status test to check connectors
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- systemtest/src/main/java/io/strimzi/systemtest/utils/kafkaUtils/KafkaMirrorMaker2Utils.java
+++ systemtest/src/main/java/io/strimzi/systemtest/utils/kafkaUtils/KafkaMirrorMaker2Utils.java
@@ -5,8 +5,13 @@
 package io.strimzi.systemtest.utils.kafkaUtils;
 
 import io.strimzi.api.kafka.model.KafkaMirrorMaker2;
+import io.strimzi.api.kafka.model.status.KafkaMirrorMaker2Status;
+import io.strimzi.systemtest.Constants;
 import io.strimzi.systemtest.resources.ResourceManager;
 import io.strimzi.systemtest.resources.crd.KafkaMirrorMaker2Resource;
+import io.strimzi.test.TestUtils;
+
+import java.util.Map;
 
 import static io.strimzi.systemtest.enums.CustomResourceStatus.NotReady;
 import static io.strimzi.systemtest.enums.CustomResourceStatus.Ready;
@@ -46,5 +51,17 @@ public class KafkaMirrorMaker2Utils {
 
     public static boolean waitForKafkaMirrorMaker2NotReady(String clusterName) {
         return waitForKafkaMirrorMaker2Status(clusterName, NotReady);
+    }
+
+    public static void waitForKafkaMirrorMaker2ConnectorReadiness(String namespaceName, String clusterName) {
+        TestUtils.waitFor("KafkaMirrorMaker2 connectors readiness", Constants.GLOBAL_POLL_INTERVAL, Constants.GLOBAL_TIMEOUT, () -> {
+            KafkaMirrorMaker2Status kafkaMirrorMaker2Status = KafkaMirrorMaker2Resource.kafkaMirrorMaker2Client().inNamespace(namespaceName).withName(clusterName).get().getStatus();
+            for (Map<String, Object> connector : kafkaMirrorMaker2Status.getConnectors()) {
+                if (!((Map<String, String>) connector.get("connector")).get("state").equals("RUNNING")) {
+                    return false;
+                }
+            }
+            return true;
+        });
     }
 }
