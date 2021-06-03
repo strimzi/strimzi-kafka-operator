c5ec66150ee9f058dd95af194345f19005d6ae69
cytar <cytar@infradvisor.fr>
2021-06-03 11:34:34 +0200

    test3

--- .azure/scripts/install_yq.sh
+++ .azure/scripts/install_yq.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/mikefarah/yq/releases/download/v4.2.1/yq_linux_amd64 > yq && chmod +x yq
+curl -kL https://repo.phenix.carrefour.com/common/yq/yq.v4.2.1 > yq && chmod +x yq
 sudo cp yq /usr/bin/
