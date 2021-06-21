9ffebd1440b83fb0b81242fc48371f39c3b88ae7
Liam Clarke-Hutchinson <lclarkeh@redhat.com>
2021-06-21 15:19:38 +1200

    Remove unused import
    Signed-off-by: Liam Clarke-Hutchinson <lclarkeh@redhat.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaConnectApiTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaConnectApiTest.java
@@ -44,7 +44,6 @@ import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(VertxExtension.class)
