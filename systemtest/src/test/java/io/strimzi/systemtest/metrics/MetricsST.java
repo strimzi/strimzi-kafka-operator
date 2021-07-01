c1f5a5cb0e0ea92d879c5a549d2cafb583f1f2ff
morsak <xorsak02@stud.fit.vutbr.cz>
2021-07-01 12:37:07 +0200

    disable one test
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/test/java/io/strimzi/systemtest/metrics/MetricsST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/metrics/MetricsST.java
@@ -52,6 +52,7 @@ import io.strimzi.test.executor.Exec;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
@@ -256,6 +257,7 @@ public class MetricsST extends AbstractST {
     }
 
     @ParallelTest
+    @Disabled("Because of this PR https://github.com/strimzi/strimzi-kafka-operator/pull/5137.")
     @Tag(ACCEPTANCE)
     void testClusterOperatorMetrics(ExtensionContext extensionContext) {
         clusterOperatorMetricsData = MetricsUtils.collectClusterOperatorPodMetrics(kafkaClientsPodName);
