f579c2cb10995a625777b32372184eec42be94d8
Tom Bentley <tbentley@redhat.com>
2021-07-08 08:27:21 +0100

    Factor out method for building the client properties
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- topic-operator/src/main/java/io/strimzi/operator/topic/Session.java
+++ topic-operator/src/main/java/io/strimzi/operator/topic/Session.java
@@ -187,55 +187,11 @@ public class Session extends AbstractVerticle {
     @Override
     public void start(Promise<Void> start) {
         LOGGER.info("Starting");
-        Properties kafkaClientProps = new Properties();
 
         String dnsCacheTtl = System.getenv("STRIMZI_DNS_CACHE_TTL") == null ? "30" : System.getenv("STRIMZI_DNS_CACHE_TTL");
         Security.setProperty("networkaddress.cache.ttl", dnsCacheTtl);
-        kafkaClientProps.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.get(Config.KAFKA_BOOTSTRAP_SERVERS));
-        kafkaClientProps.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, config.get(Config.APPLICATION_ID));
 
-        String securityProtocol = config.get(Config.SECURITY_PROTOCOL);
-        boolean tlsEnabled = Boolean.parseBoolean(config.get(Config.TLS_ENABLED));
-
-        if (tlsEnabled && !securityProtocol.isEmpty()) {
-            if (!securityProtocol.equals("SASL_SSL") || !securityProtocol.equals("SSL")) {
-                throw new InvalidConfigurationException("TLS is enabled but the security protocol does not match SSL or SASL_SSL");
-            }
-        }
-
-        if (!securityProtocol.isEmpty()) {
-            kafkaClientProps.setProperty(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, securityProtocol);
-        } else if (tlsEnabled) {
-            kafkaClientProps.setProperty(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SSL");
-        } else {
-            kafkaClientProps.setProperty(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT");
-        }
-
-        if (securityProtocol.equals("SASL_SSL") || securityProtocol.equals("SSL") || tlsEnabled) {
-            kafkaClientProps.setProperty(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, config.get(Config.TLS_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM));
-
-            if (!config.get(Config.TLS_TRUSTSTORE_LOCATION).isEmpty()) {
-                kafkaClientProps.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, config.get(Config.TLS_TRUSTSTORE_LOCATION));
-            }
-
-            if (!config.get(Config.TLS_TRUSTSTORE_PASSWORD).isEmpty()) {
-                if (config.get(Config.TLS_TRUSTSTORE_LOCATION).isEmpty()) {
-                    throw new InvalidConfigurationException("TLS_TRUSTSTORE_PASSWORD was supplied but TLS_TRUSTSTORE_LOCATION was not supplied");
-                }
-                kafkaClientProps.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, config.get(Config.TLS_TRUSTSTORE_PASSWORD));
-            }
-
-            if (!config.get(Config.TLS_KEYSTORE_LOCATION).isEmpty() && !config.get(Config.TLS_KEYSTORE_PASSWORD).isEmpty()) {
-                kafkaClientProps.setProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, config.get(Config.TLS_KEYSTORE_LOCATION));
-                kafkaClientProps.setProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, config.get(Config.TLS_KEYSTORE_PASSWORD));
-            }
-        }
-
-        if (Boolean.parseBoolean(config.get(Config.SASL_ENABLED))) {
-            setSaslConfigs(kafkaClientProps);
-        }
-
-        this.adminClient = AdminClient.create(kafkaClientProps);
+        this.adminClient = AdminClient.create(adminClientProperties());
         LOGGER.debug("Using AdminClient {}", adminClient);
         this.kafka = new KafkaImpl(adminClient, vertx);
         LOGGER.debug("Using Kafka {}", kafka);
@@ -268,10 +224,10 @@ public class Session extends AbstractVerticle {
                     boolean exists = zk.getPathExists(topicsPath);
                     CompletionStage<KafkaStreamsTopicStoreService> cs;
                     if (exists) {
-                        cs = Zk2KafkaStreams.upgrade(zk, config, kafkaClientProps, false);
+                        cs = Zk2KafkaStreams.upgrade(zk, config, adminClientProperties(), false);
                     } else {
                         KafkaStreamsTopicStoreService ksc = new KafkaStreamsTopicStoreService();
-                        cs = ksc.start(config, kafkaClientProps).thenCompose(s -> CompletableFuture.completedFuture(ksc));
+                        cs = ksc.start(config, adminClientProperties()).thenCompose(s -> CompletableFuture.completedFuture(ksc));
                     }
                     topicStore = ConcurrentUtil.result(
                             cs.handle((s, t) -> {
@@ -339,6 +295,54 @@ public class Session extends AbstractVerticle {
                 periodic.handle(null);
                 LOGGER.info("Started");
             });
+    }
+
+    private Properties adminClientProperties() {
+        Properties kafkaClientProps = new Properties();
+        kafkaClientProps.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.get(Config.KAFKA_BOOTSTRAP_SERVERS));
+        kafkaClientProps.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, config.get(Config.APPLICATION_ID));
+
+        String securityProtocol = config.get(Config.SECURITY_PROTOCOL);
+        boolean tlsEnabled = Boolean.parseBoolean(config.get(Config.TLS_ENABLED));
+
+        if (tlsEnabled && !securityProtocol.isEmpty()) {
+            if (!securityProtocol.equals("SASL_SSL") || !securityProtocol.equals("SSL")) {
+                throw new InvalidConfigurationException("TLS is enabled but the security protocol does not match SSL or SASL_SSL");
+            }
+        }
+
+        if (!securityProtocol.isEmpty()) {
+            kafkaClientProps.setProperty(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+        } else if (tlsEnabled) {
+            kafkaClientProps.setProperty(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SSL");
+        } else {
+            kafkaClientProps.setProperty(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT");
+        }
+
+        if (securityProtocol.equals("SASL_SSL") || securityProtocol.equals("SSL") || tlsEnabled) {
+            kafkaClientProps.setProperty(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, config.get(Config.TLS_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM));
+
+            if (!config.get(Config.TLS_TRUSTSTORE_LOCATION).isEmpty()) {
+                kafkaClientProps.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, config.get(Config.TLS_TRUSTSTORE_LOCATION));
+            }
+
+            if (!config.get(Config.TLS_TRUSTSTORE_PASSWORD).isEmpty()) {
+                if (config.get(Config.TLS_TRUSTSTORE_LOCATION).isEmpty()) {
+                    throw new InvalidConfigurationException("TLS_TRUSTSTORE_PASSWORD was supplied but TLS_TRUSTSTORE_LOCATION was not supplied");
+                }
+                kafkaClientProps.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, config.get(Config.TLS_TRUSTSTORE_PASSWORD));
+            }
+
+            if (!config.get(Config.TLS_KEYSTORE_LOCATION).isEmpty() && !config.get(Config.TLS_KEYSTORE_PASSWORD).isEmpty()) {
+                kafkaClientProps.setProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, config.get(Config.TLS_KEYSTORE_LOCATION));
+                kafkaClientProps.setProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, config.get(Config.TLS_KEYSTORE_PASSWORD));
+            }
+        }
+
+        if (Boolean.parseBoolean(config.get(Config.SASL_ENABLED))) {
+            setSaslConfigs(kafkaClientProps);
+        }
+        return kafkaClientProps;
     }
 
     Future<Void> startWatcher() {
