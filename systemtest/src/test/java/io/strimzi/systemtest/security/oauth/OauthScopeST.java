c585b6b9fdb01830c551a666fce820993b77de12
morsak <xorsak02@stud.fit.vutbr.cz>
2021-07-08 10:36:22 +0200

    fix
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/test/java/io/strimzi/systemtest/security/oauth/OauthScopeST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/security/oauth/OauthScopeST.java
@@ -4,19 +4,19 @@
  */
 package io.strimzi.systemtest.security.oauth;
 
+import io.strimzi.api.kafka.model.KafkaConnectResources;
 import io.strimzi.api.kafka.model.KafkaResources;
 import io.strimzi.api.kafka.model.listener.KafkaListenerAuthenticationOAuth;
 import io.strimzi.api.kafka.model.listener.arraylistener.GenericKafkaListener;
 import io.strimzi.api.kafka.model.listener.arraylistener.GenericKafkaListenerBuilder;
 import io.strimzi.api.kafka.model.listener.arraylistener.KafkaListenerType;
 import io.strimzi.systemtest.Constants;
-import io.strimzi.systemtest.annotations.ParallelNamespaceTest;
+import io.strimzi.systemtest.annotations.ParallelTest;
 import io.strimzi.systemtest.resources.ResourceOperation;
 import io.strimzi.systemtest.resources.crd.KafkaResource;
 import io.strimzi.systemtest.templates.crd.KafkaClientsTemplates;
 import io.strimzi.systemtest.templates.crd.KafkaConnectTemplates;
 import io.strimzi.systemtest.templates.crd.KafkaTemplates;
-import io.strimzi.systemtest.utils.StUtils;
 import io.strimzi.systemtest.utils.kubeUtils.controllers.StatefulSetUtils;
 import io.strimzi.systemtest.utils.kubeUtils.objects.PodUtils;
 import io.strimzi.systemtest.utils.specific.KeycloakUtils;
@@ -41,10 +41,9 @@ public class OauthScopeST extends OauthAbstractST {
 
     private final String oauthClusterName = "oauth-cluster-scope-name";
 
-    @ParallelNamespaceTest
+    @ParallelTest
     @Tag(CONNECT)
     void testClientSideScopeKafkaConnect(ExtensionContext extensionContext) throws UnexpectedException {
-        final String namespaceName = StUtils.getNamespaceBasedOnRbac(NAMESPACE, extensionContext);
         final String kafkaClientsName = mapWithKafkaClientNames.get(extensionContext.getDisplayName());
         final String clusterName = mapWithClusterNames.get(extensionContext.getDisplayName());
         final String producerName = OAUTH_PRODUCER_NAME + "-" + clusterName;
@@ -103,24 +102,24 @@ public class OauthScopeST extends OauthAbstractST {
                 .endSpec()
             .build());
 
-        String kafkaConnectPodName = PodUtils.getPodsByPrefixInNameWithDynamicWait(namespaceName, clusterName).get(0).getMetadata().getName();
+        String kafkaConnectPodName = PodUtils.getPodsByPrefixInNameWithDynamicWait(NAMESPACE, KafkaConnectResources.deploymentName(clusterName)).get(0).getMetadata().getName();
         // kafka connect should fail because we do not specify audience but only scope
-        PodUtils.waitUntilPodIsInCrashLoopBackOff(namespaceName, kafkaConnectPodName);
+        PodUtils.waitUntilPodIsInCrashLoopBackOff(NAMESPACE, kafkaConnectPodName);
 
         // re-configuring Kafka listener to have client scope assigned to null
         KafkaResource.replaceKafkaResourceInSpecificNamespace(clusterName, kafka -> {
-            List<GenericKafkaListener> audieListener = kafka.getSpec().getKafka().getListeners()
+            List<GenericKafkaListener> audienceListeners = kafka.getSpec().getKafka().getListeners()
                 .stream()
                 .filter(listener -> listener.getName().equals(audienceListener))
                 .collect(Collectors.toList());
 
-            ((KafkaListenerAuthenticationOAuth) audieListener.get(0).getAuth()).setClientScope(null);
-            kafka.getSpec().getKafka().getListeners().set(0, audieListener.get(0));
-        }, namespaceName);
+            ((KafkaListenerAuthenticationOAuth) audienceListeners.get(0).getAuth()).setClientScope(null);
+            kafka.getSpec().getKafka().getListeners().set(0, audienceListeners.get(0));
+        }, NAMESPACE);
 
-        StatefulSetUtils.waitForAllStatefulSetPodsReady(namespaceName, KafkaResources.kafkaStatefulSetName(clusterName), 1, ResourceOperation.getTimeoutForResourceReadiness(Constants.STATEFUL_SET));
+        StatefulSetUtils.waitForAllStatefulSetPodsReady(NAMESPACE, KafkaResources.kafkaStatefulSetName(clusterName), 1, ResourceOperation.getTimeoutForResourceReadiness(Constants.STATEFUL_SET));
         // kafka connect should fail because we do not specify audience and also not scope
-        PodUtils.waitUntilPodIsInCrashLoopBackOff(namespaceName, kafkaConnectPodName);
+        PodUtils.waitUntilPodIsInCrashLoopBackOff(NAMESPACE, kafkaConnectPodName);
     }
 
     @BeforeAll
