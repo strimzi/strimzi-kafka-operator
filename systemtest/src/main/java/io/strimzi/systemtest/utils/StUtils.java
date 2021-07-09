f6eeddb65f22064dce8593b0990593ae66f12540
morsak <xorsak02@stud.fit.vutbr.cz>
2021-07-09 11:18:10 +0200

    ds
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/main/java/io/strimzi/systemtest/utils/StUtils.java
+++ systemtest/src/main/java/io/strimzi/systemtest/utils/StUtils.java
@@ -22,7 +22,6 @@ import io.vertx.core.json.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import scala.collection.immutable.Stream;
 
 import java.io.File;
 import java.io.IOException;
