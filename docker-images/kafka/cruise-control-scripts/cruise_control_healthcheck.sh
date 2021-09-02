b67e1d6774102757b405d6161fadd306eaaeef50
Kyle Liberti <kliberti@redhat.com>
2021-09-02 12:39:52 -0400

    Update healthcheck script to pass shellcheck
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

--- docker-images/kafka/cruise-control-scripts/cruise_control_healthcheck.sh
+++ docker-images/kafka/cruise-control-scripts/cruise_control_healthcheck.sh
@@ -2,21 +2,21 @@
 set -e
 
 SCHEME="http"
-HOST=$(hostname | rev | cut -d "-" -f3- | rev):$API_PORT
-ARGS="--resolve ${HOST}:0.0.0.0"
+HOST="$(hostname | rev | cut -d "-" -f3- | rev):${API_PORT}"
+ARGS=(--resolve "${HOST}:0.0.0.0")
 
 if [ "$API_AUTHENTICATION_ENABLED" = true ] ; then
   SCHEME="https"
-  ARGS="${ARGS}
-  --cert-type P12 \
-  --cert /etc/tls-sidecar/cc-certs/cruise-control.p12 \
-  --pass $(cat /etc/tls-sidecar/cc-certs/cruise-control.password) \
-  --cacert /etc/tls-sidecar/cc-certs/cruise-control.crt"
+  ARGS+=(
+  --cert-type P12
+  --cert /etc/tls-sidecar/cc-certs/cruise-control.p12
+  --pass "$(cat /etc/tls-sidecar/cc-certs/cruise-control.password)"
+  --cacert /etc/tls-sidecar/cc-certs/cruise-control.crt
+  )
 fi
 
 if [ "$API_AUTHORIZATION_ENABLED" = true ] ; then
-  ARGS="${ARGS} --user ${API_USER}:$(cat /opt/cruise-control/api-auth-config/cruise-control.apiUserPassword)"
+  ARGS+=(--user "${API_USER}:$(cat /opt/cruise-control/api-auth-config/cruise-control.apiUserPassword)")
 fi
 
-curl ${ARGS} "${SCHEME}://${HOST}${API_HEALTHCHECK_PATH}"
-
+curl "${ARGS[@]}" "${SCHEME}://${HOST}${API_HEALTHCHECK_PATH}"
