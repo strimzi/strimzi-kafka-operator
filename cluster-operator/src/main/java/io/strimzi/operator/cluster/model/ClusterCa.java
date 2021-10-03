df4c8a049a31284c07f045e8249084120fccbd10
Jakub Scholz <www@scholzj.com>
2021-10-04 01:32:46 +0200

    Minor fixes to KafkaAssemblyOperator and ClusterCa
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/model/ClusterCa.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/ClusterCa.java
@@ -79,7 +79,6 @@ public class ClusterCa extends Ca {
         return "cluster-ca";
     }
 
-    @SuppressWarnings("deprecation")
     public void initCaSecrets(List<Secret> secrets) {
         for (Secret secret: secrets) {
             String name = secret.getMetadata().getName();
