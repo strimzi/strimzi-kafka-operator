5c24c72c98066624e71a56f1d8c48c38b073b88e
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2021-10-05 14:41:36 +0200

    [systemtest] Test check for JMX metrics on Zookeper nodes with secret custom labels and annotations (#5655)
    * add test for jmx metrics and zookeeper - with secret custom labels and annotations
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * merge two tests into one
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/metrics/JmxST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/metrics/JmxST.java
@@ -4,6 +4,7 @@
  */
 package io.strimzi.systemtest.metrics;
 
+import io.fabric8.kubernetes.api.model.Secret;
 import io.strimzi.api.kafka.model.KafkaConnectResources;
 import io.strimzi.api.kafka.model.KafkaJmxAuthenticationPassword;
 import io.strimzi.api.kafka.model.KafkaResources;
@@ -17,9 +18,14 @@ import io.strimzi.systemtest.templates.crd.KafkaConnectTemplates;
 import io.strimzi.systemtest.templates.crd.KafkaTemplates;
 import io.strimzi.systemtest.utils.StUtils;
 import io.strimzi.systemtest.utils.specific.JmxUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.Collections;
+import java.util.Map;
 
 import static io.strimzi.systemtest.Constants.CONNECT;
 import static io.strimzi.systemtest.Constants.CONNECT_COMPONENTS;
@@ -27,19 +33,25 @@ import static io.strimzi.systemtest.Constants.REGRESSION;
 import static io.strimzi.test.k8s.KubeClusterResource.kubeClient;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag(REGRESSION)
 public class JmxST extends AbstractST {
 
     public static final String NAMESPACE = "jmx-cluster-test";
+    private static final Logger LOGGER = LogManager.getLogger(JmxST.class);
 
     @ParallelNamespaceTest
     @Tag(CONNECT)
     @Tag(CONNECT_COMPONENTS)
-    void testKafkaAndKafkaConnectWithJMX(ExtensionContext extensionContext) {
+    void testKafkaZookeeperAndKafkaConnectWithJMX(ExtensionContext extensionContext) {
         final String clusterName = mapWithClusterNames.get(extensionContext.getDisplayName());
         final String kafkaClientsName = mapWithKafkaClientNames.get(extensionContext.getDisplayName());
         final String namespaceName = StUtils.getNamespaceBasedOnRbac(NAMESPACE, extensionContext);
+        final String zkSecretName = clusterName + "-zookeeper-jmx";
+
+        Map<String, String> jmxSecretLabels = Collections.singletonMap("my-label", "my-value");
+        Map<String, String> jmxSecretAnnotations = Collections.singletonMap("my-annotation", "some-value");
 
         resourceManager.createResource(extensionContext, KafkaTemplates.kafkaEphemeral(clusterName, 3)
             .editOrNewSpec()
@@ -48,6 +60,19 @@ public class JmxST extends AbstractST {
                         .withAuthentication(new KafkaJmxAuthenticationPassword())
                     .endJmxOptions()
                 .endKafka()
+                .editOrNewZookeeper()
+                    .withNewJmxOptions()
+                        .withAuthentication(new KafkaJmxAuthenticationPassword())
+                    .endJmxOptions()
+                    .editOrNewTemplate()
+                        .withNewJmxSecret()
+                            .withNewMetadata()
+                                .withLabels(jmxSecretLabels)
+                                .withAnnotations(jmxSecretAnnotations)
+                            .endMetadata()
+                        .endJmxSecret()
+                    .endTemplate()
+                .endZookeeper()
             .endSpec()
             .build());
 
@@ -62,11 +87,19 @@ public class JmxST extends AbstractST {
             .endSpec()
             .build());
 
+        Secret jmxZkSecret = kubeClient().getSecret(namespaceName, zkSecretName);
+
         String kafkaResults = JmxUtils.execJmxTermAndGetResult(namespaceName, KafkaResources.brokersServiceName(clusterName), clusterName + "-kafka-jmx", clientsPodName, "bean kafka.server:type=app-info\nget -i *");
         String kafkaConnectResults = JmxUtils.execJmxTermAndGetResult(namespaceName, KafkaConnectResources.serviceName(clusterName), clusterName + "-kafka-connect-jmx", clientsPodName, "bean kafka.connect:type=app-info\nget -i *");
+        String zkResults = JmxUtils.execJmxTermAndGetResult(namespaceName, KafkaResources.zookeeperHeadlessServiceName(clusterName), zkSecretName, clientsPodName, "bean org.apache.ZooKeeperService:name0=ReplicatedServer_id1\nget -i *");
 
         assertThat("Result from Kafka JMX doesn't contain right version of Kafka, result: " + kafkaResults, kafkaResults, containsString("version = " + Environment.ST_KAFKA_VERSION));
         assertThat("Result from KafkaConnect JMX doesn't contain right version of Kafka, result: " + kafkaConnectResults, kafkaConnectResults, containsString("version = " + Environment.ST_KAFKA_VERSION));
+        assertThat("Result from Zookeeper JMX doesn't contain right quorum size, result: " + zkResults, zkResults, containsString("QuorumSize = 3"));
+
+        LOGGER.info("Checking that Zookeeper JMX secret is created with custom labels and annotations");
+        assertTrue(jmxZkSecret.getMetadata().getLabels().entrySet().containsAll(jmxSecretLabels.entrySet()));
+        assertTrue(jmxZkSecret.getMetadata().getAnnotations().entrySet().containsAll(jmxSecretAnnotations.entrySet()));
     }
 
     @BeforeAll
