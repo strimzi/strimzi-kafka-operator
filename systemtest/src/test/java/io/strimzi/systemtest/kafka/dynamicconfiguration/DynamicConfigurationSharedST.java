d46e8c695815002cb14f1bc9f3127805eecf2cfb
Orsák Maroš <30839163+see-quick@users.noreply.github.com>
2021-10-08 11:08:40 +0200

    [MO] - elimination test cases, which repeat code path (CFG) in dyn. conf (#5657)
    * [MO] - elimination test cases, which repeat code path (CFG) in dynamic configuration
    
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>
    
    * correction
    
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>
    
    * correction 2
    
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>
    
    * comments
    
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>
    
    * checkstyle
    
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/test/java/io/strimzi/systemtest/kafka/dynamicconfiguration/DynamicConfigurationSharedST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/kafka/dynamicconfiguration/DynamicConfigurationSharedST.java
@@ -53,15 +53,21 @@ public class DynamicConfigurationSharedST extends AbstractST {
         List<DynamicTest> dynamicTests = new ArrayList<>(40);
 
         Map<String, Object> testCases = generateTestCases(TestKafkaVersion.getKafkaVersionsInMap().get(Environment.ST_KAFKA_VERSION).version());
+        List<String> chosenTestCases = stochasticSelection(testCases);
 
-        testCases.forEach((key, value) -> dynamicTests.add(DynamicTest.dynamicTest("Test " + key + "->" + value, () -> {
-            // exercise phase
-            KafkaUtils.updateConfigurationWithStabilityWait(dynamicConfigurationSharedClusterName, key, value);
+        for (String key : chosenTestCases) {
+            final Object value = testCases.get(key);
 
-            // verify phase
-            assertThat(KafkaUtils.verifyCrDynamicConfiguration(dynamicConfigurationSharedClusterName, key, value), is(true));
-            assertThat(KafkaUtils.verifyPodDynamicConfiguration(KafkaResources.kafkaStatefulSetName(dynamicConfigurationSharedClusterName), key, value), is(true));
-        })));
+            dynamicTests.add(DynamicTest.dynamicTest("Test " + key + "->" + value, () -> {
+                // exercise phase
+                KafkaUtils.updateConfigurationWithStabilityWait(dynamicConfigurationSharedClusterName, key, value);
+
+                // verify phase
+                assertThat(KafkaUtils.verifyCrDynamicConfiguration(dynamicConfigurationSharedClusterName, key, value), is(true));
+                assertThat(KafkaUtils.verifyPodDynamicConfiguration(KafkaResources.kafkaStatefulSetName(dynamicConfigurationSharedClusterName), key, value), is(true));
+            }));
+        }
+
         return dynamicTests.iterator();
     }
 
@@ -177,6 +183,32 @@ public class DynamicConfigurationSharedST extends AbstractST {
         });
 
         return testCases;
+    }
+
+    /**
+     * Method, which randomly choose 3 dynamic properties for verification from @see{testCases}. In this case we are ok
+     * with stochastic selection, because we don't care, which configuration is used. Furthermore, it's the same path
+     * of code (i.e., same CFG (control flow graph), which does not triggers RollingUpdate).
+     * @param testCases test cases, where each consist of one dynamic property
+     * @return List 3 chosen dynamic properties
+     */
+    private static List<String> stochasticSelection(final Map<String, Object> testCases) {
+        final List<String> testCaseKeys = new ArrayList<>(testCases.keySet());
+        final List<String> chosenDynConfigurations = new ArrayList<>(3);
+
+        for (int i = 0; i < 3; i++) {
+            final int stochasticNumber = rng.nextInt(testCaseKeys.size());
+            final String chosenDynConfiguration = testCaseKeys.get(stochasticNumber);
+
+            LOGGER.debug("New configuration in List of dynamic configuration:\n{}", chosenDynConfigurations.toString());
+            chosenDynConfigurations.add(chosenDynConfiguration);
+            // remove it from `testCaseKeys` list to not include it twice
+            testCaseKeys.remove(chosenDynConfiguration);
+        }
+
+        LOGGER.debug("Chosen dynamic configuration are:\n{}", chosenDynConfigurations.toString());
+
+        return chosenDynConfigurations;
     }
 
     @BeforeAll
