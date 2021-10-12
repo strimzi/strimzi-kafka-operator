2a4f49d0408bdc31819614b64f025d6260e846ed
Jakub Scholz <www@scholzj.com>
2021-10-12 16:02:34 +0200

    Fix unused imports 🙄🔫
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- systemtest/src/test/java/io/strimzi/systemtest/security/OpaIntegrationST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/security/OpaIntegrationST.java
@@ -24,7 +24,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
