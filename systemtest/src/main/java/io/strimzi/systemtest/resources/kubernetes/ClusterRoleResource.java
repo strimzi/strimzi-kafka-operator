7f07a9db68584d90fdb313556ec90c5ed25f8698
Orsák Maroš <30839163+see-quick@users.noreply.github.com>
2021-10-19 17:58:46 +0200

    [MO] - ClusterRole and ClusterRoleBinding fix operation namespace (#5740)
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/main/java/io/strimzi/systemtest/resources/kubernetes/ClusterRoleResource.java
+++ systemtest/src/main/java/io/strimzi/systemtest/resources/kubernetes/ClusterRoleResource.java
@@ -24,14 +24,12 @@ public class ClusterRoleResource implements ResourceType<ClusterRole> {
     @Override
     public void create(ClusterRole resource) {
         // ClusterRole his operation namespace is only 'default'
-        resource.getMetadata().setNamespace(KubeClusterResource.getInstance().defaultNamespace());
-        kubeClient().createOrReplaceClusterRoles(resource);
+        kubeClient(KubeClusterResource.getInstance().defaultNamespace()).createOrReplaceClusterRoles(resource);
     }
     @Override
     public void delete(ClusterRole resource) {
         // ClusterRole his operation namespace is only 'default'
-        resource.getMetadata().setNamespace(KubeClusterResource.getInstance().defaultNamespace());
-        kubeClient().deleteClusterRole(resource);
+        kubeClient(KubeClusterResource.getInstance().defaultNamespace()).deleteClusterRole(resource);
     }
     @Override
     public boolean waitForReadiness(ClusterRole resource) {
