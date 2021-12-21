8cddf4777a456e807c5799bf83af2dad06d3bc7f
Federico Valeri <fvaleri@users.noreply.github.com>
2021-12-21 12:39:20 +0100

    Add Kafka log dump tool (#5694)
    * Add Kafka log dump tool
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Add number of partitions parameter and improve help
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Remove unused global variable
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Fix internal topics name issue and add unknown param detection
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Add cluster metadata dump (KRaft)
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Remove klog dependency
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Add missing dry run options
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>

---
+++ tools/log-dump/README.md
@@ -0,0 +1,130 @@
+## Kafka log dump
+
+Bash script for topic's partitions dump.
+
+Dumping partitions is not easy, because a single partition is composed by X segments, all segments are replicated in the same set of brokers, and they can be in any of the attached disks. 
+
+When dealing with the internal topics `__consumer_offsets` and `__transaction_state`, you need a way to identify which partition is needed, so that you don't have to download all 50 partitions (default value).
+
+Partition dumps can be useful to troubleshoot Kafka issues. For example, we can identify a pending transaction causing `read_committed` consumers to hang and last stable offset (LSO) to grow indefinitely.
+
+## Requirements
+
+- bash 5+ (GNU)
+- kubectl 1.16+ (K8s CLI)
+- yq 4.6+ (YAML processor)
+- jshell (JDK 9+)
+
+## Additional logging
+
+Advanced analysis also requires correlation with Kafka broker logs. If transactions are enabled, then also logs from producer's `TransactionManager` are useful.
+
+```shell
+# Kafka broker
+kubectl edit k $CLUSTER_NAME
+spec:
+  kafka:
+    logging:
+      type: inline
+      loggers:
+        kafka.root.logger.level: "INFO"
+        log4j.logger.kafka.coordinator.transaction: "DEBUG"
+        log4j.logger.kafka.request.logger: "DEBUG"
+        log4j.logger.kafka.log.LogCleaner: "DEBUG"
+        
+# Logback client application
+cat <<EOF > src/main/resources/logback.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<configuration>
+    <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
+        <encoder>
+            <pattern>%d %highlight(%p) [%t] %c{0} - %m%n</pattern>
+        </encoder>
+    </appender>
+    <root level="INFO">
+        <appender-ref ref="STDOUT" />
+    </root>
+    <logger name="org.apache.kafka.clients.producer.internals.TransactionManager" level="DEBUG" />
+</configuration>
+EOF
+```
+
+## Usage example
+
+This example shows how to dump partitions related to a simple word count transactional application running on Kubernetes. In this case we have 2 disks per broker and an output topic with 1 partition and RF=1.
+
+```shell
+# Print partition segments across the cluster
+./tools/log-dump/run.sh partition --namespace test --cluster my-cluster \
+  --topic my-topic --partition 0 --dry-run
+    
+brokers: 3, storage: jbod, disks: 2
+wc-output-0 segments in kafka-0-disk-0
+No segment found
+wc-output-0 segments in kafka-0-disk-1
+No segment found
+wc-output-0 segments in kafka-1-disk-0
+00000000000000000000.log
+wc-output-0 segments in kafka-1-disk-1
+No segment found
+wc-output-0 segments in kafka-2-disk-0
+No segment found
+wc-output-0 segments in kafka-2-disk-1
+No segment found
+
+# Dump partition segments across the cluster
+./tools/log-dump/run.sh partition --namespace test --cluster my-cluster \
+  --topic my-topic --partition 0 --out-path ~/Downloads/my-dump
+
+brokers: 3, storage: jbod, disks: 2
+wc-output-0 segments in kafka-0-disk-0
+No segment found
+wc-output-0 segments in kafka-0-disk-1
+No segment found
+wc-output-0 segments in kafka-1-disk-0
+00000000000000000000.log
+wc-output-0 segments in kafka-1-disk-1
+No segment found
+wc-output-0 segments in kafka-2-disk-0
+No segment found
+wc-output-0 segments in kafka-2-disk-1
+No segment found
+
+# Dump consumer group offsets segments
+./tools/log-dump/run.sh cg_offsets --namespace test --cluster my-cluster \
+  --group-id my-group --out-path ~/Downloads/my-dump
+  
+brokers: 3, storage: jbod, disks: 2
+my-group coordinating partition: 12
+__consumer_offsets-12 segments in kafka-0-disk-0
+00000000000000000000.log
+__consumer_offsets-12 segments in kafka-0-disk-1
+No segment found
+__consumer_offsets-12 segments in kafka-1-disk-0
+No segment found
+__consumer_offsets-12 segments in kafka-1-disk-1
+00000000000000000000.log
+__consumer_offsets-12 segments in kafka-2-disk-0
+00000000000000000000.log
+__consumer_offsets-12 segments in kafka-2-disk-1
+No segment found
+
+# Dump application transaction state segments
+./tools/log-dump/run.sh txn_state --namespace test --cluster my-cluster \
+  --txn-id my-app-my-group-my-topic-0 --out-path ~/Downloads/my-dump
+  
+brokers: 3, storage: jbod, disks: 2
+kafka-trans-my-group-wc-input-0 coordinating partition: 31
+__transaction_state-31 segments in kafka-0-disk-0
+00000000000000000000.log
+__transaction_state-31 segments in kafka-0-disk-1
+No segment found
+__transaction_state-31 segments in kafka-1-disk-0
+00000000000000000000.log
+__transaction_state-31 segments in kafka-1-disk-1
+No segment found
+__transaction_state-31 segments in kafka-2-disk-0
+No segment found
+__transaction_state-31 segments in kafka-2-disk-1
+00000000000000000000.log
+```
---
+++ tools/log-dump/run.sh
@@ -0,0 +1,343 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+if [[ $(uname -s) == "Darwin" ]]; then
+  shopt -s expand_aliases
+  alias echo="gecho"; alias dirname="gdirname"; alias grep="ggrep"; alias readlink="greadlink"
+  alias tar="gtar"; alias sed="gsed"; alias start_offsetrt="gstart_offsetrt"; alias date="gdate"; alias wc="gwc"
+fi
+readonly CO_TOPIC="__consumer_offsets"
+readonly TS_TOPIC="__transaction_state"
+readonly CM_TOPIC="__cluster_metadata"
+KAFKA_BROKERS=0
+STORAGE_TYPE=""
+JBOD_DISKS=0
+
+# user input
+COMMAND=""
+OUT_PATH="/tmp/log-dump"
+NAMESPACE=""
+CLUSTER=""
+TOPIC=""
+PARTITION=0
+SEGMENT=""
+GROUP_ID=""
+TXN_ID=""
+TOT_PART=50
+DRY_RUN=false
+DATA=false
+
+error() {
+  echo "$@" 1>&2
+  exit 1
+}
+
+check_number() {
+  local value="$1"
+  local regex='^[0-9]+$'
+  if ! [[ $value =~ $regex ]]; then
+    error "Not a number"
+  fi
+}
+
+check_kube_conn() {
+  kubectl version --request-timeout=10s 1>/dev/null
+}
+
+get_kafka_setup() {
+  KAFKA_BROKERS=$(kubectl -n $NAMESPACE get k $CLUSTER -o yaml 2>/dev/null | yq eval ".spec.kafka.replicas" -) ||true
+  STORAGE_TYPE=$(kubectl -n $NAMESPACE get k $CLUSTER -o yaml 2>/dev/null | yq eval ".spec.kafka.storage.type" -) ||true
+  JBOD_DISKS=$(kubectl -n $NAMESPACE get k $CLUSTER -o yaml 2>/dev/null | yq eval ".spec.kafka.storage.volumes | length" -) ||true
+  if [[ $KAFKA_BROKERS != "null" ]]; then
+    echo "brokers: $KAFKA_BROKERS, storage: $STORAGE_TYPE, disks: $JBOD_DISKS"
+  else
+    error "Kafka cluster $CLUSTER not found in namespace $NAMESPACE"
+  fi
+}
+
+coord-partition() {
+  local id="$1"
+  local part="${2-$TOT_PART}"
+  if [[ -n $id && -n $part ]]; then
+    echo 'public void run(String id, int part) { System.out.println(abs(id.hashCode()) % part); }    
+      private int abs(int n) { return (n == Integer.MIN_VALUE) ? 0 : Math.abs(n); } 
+      run("'$id'", '$part');' \
+      | jshell -
+  fi
+}
+
+find_segments() {
+  local broker="$1"
+  local log_dir="$2"
+  if [[ -n $broker && -n $log_dir ]]; then
+    local seg_files=$(kubectl -n $NAMESPACE exec $CLUSTER-kafka-$broker -- \
+      find $log_dir -printf "%f\n" 2>/dev/null | grep ".log")
+    echo $seg_files
+  else
+    error "Missing required parameters"
+  fi
+}
+
+dump_segments() {
+  local seg_files="$1"
+  if [[ -n $seg_files && $(echo "$seg_files" | sed '/^\s*$/d' | wc -l) -gt 0 ]]; then
+    
+    local flags="--deep-iteration"
+    case "$topic" in
+      "$CO_TOPIC")
+        flags="$flags --offsets-decoder"
+        ;;
+      "$TS_TOPIC")
+        flags="$flags --transaction-log-decoder"
+        ;;
+      "$CM_TOPIC")
+        flags="$flags --cluster-metadata-decoder"
+        ;;
+    esac
+    flags="$flags --files"
+    
+    for seg_file in $(echo $seg_files); do
+      if [[ -n $SEGMENT && $SEGMENT != "${seg_file%.log}" ]]; then
+        continue
+      fi
+      echo $seg_file
+      if [[ $DRY_RUN == false ]]; then
+        mkdir -p $out_dir
+        kubectl -n $NAMESPACE exec $CLUSTER-kafka-$i -- \
+          ./bin/kafka-dump-log.sh $flags $log_dir/$seg_file > $out_dir/$seg_file
+      fi
+    done
+    
+  else
+    echo "No segment found"
+  fi
+}
+
+dump_partition() {
+  local topic="$1"
+  local partition="$2"
+  local broker="$3"
+  local disk="${4-}"
+  if [[ -n $topic && -n $partition && -n $broker ]]; then
+    
+    # context setup
+    local disk_label="$topic-$partition segments in kafka-$broker"
+    local log_dir="/var/lib/kafka/data/kafka-log$broker/$topic-$partition"
+    local out_dir="$OUT_PATH/$topic/kafka-$broker-$topic-$partition"
+    
+    if [[ -n $disk && $disk -ge 0 ]]; then
+      disk_label="$topic-$partition segments in kafka-$broker-disk-$disk"
+      log_dir="/var/lib/kafka/data-$disk/kafka-log$broker/$topic-$partition";
+      out_dir="$OUT_PATH/$topic/kafka-$broker-disk-$disk-$topic-$partition"
+    fi
+    
+    # segment dump
+    local seg_files=$(find_segments $broker $log_dir)
+    echo $disk_label
+    dump_segments "$seg_files"
+    
+  else
+    error "Missing required parameters"
+  fi
+}
+
+partition() {
+  if [[ -n $NAMESPACE && -n $CLUSTER && -n $TOPIC && -n $PARTITION ]]; then
+    check_kube_conn
+    get_kafka_setup
+    
+    # dump topic partition across the cluster (including replicas)
+    for i in $(seq 0 $(($KAFKA_BROKERS-1))); do
+      if [[ $STORAGE_TYPE == "jbod" ]]; then
+        for j in $(seq 0 $(($JBOD_DISKS-1))); do
+          dump_partition $TOPIC $PARTITION $i $j
+        done
+      else 
+        dump_partition $TOPIC $PARTITION $i
+      fi
+    done
+    
+  else
+    error "Missing required options"
+  fi
+}
+
+cg_offsets() {
+  if [[ -n $NAMESPACE && -n $CLUSTER && -n $GROUP_ID ]]; then
+    check_kube_conn
+    get_kafka_setup
+          
+    # dump CG offsets coordinating partition across the cluster (including replicas)
+    local partition=$(coord-partition "$GROUP_ID" "$TOT_PART")
+    echo "$GROUP_ID coordinating partition: $partition"
+    for i in $(seq 0 $(($KAFKA_BROKERS-1))); do
+      if [[ $STORAGE_TYPE == "jbod" ]]; then
+        for j in $(seq 0 $(($JBOD_DISKS-1))); do
+          dump_partition $CO_TOPIC $partition $i $j
+        done
+      else
+        dump_partition $CO_TOPIC $partition $i
+      fi
+    done
+    
+  else
+    error "Missing required options"
+  fi
+}
+
+txn_state() {
+  if [[ -n $NAMESPACE && -n $CLUSTER && -n $TXN_ID ]]; then
+    check_kube_conn
+    get_kafka_setup
+        
+    # dump TX state coordinating partition across the cluster (including replicas)
+    local partition=$(coord-partition "$TXN_ID" "$TOT_PART")
+    echo "$TXN_ID coordinating partition: $partition"
+    for i in $(seq 0 $(($KAFKA_BROKERS-1))); do
+      if [[ $STORAGE_TYPE == "jbod" ]]; then
+        for j in $(seq 0 $(($JBOD_DISKS-1))); do
+          dump_partition $TS_TOPIC $partition $i $j
+        done
+      else
+        dump_partition $TS_TOPIC $partition $i
+      fi
+    done
+    
+  else
+    error "Missing required options"
+  fi
+}
+
+cluster_meta() {
+  if [[ -n $NAMESPACE && -n $CLUSTER ]]; then
+    check_kube_conn
+    get_kafka_setup
+        
+    # dump cluster metadata partition across the cluster (including replicas)
+    local partition="0"
+    for i in $(seq 0 $(($KAFKA_BROKERS-1))); do
+      if [[ $STORAGE_TYPE == "jbod" ]]; then
+        for j in $(seq 0 $(($JBOD_DISKS-1))); do
+          dump_partition $CM_TOPIC $partition $i $j
+        done
+      else
+        dump_partition $CM_TOPIC $partition $i
+      fi
+    done
+
+  else
+    error "Missing required options"
+  fi
+}
+
+readonly USAGE="
+Usage: $0 [command] [params]
+
+  partition       Dump partition
+    --namespace     Kubernetes namespace
+    --cluster       Kafka cluster name
+    --topic         Data topic name
+    --partition     Partition number (zero-based)
+    --segment       Filter by segment name
+    --out-path      Output path (default: $OUT_PATH)
+    --dry-run       Run without dumping (default: $DRY_RUN)
+    --data          Include record payload (default: $DATA)
+
+  cg_offsets      Dump consumer group offsets by group.id
+    --namespace     Kubernetes namespace
+    --cluster       Kafka cluster name
+    --group-id      Consumer group id
+    --tot-part      Number of __consumer_offsets partitions (default: $TOT_PART)
+    --out-path      Output path (default: $OUT_PATH)
+    --dry-run       Run without dumping (default: $DRY_RUN)
+  
+  txn_state       Dump transactions state by transactional.id
+    --namespace     Kubernetes namespace
+    --cluster       Kafka cluster name
+    --txn-id        Transactional id
+    --tot-part      Number of __transaction_state partitions (default: $TOT_PART)
+    --out-path      Output path (default: $OUT_PATH)
+    --dry-run       Run without dumping (default: $DRY_RUN)
+    
+  cluster_meta    Dump cluster metadata (KRaft)
+    --namespace     Kubernetes namespace
+    --cluster       Kafka cluster name
+    --out-path      Output path (default: $OUT_PATH)
+    --dry-run       Run without dumping (default: $DRY_RUN)
+"
+readonly PARAMS="${@}"
+readonly PARRAY=($PARAMS)
+i=0
+for param in $PARAMS; do
+  i=$(($i+1))
+  case $param in
+    --namespace)
+      export NAMESPACE=${PARRAY[i]}
+      readonly NAMESPACE
+      ;;
+    --cluster)
+      export CLUSTER=${PARRAY[i]}
+      readonly CLUSTER
+      ;;
+    --topic)
+      export TOPIC=${PARRAY[i]}
+      readonly TOPIC
+      ;;
+    --partition)
+      export PARTITION=${PARRAY[i]}
+      readonly PARTITION
+      check_number $PARTITION
+      ;;
+    --segment)
+      export SEGMENT=${PARRAY[i]}
+      readonly SEGMENT
+      ;;
+    --group-id)
+      export GROUP_ID=${PARRAY[i]}
+      readonly GROUP_ID
+      ;;
+    --txn-id)
+      export TXN_ID=${PARRAY[i]}
+      readonly TXN_ID
+      ;;
+    --tot-part)
+      export TOT_PART=${PARRAY[i]}
+      readonly TOT_PART
+      check_number $TOT_PART
+      ;;
+    --out-path)
+      export OUT_PATH=${PARRAY[i]}
+      readonly OUT_PATH
+      ;;
+    --dry-run)
+      export DRY_RUN=true
+      readonly DRY_RUN
+      ;;
+    --data)
+      export DATA=true
+      readonly DATA
+      ;;
+    *)
+      if [[ $param == --* ]]; then
+        error "Unknown parameter $param" 
+      fi
+      ;;
+  esac
+done
+readonly COMMAND="${1-}"
+case "$COMMAND" in
+  partition)
+    partition
+    ;;
+  cg_offsets)
+    cg_offsets
+    ;;
+  txn_state)
+    txn_state
+    ;;
+  cluster_meta)
+    cluster_meta
+    ;;
+  *)
+    error "$USAGE"
+    ;;
+esac
