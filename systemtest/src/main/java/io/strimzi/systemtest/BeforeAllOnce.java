3739f32ab7808f968a59baec7f7eb0a7a9379dc7
morsak <xorsak02@stud.fit.vutbr.cz>
2022-02-11 23:33:35 +0100

    before all once fixed for method wide paralleism
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- systemtest/src/main/java/io/strimzi/systemtest/BeforeAllOnce.java
+++ systemtest/src/main/java/io/strimzi/systemtest/BeforeAllOnce.java
@@ -39,7 +39,7 @@ public class BeforeAllOnce implements BeforeAllCallback, ExtensionContext.Store.
             sharedExtensionContext = extensionContext.getRoot();
 
             // we skip creation of shared Cluster Operator (because @IsolatedSuite has to have deploy brand-new configuration)
-            if (StUtils.isParallelSuite(extensionContext)) {
+            if (!StUtils.isIsolatedSuite(extensionContext)) {
                 BeforeAllOnce.systemReady = true;
                 // ---start
                 // This is needed for one scenario (2 in parallel) and @ParallelSuite is selected for deployment of .
