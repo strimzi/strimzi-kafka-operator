64f911d09458628e47c2b625c76ed5cd5e3f97b7
Yaodong Yang <yyang48@apple.com>
2022-02-18 22:55:48 -0800

    Update the test k8s version.
    Signed-off-by: Yaodong Yang <yyang48@apple.com>

--- .travis/setup-kubernetes.sh
+++ .travis/setup-kubernetes.sh
@@ -68,15 +68,15 @@ if [ "$TEST_CLUSTER" = "minikube" ]; then
     export MINIKUBE_WANTREPORTERRORPROMPT=false
     export MINIKUBE_HOME=$HOME
     export CHANGE_MINIKUBE_NONE_USER=true
-    
+
     mkdir $HOME/.kube || true
     touch $HOME/.kube/config
 
     docker run -d -p 5000:5000 registry
 
     export KUBECONFIG=$HOME/.kube/config
-    sudo -E minikube start --vm-driver=none --kubernetes-version=v1.16.0 \
-      --insecure-registry=localhost:5000 --extra-config=apiserver.authorization-mode=RBAC
+    sudo -E minikube start --vm-driver=none --kubernetes-version=v1.15.0  \
+      --insecure-registry=localhost:5000 --extra-config=apiserver.authorization-mode=RBAC --force
     sudo chown -R travis: /home/travis/.minikube/
     sudo -E minikube addons enable default-storageclass
 
