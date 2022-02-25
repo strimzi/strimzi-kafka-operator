82f0f9d471fb9dda6f50876358144e3453f97088
Federico Valeri <fvaleri@users.noreply.github.com>
2022-02-25 12:14:29 +0100

    Shellcheck compliant (#6407)
    * Make log-dump and cold-backup tools shellcheck compliant
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Fix cold-backup script ST
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Feedback changes
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    
    * Fix related system tests
    
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>
    Signed-off-by: Abel Prins <prins.ab@tkppensioen.nl>

--- .azure/scripts/shellcheck.sh
+++ .azure/scripts/shellcheck.sh
@@ -12,6 +12,8 @@ docker-images/kafka-based/kafka/stunnel-scripts/*.sh
 docker-images/kafka-based/kafka/exporter-scripts/*.sh
 docker-images/jmxtrans/*.sh
 tools/olm-bundle/*.sh
+tools/cold-backup/*.sh
+tools/log-dump/*.sh
 tools/*.sh"
 
 for SCRIPTS in $SCRIPT_DIRS; do
