cd9a4197f7dca153af886186ca47a3e3ab56aabb
ShubhamRwt <srawat@redhat.com>
2022-03-15 19:22:36 +0530

    renamed to idOfPod
    Signed-off-by: ShubhamRwt <srawat@redhat.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/resource/KafkaRoller.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/resource/KafkaRoller.java
@@ -193,7 +193,7 @@ public class KafkaRoller {
             for (int podIndex = 0; podIndex < podList.size(); podIndex++) {
                 // Order the podNames unready first otherwise repeated reconciliations might each restart a pod
                 // only for it not to become ready and thus drive the cluster to a worse state.
-                pods.add(podOperations.isReady(namespace, podList.get(podIndex)) ? pods.size() : 0, new PodRef(podList.get(podIndex), indexOfPod(podList.get(podIndex))));
+                pods.add(podOperations.isReady(namespace, podList.get(podIndex)) ? pods.size() : 0, new PodRef(podList.get(podIndex), idOfPod(podList.get(podIndex))));
             }
             LOGGER.debugCr(reconciliation, "Initial order for rolling restart {}", pods);
             List<Future> futures = new ArrayList<>(podList.size());
@@ -399,7 +399,7 @@ public class KafkaRoller {
         return false;
     }
 
-    private int indexOfPod(String podName)  {
+    private int idOfPod(String podName)  {
         return Integer.parseInt(podName.substring(podName.lastIndexOf("-") + 1));
     }
 
