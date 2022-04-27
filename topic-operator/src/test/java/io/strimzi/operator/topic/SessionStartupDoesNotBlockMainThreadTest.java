c39ad3b59638de6cfb94713c4718634cd16be79e
Jakub Scholz <www@scholzj.com>
2022-04-28 00:49:26 +0200

    Disable the SessionStartupDoesNotBlockMainThreadTest tests
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- topic-operator/src/test/java/io/strimzi/operator/topic/SessionStartupDoesNotBlockMainThreadTest.java
+++ topic-operator/src/test/java/io/strimzi/operator/topic/SessionStartupDoesNotBlockMainThreadTest.java
@@ -25,6 +25,7 @@ import org.apache.kafka.clients.admin.AdminClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
@@ -43,6 +44,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+// Disabled because they were flaky in some environments.
+// Should be re-enabled after Vert.x 4.3.0 upgrade => https://github.com/strimzi/strimzi-kafka-operator/issues/6741
+@Disabled
 public class SessionStartupDoesNotBlockMainThreadTest {
 
     @Mock
