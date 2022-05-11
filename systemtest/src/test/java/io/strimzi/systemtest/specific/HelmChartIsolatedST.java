f2724e25b178a48dfd481dbec6fd6d449b206310
jkalinic <jkalinic@redhat.com>
2022-05-11 11:26:28 +0200

    small addition
    Signed-off-by: jkalinic <jkalinic@redhat.com>

--- systemtest/src/test/java/io/strimzi/systemtest/specific/HelmChartIsolatedST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/specific/HelmChartIsolatedST.java
@@ -45,7 +45,7 @@ class HelmChartIsolatedST extends AbstractST {
         resourceManager.createResource(extensionContext,
             KafkaTopicTemplates.topic(clusterName, topicName).build(),
             // Deploy KafkaConnect and wait for readiness
-            KafkaConnectTemplates.kafkaConnectWithFilePlugin(extensionContext, Constants.INFRA_NAMESPACE, clusterName, 1).editMetadata()
+            KafkaConnectTemplates.kafkaConnectWithFilePlugin(extensionContext, clusterOperator.getDeploymentNamespace(), clusterName, 1).editMetadata()
                 .addToAnnotations(Annotations.STRIMZI_IO_USE_CONNECTOR_RESOURCES, "true")
                 .endMetadata().build(),
             // Deploy KafkaBridge (different image than Kafka) and wait for readiness
