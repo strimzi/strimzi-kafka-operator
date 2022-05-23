5477c551bb07040494075b8f794b6bee937b2484
Lukas Kral <lukywill16@gmail.com>
2022-05-23 11:20:46 +0200

    checkstyle
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/upgrade/AbstractUpgradeST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/upgrade/AbstractUpgradeST.java
@@ -47,7 +47,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
