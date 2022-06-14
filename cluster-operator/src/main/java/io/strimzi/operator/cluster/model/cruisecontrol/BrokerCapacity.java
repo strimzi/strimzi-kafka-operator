00a9527f203d3784637c91177d779cd30cfa5a70
Kyle Liberti <kliberti@redhat.com>
2022-06-14 08:27:36 -0400

    Update default CC config doc
    Signed-off-by: Kyle Liberti <kliberti@redhat.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/model/cruisecontrol/BrokerCapacity.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/cruisecontrol/BrokerCapacity.java
@@ -8,7 +8,7 @@ public class BrokerCapacity {
     // CC allows specifying a generic "default" broker entry in the capacity configuration to apply to all brokers without a specific broker entry.
     // CC designates the id of this default broker entry as "-1".
     public static final int DEFAULT_BROKER_ID = -1;
-    public static final String DEFAULT_BROKER_DOC = "This is the default capacity. Capacity unit used for disk is in MiB, cpu is in percentage, network throughput is in KiB.";
+    public static final String DEFAULT_BROKER_DOC = "This is the default capacity. Capacity unit used for disk is in MiB, cpu is in number of cores, network throughput is in KiB.";
     public static final String DEFAULT_CPU_CORE_CAPACITY = "1";
     public static final String DEFAULT_DISK_CAPACITY_IN_MIB = "100000";
     public static final String DEFAULT_INBOUND_NETWORK_CAPACITY_IN_KIB_PER_SECOND = "10000";
