3c2cc5ac42aefddf485878106bb8ea7905f574c1
Jan Kalinic <jkalinic@redhat.com>
2022-06-16 10:59:41 +0200

    [ST Enhancement] Load upgrade/downgrade ST data from yaml instead of json (#6861)
    * Rewrite upgrade json data to yaml
    
    Load upgrade data to class
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * fixes
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * enhancements
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * PR changes
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * Pr modifications
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * fix for namespace
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * pr modifications
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * pr fix
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * fix names
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * changed dependency scope
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * azure missing json
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * jenkins pipeline json yaml
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * fix
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * fix url
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * modified kafka version
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>
    
    * bump 0.29.0 version
    
    Signed-off-by: jkalinic <jkalinic@redhat.com>

--- .jenkins/jenkins.groovy
+++ .jenkins/jenkins.groovy
@@ -40,11 +40,11 @@ def buildStrimziImages() {
 def prepareUpgradeSTs(String workspace, String dockerRegistry, String dockerTag) {
     println("[INFO] Update files for upgrade procedure")
     sh(script: """
-        sed -i 's#:latest#:${dockerTag}#g' ${workspace}/systemtest/src/test/resources/upgrade/StrimziUpgradeST.json ${workspace}/install/cluster-operator/060-Deployment-strimzi-cluster-operator.yaml
+        sed -i 's#:latest#:${dockerTag}#g' ${workspace}/systemtest/src/test/resources/upgrade/StrimziUpgradeST.yaml ${workspace}/install/cluster-operator/060-Deployment-strimzi-cluster-operator.yaml
         sed -i 's#quay.io/strimzi/#${dockerRegistry}/strimzi/#g' ${workspace}/install/cluster-operator/060-Deployment-strimzi-cluster-operator.yaml
         sed -i 's#/opt/${dockerRegistry}#/opt#g' ${workspace}/install/cluster-operator/060-Deployment-strimzi-cluster-operator.yaml
     """)
-    sh(script: "cat ${workspace}/systemtest/src/test/resources/upgrade/StrimziUpgradeST.json")
+    sh(script: "cat ${workspace}/systemtest/src/test/resources/upgrade/StrimziUpgradeST.yaml")
     sh(script: "cat ${workspace}/install/cluster-operator/060-Deployment-strimzi-cluster-operator.yaml")
 }
 
