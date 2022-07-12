83c1548cc6772f3fdb0151fceca2990694d62524
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2022-07-12 16:19:42 +0200

    Check if cgroups v2 file contains number or not (#7003)
    * add check for cgroups v2 file
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * check max and not a number
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * Jakub's comment
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- docker-images/operator/scripts/launch_java.sh
+++ docker-images/operator/scripts/launch_java.sh
@@ -44,14 +44,25 @@ JAVA_OPTS="${JAVA_OPTS} --illegal-access=deny"
 # Exit when we run out of heap memory
 JAVA_OPTS="${JAVA_OPTS} -XX:+ExitOnOutOfMemoryError"
 
+DEFAULT_MEMORY_OPTIONS="-XX:MinRAMPercentage=10 -XX:MaxRAMPercentage=20 -XX:InitialRAMPercentage=10"
+
 # Default memory options used when the user didn't configured any of these options, we set the defaults
 if [[ ! -r "${mem_file_cgroups_v2}" && "$JAVA_OPTS" != *"MinRAMPercentage"* && "$JAVA_OPTS" != *"MaxRAMPercentage"* && "$JAVA_OPTS" != *"InitialRAMPercentage"* ]]; then
-  JAVA_OPTS="${JAVA_OPTS} -XX:MinRAMPercentage=10 -XX:MaxRAMPercentage=20 -XX:InitialRAMPercentage=10"
+  JAVA_OPTS="${JAVA_OPTS} ${DEFAULT_MEMORY_OPTIONS}"
 elif [[ -r "${mem_file_cgroups_v2}" && "$JAVA_OPTS" != *"Xmx"* ]]; then
   # This workaround for cgroups v2 must be removed once java version used by the operator is updated.
   # https://developers.redhat.com/articles/2022/04/19/java-17-whats-new-openjdks-container-awareness#
-  # Calculate -Xmx java option
-  JAVA_OPTS="${JAVA_OPTS} $(calc_max_memory)"
+
+  # Check whether /sys/fs/cgroup/memory.max contains 'max' or a number
+  value=$(cat "${mem_file_cgroups_v2}")
+
+  if [[ $value != "max" ]]; then
+    # Calculate -Xmx java option
+    JAVA_OPTS="${JAVA_OPTS} $(calc_max_memory)"
+  else
+    # Because /sys/fs/cgroup/memory.max contains 'max', pick defaults
+    JAVA_OPTS="${JAVA_OPTS} ${DEFAULT_MEMORY_OPTIONS}"
+  fi
 fi
 
 # Disable FIPS if needed
