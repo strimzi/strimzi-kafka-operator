339f243ab316a166b5dbd12fc9d60bf21069f21a
Jakub Scholz <www@scholzj.com>
2022-07-26 21:50:43 +0200

    Fix flaky Kafka Connect IT tests (#7092)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/ConnectCluster.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/ConnectCluster.java
@@ -5,18 +5,19 @@
 package io.strimzi.operator.cluster.operator.assembly;
 
 import org.apache.kafka.connect.cli.ConnectDistributed;
+import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.runtime.Connect;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ConnectCluster {
-
-    private static final int STARTING_PORT = 8083;
-
     private int numNodes;
     private String brokerList;
     private final List<Connect> connectInstances = new ArrayList<>();
@@ -35,7 +36,7 @@ public class ConnectCluster {
     public void startup() throws InterruptedException {
         for (int i = 0; i < numNodes; i++) {
             Map<String, String> workerProps = new HashMap<>();
-            workerProps.put("listeners", "http://localhost:" + getPort(i));
+            workerProps.put("listeners", "http://localhost:" + getFreePort());
             workerProps.put("plugin.path", String.join(",", pluginPath));
             workerProps.put("group.id", toString());
             workerProps.put("key.converter", "org.apache.kafka.connect.json.JsonConverter");
@@ -49,20 +50,31 @@ public class ConnectCluster {
             workerProps.put("status.storage.topic", getClass().getSimpleName() + "-status");
             workerProps.put("status.storage.replication.factor", "3");
             workerProps.put("bootstrap.servers", brokerList);
-            //DistributedConfig config = new DistributedConfig(workerProps);
-            //RestServer rest = new RestServer(config);
-            //rest.initializeServer();
-            CountDownLatch l = new CountDownLatch(1);
+
+            CountDownLatch l = new CountDownLatch(1); // Indicates that the Kafka Connect cluster startup is finished (successfully or not)
+            AtomicReference<Exception> startupException = new AtomicReference<>(); // Indicates whether any exception was raised during the Kafka Connect node start
+
             Thread thread = new Thread(() -> {
-                ConnectDistributed connectDistributed = new ConnectDistributed();
-                Connect connect = connectDistributed.startConnect(workerProps);
-                l.countDown();
-                connectInstances.add(connect);
-                connect.awaitStop();
+                try {
+                    ConnectDistributed connectDistributed = new ConnectDistributed();
+                    Connect connect = connectDistributed.startConnect(workerProps);
+                    l.countDown();
+                    connectInstances.add(connect);
+                    connect.awaitStop();
+                } catch (ConnectException e)    {
+                    startupException.set(e);
+                    l.countDown();
+                }
             });
+
             thread.setDaemon(false);
             thread.start();
             l.await();
+
+            if (startupException.get() != null) {
+                // If the Kafka Connect node failed to start (i.e. raised an exception during the start), we should throw an exception to fail the tests
+                throw new RuntimeException("Failed to start node " + i + " of the Kafka Connect cluster", startupException.get());
+            }
         }
     }
 
@@ -76,13 +88,26 @@ public class ConnectCluster {
     }
 
     /**
-     * Gets the port used for given Connect node. The nudes start from 0.
+     * Gets the port used for given Connect node. The nodes start from 0.
      *
      * @param node  ID of the node for which we want to get the port number (node numbers start with 0)
      *
-     * @return      Port which can be used to connect to given Connect node
+     * @return      Port which is used by given Connect node
      */
     public int getPort(int node) {
-        return STARTING_PORT + node * 10_000;
+        return connectInstances.get(node).adminUrl().getPort();
+    }
+
+    /**
+     * Finds a free server port which can be used by the Connect REST API
+     *
+     * @return  A free TCP port
+     */
+    private int getFreePort()   {
+        try (ServerSocket serverSocket = new ServerSocket(0)) {
+            return serverSocket.getLocalPort();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to find free port", e);
+        }
     }
 }
