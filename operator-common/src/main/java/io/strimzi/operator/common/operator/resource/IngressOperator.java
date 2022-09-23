41f116285fbdf729ef7bd22d06b7c67f7d6919f0
Jakub Scholz <www@scholzj.com>
2022-09-23 18:17:00 +0200

    MAke sure assigned IngressClass name is kept when patching Ingress resources - Closes #7362
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- operator-common/src/main/java/io/strimzi/operator/common/operator/resource/IngressOperator.java
+++ operator-common/src/main/java/io/strimzi/operator/common/operator/resource/IngressOperator.java
@@ -33,6 +33,52 @@ public class IngressOperator extends AbstractResourceOperator<KubernetesClient, 
     }
 
     /**
+     * Patches the resource with the given namespace and name to match the given desired resource
+     * and completes the given future accordingly.
+     *
+     * IngressOperator needs its own version of this method to patch the Ingress Class name when the default class is
+     * used. When the default IngressClass is used, the class name in the Kafka CR should not be set at all and the
+     * Ingress resources will be created without it. The default Kubernetes Nginx Ingress will then pick up the Ingress
+     * resource and inject the class name into it. So when patching the Ingress resource, we need to set the IngressClass
+     * name in the desired object to match the current. This needs to be done in order to:
+     *     - Avoid unnecessary patching
+     *     - Because if you change the class, the Kubernetes Nginx Ingress controller will not pick it up again, and it
+     *       will stop working
+     *
+     * This is done only when the IngressClass set in Kafka CR is null. If it is set to some specific class, it will be
+     * of course kept.
+     *
+     * @param reconciliation    The reconciliation
+     * @param namespace         Namespace of the Ingress resource
+     * @param name              Name of the Ingress resource
+     * @param current           Current Ingress resource
+     * @param desired           Desired Ingress resource
+     *
+     * @return  Future with reconciliation result
+     */
+    @Override
+    protected Future<ReconcileResult<Ingress>> internalPatch(Reconciliation reconciliation, String namespace, String name, Ingress current, Ingress desired) {
+        patchIngressClassName(current, desired);
+
+        return super.internalPatch(reconciliation, namespace, name, current, desired);
+    }
+
+    /**
+     * Patches the IngressClass name from the current to the desired Ingress resource. The patching is only done if the
+     * desired IngressClass name is not set.
+     *
+     * @param current           Current Ingress resource
+     * @param desired           Desired Ingress resource
+     */
+    /* test */ void patchIngressClassName(Ingress current, Ingress desired) {
+        if (desired.getSpec() != null
+                && current.getSpec() != null
+                && desired.getSpec().getIngressClassName() == null)   {
+            desired.getSpec().setIngressClassName(current.getSpec().getIngressClassName());
+        }
+    }
+
+    /**
      * Succeeds when the Service has an assigned address
      *
      * @param reconciliation The reconciliation
