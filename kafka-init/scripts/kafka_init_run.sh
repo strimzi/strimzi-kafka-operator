e21b8559eeadec12da1f55729bf60b0d812f0be6
Jakub Scholz <www@scholzj.com>
2022-12-13 00:22:08 +0100

    Add Log4j-jul to properly integrate OkHttp logging
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- kafka-init/scripts/kafka_init_run.sh
+++ kafka-init/scripts/kafka_init_run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# The java.util.logging.manager is set because of OkHttp client which is using JUL logging
+export JAVA_OPTS="${JAVA_OPTS} -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager"
+
 export JAVA_CLASSPATH=lib/io.strimzi.@project.build.finalName@.@project.packaging@:@project.dist.classpath@
 export JAVA_MAIN=io.strimzi.kafka.init.Main
 exec "${STRIMZI_HOME}/bin/launch_java.sh"
