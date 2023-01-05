37d27d2fb20186f27f5302a7287f3523fe13f07a
Shubham Rawat <40595489+ShubhamRwt@users.noreply.github.com>
2023-01-06 05:25:05 +0530

    Refactor ACLBuilder class in Topic Operator module (#7869)
    Signed-off-by: ShubhamRwt <srawat@redhat.com>
    Co-authored-by: ShubhamRwt <srawat@redhat.com>

--- topic-operator/src/main/java/io/strimzi/operator/topic/zk/AclBuilder.java
+++ topic-operator/src/main/java/io/strimzi/operator/topic/zk/AclBuilder.java
@@ -11,9 +11,7 @@ import org.apache.zookeeper.data.Id;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static java.util.Arrays.asList;
 
@@ -50,54 +48,12 @@ public class AclBuilder {
         }
     }
     private ACL world;
-    private ACL auth;
-    private Map<String, ACL> digests;
-    private Map<String, ACL> hosts;
-    private Map<String, ACL> ips;
 
     protected static final List<ACL> PUBLIC = Collections.singletonList(new ACL(Permission.encode(EnumSet.allOf(Permission.class)), new Id("world", "anyone")));
 
     /** Constructor */
     public AclBuilder() {
 
-    }
-
-    /**
-     * Constructor
-     *
-     * @param acls  List of ACL's
-     */
-    public AclBuilder(List<ACL> acls) {
-        for (ACL acl: acls) {
-            String scheme = acl.getId().getScheme();
-            switch (scheme) {
-                case "world":
-                    world = acl;
-                    break;
-                case "auth":
-                    auth = acl;
-                    break;
-                case "digest":
-                    String[] username = acl.getId().getId().split(":", 2);
-                    getDigests().put(username[0], acl);
-                    break;
-                case "host":
-                    getHosts().put(acl.getId().getId(), acl);
-                    break;
-                case "ip":
-                    getIps().put(acl.getId().getId(), acl);
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unsupported scheme " + scheme);
-            }
-        }
-    }
-
-    private Map<String, ACL> getDigests() {
-        if (digests == null) {
-            digests = new HashMap<>();
-        }
-        return digests;
     }
 
     /**
@@ -115,42 +71,6 @@ public class AclBuilder {
         return this;
     }
 
-    private Map<String, ACL> getHosts() {
-        if (hosts == null) {
-            hosts = new HashMap<>();
-        }
-        return hosts;
-    }
-
-    /**
-     * Set the given permissions for users connecting from the most
-     * significant {@code bits} given IP {@code address}.
-     *
-     * @param address The IP address to add.
-     * @param bits The number of bits in the IP address.
-     * @param permissions The permissions for users connecting from matching IP addresses.
-     * @return This instance.
-     */
-    public AclBuilder addIp(String address, int bits, Permission... permissions) {
-        Map<String, ACL> ips = getIps();
-        String cidr = address + "/" + bits;
-        ACL a = ips.get(cidr);
-        if (a == null) {
-            a = new ACL();
-            ips.put(cidr, a);
-        }
-        a.setId(new Id("ip", cidr));
-        a.setPerms(Permission.encode(permissions));
-        return this;
-    }
-
-    private Map<String, ACL> getIps() {
-        if (ips == null) {
-            ips = new HashMap<>();
-        }
-        return ips;
-    }
-
     /**
      * Build the result.
      * @return a list of ACLs from the accumulated state.
@@ -159,14 +79,6 @@ public class AclBuilder {
         List<ACL> result = new ArrayList<>();
         if (world != null) {
             result.add(world);
-        }
-        if (auth != null) {
-            result.add(auth);
-        }
-        for (Map<String, ACL> m : asList(digests, hosts, ips)) {
-            if (m != null) {
-                result.addAll(m.values());
-            }
         }
         return result;
     }
