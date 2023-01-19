f780f67859c7c48a2cc9d667e39ce54da61ef40a
Lukas Kral <lukywill16@gmail.com>
2023-01-20 00:41:18 +0100

    change number of kafkatopics to 200 instead of 1000
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/operators/topic/TopicScalabilityIsolatedST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/operators/topic/TopicScalabilityIsolatedST.java
@@ -31,7 +31,7 @@ import static io.strimzi.systemtest.Constants.SCALABILITY;
 public class TopicScalabilityIsolatedST extends AbstractST {
 
     private static final Logger LOGGER = LogManager.getLogger(TopicScalabilityIsolatedST.class);
-    private static final int NUMBER_OF_TOPICS = 1000;
+    private static final int NUMBER_OF_TOPICS = 200;
     private final String sharedClusterName = "topic-scalability-shared-cluster-name";
     final String topicPrefix = "example-topic";
 
