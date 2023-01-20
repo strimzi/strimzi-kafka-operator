d09672c1612425289a5c38933601a86e4bb9fef6
Jakub Scholz <www@scholzj.com>
2023-01-20 11:30:38 +0100

    Fix Maven Sonatype plugin version and configuration and push the `test` module (#7946)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/push-to-nexus.sh
+++ .azure/scripts/push-to-nexus.sh
@@ -5,7 +5,7 @@ export GPG_TTY=$(tty)
 echo $GPG_SIGNING_KEY | base64 -d > signing.gpg
 gpg --batch --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn -B $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -pl ./,crd-annotations,crd-generator,api -P ossrh deploy
+GPG_EXECUTABLE=gpg mvn -B $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -pl ./,crd-annotations,crd-generator,test,api -P ossrh deploy
 
 rm -rf signing.gpg
 gpg --delete-keys
