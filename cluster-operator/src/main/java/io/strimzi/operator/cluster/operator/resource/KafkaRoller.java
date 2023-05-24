d7b8be846ff17706eedac345bef612582b27f25e
Jakub Scholz <www@scholzj.com>
2023-05-24 12:40:36 +0200

    Fix Pod is Unschedulable error message in Kafka roller
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/resource/KafkaRoller.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/resource/KafkaRoller.java
@@ -495,7 +495,7 @@ public class KafkaRoller {
             // If the pod is unschedulable then deleting it, or trying to open an Admin client to it will make no difference
             // Treat this as fatal because if it's not possible to schedule one pod then it's likely that proceeding
             // and deleting a different pod in the meantime will likely result in another unschedulable pod.
-            throw new FatalProblem("Pod is unschedulable");
+            throw new FatalProblem("Pod is unschedulable or is not starting");
         }
         // Unless the annotation is present, check the pod is at least ready.
         boolean needsRestart = reasonToRestartPod.shouldRestart();
