4567a7701193f8a332e4cb58f66bdb4201a8a33f
Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>
2023-06-27 15:31:52 +0530

    Update build_keycloak_opa-ppc64le.sh
    Signed-off-by: Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>
    Signed-off-by: Vaibhav Nazare <Vaibhav.Nazare@ibm.com>

--- .jenkins/scripts/build_keycloak_opa-ppc64le.sh
+++ .jenkins/scripts/build_keycloak_opa-ppc64le.sh
@@ -6,7 +6,7 @@ sed -i 's#kubectl apply -n \${KEYCLOAK_OPERATOR_NAMESPACE} -f https://github.com
 sed -i 's#kubectl delete -n \${KEYCLOAK_OPERATOR_NAMESPACE} -f https://github.com/keycloak/keycloak-operator/raw/\${KEYCLOAK_VERSION}/deploy/operator.yaml#curl -s https://raw.githubusercontent.com/keycloak/keycloak-operator/\${KEYCLOAK_VERSION}/deploy/operator.yaml | sed "s\#Always\#IfNotPresent\#g" | kubectl delete -n \${KEYCLOAK_OPERATOR_NAMESPACE} -f -#g' systemtest/src/test/resources/oauth2/teardown_keycloak_operator.sh
 sed -i '/.*openpolicyagent\/opa.*/ a \          imagePullPolicy: IfNotPresent' systemtest/src/test/resources/opa/opa.yaml
 
-docker load < $HOME/$PPC64LE_IMAGE_TARBALL_DIR/keycloak-15.0.2.tar.gz
+docker load < $HOME/$PPC64LE_IMAGE_TARBALL_DIR/keycloak-21.0.0.tar.gz
 docker load < $HOME/$PPC64LE_IMAGE_TARBALL_DIR/keycloak-init-container-master.tar.gz
-docker load < $HOME/$PPC64LE_IMAGE_TARBALL_DIR/keycloak-operator-15.0.2.tar.gz
+docker load < $HOME/$PPC64LE_IMAGE_TARBALL_DIR/keycloak-operator-21.0.0.tar.gz
 docker load < $HOME/$PPC64LE_IMAGE_TARBALL_DIR/opa-latest.tar.gz
