7b02176f87b6217b588d6b8d30c70a51459752d8
Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>
2023-07-12 15:44:11 +0530

    Update setup_shellcheck.sh
    Signed-off-by: Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>

--- .azure/scripts/setup_shellcheck.sh
+++ .azure/scripts/setup_shellcheck.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-readonly VERSION="0.7.2"
+readonly VERSION="0.9.0"
 wget https://github.com/koalaman/shellcheck/releases/download/v$VERSION/shellcheck-v$VERSION.linux.x86_64.tar.xz -O shellcheck.tar.xz
 tar xf shellcheck.tar.xz -C /tmp --strip-components 1
 chmod +x /tmp/shellcheck
