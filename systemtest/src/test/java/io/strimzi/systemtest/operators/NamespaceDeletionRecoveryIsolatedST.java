0059d8b94c75f3f03625eb56148974bf456832f6
Lukas Kral <lukywill16@gmail.com>
2023-07-13 10:32:07 +0200

    disable whole class for now
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/operators/NamespaceDeletionRecoveryIsolatedST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/operators/NamespaceDeletionRecoveryIsolatedST.java
@@ -35,6 +35,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
@@ -52,6 +53,7 @@ import static io.strimzi.test.k8s.KubeClusterResource.kubeClient;
  */
 @Tag(RECOVERY)
 @Tag(REGRESSION)
+@Disabled("Tests are not working on Minikube at the moment")
 @KRaftNotSupported("Topic Operator is not supported by KRaft mode and is used in this test class")
 class NamespaceDeletionRecoveryIsolatedST extends AbstractST {
     private static final Logger LOGGER = LogManager.getLogger(NamespaceDeletionRecoveryIsolatedST.class);
