5fdd068c066117a617eb1fcf68eee6aa75d7bb0a
Paolo Patierno <ppatierno@live.com>
2023-09-05 21:14:11 +0200

    Customize silent curl Kafka binaries download (#9073)
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- docker-images/artifacts/build.sh
+++ docker-images/artifacts/build.sh
@@ -153,14 +153,14 @@ function download_kafka_binaries_from_cdn {
     
     local remote_path="${archive_url/https:\/\/archive.apache.org\/dist\//}"
     local cdn_url="https://dlcdn.apache.org/${remote_path}"
-    
+
     echo "Downloading from CDN: ${cdn_url}"
-    local cdn_code=$(curl -Ls -o "${local_path}" -w %{http_code} "${cdn_url}")
+    local cdn_code=$(curl -L ${CURL_ARGS} -o "${local_path}" -w %{http_code} "${cdn_url}")
     echo "CDN HTTP code: ${cdn_code}"
 
     if [[ "${cdn_code}" != "200" ]]; then
         echo "Download from CDN failed. Retrying with archive: ${archive_url}"
-        local archive_code=$(curl -Ls -o "${local_path}" -w %{http_code} "${archive_url}")
+        local archive_code=$(curl -L ${CURL_ARGS} -o "${local_path}" -w %{http_code} "${archive_url}")
         echo "Archive HTTP code: ${archive_code}"
     fi
 }
