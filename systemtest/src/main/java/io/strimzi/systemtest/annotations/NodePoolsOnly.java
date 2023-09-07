5a76700e902256b287b16abb8c6afcd746cbe8b8
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2023-09-07 18:34:41 +0200

    [systemtest] Add annotation for NodePools only mode in tests (#9088)
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

---
+++ systemtest/src/main/java/io/strimzi/systemtest/annotations/NodePoolsOnly.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.systemtest.annotations;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(NodePoolsOnlyCondition.class)
+public @interface NodePoolsOnly {
+}
