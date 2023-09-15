89a1226ecb1e152fa834d19a293e69fb41dae1ec
Jakub Scholz <www@scholzj.com>
2023-09-15 14:26:57 +0200

    Add support for Software Bills of Materials (SBOMs) to Strimzi CI
    Signed-off-by: Jakub Scholz <www@scholzj.com>

---
+++ .azure/scripts/install_syft.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+readonly VERSION="0.90.0"
+
+ARCH=$1
+if [ -z "$ARCH" ]; then
+    ARCH="amd64"
+fi
+
+wget https://github.com/anchore/syft/releases/download/v${VERSION}/syft_${VERSION}_linux_${ARCH}.tar.gz -O syft.tar.gz
+tar xf syft.tar.xz -C /tmp
+chmod +x /tmp/syft
+sudo mv /tmp/syft /usr/bin
