fb07efaa52030a906703649b068e02ca01c34505
see-quick <maros.orsak159@gmail.com>
2023-09-17 16:45:38 +0200

    update packit + checkstyle
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/connect/ConnectBuilderST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/connect/ConnectBuilderST.java
@@ -72,7 +72,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @Tag(REGRESSION)
