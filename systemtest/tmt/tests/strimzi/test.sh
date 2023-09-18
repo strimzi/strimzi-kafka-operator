44718d5faf1fc41b3bf88fc3352a00ff08e0bd06
see-quick <maros.orsak159@gmail.com>
2023-09-18 13:24:54 +0200

    fix ipv4 retrieve
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- systemtest/tmt/tests/strimzi/test.sh
+++ systemtest/tmt/tests/strimzi/test.sh
@@ -4,7 +4,7 @@
 cd ../../../../
 
 #run tests
-export DOCKER_REGISTRY="$(hostname --ip-address):5001"
+export DOCKER_REGISTRY="$(hostname -I | awk '/^10\./ && !/127\.0\.0\.1/ { print $1 }' | head -1):5001"
 mvn compile -pl config-model-generator -DskipTests -Dmaven.javadoc.skip=true --no-transfer-progress
 mvn verify -pl systemtest -P ${TEST_PROFILE} \
     $([[ "${TEST_GROUPS}" != "" ]] && echo "-Dgroups=${TEST_GROUPS}" || echo "") \
