6005ee4eca3dd6306fddaaad5c5df1a54cb288b3
Jakub Scholz <www@scholzj.com>
2023-09-21 15:16:31 +0200

    Use version variable in Cosign installation
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/install_cosign.sh
+++ .azure/scripts/install_cosign.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+readonly VERSION="2.2.0"
+
 ARCH=$1
 if [ -z "$ARCH" ]; then
     ARCH="amd64"
 fi
 
-curl -L https://github.com/sigstore/cosign/releases/download/v2.2.0/cosign-linux-${ARCH} > cosign && chmod +x cosign
+curl -L https://github.com/sigstore/cosign/releases/download/v${VERSION}/cosign-linux-${ARCH} > cosign && chmod +x cosign
 sudo mv cosign /usr/bin/
