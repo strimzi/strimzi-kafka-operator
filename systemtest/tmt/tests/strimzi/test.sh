01b88069808be62e9514825ea4e9042e532c3b1c
see-quick <maros.orsak159@gmail.com>
2023-09-22 11:43:17 +0200

    remove space because of bash great interpret
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- systemtest/tmt/tests/strimzi/test.sh
+++ systemtest/tmt/tests/strimzi/test.sh
@@ -7,7 +7,7 @@ cd ../../../../
 if [[ ${IP_FAMILY} == "ipv4" ]]; then
     DOCKER_REGISTRY=$(hostname --ip-address | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | awk '$1 != "127.0.0.1" { print $1 }' | head -1)
 elif [[ ${IP_FAMILY} == "ipv6" ]]; then
-    DOCKER_REGISTRY= "myregistry.local"
+    DOCKER_REGISTRY="myregistry.local"
 fi
 
 export DOCKER_REGISTRY="$DOCKER_REGISTRY:5001"
