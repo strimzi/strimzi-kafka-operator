dabc9ecd75541ee6c235664cb60de8eddb9f46e2
Jakub Scholz <www@scholzj.com>
2023-10-04 14:44:09 +0200

    Improve the description of the `connectCluster` field in MM2 (#9202)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- api/src/main/java/io/strimzi/api/kafka/model/KafkaMirrorMaker2Spec.java
+++ api/src/main/java/io/strimzi/api/kafka/model/KafkaMirrorMaker2Spec.java
@@ -42,7 +42,9 @@ public class KafkaMirrorMaker2Spec extends AbstractKafkaConnectSpec {
         this.clusters = clusters;
     }
 
-    @Description("The cluster alias used for Kafka Connect. The alias must match a cluster in the list at `spec.clusters`.")
+    @Description("The cluster alias used for Kafka Connect. " +
+            "The value must match the alias of the *target* Kafka cluster as specified in the `spec.clusters` configuration. " +
+            "The target Kafka cluster is used by the underlying Kafka Connect framework for its internal topics.")
     @JsonProperty(required = true)
     public String getConnectCluster() {
         return connectCluster;
