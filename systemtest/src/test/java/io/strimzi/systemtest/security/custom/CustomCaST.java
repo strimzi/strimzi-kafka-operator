3bb5d4a82bbeadf4f1e6ce846343cf806fce18fb
jkalinic <jkalinic@redhat.com>
2023-11-22 12:22:43 +0100

    fix import
    Signed-off-by: jkalinic <jkalinic@redhat.com>

--- systemtest/src/test/java/io/strimzi/systemtest/security/custom/CustomCaST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/security/custom/CustomCaST.java
@@ -49,7 +49,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.strimzi.systemtest.TestConstants.REGRESSION;
-import static io.strimzi.test.k8s.KubeClusterResource.cmdKubeClient;
 import static io.strimzi.test.k8s.KubeClusterResource.kubeClient;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
