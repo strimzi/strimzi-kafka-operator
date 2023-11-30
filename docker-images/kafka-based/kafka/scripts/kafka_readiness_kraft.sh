05f5ddec9ef7c4ba14c8a3d7466a0341298f30b5
Jakub Scholz <www@scholzj.com>
2023-11-30 15:28:31 +0100

    Fix netstat command in KRaft controller readiness to work with TCP6
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- docker-images/kafka-based/kafka/scripts/kafka_readiness_kraft.sh
+++ docker-images/kafka-based/kafka/scripts/kafka_readiness_kraft.sh
@@ -6,7 +6,7 @@ test -f $file
 roles=$(awk -F "process.roles=" '{print $2}' "$file")
 if [[ "$roles" =~ "controller" ]] && [[ ! "$roles" =~ "broker" ]]; then
   # For controller only mode, check if it is listening on port 9090 (configured in controller.listener.names).
-  netstat -lnt | grep -E 'tcp?[[:space:]]+[0-9]+[[:space:]]+[0-9]+[[:space:]]+[^ ]+:9090+[[:space:]]+[^ ]+[[:space:]]+LISTEN*'
+  netstat -lnt | grep -Eq 'tcp6?[[:space:]]+[0-9]+[[:space:]]+[0-9]+[[:space:]]+[^ ]+:9090.*LISTEN[[:space:]]*'
 else
   # For combined or broker only mode, check readiness via HTTP endpoint exposed by Kafka Agent.
   # The endpoint returns 204 when broker state is 3 (RUNNING).
