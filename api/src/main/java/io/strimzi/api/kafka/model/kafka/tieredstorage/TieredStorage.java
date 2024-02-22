11d02e1b68553f07bc647e014bc7de0cf8eb1593
Lixin Yao <lixin_yao@apple.com>
2024-02-21 19:17:10 -0800

    initial impl
    Signed-off-by: Lixin Yao <lixin_yao@apple.com>

---
+++ api/src/main/java/io/strimzi/api/kafka/model/kafka/tieredstorage/TieredStorage.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.strimzi.api.kafka.model.kafka.tieredstorage;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.strimzi.api.kafka.model.common.UnknownPropertyPreserving;
+import io.strimzi.crdgenerator.annotations.Description;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Abstract baseclass for different representations of tieredStorage, discriminated by {@link #getType() type}.
+ */
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.EXISTING_PROPERTY,
+    property = "type"
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = TieredStorageCustom.class, name = TieredStorage.TYPE_CUSTOM)}
+)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode
+public abstract class TieredStorage implements UnknownPropertyPreserving, Serializable {
+
+    private static final long serialVersionUID = 1L;
+    public static final String TYPE_CUSTOM = "custom";
+
+    private final Map<String, Object> additionalProperties = new HashMap<>(0);
+
+    @Description("Storage type, must be either 'custom'.")
+    public abstract String getType();
+
+    @Override
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @Override
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
+
---
+++ api/src/main/java/io/strimzi/api/kafka/model/kafka/tieredstorage/TieredStorageCustom.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.strimzi.api.kafka.model.kafka.tieredstorage;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.strimzi.api.kafka.model.common.Constants;
+import io.strimzi.crdgenerator.annotations.Description;
+import io.strimzi.crdgenerator.annotations.DescriptionFile;
+import io.sundr.builder.annotations.Buildable;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Configures a tieredStorage to use custom type.
+ */
+@DescriptionFile
+@Buildable(
+    editableEnabled = false,
+    builderPackage = Constants.FABRIC8_KUBERNETES_API
+)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode
+public class TieredStorageCustom extends TieredStorage {
+    private static final long serialVersionUID = 1L;
+
+    public static final String TYPE_CUSTOM = "custom";
+
+    private RemoteStorageManager remoteStorageManager;
+
+    @Description("Must be `" + TYPE_CUSTOM + "`")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Override
+    public String getType() {
+        return TYPE_CUSTOM;
+    }
+
+    @Description("Configuration for the remoteStorageManager.")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public RemoteStorageManager getRemoteStorageManager() {
+        return remoteStorageManager;
+    }
+
+    public void setRemoteStorageManager(RemoteStorageManager remoteStorageManager) {
+        this.remoteStorageManager = remoteStorageManager;
+    }
+}
