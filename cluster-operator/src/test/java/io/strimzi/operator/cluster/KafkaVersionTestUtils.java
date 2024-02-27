11132029a2b200ec73e2fd854f351fe487f15261
Jakub Scholz <www@scholzj.com>
2024-02-27 10:07:44 +0100

    Review comment PP
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/KafkaVersionTestUtils.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/KafkaVersionTestUtils.java
@@ -33,7 +33,7 @@ public class KafkaVersionTestUtils {
     public static final String LATEST_KAFKA_MIRROR_MAKER_IMAGE = KAFKA_MIRROR_MAKER_IMAGE_STR + LATEST_KAFKA_VERSION;
     public static final String LATEST_KAFKA_MIRROR_MAKER_2_IMAGE = KAFKA_MIRROR_MAKER_2_IMAGE_STR + LATEST_KAFKA_VERSION;
 
-    public static final String PREVIOUS_KAFKA_VERSION = "3.6.0";
+    public static final String PREVIOUS_KAFKA_VERSION = "3.6.1";
     public static final String PREVIOUS_FORMAT_VERSION = "3.6";
     public static final String PREVIOUS_PROTOCOL_VERSION = "3.6";
     public static final String PREVIOUS_METADATA_VERSION = "3.6-IV2";
