211d1f17b51d109e2a6aefa207d346d2de57a5df
Steffen Karlsson <steffen.karlsson@maersk.com>
2024-03-12 19:54:36 +0100

    Adding objects to include into `http` for enabling `consumer` and `producer`
    Signed-off-by: Steffen Karlsson <steffen.karlsson@maersk.com>

---
+++ api/src/main/java/io/strimzi/api/kafka/model/bridge/KafkaBridgeConsumerEnablement.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.api.kafka.model.bridge;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.strimzi.api.kafka.model.common.Constants;
+import io.strimzi.api.kafka.model.common.UnknownPropertyPreserving;
+import io.strimzi.crdgenerator.annotations.Description;
+import io.strimzi.crdgenerator.annotations.DescriptionFile;
+import io.sundr.builder.annotations.Buildable;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+
+@DescriptionFile
+@Buildable(
+        editableEnabled = false,
+        builderPackage = Constants.FABRIC8_KUBERNETES_API
+)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"enabled"})
+@EqualsAndHashCode
+@ToString
+public class KafkaBridgeConsumerEnablement implements UnknownPropertyPreserving, Serializable {
+
+    private boolean enabled = true;
+    private Map<String, Object> additionalProperties;
+
+    @Description("Whether the HTTP consumer should be enabled or disabled")
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties != null ? this.additionalProperties : emptyMap();
+    }
+
+    @Override
+    public void setAdditionalProperty(String name, Object value) {
+        if (this.additionalProperties == null) {
+            this.additionalProperties = new HashMap<>(1);
+        }
+        this.additionalProperties.put(name, value);
+    }
+}
