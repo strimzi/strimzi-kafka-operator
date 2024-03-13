67abde33264d6a4b2fdd35e262c082b486075da3
Paolo Patierno <ppatierno@live.com>
2024-03-13 18:41:37 +0100

    Some minor refactoring on migration code
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaMetadataStateManager.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaMetadataStateManager.java
@@ -14,6 +14,7 @@ import io.strimzi.operator.common.ReconciliationLogger;
 import io.strimzi.operator.common.model.StatusUtils;
 
 import java.util.Locale;
+import java.util.Objects;
 
 /**
  * Class used to reconcile the metadata state which represents where metadata are stored (ZooKeeper or KRaft)
@@ -47,7 +48,7 @@ public class KafkaMetadataStateManager {
 
     private KafkaMetadataState metadataState;
 
-    private String kraftAnno;
+    private final String kraftAnno;
 
     private boolean isMigrationDone = false;
 
@@ -66,11 +67,7 @@ public class KafkaMetadataStateManager {
         this.kraftAnno = kraftAnnotation(kafkaCr);
         KafkaMetadataState metadataStateFromKafkaCr = kafkaCr.getStatus() != null ? kafkaCr.getStatus().getKafkaMetadataState() : null;
         // missing metadata state means reconciling an already existing Kafka resource with newer operator supporting metadata state or first reconcile
-        if (metadataStateFromKafkaCr == null) {
-            this.metadataState = isKRaftAnnoEnabled() ? KafkaMetadataState.KRaft : KafkaMetadataState.ZooKeeper;
-        } else {
-            this.metadataState = metadataStateFromKafkaCr;
-        }
+        this.metadataState = Objects.requireNonNullElseGet(metadataStateFromKafkaCr, () -> isKRaftAnnoEnabled() ? KafkaMetadataState.KRaft : KafkaMetadataState.ZooKeeper);
         if (!useKRaftFeatureGateEnabled && (isKRaftAnnoEnabled() || isKRaftAnnoMigration())) {
             LOGGER.errorCr(reconciliation, "Trying to reconcile a KRaft enabled cluster or migrating to KRaft without the useKRaft feature gate enabled");
             throw new IllegalArgumentException("Failed to reconcile a KRaft enabled cluster or migration to KRaft because useKRaft feature gate is disabled");
