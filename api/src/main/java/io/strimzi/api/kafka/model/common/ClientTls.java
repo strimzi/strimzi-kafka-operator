8ee94895f60b9508dc30987ade300bac418ed854
Steffen Karlsson <steffen.karlsson@maersk.com>
2024-03-13 18:37:25 +0100

    Unused imports
    Signed-off-by: Steffen Karlsson <steffen.karlsson@maersk.com>

--- api/src/main/java/io/strimzi/api/kafka/model/common/ClientTls.java
+++ api/src/main/java/io/strimzi/api/kafka/model/common/ClientTls.java
@@ -15,8 +15,6 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyMap;
 
