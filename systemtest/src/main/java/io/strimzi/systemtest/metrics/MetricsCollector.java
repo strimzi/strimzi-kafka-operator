f42422461941ccb472e00f1f67ef8503e04208f7
Jakub Scholz <www@scholzj.com>
2024-06-03 10:51:25 +0200

    Remove unneeded method in metrics collector
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- systemtest/src/main/java/io/strimzi/systemtest/metrics/MetricsCollector.java
+++ systemtest/src/main/java/io/strimzi/systemtest/metrics/MetricsCollector.java
@@ -154,7 +154,6 @@ public class MetricsCollector {
         componentType = builder.componentType;
 
         if (builder.metricsPort <= 0) builder.metricsPort = getDefaultMetricsPortForComponent();
-        if (builder.metricsPath == null || builder.metricsPath.isEmpty()) builder.metricsPath = getDefaultMetricsPathForComponent();
 
         namespaceName = builder.namespaceName;
         scraperPodName = builder.scraperPodName;
@@ -185,19 +184,6 @@ public class MetricsCollector {
                 return kubeClient().getDeploymentSelectors(namespaceName, KafkaBridgeResources.componentName(componentName));
             default:
                 return new LabelSelector();
-        }
-    }
-
-    private String getDefaultMetricsPathForComponent() {
-        switch (this.componentType) {
-            case KafkaExporter:
-            case UserOperator:
-            case TopicOperator:
-            case ClusterOperator:
-            case KafkaBridge:
-                return "/metrics";
-            default:
-                return "";
         }
     }
 
