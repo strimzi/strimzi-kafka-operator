b19c9a2826b528ed8d4da828b57d5c36a1610c73
Jakub Stejskal <xstejs24@gmail.com>
2024-06-10 13:01:39 +0200

    Try to resolve ipv6 issues
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- .azure/scripts/setup-kind.sh
+++ .azure/scripts/setup-kind.sh
@@ -126,19 +126,6 @@ function adjust_inotify_limits {
     echo "Inotify limits adjusted successfully."
 }
 
-function setup_local_registry {
-	# See https://kind.sigs.k8s.io/docs/user/local-registry/
-	REGISTRY_DIR="/etc/containerd/certs.d/${hostname}:${reg_port}"
-
-    for node in $(kind get nodes --name kind-cluster); do
-        echo "Executing command in node:${node}"
-        docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
-        cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
-    [host."http://${reg_name}:5000"]
-EOF
-    done
-}
-
 setup_kube_directory
 install_kubectl
 install_kubernetes_provisioner
@@ -195,7 +182,16 @@ EOF
     # We want a consistent name that works from both ends, so we tell containerd to
     # alias localhost:${reg_port} to the registry container when pulling images
     # note: kind get nodes (default name `kind` and with specifying new name we have to use --name <cluster-name>
-    setup_local_registry
+	# See https://kind.sigs.k8s.io/docs/user/local-registry/
+	REGISTRY_DIR="/etc/containerd/certs.d/${hostname}:${reg_port}"
+
+    for node in $(kind get nodes --name kind-cluster); do
+        echo "Executing command in node:${node}"
+        docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
+        cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
+    [host."http://${reg_name}:5000"]
+EOF
+    done
 
 elif [[ "$IP_FAMILY" = "ipv6" ]]; then
     # for ipv6 configuration
@@ -243,7 +239,14 @@ EOF
     echo "${ula_fixed_ipv6}::1    ${registry_dns}" >> /etc/hosts
 
     # note: kind get nodes (default name `kind` and with specifying new name we have to use --name <cluster-name>
-    setup_local_registry
+	# See https://kind.sigs.k8s.io/docs/user/local-registry/
+
+    for node in $(kind get nodes --name kind-cluster); do
+        echo "Executing command in node:${node}"
+        cat <<EOF | docker exec -i "${node}" cp /dev/stdin "/etc/hosts"
+    "${ula_fixed_ipv6}::1    ${registry_dns}"
+EOF
+    done
 fi
 
 # Connect the registry to the cluster network if not already connected
