6b070bd155c6ec045ac210fbc73a6293bf074e50
Jakub Scholz <www@scholzj.com>
2024-06-30 17:53:54 +0200

    Fix pushing of artifacts to Nexus - Once again
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/push-to-nexus.sh
+++ .azure/scripts/push-to-nexus.sh
@@ -14,6 +14,6 @@ export GPG_TTY=$(tty)
 echo $GPG_SIGNING_KEY | base64 -d > signing.gpg
 gpg --batch --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -P ossrh verify deploy
+GPG_EXECUTABLE=gpg mvn $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -pl ./,crd-annotations,crd-generator,test,api -P ossrh deploy
 
 cleanup
