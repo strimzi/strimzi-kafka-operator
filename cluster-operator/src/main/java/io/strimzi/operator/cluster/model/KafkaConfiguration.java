f9986e5a2279f3e8c03e91b570631dd754da4649
Jakub Scholz <www@scholzj.com>
2024-09-27 14:16:54 +0200

    Decide if option is custom or not in a single place (#10643)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/model/KafkaConfiguration.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/KafkaConfiguration.java
@@ -2,7 +2,6 @@
  * Copyright Strimzi authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-
 package io.strimzi.operator.cluster.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -288,7 +287,7 @@ public class KafkaConfiguration extends AbstractConfiguration {
         Map<String, ConfigModel> configModel = readConfigModel(kafkaVersion);
         Set<String> result = new HashSet<>();
         for (Map.Entry<String, String> e :this.asOrderedProperties().asMap().entrySet()) {
-            if (!configModel.containsKey(e.getKey())) {
+            if (isCustomConfigurationOption(e.getKey(), configModel)) {
                 result.add(e.getKey() + "=" + e.getValue());
             }
         }
@@ -317,6 +316,21 @@ public class KafkaConfiguration extends AbstractConfiguration {
      * @return  True if the configuration is empty. False otherwise.
      */
     public boolean isEmpty() {
-        return this.asOrderedProperties().asMap().size() == 0;
+        return this.asOrderedProperties().asMap().isEmpty();
+    }
+
+    /**
+     * Checks if the Kafka configuration option is part of the Kafka configuration or is a custom option not recognized
+     * by Kafka broker configuration APIs. Custom options can be for example options used by plugins etc. But right now,
+     * also the options prefixed with the listener prefix as considered custom by this method as well (which is correct
+     * for the time being as we anyway do a rolling update when they change).
+     *
+     * @param optionName    Name of the option to check
+     * @param configModel   Configuration model for given Kafka version
+     *
+     * @return  True if entry is custom (not default). False otherwise.
+     */
+    public static boolean isCustomConfigurationOption(String optionName, Map<String, ConfigModel> configModel) {
+        return !configModel.containsKey(optionName);
     }
 }
