692ddc4af18fb4f9ac40072dca02ec4e2c0347d5
Jakub Stejskal <xstejs24@gmail.com>
2024-11-11 09:01:34 +0100

    Prepare upgrade files in packit pipelines in test phase where all envs are set properly (#10819)
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- .azure/scripts/setup_upgrade.sh
+++ .azure/scripts/setup_upgrade.sh
@@ -2,13 +2,13 @@
 
 set -x
 
-RELEASE=$(cat release.version)
+RELEASE_VERSION=$(cat release.version)
 
 # The commands 1) and 2) are used for change tag in installation files for CO. For each branch only of them match and apply changes
 # 1) The following command is applied only for branches, where release.version contains *SNAPSHOT* (main + others)
 sed -i "s#:latest#:${DOCKER_TAG}#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 # 2) The following command is applied only for release branches, where release.version contains a final version like 0.29.0
-sed -i "s#:${RELEASE}#:${DOCKER_TAG}#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
+sed -i "s#:${RELEASE_VERSION}#:${DOCKER_TAG}#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
 
 # Change registry and org
 sed -i "s#/opt/${DOCKER_REGISTRY}#/opt#g" packaging/install/cluster-operator/*-Deployment-strimzi-cluster-operator.yaml
