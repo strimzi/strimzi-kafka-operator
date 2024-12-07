199b6d2e5cb3b4bd2d2a3948b105d0ed106c3056
Jakub Scholz <www@scholzj.com>
2024-12-07 02:07:53 +0100

    Update Prometheus JMX Exporter to 1.1.0 (#10917)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- docker-images/kafka-based/kafka/scripts/kafka_connect_run.sh
+++ docker-images/kafka-based/kafka/scripts/kafka_connect_run.sh
@@ -45,7 +45,7 @@ export LOG_DIR="$KAFKA_HOME"
 
 # enabling Prometheus JMX exporter as Java agent
 if [ "$KAFKA_CONNECT_METRICS_ENABLED" = "true" ]; then
-    KAFKA_OPTS="${KAFKA_OPTS} -javaagent:$(ls "$KAFKA_HOME"/libs/jmx_prometheus_javaagent*.jar)=9404:$KAFKA_HOME/custom-config/metrics-config.json"
+    KAFKA_OPTS="${KAFKA_OPTS} -javaagent:$(ls "$JMX_EXPORTER_HOME"/jmx_prometheus_javaagent*.jar)=9404:$KAFKA_HOME/custom-config/metrics-config.json"
     export KAFKA_OPTS
 fi
 
