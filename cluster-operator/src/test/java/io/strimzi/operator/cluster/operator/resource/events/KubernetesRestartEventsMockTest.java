b5060222ffc07b67b7efe003bd326e48b2896fc8
Gantigmaa Selenge <39860586+tinaselenge@users.noreply.github.com>
2024-12-12 22:58:39 +0000

    Revert "Allow KafkaRoller talk to controller directly (#10016)" from 0.45.x release (#10944)
    Signed-off-by: Gantigmaa Selenge <tina.selenge@gmail.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/resource/events/KubernetesRestartEventsMockTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/resource/events/KubernetesRestartEventsMockTest.java
@@ -617,17 +617,7 @@ public class KubernetesRestartEventsMockTest {
             }
 
             @Override
-            public Admin createControllerAdminClient(String controllerBootstrapHostnames, PemTrustSet kafkaCaTrustSet, PemAuthIdentity authIdentity) {
-                return adminClientSupplier.get();
-            }
-
-            @Override
             public Admin createAdminClient(String bootstrapHostnames, PemTrustSet kafkaCaTrustSet, PemAuthIdentity authIdentity, Properties config) {
-                return adminClientSupplier.get();
-            }
-
-            @Override
-            public Admin createControllerAdminClient(String controllerBootstrapHostnames, PemTrustSet kafkaCaTrustSet, PemAuthIdentity authIdentity, Properties config) {
                 return adminClientSupplier.get();
             }
         };
