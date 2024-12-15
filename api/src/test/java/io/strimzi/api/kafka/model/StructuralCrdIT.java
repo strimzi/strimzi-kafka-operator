998cf625639e3d006161b6f7dab35cec5919c811
Jakub Scholz <www@scholzj.com>
2024-12-15 13:02:43 +0100

    Rename `04A-Crd-kafkanodepool.yaml` to `045-Crd-kafkanodepool.yaml` (#10953)

--- api/src/test/java/io/strimzi/api/kafka/model/StructuralCrdIT.java
+++ api/src/test/java/io/strimzi/api/kafka/model/StructuralCrdIT.java
@@ -37,7 +37,7 @@ public class StructuralCrdIT extends AbstractCrdIT {
             "kafkaconnectors.kafka.strimzi.io", "047-Crd-kafkaconnector.yaml",
             "kafkamirrormaker2s.kafka.strimzi.io", "048-Crd-kafkamirrormaker2.yaml",
             "kafkarebalances.kafka.strimzi.io", "049-Crd-kafkarebalance.yaml",
-            "kafkanodepools.kafka.strimzi.io", "04A-Crd-kafkanodepool.yaml"
+            "kafkanodepools.kafka.strimzi.io", "045-Crd-kafkanodepool.yaml"
     );
 
     @BeforeEach
