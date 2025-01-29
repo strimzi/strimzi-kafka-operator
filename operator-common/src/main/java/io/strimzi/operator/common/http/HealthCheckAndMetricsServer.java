af1a9656c415b04e64a123e80f50fb72b47fe9f7
Jakub Scholz <www@scholzj.com>
2025-01-29 14:44:52 +0100

    Update Jetty used by Strimzi operators to Jetty 12
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- operator-common/src/main/java/io/strimzi/operator/common/http/HealthCheckAndMetricsServer.java
+++ operator-common/src/main/java/io/strimzi/operator/common/http/HealthCheckAndMetricsServer.java
@@ -8,17 +8,17 @@ import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.strimzi.operator.common.MetricsProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.util.Callback;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -84,7 +84,7 @@ public class HealthCheckAndMetricsServer {
         ContextHandler contextHandler = new ContextHandler();
         contextHandler.setContextPath(path);
         contextHandler.setHandler(handler);
-        contextHandler.setAllowNullPathInfo(true);
+        contextHandler.setAllowNullPathInContext(true);
         return contextHandler;
     }
 
@@ -115,62 +115,66 @@ public class HealthCheckAndMetricsServer {
     /**
      * Handler responsible for the liveness check
      */
-    class HealthyHandler extends AbstractHandler {
+    class HealthyHandler extends Handler.Abstract {
         @Override
-        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
-            response.setContentType("application/json");
+        public boolean handle(Request request, Response response, Callback callback) throws Exception {
+            response.getHeaders().put(HttpHeader.CONTENT_TYPE, "application/json; charset=UTF-8");
 
             if (liveness.isAlive()) {
                 response.setStatus(HttpServletResponse.SC_OK);
-                response.getWriter().println("{\"status\": \"ok\"}");
+                response.write(true, StandardCharsets.UTF_8.encode("{\"status\": \"ok\"}"), callback);
             } else {
                 response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                response.getWriter().println("{\"status\": \"not-ok\"}");
+                response.write(true, StandardCharsets.UTF_8.encode("{\"status\": \"not-ok\"}"), callback);
             }
+
             LOGGER.debug("Responding {} to GET /healthy", response.getStatus());
-            baseRequest.setHandled(true);
+
+            return true;
         }
     }
 
     /**
      * Handler responsible for the readiness check
      */
-    class ReadyHandler extends AbstractHandler {
+    class ReadyHandler extends Handler.Abstract {
         @Override
-        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
-            response.setContentType("application/json");
+        public boolean handle(Request request, Response response, Callback callback) throws Exception {
+            response.getHeaders().put(HttpHeader.CONTENT_TYPE, "application/json; charset=UTF-8");
 
             if (readiness.isReady()) {
                 response.setStatus(HttpServletResponse.SC_OK);
-                response.getWriter().println("{\"status\": \"ok\"}");
+                response.write(true, StandardCharsets.UTF_8.encode("{\"status\": \"ok\"}"), callback);
             } else {
                 response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                response.getWriter().println("{\"status\": \"not-ok\"}");
-
+                response.write(true, StandardCharsets.UTF_8.encode("{\"status\": \"not-ok\"}"), callback);
             }
-            LOGGER.debug("Responding {} to GET /ready", response.getStatus());
-            baseRequest.setHandled(true);
+
+            LOGGER.debug("Responding {} to GET /healthy", response.getStatus());
+
+            return true;
         }
     }
 
     /**
      * Handler responsible for the metrics
      */
-    class MetricsHandler extends AbstractHandler {
+    class MetricsHandler extends Handler.Abstract {
         @Override
-        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        public boolean handle(Request request, Response response, Callback callback) throws Exception {
             if (prometheusMeterRegistry != null) {
-                response.setContentType("text/plain; version=0.0.4");
-                response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+                response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/plain; version=0.0.4; charset=UTF-8");
                 response.setStatus(HttpServletResponse.SC_OK);
-                prometheusMeterRegistry.scrape(response.getWriter());
+                response.write(true, StandardCharsets.UTF_8.encode(prometheusMeterRegistry.scrape()), callback);
             } else {
-                response.setContentType("text/plain");
+                response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/plain; charset=UTF-8");
                 response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
-                response.getWriter().println("Prometheus metrics are not enabled");
+                response.write(true, StandardCharsets.UTF_8.encode("Prometheus metrics are not enabled"), callback);
             }
 
-            baseRequest.setHandled(true);
+            LOGGER.debug("Responding {} to GET /metrics", response.getStatus());
+
+            return true;
         }
     }
 }
