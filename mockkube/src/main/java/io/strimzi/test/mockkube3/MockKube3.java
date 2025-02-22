4ccd6dada2eaf255c2e6b290cdc50743c392795e
Jakub Scholz <www@scholzj.com>
2025-02-22 18:21:38 +0100

    Update KindContainer to 1.4.9 and to Kube 1.32 (#11173)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- mockkube/src/main/java/io/strimzi/test/mockkube3/MockKube3.java
+++ mockkube/src/main/java/io/strimzi/test/mockkube3/MockKube3.java
@@ -25,6 +25,7 @@ import io.strimzi.test.mockkube3.controllers.MockDeletionController;
 import io.strimzi.test.mockkube3.controllers.MockDeploymentController;
 import io.strimzi.test.mockkube3.controllers.MockPodController;
 import io.strimzi.test.mockkube3.controllers.MockServiceController;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,6 +40,8 @@ import java.util.concurrent.TimeUnit;
  * running controllers.
  */
 public class MockKube3 {
+    private final static String ETCD_IMAGE = "registry.k8s.io/etcd:3.5.17-0";
+
     private final ApiServerContainer<?> apiServer;
     private final List<AbstractMockController> controllers = new ArrayList<>();
     private final List<String> crds = new ArrayList<>();
@@ -53,7 +56,7 @@ public class MockKube3 {
      * Constructs the Kubernetes Mock Kube Server
      */
     public MockKube3() {
-        this.apiServer = new ApiServerContainer<>();
+        this.apiServer = new ApiServerContainer<>().withEtcdImage(DockerImageName.parse(ETCD_IMAGE));
     }
 
     /**
