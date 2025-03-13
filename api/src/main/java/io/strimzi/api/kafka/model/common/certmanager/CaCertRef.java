dd1bdb9ff534e7b20a6e5e10a7abce262c6a88dd
Katherine Stanley <11195226+katheris@users.noreply.github.com>
2025-03-13 11:52:25 +0000

    Add cert-manager API classes
    Signed-off-by: Katherine Stanley <11195226+katheris@users.noreply.github.com>

---
+++ api/src/main/java/io/strimzi/api/kafka/model/common/certmanager/CaCertRef.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.api.kafka.model.common.certmanager;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.strimzi.api.kafka.model.common.Constants;
+import io.strimzi.api.kafka.model.common.UnknownPropertyPreserving;
+import io.strimzi.crdgenerator.annotations.Description;
+import io.sundr.builder.annotations.Buildable;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Description("Reference to the Secret containing the CA certificate (public key) " +
+        "that trusts certificates issued by cert-manager. " +
+        "This only applies if the CA type is set to `cert-manager.io`.")
+@Buildable(
+        editableEnabled = false,
+        builderPackage = Constants.FABRIC8_KUBERNETES_API
+)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "secretName", "certificate" })
+@EqualsAndHashCode
+@ToString
+public class CaCertRef implements UnknownPropertyPreserving {
+    private String secretName;
+    private String certificate;
+    private Map<String, Object> additionalProperties;
+
+    @Description("The name of the Secret. " +
+            "Required.")
+    public String getSecretName() {
+        return secretName;
+    }
+
+    public void setSecretName(String secretName) {
+        this.secretName = secretName;
+    }
+
+    @Description("The name of the file certificate in the Secret." +
+            "Required.")
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    @Override
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties != null ? this.additionalProperties : Map.of();
+    }
+
+    @Override
+    public void setAdditionalProperty(String name, Object value) {
+        if (this.additionalProperties == null) {
+            this.additionalProperties = new HashMap<>(2);
+        }
+        this.additionalProperties.put(name, value);
+    }
+}
