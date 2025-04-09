f524214b8e3ae81fda17b81b53d4d3206d1cb916
Jakub Stejskal <xstejs24@gmail.com>
2025-04-09 12:24:48 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! PoC of system tests pipeline on GitHub Actions
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- .azure/scripts/setup_shellcheck.sh
+++ .azure/scripts/setup_shellcheck.sh
@@ -6,6 +6,11 @@ if [ -z "$ARCH" ] || [ "$ARCH" == "amd64" ]; then
     ARCH="x86_64"
 fi
 
+if [ "$ARCH" == "arm64" ]; then
+    ARCH="aarch64"
+fi
+
+
 readonly VERSION="0.9.0"
 wget https://github.com/koalaman/shellcheck/releases/download/v$VERSION/shellcheck-v$VERSION.linux.$ARCH.tar.xz -O shellcheck.tar.xz
 tar xf shellcheck.tar.xz -C /tmp --strip-components 1
