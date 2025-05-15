a2826bd2b3e0a4ed1be5cb3ce851ab32df933f17
Jakub Stejskal <xstejs24@gmail.com>
2025-05-15 13:52:00 +0200

    Enhance CI scripts to better work with arm runners and bump Minikube version
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- .azure/scripts/setup_shellcheck.sh
+++ .azure/scripts/setup_shellcheck.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+ARCH=$1
+if [ -z "$ARCH" ] || [ "$ARCH" == "amd64" ]; then
+    ARCH="x86_64"
+fi
+
+if [ "$ARCH" == "arm64" ]; then
+    ARCH="aarch64"
+fi
+
 readonly VERSION="0.9.0"
-wget https://github.com/koalaman/shellcheck/releases/download/v$VERSION/shellcheck-v$VERSION.linux.x86_64.tar.xz -O shellcheck.tar.xz
+wget https://github.com/koalaman/shellcheck/releases/download/v$VERSION/shellcheck-v$VERSION.linux.$ARCH.tar.xz -O shellcheck.tar.xz
 tar xf shellcheck.tar.xz -C /tmp --strip-components 1
 chmod +x /tmp/shellcheck
 sudo mv /tmp/shellcheck /usr/bin
