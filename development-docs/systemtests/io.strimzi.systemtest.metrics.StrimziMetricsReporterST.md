d498fdeade723d3e34310847836fd1d7848f7591
Federico Valeri <fedevaleri@gmail.com>
2025-06-20 13:39:21 +0200

    Add system tests, examples and dashboards
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>

--- development-docs/systemtests/io.strimzi.systemtest.metrics.StrimziMetricsReporterST.md
+++ development-docs/systemtests/io.strimzi.systemtest.metrics.StrimziMetricsReporterST.md
@@ -9,7 +9,7 @@
 | 1. | Create namespace {@namespace}. | Namespace {@namespace} is created. |
 | 2. | Deploy Cluster Operator. | Cluster Operator is deployed. |
 | 3. | Deploy Kafka {@clusterName} with Strimzi Metrics Reporter. | Kafka @{clusterName} is deployed. |
-| 4. | Deploy scraper Pod in namespace {@namespace} for collecting metrics from Strimzi pods. | Scraper Pods is deployed. |
+| 4. | Deploy scraper Pod in namespace {@namespace} for collecting metrics from Strimzi pods. | Scraper Pod is deployed. |
 | 5. | Create KafkaTopic resource. | KafkaTopic resource is Ready. |
 | 6. | Create collector for Kafka. | Metrics collected in collectors structs. |
 
@@ -25,6 +25,26 @@
 
 <hr style="border:1px solid">
 
+## testKafkaConnectAndConnectorMetrics
+
+**Description:** This test case checks several random metrics exposed by Kafka Connect.
+
+**Steps:**
+
+| Step | Action | Result |
+| - | - | - |
+| 1. | Deploy KafkaConnect into {@namespace}. | KafkaConnect is up and running. |
+| 2. | Create KafkaConnector for KafkaConnect from step 1. | KafkaConnector is in Ready state. |
+| 3. | Create metrics collector and collect metrics from KafkaConnect Pods. | Metrics are collected. |
+| 4. | Check if specific metric is available in collected metrics from KafkaConnect Pods. | Metric is available with expected value. |
+
+**Labels:**
+
+* [kafka](labels/kafka.md)
+* [metrics](labels/metrics.md)
+* [connect](labels/connect.md)
+
+
 ## testKafkaMetrics
 
 **Description:** This test case checks several metrics exposed by Kafka.
@@ -39,4 +59,23 @@
 
 * [kafka](labels/kafka.md)
 * [metrics](labels/metrics.md)
+
+
+## testMirrorMaker2Metrics
+
+**Description:** This test case checks several metrics exposed by KafkaMirrorMaker2.
+
+**Steps:**
+
+| Step | Action | Result |
+| - | - | - |
+| 1. | Deploy KafkaMirrorMaker2 into {@namespace}. | KafkaMirrorMaker2 is in Ready state. |
+| 2. | Collect metrics from KafkaMirrorMaker2 pod. | Metrics are collected. |
+| 3. | Check if specific metric is available in collected metrics from KafkaMirrorMaker2 pods. | Metric is available with expected value. |
+
+**Labels:**
+
+* [kafka](labels/kafka.md)
+* [metrics](labels/metrics.md)
+* `mirror-maker-2` (description file doesn't exist)
 
--- systemtest/src/test/java/io/strimzi/systemtest/metrics/StrimziMetricsReporterST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/metrics/StrimziMetricsReporterST.java
@@ -18,6 +18,9 @@ import io.strimzi.systemtest.docs.TestDocsLabels;
 import io.strimzi.systemtest.performance.gather.collectors.BaseMetricsCollector;
 import io.strimzi.systemtest.resources.operator.SetupClusterOperator;
 import io.strimzi.systemtest.storage.TestStorage;
+import io.strimzi.systemtest.templates.crd.KafkaConnectTemplates;
+import io.strimzi.systemtest.templates.crd.KafkaConnectorTemplates;
+import io.strimzi.systemtest.templates.crd.KafkaMirrorMaker2Templates;
 import io.strimzi.systemtest.templates.crd.KafkaNodePoolTemplates;
 import io.strimzi.systemtest.templates.crd.KafkaTemplates;
 import io.strimzi.systemtest.templates.crd.KafkaTopicTemplates;
@@ -31,12 +34,16 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 import static io.strimzi.systemtest.TestTags.ACCEPTANCE;
+import static io.strimzi.systemtest.TestTags.CONNECT;
+import static io.strimzi.systemtest.TestTags.CONNECT_COMPONENTS;
 import static io.strimzi.systemtest.TestTags.METRICS;
+import static io.strimzi.systemtest.TestTags.MIRROR_MAKER2;
 import static io.strimzi.systemtest.TestTags.REGRESSION;
 import static io.strimzi.systemtest.TestTags.SANITY;
 import static io.strimzi.systemtest.utils.specific.MetricsUtils.assertMetricCountHigherThan;
 import static io.strimzi.systemtest.utils.specific.MetricsUtils.assertMetricValue;
 import static io.strimzi.systemtest.utils.specific.MetricsUtils.assertMetricValueCount;
+import static io.strimzi.systemtest.utils.specific.MetricsUtils.assertMetricValueHigherThanOrEqualTo;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 @Tag(SANITY)
@@ -48,7 +55,7 @@ import static org.junit.jupiter.api.Assumptions.assumeFalse;
         @Step(value = "Create namespace {@namespace}.", expected = "Namespace {@namespace} is created."),
         @Step(value = "Deploy Cluster Operator.", expected = "Cluster Operator is deployed."),
         @Step(value = "Deploy Kafka {@clusterName} with Strimzi Metrics Reporter.", expected = "Kafka @{clusterName} is deployed."),
-        @Step(value = "Deploy scraper Pod in namespace {@namespace} for collecting metrics from Strimzi pods.", expected = "Scraper Pods is deployed."),
+        @Step(value = "Deploy scraper Pod in namespace {@namespace} for collecting metrics from Strimzi pods.", expected = "Scraper Pod is deployed."),
         @Step(value = "Create KafkaTopic resource.", expected = "KafkaTopic resource is Ready."),
         @Step(value = "Create collector for Kafka.", expected = "Metrics collected in collectors structs.")
     },
@@ -77,9 +84,69 @@ public class StrimziMetricsReporterST extends AbstractST {
         }
     )
     void testKafkaMetrics() {
+        kafkaCollector.collectMetricsFromPods(TestConstants.METRICS_COLLECT_TIMEOUT);
+
         assertMetricValueCount(kafkaCollector, "kafka_server_replicamanager_leadercount", 3.0);
         assertMetricCountHigherThan(kafkaCollector, "kafka_server_replicamanager_partitioncount", 2);
         assertMetricValue(kafkaCollector, "kafka_server_replicamanager_underreplicatedpartitions", 0.0);
+    }
+
+    @ParallelTest
+    @Tag(CONNECT)
+    @Tag(CONNECT_COMPONENTS)
+    @Tag(ACCEPTANCE)
+    @TestDoc(
+        description = @Desc("This test case checks several random metrics exposed by Kafka Connect."),
+        steps = {
+            @Step(value = "Deploy KafkaConnect into {@namespace}.", expected = "KafkaConnect is up and running."),
+            @Step(value = "Create KafkaConnector for KafkaConnect from step 1.", expected = "KafkaConnector is in Ready state."),
+            @Step(value = "Create metrics collector and collect metrics from KafkaConnect Pods.", expected = "Metrics are collected."),
+            @Step(value = "Check if specific metric is available in collected metrics from KafkaConnect Pods.", expected = "Metric is available with expected value.")
+        },
+        labels = {
+            @Label(value = TestDocsLabels.KAFKA),
+            @Label(value = TestDocsLabels.METRICS),
+            @Label(value = TestDocsLabels.CONNECT)
+        }
+    )
+    void testKafkaConnectAndConnectorMetrics() {
+        BaseMetricsCollector kafkaConnectCollector = kafkaCollector.toBuilder()
+            .withComponent(KafkaConnectMetricsComponent.create("my-connect"))
+            .build();
+
+        kafkaConnectCollector.collectMetricsFromPods(TestConstants.METRICS_COLLECT_TIMEOUT);
+
+        assertMetricValueHigherThanOrEqualTo(kafkaConnectCollector, "kafka_connect_connect_node_metrics_request\\{client_id=\".*\"}", 0.0);
+        assertMetricValueHigherThanOrEqualTo(kafkaConnectCollector, "kafka_connect_connect_node_metrics_response\\{client_id=\".*\"}", 0.0);
+        assertMetricValueHigherThanOrEqualTo(kafkaConnectCollector, "kafka_connect_connect_metrics_network_io\\{client_id=\".*\"}", 0.0);
+    }
+
+    @ParallelTest
+    @Tag(MIRROR_MAKER2)
+    @Tag(CONNECT_COMPONENTS)
+    @Tag(ACCEPTANCE)
+    @TestDoc(
+        description = @Desc("This test case checks several metrics exposed by KafkaMirrorMaker2."),
+        steps = {
+            @Step(value = "Deploy KafkaMirrorMaker2 into {@namespace}.", expected = "KafkaMirrorMaker2 is in Ready state."),
+            @Step(value = "Collect metrics from KafkaMirrorMaker2 pod.", expected = "Metrics are collected."),
+            @Step(value = "Check if specific metric is available in collected metrics from KafkaMirrorMaker2 pods.", expected = "Metric is available with expected value.")
+        },
+        labels = {
+            @Label(value = TestDocsLabels.KAFKA),
+            @Label(value = TestDocsLabels.METRICS),
+            @Label(value = TestDocsLabels.MIRROR_MAKER_2)
+        }
+    )
+    void testMirrorMaker2Metrics() {
+        BaseMetricsCollector kmm2Collector = kafkaCollector.toBuilder()
+            .withComponent(KafkaMirrorMaker2MetricsComponent.create("my-mm2"))
+            .build();
+
+        kmm2Collector.collectMetricsFromPods(TestConstants.METRICS_COLLECT_TIMEOUT);
+
+        assertMetricValue(kmm2Collector, "kafka_connect_connect_worker_metrics_connector_count", 2.0);
+        assertMetricValueHigherThanOrEqualTo(kmm2Collector, "kafka_connect_connect_worker_metrics_task_count", 0.0);
     }
 
     @BeforeAll
@@ -98,8 +165,10 @@ public class StrimziMetricsReporterST extends AbstractST {
 
         KubeResourceManager.get().createResourceWithWait(
             KafkaNodePoolTemplates.brokerPool(testStorage.getNamespaceName(), testStorage.getBrokerPoolName(), testStorage.getClusterName(), 3).build(),
-            KafkaNodePoolTemplates.controllerPool(testStorage.getNamespaceName(), testStorage.getControllerPoolName(), testStorage.getClusterName(), 3).build()
+            KafkaNodePoolTemplates.controllerPool(testStorage.getNamespaceName(), testStorage.getControllerPoolName(), testStorage.getClusterName(), 3).build(),
+            KafkaNodePoolTemplates.mixedPool(testStorage.getNamespaceName(), testStorage.getBrokerPoolName() + "-tgt", testStorage.getClusterName() + "-tgt", 1).build()
         );
+
         KubeResourceManager.get().createResourceWithWait(
             KafkaTemplates.kafka(testStorage.getNamespaceName(), testStorage.getClusterName(), 3)
                 .editSpec()
@@ -110,12 +179,35 @@ public class StrimziMetricsReporterST extends AbstractST {
                             .endValues()
                         .endStrimziMetricsReporterConfig()
                     .endKafka()
-                .endSpec().build()
+                .endSpec().build(),
+            KafkaTemplates.kafkaWithoutEntityOperator(testStorage.getNamespaceName(), testStorage.getClusterName() + "-tgt", 1).build()
         );
 
         KubeResourceManager.get().createResourceWithWait(
+            KafkaConnectTemplates.kafkaConnectWithFilePlugin(testStorage.getNamespaceName(), "my-connect",
+                    testStorage.getClusterName(), 1)
+                .editOrNewSpec()
+                    .withNewStrimziMetricsReporterConfig()
+                        .withNewValues()
+                            .withAllowList("kafka_connect.*")
+                        .endValues()
+                    .endStrimziMetricsReporterConfig()
+                .endSpec().build(),
+            KafkaMirrorMaker2Templates.kafkaMirrorMaker2(testStorage.getNamespaceName(), "my-mm2",
+                    testStorage.getClusterName(), testStorage.getClusterName() + "-tgt", 1, false)
+                .editOrNewSpec()
+                    .withNewStrimziMetricsReporterConfig()
+                        .withNewValues()
+                            .withAllowList("kafka_connect.*")
+                        .endValues()
+                    .endStrimziMetricsReporterConfig()
+                .endSpec().build()
+        );
+
+        KubeResourceManager.get().createResourceWithoutWait(
             ScraperTemplates.scraperPod(testStorage.getNamespaceName(), testStorage.getScraperName()).build(),
-            KafkaTopicTemplates.topic(testStorage.getNamespaceName(), testStorage.getTopicName(), testStorage.getClusterName(), 5, 2).build()
+            KafkaTopicTemplates.topic(testStorage.getNamespaceName(), testStorage.getTopicName(), testStorage.getClusterName(), 5, 2).build(),
+            KafkaConnectorTemplates.kafkaConnector(testStorage.getNamespaceName(), "my-connect").build()
         );
 
         // wait some time for metrics to be stable, at least reconciliation interval + 10s
@@ -129,7 +221,5 @@ public class StrimziMetricsReporterST extends AbstractST {
             .withNamespaceName(testStorage.getNamespaceName())
             .withComponent(KafkaMetricsComponent.create(testStorage.getClusterName()))
             .build();
-
-        kafkaCollector.collectMetricsFromPods(TestConstants.METRICS_COLLECT_TIMEOUT);
     }
 }
