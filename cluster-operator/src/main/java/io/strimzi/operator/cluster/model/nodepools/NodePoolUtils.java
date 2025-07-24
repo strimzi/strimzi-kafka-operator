6fb1b3fd8e257dcfa119a65a776bb987c6c2abf7
Lukas Kral <lukywill16@gmail.com>
2025-07-24 17:43:22 +0200

    remove check for Kafka version from the validateKRaftJbodStorage and update comment
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/model/nodepools/NodePoolUtils.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/nodepools/NodePoolUtils.java
@@ -167,10 +167,9 @@ public class NodePoolUtils {
         for (KafkaNodePool pool : nodePools)    {
             if (pool.getSpec().getStorage() != null
                     && pool.getSpec().getStorage() instanceof JbodStorage jbod) {
-                if (jbod.getVolumes().size() > 1
-                        && (KafkaVersion.compareDottedVersions(versionChange.from().version(), "3.7.0") < 0 || KafkaVersion.compareDottedIVVersions(versionChange.metadataVersion(), "3.7-IV2") < 0)) {
+                if (jbod.getVolumes().size() > 1 && KafkaVersion.compareDottedIVVersions(versionChange.metadataVersion(), "3.7-IV2") < 0) {
                     // When running Kafka older than 3.7.0, JBOD storage is not supported in KRaft.
-                    // This check should be removed when we remove support for Kafka 3.6.x.
+                    // This check should be removed when all the Kafka versions supported by Strimzi have minimal metadata version 3.7-IV2 and newer (kept for upgrade purposes).
                     // This is tracked in https://github.com/strimzi/strimzi-kafka-operator/issues/9960.
                     errors.add("Using more than one disk in a JBOD storage in KRaft mode is supported only with Apache Kafka 3.7.0 or newer and metadata version 3.7-IV2 or newer (in KafkaNodePool " + pool.getMetadata().getName() + ")");
                 }
