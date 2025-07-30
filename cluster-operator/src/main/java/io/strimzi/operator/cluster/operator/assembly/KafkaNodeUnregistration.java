966d8883682457b100734eef37ff2330f66fa271
Paolo Patierno <ppatierno@live.com>
2025-07-30 10:08:43 +0200

    Refactored broker nodes unregistration by using the Admin Client API (KIP-1073)
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaNodeUnregistration.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/operator/assembly/KafkaNodeUnregistration.java
@@ -15,11 +15,15 @@ import io.strimzi.operator.common.auth.PemTrustSet;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.DescribeClusterOptions;
 import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.Node;
 import org.apache.kafka.common.errors.BrokerIdNotRegisteredException;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Contains utility methods for unregistering KRaft nodes from a Kafka cluster after scale-down
@@ -45,7 +49,7 @@ public class KafkaNodeUnregistration {
             AdminClientProvider adminClientProvider,
             PemTrustSet pemTrustSet,
             PemAuthIdentity pemAuthIdentity,
-            List<Integer> nodeIdsToUnregister
+            Set<Integer> nodeIdsToUnregister
     ) {
         try {
             String bootstrapHostname = KafkaResources.bootstrapServiceName(reconciliation.name()) + "." + reconciliation.namespace() + ".svc:" + KafkaCluster.REPLICATION_PORT;
@@ -64,6 +68,43 @@ public class KafkaNodeUnregistration {
                     .mapEmpty();
         } catch (KafkaException e) {
             LOGGER.warnCr(reconciliation, "Failed to unregister nodes", e);
+            return Future.failedFuture(e);
+        }
+    }
+
+    /**
+     * List Kafka nodes within a KRaft-based cluster
+     *
+     * @param reconciliation        Reconciliation marker
+     * @param vertx                 Vert.x instance
+     * @param adminClientProvider   Kafka Admin API client provider
+     * @param pemTrustSet           Trust set for the admin client to connect to the Kafka cluster
+     * @param pemAuthIdentity       Key set  for the admin client to connect to the Kafka cluster
+     * @param includeFencedBrokers  If listing should include fenced brokers
+     *
+     * @return  Future that completes when all nodes are listed
+     */
+    public static Future<Collection<Node>> listRegisteredNodes(
+            Reconciliation reconciliation,
+            Vertx vertx,
+            AdminClientProvider adminClientProvider,
+            PemTrustSet pemTrustSet,
+            PemAuthIdentity pemAuthIdentity,
+            boolean includeFencedBrokers) {
+
+        try {
+            String bootstrapHostname = KafkaResources.bootstrapServiceName(reconciliation.name()) + "." + reconciliation.namespace() + ".svc:" + KafkaCluster.REPLICATION_PORT;
+            Admin adminClient = adminClientProvider.createAdminClient(bootstrapHostname, pemTrustSet, pemAuthIdentity);
+
+            DescribeClusterOptions option = new DescribeClusterOptions().includeFencedBrokers(includeFencedBrokers);
+            return VertxUtil
+                    .kafkaFutureToVertxFuture(reconciliation, vertx, adminClient.describeCluster(option).nodes())
+                    .compose(nodes -> {
+                        LOGGER.debugCr(reconciliation, "Describe cluster: nodes (fanced included) = {}", nodes);
+                        return Future.succeededFuture(nodes);
+                    });
+        } catch (KafkaException e) {
+            LOGGER.warnCr(reconciliation, "Failed to list nodes", e);
             return Future.failedFuture(e);
         }
     }
