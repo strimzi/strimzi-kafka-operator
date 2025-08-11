6ef0ad3ad7cce84fe2e81593d54bedd7ebf5ab71
Paolo Patierno <ppatierno@live.com>
2025-08-11 14:24:20 +0200

    Removed unneeded test
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaNodeUnregistrationTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaNodeUnregistrationTest.java
@@ -14,7 +14,6 @@ import io.vertx.junit5.VertxTestContext;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.UnregisterBrokerResult;
 import org.apache.kafka.common.KafkaFuture;
-import org.apache.kafka.common.errors.BrokerIdNotRegisteredException;
 import org.apache.kafka.common.errors.TimeoutException;
 import org.apache.kafka.common.internals.KafkaFutureImpl;
 import org.junit.jupiter.api.AfterAll;
@@ -53,37 +52,6 @@ public class KafkaNodeUnregistrationTest {
         when(ubr.all()).thenReturn(KafkaFuture.completedFuture(null));
         ArgumentCaptor<Integer> unregisteredNodeIdCaptor = ArgumentCaptor.forClass(Integer.class);
         when(mockAdmin.unregisterBroker(unregisteredNodeIdCaptor.capture())).thenReturn(ubr);
-
-        AdminClientProvider mockProvider = ResourceUtils.adminClientProvider(mockAdmin);
-
-        Checkpoint async = context.checkpoint();
-        KafkaNodeUnregistration.unregisterBrokerNodes(Reconciliation.DUMMY_RECONCILIATION, vertx, mockProvider, null, null, Set.of(1874, 1919))
-                .onComplete(context.succeeding(v -> context.verify(() -> {
-                    assertThat(unregisteredNodeIdCaptor.getAllValues().size(), is(2));
-                    assertThat(unregisteredNodeIdCaptor.getAllValues(), hasItems(1874, 1919));
-
-                    async.flag();
-                })));
-    }
-
-    @Test
-    void testUnknownNodeUnregistration(VertxTestContext context) {
-        Admin mockAdmin = ResourceUtils.adminClient();
-
-        ArgumentCaptor<Integer> unregisteredNodeIdCaptor = ArgumentCaptor.forClass(Integer.class);
-        when(mockAdmin.unregisterBroker(unregisteredNodeIdCaptor.capture())).thenAnswer(i -> {
-            if (i.getArgument(0, Integer.class) == 1919)   {
-                KafkaFutureImpl<Void> unregistrationFuture = new KafkaFutureImpl<>();
-                unregistrationFuture.completeExceptionally(new BrokerIdNotRegisteredException("Unknown node"));
-                UnregisterBrokerResult ubr = mock(UnregisterBrokerResult.class);
-                when(ubr.all()).thenReturn(unregistrationFuture);
-                return ubr;
-            } else {
-                UnregisterBrokerResult ubr = mock(UnregisterBrokerResult.class);
-                when(ubr.all()).thenReturn(KafkaFuture.completedFuture(null));
-                return ubr;
-            }
-        });
 
         AdminClientProvider mockProvider = ResourceUtils.adminClientProvider(mockAdmin);
 
