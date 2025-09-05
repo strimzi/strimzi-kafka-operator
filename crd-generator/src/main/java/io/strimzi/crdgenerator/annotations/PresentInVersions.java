469f00346f448be7801b937f9fda2fa786dc49f7
Jakub Scholz <www@scholzj.com>
2025-09-05 20:34:20 +0200

    Allow typed APIs to be present only in some API versions
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- crd-generator/src/main/java/io/strimzi/crdgenerator/annotations/PresentInVersions.java
+++ crd-generator/src/main/java/io/strimzi/crdgenerator/annotations/PresentInVersions.java
@@ -10,10 +10,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Used when a property is present from or until a particular CR API version
+ * Used when a property is present from or until a particular CR API version. It can be also used on a type definition,
+ * in which case the type will be included only in some API versions when it is used in some typed APIs (based on the
+ * "type: xxx" differentiator).
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
 public @interface PresentInVersions {
     /**
      * @return The versions in which the annotated property is present
