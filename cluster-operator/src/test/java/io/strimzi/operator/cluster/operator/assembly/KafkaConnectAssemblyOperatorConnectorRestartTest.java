fd2bdca529d63b84921e1d07f95d2522ab6366ad
Ronaldo Lanhellas <ronaldo.lanhellas@gmail.com>
2025-09-15 15:42:54 -0300

    test: moving tests to restart class
    Signed-off-by: Ronaldo Lanhellas <ronaldo.lanhellas@gmail.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaConnectAssemblyOperatorConnectorRestartTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaConnectAssemblyOperatorConnectorRestartTest.java
@@ -4,6 +4,8 @@
  */
 package io.strimzi.operator.cluster.operator.assembly;
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.strimzi.api.kafka.model.connect.KafkaConnectBuilder;
 import io.strimzi.api.kafka.model.connector.KafkaConnector;
 import io.strimzi.api.kafka.model.connector.KafkaConnectorBuilder;
 import io.strimzi.operator.cluster.PlatformFeaturesAvailability;
@@ -22,11 +24,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import static io.strimzi.operator.cluster.operator.assembly.AbstractConnectOperator.STRIMZI_IO_RESTART_INCLUDE_TASKS_ARG;
 import static io.strimzi.operator.cluster.operator.assembly.AbstractConnectOperator.STRIMZI_IO_RESTART_ONLY_FAILED_ARG;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -39,6 +44,16 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(VertxExtension.class)
 public class KafkaConnectAssemblyOperatorConnectorRestartTest {
+
+    /**
+     * Helper class to represent a request to test an annotation value. Important to note that method to check if we have a
+     * restart request is different from method to check if the annotation value is valid.
+     * @param annotationValue The value of the annotation
+     * @param shouldRestart Whether the operator should interpret the annotation as a restart request
+     * @param shouldBeValid Whether the operator should interpret the annotation as a valid value
+     * */
+    record AnnotationValueRequest(String annotationValue, boolean shouldRestart, boolean shouldBeValid) { }
+
     protected static Vertx vertx;
 
     @BeforeAll
@@ -154,6 +169,63 @@ public class KafkaConnectAssemblyOperatorConnectorRestartTest {
                     checkpoint.flag();
                 })));
 
+    }
+
+    @Test
+    public void testRestartAnnotationValuesIsValid() {
+        KafkaConnectAssemblyOperator op = new KafkaConnectAssemblyOperator(null, new PlatformFeaturesAvailability(true, KubernetesVersion.MINIMAL_SUPPORTED_VERSION),
+                ResourceUtils.supplierWithMocks(false), ResourceUtils.dummyClusterOperatorConfig());
+        for (AnnotationValueRequest annoValue : generateAnnotationValues()) {
+            HasMetadata resource = new KafkaConnectBuilder()
+                    .withNewMetadata()
+                    .withAnnotations(Map.of(Annotations.ANNO_STRIMZI_IO_RESTART, annoValue.annotationValue))
+                    .endMetadata()
+                    .build();
+            boolean isValid = op.restartAnnotationIsValid(resource, null);
+            boolean shouldBeValid = annoValue.shouldBeValid;
+            assertThat(String.format("value '%s' is expected to %s", annoValue, shouldBeValid ? "be valid" : "NOT be valid"), isValid, equalTo(shouldBeValid));
+        }
+    }
+
+    @Test
+    public void testRestartAnnotationValueHasRestart() {
+        KafkaConnectAssemblyOperator op = new KafkaConnectAssemblyOperator(null, new PlatformFeaturesAvailability(true, KubernetesVersion.MINIMAL_SUPPORTED_VERSION),
+                ResourceUtils.supplierWithMocks(false), ResourceUtils.dummyClusterOperatorConfig());
+        for (AnnotationValueRequest annoValue : generateAnnotationValues()) {
+            HasMetadata resource = new KafkaConnectBuilder()
+                    .withNewMetadata()
+                    .withAnnotations(Map.of(Annotations.ANNO_STRIMZI_IO_RESTART, annoValue.annotationValue))
+                    .endMetadata()
+                    .build();
+            boolean hasRestart = op.hasRestartAnnotation(resource, null);
+            boolean shouldRestart = annoValue.shouldRestart;
+            assertThat(String.format("value '%s' is expected to %s", annoValue, shouldRestart ? "restart" : "NOT restart"), hasRestart, equalTo(shouldRestart));
+        }
+    }
+
+    private List<AnnotationValueRequest> generateAnnotationValues() {
+        return List.of(
+                new AnnotationValueRequest("true", true, true),
+                new AnnotationValueRequest("includeTasks,onlyFailed", true, true),
+                new AnnotationValueRequest("onlyFailed,includeTasks", true, true),
+                new AnnotationValueRequest("    onlyFailed   ,   includeTasks   ", true, true),
+                new AnnotationValueRequest("includeTasks", true, true),
+                new AnnotationValueRequest("onlyFailed", true, true),
+                new AnnotationValueRequest("   onlyFailed   ", true, true),
+                new AnnotationValueRequest("    includeTasks   ", true, true),
+                new AnnotationValueRequest("false", false, false),
+                new AnnotationValueRequest("true,includeTasks", true, false),
+                new AnnotationValueRequest("true,includeTasks,onlyFailed", true, false),
+                new AnnotationValueRequest("true,onlyFailed", true, false),
+                new AnnotationValueRequest("true,invalidArg,onlyFailed", true, false),
+                new AnnotationValueRequest("invalidArg,onlyFailed", true, false),
+                new AnnotationValueRequest("onlyFailed,invalidArg", true, false),
+                new AnnotationValueRequest("includeTasks,invalidArg", true, false),
+                new AnnotationValueRequest("invalidArg,includeTasks", true, false),
+                new AnnotationValueRequest("", false, false),
+                new AnnotationValueRequest("     ", false, false),
+                new AnnotationValueRequest("   b  ", true, false)
+        );
     }
 
     private KafkaConnector buildKafkaConnector(String restartAnnotationValue) {
--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaMirrorMaker2AssemblyOperatorConnectorRestartTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/assembly/KafkaMirrorMaker2AssemblyOperatorConnectorRestartTest.java
@@ -4,6 +4,7 @@
  */
 package io.strimzi.operator.cluster.operator.assembly;
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.strimzi.api.kafka.model.connector.KafkaConnector;
 import io.strimzi.api.kafka.model.connector.KafkaConnectorBuilder;
 import io.strimzi.api.kafka.model.mirrormaker2.KafkaMirrorMaker2;
@@ -26,11 +27,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import static io.strimzi.operator.cluster.operator.assembly.AbstractConnectOperator.STRIMZI_IO_RESTART_INCLUDE_TASKS_ARG;
 import static io.strimzi.operator.cluster.operator.assembly.AbstractConnectOperator.STRIMZI_IO_RESTART_ONLY_FAILED_ARG;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -43,6 +47,19 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(VertxExtension.class)
 public class KafkaMirrorMaker2AssemblyOperatorConnectorRestartTest {
+
+    /**
+     * Helper class to represent a request to test an annotation value. Important to note that method to check if we have a
+     * restart request is different from method to check if the annotation value is valid.
+     *
+     * @param annotationValue The value of the annotation
+     * @param connectorName The name of the connector to restart
+     * @param shouldRestart Whether the operator should interpret the annotation as a restart request
+     * @param shouldBeValid Whether the operator should interpret the annotation as a valid value
+     * */
+    record AnnotationValueRequest(String annotationValue, String connectorName,  boolean shouldRestart, boolean shouldBeValid) { }
+
+
     protected static Vertx vertx;
 
     @BeforeAll
@@ -163,6 +180,56 @@ public class KafkaMirrorMaker2AssemblyOperatorConnectorRestartTest {
                     checkpoint.flag();
                 })));
 
+    }
+
+    @Test
+    public void testRestartAnnotationValuesIsValid() {
+        KafkaMirrorMaker2AssemblyOperator op = new KafkaMirrorMaker2AssemblyOperator(null, new PlatformFeaturesAvailability(true, KubernetesVersion.MINIMAL_SUPPORTED_VERSION),
+                ResourceUtils.supplierWithMocks(false), ResourceUtils.dummyClusterOperatorConfig());
+        for (AnnotationValueRequest annoValue : generateAnnotationValues()) {
+            HasMetadata resource = new KafkaMirrorMaker2Builder()
+                    .withNewMetadata()
+                    .withAnnotations(Map.of(Annotations.ANNO_STRIMZI_IO_RESTART_CONNECTOR, annoValue.annotationValue))
+                    .endMetadata()
+                    .build();
+            boolean isValid = op.restartAnnotationIsValid(resource, annoValue.connectorName);
+            boolean shouldBeValid = annoValue.shouldBeValid;
+            assertThat(String.format("value '%s' is expected to %s", annoValue, shouldBeValid ? "be valid" : "NOT be valid"), isValid, equalTo(shouldBeValid));
+        }
+    }
+
+    @Test
+    public void testRestartAnnotationValueHasRestart() {
+        KafkaMirrorMaker2AssemblyOperator op = new KafkaMirrorMaker2AssemblyOperator(null, new PlatformFeaturesAvailability(true, KubernetesVersion.MINIMAL_SUPPORTED_VERSION),
+                ResourceUtils.supplierWithMocks(false), ResourceUtils.dummyClusterOperatorConfig());
+        for (AnnotationValueRequest annoValue : generateAnnotationValues()) {
+            HasMetadata resource = new KafkaMirrorMaker2Builder()
+                    .withNewMetadata()
+                    .withAnnotations(Map.of(Annotations.ANNO_STRIMZI_IO_RESTART_CONNECTOR, annoValue.annotationValue))
+                    .endMetadata()
+                    .build();
+            boolean hasRestart = op.hasRestartAnnotation(resource, annoValue.connectorName);
+            boolean shouldRestart = annoValue.shouldRestart;
+            assertThat(String.format("value '%s' is expected to %s", annoValue, shouldRestart ? "restart" : "NOT restart"), hasRestart, equalTo(shouldRestart));
+        }
+    }
+
+    private List<AnnotationValueRequest> generateAnnotationValues() {
+        String connectorName = "my-connector";
+        return List.of(
+                new AnnotationValueRequest(connectorName, connectorName, true, true),
+                new AnnotationValueRequest(connectorName + ":includeTasks,onlyFailed", connectorName, true, true),
+                new AnnotationValueRequest(connectorName + ":onlyFailed,includeTasks", connectorName, true, true),
+                new AnnotationValueRequest(connectorName + ":    onlyFailed   ,   includeTasks   ", connectorName, true, true),
+                new AnnotationValueRequest(connectorName + ":includeTasks", connectorName, true, true),
+                new AnnotationValueRequest(connectorName + ":onlyFailed", connectorName, true, true),
+                new AnnotationValueRequest(connectorName + ":   onlyFailed   ", connectorName, true, true),
+                new AnnotationValueRequest(connectorName + ":    includeTasks   ", connectorName, true, true),
+                new AnnotationValueRequest("    " + connectorName + ":    includeTasks   ", connectorName, true, false),
+                new AnnotationValueRequest("", connectorName, false, false),
+                new AnnotationValueRequest("     ", connectorName, false, false),
+                new AnnotationValueRequest("   b  ", connectorName, false, false)
+        );
     }
 
     private KafkaConnector buildKafkaConnector() {
