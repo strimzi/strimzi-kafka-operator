f489ea7c041d09a83d937c0d45bec179ef983645
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2025-10-13 19:08:37 +0530

    used the same kube-registry-proxy for s390x like amd
    Signed-off-by: Mujahid-Dandoti <Mujahid.Dandoti@ibm.com>

--- .azure/scripts/setup-kubernetes.sh
+++ .azure/scripts/setup-kubernetes.sh
@@ -89,16 +89,7 @@ if [ "$TEST_CLUSTER" = "minikube" ]; then
       set -ex
     fi
 
-    if [ "$ARCH" = "s390x" ]; then
-        git clone -b v1.9.11 --depth 1 https://github.com/kubernetes/kubernetes.git
-        sed -i 's/:1.11/:1.26.0/' kubernetes/cluster/addons/registry/images/Dockerfile
-	mv kubernetes/cluster/addons/registry/images/rootfs/bin kubernetes/cluster/addons/registry/images/rootfs/roofs-bin
- 	sed -i 's/bin/roofs-bin/' kubernetes/cluster/addons/registry/images/Dockerfile
-        docker build --pull -t gcr.io/google_containers/kube-registry-proxy:0.4-${ARCH} kubernetes/cluster/addons/registry/images/
-        minikube image load ${ARCH}/registry:2.8.2 gcr.io/google_containers/kube-registry-proxy:0.4-${ARCH}
-        minikube addons enable registry --images="Registry=${ARCH}/registry:2.8.2,KubeRegistryProxy=gcr.io/google_containers/kube-registry-proxy:0.4-${ARCH}"
-        rm -rf kubernetes
-    elif [[ "$ARCH" = "ppc64le" ]]; then
+    if [[ "$ARCH" = "ppc64le" ]]; then
         git clone -b v1.9.11 --depth 1 https://github.com/kubernetes/kubernetes.git
 	sed -i 's/:1.11/:1.22.1/' kubernetes/cluster/addons/registry/images/Dockerfile
         docker build --pull -t gcr.io/google_containers/kube-registry-proxy:0.4-${ARCH} kubernetes/cluster/addons/registry/images/
