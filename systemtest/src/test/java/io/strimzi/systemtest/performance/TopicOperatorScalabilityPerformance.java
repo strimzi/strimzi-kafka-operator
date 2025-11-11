76c1da9bf7e1c0160866ec99dbf2f33a1a1c25c7
see-quick <maros.orsak159@gmail.com>
2025-11-11 22:00:01 +0100

    [system test] [perf] Performance perf report into PR
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- systemtest/src/test/java/io/strimzi/systemtest/performance/TopicOperatorScalabilityPerformance.java
+++ systemtest/src/test/java/io/strimzi/systemtest/performance/TopicOperatorScalabilityPerformance.java
@@ -46,7 +46,7 @@ public class TopicOperatorScalabilityPerformance extends AbstractST {
     private TestStorage suiteTestStorage;
 
     // topic event batches to test
-    private final List<Integer> eventBatches = List.of(10, 100, 500, 1000);
+    private final List<Integer> eventBatches = List.of(10, 110, 500, 1000);
     private final int maxBatchSize = 100;
     private final int maxBatchLingerMs = 100;
     private final int maxQueueSize = Integer.MAX_VALUE;
