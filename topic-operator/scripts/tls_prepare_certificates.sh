3476777ce47d231cee2d86464ea0e26091b013ac
Jakub Scholz <www@scholzj.com>
2025-12-29 10:38:03 +0100

    Do not show keystore passwords in the process table - Closes #9957
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- topic-operator/scripts/tls_prepare_certificates.sh
+++ topic-operator/scripts/tls_prepare_certificates.sh
@@ -16,7 +16,7 @@ function create_truststore {
     fi
 
     # shellcheck disable=SC2086
-    keytool ${KEYTOOL_OPTS} -keystore "$1" -storepass "$2" -noprompt -alias "$4" -import -file "$3" -storetype PKCS12
+    PASSWORD=$2 keytool ${KEYTOOL_OPTS} -keystore "$1" -storepass:env PASSWORD -noprompt -alias "$4" -import -file "$3" -storetype PKCS12
 }
 
 # Parameters:
@@ -26,7 +26,7 @@ function create_truststore {
 # $4: Private key to be imported
 # $5: Alias of the certificate
 function create_keystore_without_ca_file {
-   RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$3" -inkey "$4" -name "$5" -password pass:"$2" -out "$1" -certpbe aes-128-cbc -keypbe aes-128-cbc -macalg sha256
+    PASSWORD=$2 RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$3" -inkey "$4" -name "$5" -password env:PASSWORD -out "$1" -certpbe aes-128-cbc -keypbe aes-128-cbc -macalg sha256
 }
 
 if [ "$STRIMZI_PUBLIC_CA" != "true" ]; then
@@ -34,21 +34,21 @@ if [ "$STRIMZI_PUBLIC_CA" != "true" ]; then
     STORE=/tmp/topic-operator/replication.truststore.p12
     rm -f "$STORE"
     for CRT in /etc/tls-sidecar/cluster-ca-certs/*.crt; do
-      ALIAS=$(basename "$CRT" .crt)
-      echo "Adding $CRT to truststore $STORE with alias $ALIAS"
-      create_truststore "$STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
+        ALIAS=$(basename "$CRT" .crt)
+        echo "Adding $CRT to truststore $STORE with alias $ALIAS"
+        create_truststore "$STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
     done
     echo "Preparing trust store certificates for internal communication is completed"
 fi
 
 if [ "$STRIMZI_TLS_AUTH_ENABLED" != "false" ]; then
-  echo "Preparing key store certificates for internal communication"
-  STORE=/tmp/topic-operator/replication.keystore.p12
-  rm -f "$STORE"
-  create_keystore_without_ca_file "$STORE" "$CERTS_STORE_PASSWORD" \
-      /etc/eto-certs/entity-operator.crt \
-      /etc/eto-certs/entity-operator.key \
-      /etc/tls-sidecar/cluster-ca-certs/ca.crt \
-      entity-operator
-  echo "Preparing key store certificates for internal communication is completed"
+    echo "Preparing key store certificates for internal communication"
+    STORE=/tmp/topic-operator/replication.keystore.p12
+    rm -f "$STORE"
+    create_keystore_without_ca_file "$STORE" "$CERTS_STORE_PASSWORD" \
+        /etc/eto-certs/entity-operator.crt \
+        /etc/eto-certs/entity-operator.key \
+        /etc/tls-sidecar/cluster-ca-certs/ca.crt \
+        entity-operator
+    echo "Preparing key store certificates for internal communication is completed"
 fi
