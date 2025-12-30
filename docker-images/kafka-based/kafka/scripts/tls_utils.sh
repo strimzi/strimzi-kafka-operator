41936366d8ceb463a08f718ae5b4179792e7fdb3
Jakub Scholz <www@scholzj.com>
2025-12-30 08:33:26 +0100

    Do not show keystore passwords in the process table (#12276)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- docker-images/kafka-based/kafka/scripts/tls_utils.sh
+++ docker-images/kafka-based/kafka/scripts/tls_utils.sh
@@ -11,15 +11,15 @@ set -e
 # $3: Public key to be imported
 # $4: Alias of the certificate
 function create_truststore {
-  # Disable FIPS if needed
-  if [ "$FIPS_MODE" = "disabled" ]; then
-      KEYTOOL_OPTS="${KEYTOOL_OPTS} -J-Dcom.redhat.fips=false"
-  else
-      KEYTOOL_OPTS=""
-  fi
+    # Disable FIPS if needed
+    if [ "$FIPS_MODE" = "disabled" ]; then
+        KEYTOOL_OPTS="${KEYTOOL_OPTS} -J-Dcom.redhat.fips=false"
+    else
+        KEYTOOL_OPTS=""
+    fi
 
-   # shellcheck disable=SC2086
-   keytool ${KEYTOOL_OPTS} -keystore "$1" -storepass "$2" -noprompt -alias "$4" -import -file "$3" -storetype PKCS12
+    # shellcheck disable=SC2086
+    PASSWORD=$2 keytool ${KEYTOOL_OPTS} -keystore "$1" -storepass:env PASSWORD -noprompt -alias "$4" -import -file "$3" -storetype PKCS12
 }
 
 # Parameters:
@@ -30,7 +30,7 @@ function create_truststore {
 # $5: CA public key to be imported
 # $6: Alias of the certificate
 function create_keystore {
-   RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$3" -inkey "$4" -chain -CAfile "$5" -name "$6" -password pass:"$2" -out "$1" -certpbe aes-128-cbc -keypbe aes-128-cbc -macalg sha256
+    PASSWORD=$2 RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$3" -inkey "$4" -chain -CAfile "$5" -name "$6" -password env:PASSWORD -out "$1" -certpbe aes-128-cbc -keypbe aes-128-cbc -macalg sha256
 }
 
 # Parameters:
@@ -40,7 +40,7 @@ function create_keystore {
 # $4: Private key to be imported
 # $5: Alias of the certificate
 function create_keystore_without_ca_file {
-   RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$3" -inkey "$4" -name "$5" -password pass:"$2" -out "$1" -certpbe aes-128-cbc -keypbe aes-128-cbc -macalg sha256
+    PASSWORD=$2 RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$3" -inkey "$4" -name "$5" -password env:PASSWORD -out "$1" -certpbe aes-128-cbc -keypbe aes-128-cbc -macalg sha256
 }
 
 # Searches the directory with the CAs and finds the CA matching our key.
