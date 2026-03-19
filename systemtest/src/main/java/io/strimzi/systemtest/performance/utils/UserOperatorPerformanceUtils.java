2d23643097bc586391a95e267d5d5126ce216746
see-quick <maros.orsak159@gmail.com>
2026-03-19 09:55:29 +0100

    prevent overwhelming the UO queue
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- systemtest/src/main/java/io/strimzi/systemtest/performance/utils/UserOperatorPerformanceUtils.java
+++ systemtest/src/main/java/io/strimzi/systemtest/performance/utils/UserOperatorPerformanceUtils.java
@@ -12,11 +12,13 @@ import io.strimzi.api.kafka.model.user.KafkaUserBuilder;
 import io.strimzi.api.kafka.model.user.KafkaUserQuotas;
 import io.strimzi.api.kafka.model.user.KafkaUserQuotasBuilder;
 import io.strimzi.api.kafka.model.user.acl.AclOperation;
+import io.strimzi.systemtest.TestConstants;
 import io.strimzi.systemtest.enums.UserAuthType;
 import io.strimzi.systemtest.resources.CrdClients;
 import io.strimzi.systemtest.storage.TestStorage;
 import io.strimzi.systemtest.templates.crd.KafkaUserTemplates;
 import io.strimzi.systemtest.utils.kafkaUtils.KafkaUserUtils;
+import io.strimzi.test.TestUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -124,6 +126,55 @@ public class UserOperatorPerformanceUtils {
         listOfUsers.forEach(kafkaUser ->
             KafkaUserUtils.waitForKafkaUserReady(testStorage.getNamespaceName(), kafkaUser.getMetadata().getName())
         );
+    }
+
+    public static void cleanupKafkaUsers(TestStorage testStorage) {
+        LOGGER.info("Cleaning namespace: {}", testStorage.getNamespaceName());
+
+        List<KafkaUser> kafkaUsers = CrdClients.kafkaUserClient()
+            .inNamespace(testStorage.getNamespaceName()).list().getItems()
+            .stream()
+            .filter(u -> u.getMetadata().getName().startsWith(testStorage.getUsername()))
+            .toList();
+
+        if (kafkaUsers.isEmpty()) {
+            LOGGER.info("No KafkaUsers with prefix '{}' found in namespace {}",
+                testStorage.getUsername(), testStorage.getNamespaceName());
+            return;
+        }
+
+        final int batchSize = 500;
+        final int totalBatches = (kafkaUsers.size() + batchSize - 1) / batchSize;
+        LOGGER.info("Deleting {} KafkaUsers in {} batch(es) of up to {}",
+            kafkaUsers.size(), totalBatches, batchSize);
+
+        for (int i = 0; i < kafkaUsers.size(); i += batchSize) {
+            final int batchNumber = (i / batchSize) + 1;
+            List<KafkaUser> batch = kafkaUsers.subList(i, Math.min(i + batchSize, kafkaUsers.size()));
+            final int expectedRemaining = kafkaUsers.size() - (i + batch.size());
+
+            LOGGER.info("Submitting deletion batch {}/{} ({} users, expecting {} remaining after)",
+                batchNumber, totalBatches, batch.size(), expectedRemaining);
+            KubeResourceManager.get().deleteResourceWithoutWait(batch.toArray(new KafkaUser[0]));
+
+            TestUtils.waitFor(
+                "deletion of batch " + batchNumber + "/" + totalBatches,
+                TestConstants.GLOBAL_POLL_INTERVAL,
+                TestConstants.GLOBAL_TIMEOUT,
+                () -> {
+                    try {
+                        return KafkaUserUtils.getAllKafkaUsersWithPrefix(
+                            testStorage.getNamespaceName(), testStorage.getUsername()).size() <= expectedRemaining;
+                    } catch (Exception e) {
+                        return e.getMessage().contains("Not Found")
+                            || e.getMessage().contains("the server doesn't have a resource type");
+                    }
+                });
+
+            LOGGER.info("Batch {}/{} completed", batchNumber, totalBatches);
+        }
+
+        LOGGER.info("All KafkaUsers with prefix '{}' deleted", testStorage.getUsername());
     }
 
     /**
--- systemtest/src/test/java/io/strimzi/systemtest/performance/UserOperatorScalabilityPerformance.java
+++ systemtest/src/test/java/io/strimzi/systemtest/performance/UserOperatorScalabilityPerformance.java
@@ -12,7 +12,6 @@ import io.skodjob.annotations.Step;
 import io.skodjob.annotations.SuiteDoc;
 import io.skodjob.annotations.TestDoc;
 import io.skodjob.kubetest4j.resources.KubeResourceManager;
-import io.strimzi.api.kafka.model.user.KafkaUser;
 import io.strimzi.systemtest.AbstractST;
 import io.strimzi.systemtest.Environment;
 import io.strimzi.systemtest.TestConstants;
@@ -25,13 +24,11 @@ import io.strimzi.systemtest.performance.report.UserOperatorPerformanceReporter;
 import io.strimzi.systemtest.performance.report.parser.BasePerformanceMetricsParser;
 import io.strimzi.systemtest.performance.utils.UserOperatorPerformanceUtils;
 import io.strimzi.systemtest.performance.utils.UserOperatorPerformanceUtils.LatencyMetrics;
-import io.strimzi.systemtest.resources.CrdClients;
 import io.strimzi.systemtest.resources.operator.SetupClusterOperator;
 import io.strimzi.systemtest.storage.TestStorage;
 import io.strimzi.systemtest.templates.crd.KafkaNodePoolTemplates;
 import io.strimzi.systemtest.templates.crd.KafkaTemplates;
 import io.strimzi.systemtest.templates.specific.ScraperTemplates;
-import io.strimzi.systemtest.utils.kafkaUtils.KafkaUserUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
@@ -39,7 +36,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 
 import java.io.IOException;
-import java.time.Duration;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -139,10 +135,7 @@ public class UserOperatorScalabilityPerformance extends AbstractST {
                 // number of KafkaUsers to test (each goes through full lifecycle: create, modify, delete)
                 reconciliationTimeMs = UserOperatorPerformanceUtils.processAllUsersConcurrently(testStorage, numberOfKafkaUsers, 0, 0);
             } finally {
-                LOGGER.info("Cleaning namespace: {}", testStorage.getNamespaceName());
-                List<KafkaUser> kafkaUsers = CrdClients.kafkaUserClient().inNamespace(testStorage.getNamespaceName()).list().getItems();
-                KubeResourceManager.get().deleteResourceWithoutWait(kafkaUsers.toArray(new KafkaUser[0]));
-                KafkaUserUtils.waitForUserWithPrefixDeletion(testStorage.getNamespaceName(), testStorage.getUsername());
+                UserOperatorPerformanceUtils.cleanupKafkaUsers(testStorage);
 
                 final Map<String, Object> performanceAttributes = new LinkedHashMap<>();
 
@@ -252,11 +245,7 @@ public class UserOperatorScalabilityPerformance extends AbstractST {
                 LOGGER.info("Measuring single-user modification latency with {} existing users in the system", numberOfExistingUsers);
                 latencyMetrics = UserOperatorPerformanceUtils.measureLatencyUnderLoad(testStorage, numberOfExistingUsers, numberOfModifications);
             } finally {
-                LOGGER.info("Cleaning namespace: {}", testStorage.getNamespaceName());
-                List<KafkaUser> kafkaUsers = CrdClients.kafkaUserClient().inNamespace(testStorage.getNamespaceName()).list().getItems();
-                KubeResourceManager.get().deleteResourceWithoutWait(kafkaUsers.toArray(new KafkaUser[0]));
-                final long deletionTimeoutMs = TestConstants.GLOBAL_TIMEOUT + (numberOfExistingUsers / 500) * Duration.ofMinutes(2).toMillis();
-                KafkaUserUtils.waitForUserWithPrefixDeletion(testStorage.getNamespaceName(), testStorage.getUsername(), deletionTimeoutMs);
+                UserOperatorPerformanceUtils.cleanupKafkaUsers(testStorage);
 
                 if (latencyMetrics != null) {
                     final Map<String, Object> performanceAttributes = new LinkedHashMap<>();
