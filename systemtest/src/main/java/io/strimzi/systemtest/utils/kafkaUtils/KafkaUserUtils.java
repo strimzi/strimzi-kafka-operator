898456f3dca5c0a356bb08f814af9454b527ebc4
see-quick <maros.orsak159@gmail.com>
2026-03-23 12:33:26 +0100

    checkstyle
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- systemtest/src/main/java/io/strimzi/systemtest/utils/kafkaUtils/KafkaUserUtils.java
+++ systemtest/src/main/java/io/strimzi/systemtest/utils/kafkaUtils/KafkaUserUtils.java
@@ -22,7 +22,6 @@ import io.strimzi.systemtest.utils.kubeUtils.objects.SecretUtils;
 import io.strimzi.test.TestUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
