d415473ba82866d8dda419e25884640ce010fec6
Jakub Scholz <www@scholzj.com>
2026-04-03 21:07:00 +0200

    Add ExternalLink annotation to link to external API siuch as Gateway API (#12590)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- crd-generator/src/test/java/io/strimzi/crdgenerator/DocGeneratorTest.java
+++ crd-generator/src/test/java/io/strimzi/crdgenerator/DocGeneratorTest.java
@@ -10,19 +10,14 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.io.StringWriter;
 
-import static io.strimzi.crdgenerator.DocGenerator.classInherits;
 import static java.util.Collections.singletonList;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DocGeneratorTest {
     @Test
-    public void simpleTest() throws IOException, ClassNotFoundException {
-        assertThat(classInherits(Class.forName("io.strimzi.crdgenerator.KubeLinker"), Linker.class), is(notNullValue()));
+    public void simpleTest() throws IOException {
         StringWriter w = new StringWriter();
-        DocGenerator crdGenerator = new DocGenerator(ApiVersion.V1, 1, singletonList(ExampleCrd.class), w, new KubeLinker("{KubeApiReferenceBase}"));
+        DocGenerator crdGenerator = new DocGenerator(ApiVersion.V1, 1, singletonList(ExampleCrd.class), w, new Linker("{KubeApiReferenceBase}"));
         crdGenerator.generate(ExampleCrd.class);
         String s = w.toString();
         assertEquals(CrdTestUtils.readResource("simpleTest.adoc"), s);
