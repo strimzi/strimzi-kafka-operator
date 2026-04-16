58315aceed6810145323eb8297e798eafeb34833
Jakub Scholz <www@scholzj.com>
2026-04-16 09:22:52 +0200

    Clean unused parts of our startup scripts (#12635)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- docker-images/kafka-based/kafka/scripts/tls_utils.sh
+++ docker-images/kafka-based/kafka/scripts/tls_utils.sh
@@ -43,20 +43,6 @@ function create_keystore_without_ca_file {
     PASSWORD=$2 RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$3" -inkey "$4" -name "$5" -password env:PASSWORD -out "$1" -certpbe aes-128-cbc -keypbe aes-128-cbc -macalg sha256
 }
 
-# Searches the directory with the CAs and finds the CA matching our key.
-# This is useful during certificate renewals
-#
-# Parameters:
-# $1: The directory with the CA certificates
-# $2: Public key to be imported
-function find_ca {
-    for ca in "$1"/*; do
-        if openssl verify -CAfile "$ca" "$2" &> /dev/null; then
-            echo "$ca"
-        fi
-    done
-}
-
 # Parameters:
 # $1: Path to the new truststore
 # $2: Truststore password
