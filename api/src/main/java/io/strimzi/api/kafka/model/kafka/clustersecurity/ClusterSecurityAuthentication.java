66ab4b2d38c62cd6f3cf059f4e6269ed602d26f9
Jakub Scholz <www@scholzj.com>
2026-08-22 16:29:08 +0200

    Revert unwanted changes
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- api/src/main/java/io/strimzi/api/kafka/model/kafka/clustersecurity/ClusterSecurityAuthentication.java
+++ api/src/main/java/io/strimzi/api/kafka/model/kafka/clustersecurity/ClusterSecurityAuthentication.java
@@ -25,12 +25,11 @@ import java.util.Map;
         builderPackage = Constants.FABRIC8_KUBERNETES_API
 )
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({"type", "expirationSeconds"})
+@JsonPropertyOrder({"type"})
 @EqualsAndHashCode
 @ToString
 public class ClusterSecurityAuthentication implements UnknownPropertyPreserving {
     private ClusterSecurityAuthenticationType type;
-    private Integer expirationSeconds;
     private Map<String, Object> additionalProperties;
 
     @Description("""
@@ -47,19 +46,6 @@ public class ClusterSecurityAuthentication implements UnknownPropertyPreserving 
 
     public void setType(ClusterSecurityAuthenticationType type) {
         this.type = type;
-    }
-
-    @Description("The expiration time in seconds for the Service Account tokens used for authentication. " +
-            "This field is only applicable for `type: service-account` authentication. " +
-            "Defaults to 1 hour when not set.")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @RequiredInVersions("v1+")
-    public Integer getExpirationSeconds() {
-        return expirationSeconds;
-    }
-
-    public void setExpirationSeconds(Integer expirationSeconds) {
-        this.expirationSeconds = expirationSeconds;
     }
 
     @Override
