b31198350848e9700b0be2035a85783cb6ed8bb9
Jakub Stejskal <xstejs24@gmail.com>
2026-09-19 21:39:51 +0200

    Remove fips-agent
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- docker-images/kafka-based/kafka/scripts/kafka_run.sh
+++ docker-images/kafka-based/kafka/scripts/kafka_run.sh
@@ -81,7 +81,6 @@ echo "Starting Kafka Agent with configuration:"
 tee /tmp/kafka-agent.properties < "$KAFKA_HOME/custom-config/agent.config"
 echo ""
 
-KAFKA_OPTS="${KAFKA_OPTS} -javaagent:$(ls "$KAFKA_HOME"/libs/fips-agent*.jar)"
 KAFKA_OPTS="${KAFKA_OPTS} -javaagent:$(ls "$KAFKA_HOME"/libs/kafka-agent*.jar)=/tmp/kafka-agent.properties"
 export KAFKA_OPTS
 
